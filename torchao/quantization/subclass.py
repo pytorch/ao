469c2b2900fd6ee8251d48682d1b23ec14b6fddf
HDCharles <charlesdavidhernandez@gmail.com>
2023-12-07 14:32:29 -0800

    Update base for Update on "Subclass fixes for torchbench"
    Summary:
    
    pytorch update changed flatten, now updated. Handle a number of
    behaviors needed to work with torchbench dynamo userbenchmark.
    Removed transpose and detach code and broke them into _change_shape and
    _apply_fn_to_data methods, changed subclasses to overwrite
    torch_function for torch.nn.functional.linear rather than mm and addmm
    individually since linear can also hit expand, view, bmm...etc.
    
    Test Plan: python test/test.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- torchao/quantization/subclass.py
+++ torchao/quantization/subclass.py
@@ -138,9 +138,6 @@ class QuantizedLinearWeightBase(torch.Tensor):
         if func is torch.ops.aten.detach.default:
             return return_and_correct_aliasing(func, args, kwargs, args[0]._detach())
 
-        if func is torch.ops.aten.clone.default:
-            return return_and_correct_aliasing(func, args, kwargs, args[0]._detach())
-
         if func is torch.ops.aten.t.default:
             return return_and_correct_aliasing(func, args, kwargs, args[0]._transpose())
 
@@ -158,7 +155,7 @@ class Int8DynamicallyQuantizedLinearWeight(QuantizedLinearWeightBase):
         return super().__new__(cls, int_data, transposed, shape, **kwargs)  # type: ignore[attr-defined]
 
     def __init__(self, int_data, q_scales, transposed, shape, **kwargs):
-        self.q_scales = q_scales.to(self.dtype)
+        self.q_scales = q_scales
         super().__init__(int_data, transposed)
 
     @staticmethod
@@ -201,7 +198,7 @@ class Int8DynamicallyQuantizedLinearWeight(QuantizedLinearWeightBase):
 
     def _detach(self):
         return self.__class__(
-            self.int_data.detach(), self.q_scales.detach(), self.transposed, self.shape, dtype=self.dtype
+            self.int_data, self.q_scales, self.transposed, self.shape, dtype=self.dtype
         )
 
     def _transpose(self):
@@ -359,8 +356,8 @@ class Int4WeightOnlyQuantizedLinearWeight(QuantizedLinearWeightBase):
 
     def _detach(self):
         return self.__class__(
-            self.int_data.detach(),
-            self.scales_and_zeros.detach(),
+            self.int_data,
+            self.scales_and_zeros,
             self.transposed,
             self.shape,
             self.groupsize,
