b376dae3155b1be41c6e094448d141c4ff26e852
Christian Puhrsch <cpuhrsch@fb.com>
2024-03-18 22:43:15 +0000

    Basic test harness

--- test/kernel/test_autotuner.py
+++ test/kernel/test_autotuner.py
@@ -8,16 +8,30 @@
 # This test takes a long time to run
 import unittest
 import torch
+import pytest
+from parameterized import parameterized
 
 from torchao.kernel import intmm_triton
 
 class TestQuantFlow(unittest.TestCase):
 
-    def test_int_mm(self):
+    @parameterized.expand(
+            [
+                ("cuda", torch.bfloat16),
+                ("cpu", torch.bfloat16),
+                ("cuda", torch.float16),
+                ("cpu", torch.float16),
+            ])
+    def test_int_mm(self, device, dtype):
+        dtype = torch.bfloat16
+        device = 'cuda'
+        m, k, n = (128, 64, 16)
+        x = torch.randn(m, k, dtype=dtype, device=device)
+        w = torch.randn(n, k, dtype=dtype, device=device).t()
         x_int = x.to(dtype=torch.int8)
         w_int = w.to(dtype=torch.int8)
         out32_1 = intmm_triton.safe_int_mm(x_int, w_int)
-        assert out32.dtype == torch.int32
+        assert out32_1.dtype == torch.int32
         out32_2 = intmm_triton.int_matmul(x_int, w_int)
         assert out32_2.dtype == out32_1.dtype
         import pdb; pdb.set_trace()
