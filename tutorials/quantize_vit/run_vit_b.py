db000d1edfab0c8eeda02c60d2a731f39eeefee6
Christian Puhrsch <cpuhrsch@fb.com>
2024-03-22 23:31:48 +0000

    Don't touch tutorials

--- tutorials/quantize_vit/run_vit_b.py
+++ tutorials/quantize_vit/run_vit_b.py
@@ -8,11 +8,7 @@ model = models.vit_b_16(pretrained=True)
 model.eval().cuda().to(torch.bfloat16)
 
 # Input tensor (batch_size, channels, height, width)
-<<<<<<< HEAD
-input_tensor = torch.randn(1024, 3, 224, 224, dtype=torch.bfloat16, device='cuda')
-=======
 input_tensor = torch.randn(1, 3, 224, 224, dtype=torch.bfloat16, device='cuda')
->>>>>>> 645d654a5f2cad81d7d78de979a0f1190d670e38
 
 model = torch.compile(model, mode='max-autotune')
 
