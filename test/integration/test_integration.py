1f803055d6cf4bb4c5e3e4e0bfe1051d7e73c0d1
Christian Puhrsch <cpuhrsch@fb.com>
2024-03-26 21:30:40 +0000

    no_grad guard

--- test/integration/test_integration.py
+++ test/integration/test_integration.py
@@ -1010,23 +1010,27 @@ class TestWeightOnlyInt8Quant(unittest.TestCase):
             sqnr = compute_error(y_ref, y_wo)
             self.assertGreater(sqnr, 44.0)
 
-    @torch.no_grad()
     @parameterized.expand(COMMON_DEVICE_DTYPE)
+    @torch.no_grad()
     def test_weight_only_quant_force_mixed_mm(self, device, dtype):
-        torch._inductor.config.epilogue_fusion = True
-        torch._inductor.config.force_mixed_mm = True
-        for x_shape in [[2, 4], [5, 5, 5, 4], [1, 4, 4]]:
-            torch._dynamo.reset()
-            x = torch.randn(*x_shape).to(device).to(dtype)
-            m = nn.Sequential(nn.Linear(4, 5)).to(device).to(dtype)
-            y_ref = m(x)
-            apply_weight_only_int8_quant(m)
-            m(x)
-            m_c = torch.compile(m, mode="max-autotune")
-            y_wo, (code,) = run_and_get_code(m_c, x)
-            sqnr = compute_error(y_ref, y_wo)
-            self.assertGreater(sqnr, 43.0)
-            self.assertTrue("mixed_mm" in code)
+        from torch._inductor import config
+        with config.patch({
+            "epilogue_fusion": True,
+            "force_mixed_mm": True
+            }):
+            for x_shape in [[2, 4], [5, 5, 5, 4], [1, 4, 4]]:
+                torch._dynamo.reset()
+                x = torch.randn(*x_shape).to(device).to(dtype)
+                m = nn.Sequential(nn.Linear(4, 5)).to(device).to(dtype)
+                y_ref = m(x)
+                apply_weight_only_int8_quant(m)
+                m(x)
+                m_c = torch.compile(m, mode="max-autotune")
+                y_wo, (code,) = run_and_get_code(m_c, x)
+                sqnr = compute_error(y_ref, y_wo)
+                self.assertGreater(sqnr, 43.0)
+                if device == "cuda":
+                    self.assertTrue("mixed_mm" in code)
 
     @parameterized.expand(COMMON_DEVICE_DTYPE)
     def test_weight_only_quant_use_mixed_mm(self, device, dtype):
