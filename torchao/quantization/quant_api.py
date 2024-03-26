62c787194fd48388b1f955455ed4e305ea7de23c
cpuhrsch <cpuhrsch@googlemail.com>
2024-03-25 17:37:12 -0700

    Revert "Autoquant (#82)" (#83)
    This reverts commit 8119319bf3e3bedf4d987cd3ecc3c42dafce81f8.

--- torchao/quantization/quant_api.py
+++ torchao/quantization/quant_api.py
@@ -37,12 +37,7 @@ _AFTER_TORCH_2_4_ONLY = [
     "Int8DynActInt4WeightQuantizer",
     "Int8DynActInt4WeightGPTQQuantizer",
 ]
-from .quant_primitives import (
-    get_group_qparams_symmetric,
-    per_token_dynamic_quant,
-)
-from typing import Dict, Tuple
-from .autoquant import AutoQuantizableLinearWeight, DEFAULT_CLASS_LIST
+
 
 __all__ = [
     "apply_weight_only_int8_quant",
@@ -53,10 +48,8 @@ __all__ = [
     "swap_conv2d_1x1_to_linear",
     "Quantizer",
     "TwoStepQuantizer",
-    "autoquant",
-    "change_linears_to_autoquantizable",
-    "change_autoquantizable_to_quantized",
 ] + (_AFTER_TORCH_2_4_ONLY if TORCH_VERSION_AFTER_2_4 else [])
+
 
 ############################# Unified Quantization APIs ##############################
 # API 1, single quantize call to create a quantized model with quantized state_dict
@@ -143,14 +136,10 @@ def apply_dynamic_quant(model, filter_fn=None):
 
 
 def _get_subclass_inserter(cls, **kwargs):
-    # pyre-fixme[53]: Captured variable `cls` is not annotated.
-    # pyre-fixme[3]: Return type must be annotated.
-    # pyre-fixme[2]: Parameter must be annotated.
-    method = kwargs.pop("method", "from_float")
+
     def insert_subclass(lin):
         lin.weight = torch.nn.Parameter(
-            # cls.from_float(...)
-            getattr(cls, method)(lin.weight, **kwargs), requires_grad=False
+            cls.from_float(lin.weight, **kwargs), requires_grad=False
         )
         return lin
 
@@ -202,64 +191,6 @@ def change_linear_weights_to_int4_woqtensors(model, **kwargs):
         filter_fn,
     )
 
-# pyre-fixme[3]: Return type must be annotated.
-# pyre-fixme[2]: Parameter must be annotated.
-
-def change_linears_to_autoquantizable(model, **kwargs):
-    """
-    Converts all linear weight tensors to the
-    AutoQuantizableLinearWeight tensor subclass. Expectation is that this is followed
-    by running the model and then calling change_autoquantizable_to_quantized
-    """
-    filter_fn = kwargs.pop("filter_fn", _is_linear)
-    kwargs["qtensor_class_list"] = kwargs.get("qtensor_class_list", DEFAULT_CLASS_LIST)
-    kwargs["mode"] = kwargs.get("mode", ["relu", None])
-    _replace_with_custom_fn_if_matches_filter(
-        model,
-        _get_subclass_inserter(AutoQuantizableLinearWeight, **kwargs),
-        filter_fn if filter_fn is not None else _is_linear,
-    )
-
-def change_autoquantizable_to_quantized(model, **kwargs):
-    """
-    Converts AutoQuantizableLinearWeight tensor subclasses
-    to various quantized/non-quantized tensor subclasses depending
-    on benchmark results. Expectation is that these modules are
-    torch.compiled afterwards.
-    """
-    hold =  torch._dynamo.config.automatic_dynamic_shapes
-    torch._dynamo.config.automatic_dynamic_shapes = False
-
-    filter_fn = kwargs.pop(
-        "filter_fn",
-        lambda mod, *args:
-            hasattr(mod, "weight") and isinstance(mod.weight, AutoQuantizableLinearWeight)
-    )
-    error_on_unseen=kwargs.pop("error_on_unseen", True)
-    _replace_with_custom_fn_if_matches_filter(
-        model,
-        _get_subclass_inserter(
-            AutoQuantizableLinearWeight, method="to_quantized", error_on_unseen=error_on_unseen, **kwargs
-        ),
-        filter_fn,
-    )
-    torch._dynamo.config.automatic_dynamic_shapes = hold
-    torch._dynamo.reset()
-
-@torch.no_grad()
-def autoquant(model, example_input, qtensor_class_list=DEFAULT_CLASS_LIST, filter_fn=_is_linear, mode=["relu",None], **kwargs):
-    """
-    Runs the model with example_input to record shapes and then compares benchmark performance of the seen shape
-    across the qtensor subclasses in qtensor_class_list. Determines best performing qtensor subclass for each layer
-    and applies that type of quantization.
-    """
-    change_linears_to_autoquantizable(model, filter_fn=filter_fn, qtensor_class_list=qtensor_class_list, mode=mode, **kwargs)
-    if not isinstance(example_input, (tuple, list)):
-        assert isinstance(example_input, torch.Tensor)
-        example_input = [example_input]
-    model(*example_input)
-    change_autoquantizable_to_quantized(model, **kwargs)
-    return model
 
 def swap_conv2d_1x1_to_linear(model, filter_fn=None):
     """
