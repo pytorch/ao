3298738c2940f8bd820b5630c1322a88f18c3634
Jerry Zhang <jerryzh168@gmail.com>
2024-03-26 21:07:30 -0700

    weight only quant cuda and cpu benchmark
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- test/quantization/test_quant_api.py
+++ test/quantization/test_quant_api.py
@@ -184,5 +184,81 @@ class TestQuantFlow(unittest.TestCase):
         )
         model = quantizer.quantize(model)
 
+    def test_int4_wo_on_torch_tune_model_cuda(self):
+        from torchtune.models.llama2 import llama2_7b
+        model = llama2_7b()
+        model.to(device="cuda")
+        from torchao.quantization.quant_api import change_linear_weights_to_int4_woqtensors
+        vocab_size = 32000
+        bsz = 2
+        seq_len = 100
+        example_inputs = torch.randint(0, vocab_size, (bsz, seq_len)).to(device="cuda")
+        import time
+        start = time.time()
+        model(example_inputs)
+        end = time.time()
+        print("fp32 time:", end - start)
+
+        change_linear_weights_to_int4_woqtensors(model)
+
+        start = time.time()
+        model(example_inputs)
+        end = time.time()
+        print("unlowered cuda time:", end - start)
+        model = torch.compile(model, mode="max-autotune")
+
+        ITER = 100
+        with torch.no_grad():
+            # warm up
+            for _ in range(5):
+                model(example_inputs)
+
+            t = 0.0
+            for _ in range(ITER):
+                start = time.time()
+                model(example_inputs)
+                end = time.time()
+                print("lowered cuda time:", end - start)
+                t += end - start
+            print("avg cuda time:", t / ITER)
+
+    def test_int4_wo_on_torch_tune_model_cpu(self):
+        from torchtune.models.llama2 import llama2_7b
+        model = llama2_7b()
+        model.to(device="cpu")
+        from torchao.quantization.quant_api import change_linear_weights_to_int4_woqtensors
+        vocab_size = 32000
+        bsz = 2
+        seq_len = 100
+        example_inputs = torch.randint(0, vocab_size, (bsz, seq_len)).to(device="cpu")
+        import time
+        start = time.time()
+        model(example_inputs)
+        end = time.time()
+        print("fp32 time:", end - start)
+
+        change_linear_weights_to_int4_woqtensors(model)
+
+        start = time.time()
+        model(example_inputs)
+        end = time.time()
+        print("unlowered cpu time:", end - start)
+        with torch.no_grad():
+            model = torch.compile(model, mode="max-autotune")
+            for _ in range(2):
+                # warm up
+                model(example_inputs)
+
+            ITER = 10
+            t = 0.0
+            for _ in range(ITER):
+                start = time.time()
+                model(example_inputs.to(device="cpu"))
+                end = time.time()
+                print("lowered cpu time:", end - start)
+                t += end - start
+            print("avg cpu time:", t / ITER)
+
+
 if __name__ == "__main__":
     unittest.main()
