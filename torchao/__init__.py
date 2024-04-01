9b632e2841faa36ec259007d767537180ef76291
Christian Puhrsch <cpuhrsch@fb.com>
2024-04-01 16:41:52 -0700

    Reduce top level API

--- torchao/__init__.py
+++ torchao/__init__.py
@@ -15,14 +15,5 @@ __all__ = [
     "dtypes",
     "apply_dynamic_quant",
     "apply_weight_only_int8_quant",
-    "apply_dynamic_quant",
-    "change_linear_weights_to_int8_dqtensors",
-    "change_linear_weights_to_int8_woqtensors",
-    "change_linear_weights_to_int4_woqtensors",
-    "swap_conv2d_1x1_to_linear"
-    "safe_int_mm",
     "autoquant",
-    "change_linears_to_autoquantizable",
-    "change_autoquantizable_to_quantized",
-    "dtypes"
 ]
