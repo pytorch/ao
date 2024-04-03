c3d107bac09fb92d4d2ecf7b097593b27840dd4b
jeromeku <jerome.ku@gmail.com>
2024-04-03 18:40:37 +0000

    refactor test utils

--- test/kernel/test_galore_downproj.py
+++ test/kernel/test_galore_downproj.py
@@ -1,6 +1,6 @@
 import pytest
 import torch
-from triton.testing import do_bench
+from test_utils import make_data
 
 from torchao.prototype.galore.kernels.matmul import triton_mm_launcher
 from torchao.prototype.galore.utils import TestGaLoreProjector as GaLoreProjector
@@ -9,22 +9,6 @@ torch.manual_seed(0)
 MAX_DIFF_no_tf32 = 1e-4
 
 MAX_DIFF_tf32 = 1e-2
-
-
-def make_data(M, N, rank, dtype):
-    grad = torch.randn(M, N, device="cuda", dtype=dtype)
-    params = torch.randn(M, N, device="cuda", dtype=dtype)
-
-    galore_proj = GaLoreProjector(rank=rank)
-    galore_proj.update_orthogonal_matrix(grad)
-
-    if M >= N:
-        exp_avg = torch.randn(M, rank, device="cuda", dtype=dtype)
-    else:
-        exp_avg = torch.randn(rank, N, device="cuda", dtype=dtype)
-    exp_avg2 = exp_avg**2
-
-    return exp_avg, exp_avg2, grad, galore_proj, params
 
 
 TEST_CONFIGS = [
@@ -40,14 +24,11 @@ def test_galore_downproj(M, N, rank, allow_tf32, fp8_fast_accum, dtype):
     torch.backends.cuda.matmul.allow_tf32 = allow_tf32
     MAX_DIFF = MAX_DIFF_tf32 if allow_tf32 else MAX_DIFF_no_tf32
     exp_avg, exp_avg2, grad, galore_proj, params = make_data(M, N, rank, dtype)
-    print(
-        f"Running with {M} x {N} grad / param, GaLore orthogonal matrix {list(galore_proj.ortho_matrix.shape)}, and {dtype}"
-    )
 
     if M >= N:
-        a, b = grad, galore_proj.ortho_matrix.t()
+        a, b = grad, galore_proj.t()
     else:
-        a, b = galore_proj.ortho_matrix.t(), grad
+        a, b = galore_proj.t(), grad
     low_rank_ref = lambda: a @ b
     low_rank_tt = lambda: triton_mm_launcher(
         a, b, allow_tf32=allow_tf32, fp8_fast_accum=fp8_fast_accum
--- torchao/prototype/galore/kernels/adam_downproj_fused.py
+++ torchao/prototype/galore/kernels/adam_downproj_fused.py
@@ -3,7 +3,6 @@ import logging
 import torch
 import triton
 import triton.language as tl
-from galore_fused.triton.kernels.matmul import get_mm_heuristics, to_tl_type
 from triton.ops.matmul import get_higher_dtype, init_to_zero
 from triton.ops.matmul_perf_model import early_config_prune, estimate_matmul_time
 
@@ -11,6 +10,7 @@ from .adam_step import BETA1, BETA2, EPS
 from .custom_autotune import Config, autotune
 from .matmul import TRITON_ACC_TYPES
 from .matmul import get_autotuner as default_mm_autotuner
+from .matmul import get_mm_heuristics, to_tl_type
 
 logger = logging.getLogger(__name__)
 
