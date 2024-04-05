429fd869d0a1bbe9665c8cc74b63349f4872170e
Christian Puhrsch <cpuhrsch@fb.com>
2024-04-05 16:23:39 +0000

    Clean up init

--- torchao/__init__.py
+++ torchao/__init__.py
@@ -1,13 +1,7 @@
 from torchao.quantization import (
     apply_weight_only_int8_quant,
     apply_dynamic_quant,
-    change_linear_weights_to_int8_dqtensors,
-    change_linear_weights_to_int8_woqtensors,
-    change_linear_weights_to_int4_woqtensors,
-    swap_conv2d_1x1_to_linear,
     autoquant,
-    change_linears_to_autoquantizable,
-    change_autoquantizable_to_quantized,
 )
 from . import dtypes
 
