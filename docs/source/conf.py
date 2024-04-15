0f675c16e0f2122e585b2fc8421751510f1cbf8c
Svetlana Karslioglu <svekars@meta.com>
2024-04-15 14:28:52 -0700

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -120,7 +120,7 @@ html_theme_options = {
 
 html_logo = "_static/img/pytorch-logo-dark.svg"
 
-html_css_files = ["css/custom_torchtune.css"]
+html_css_files = ["css/custom.css"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
