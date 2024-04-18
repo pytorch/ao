5f10031229c467a07314d6ec7cb92efb95082aa2
willfengg <weif@meta.com>
2024-04-18 14:52:41 -0700

    dummy change
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- test/dtypes/test_nf4.py
+++ test/dtypes/test_nf4.py
@@ -194,10 +194,9 @@ class TestNF4Linear(TestCase):
 
     @unittest.skipIf(not torch.cuda.is_available(), "Need CUDA available")
     def test_smoketest_linear_compile(self):
-        for dtype in [torch.bfloat16, torch.float16]:
+        for dtype in [torch.float16, torch.bfloat16]:
             if torch.cuda.is_available() and torch.cuda.get_device_capability() < (8, 0) and dtype == torch.bfloat16:
                 self.skipTest("test requires SM capability of at least (8, 0).")
-            dummy = 1
             a = torch.randn(32, 32, dtype=dtype, device='cuda')
             a_nf4 = torchao.dtypes.to_nf4(a, 16, 2)
             inp = torch.randn(2, 32, 32, dtype=a.dtype, device=a.device)
