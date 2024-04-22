1006cd2afbb528d445c711ec6795f1f39fbbb3f0
cpuhrsch <cpuhrsch@googlemail.com>
2024-04-22 16:06:54 -0700

    Update autoquant.py

--- torchao/quantization/autoquant.py
+++ torchao/quantization/autoquant.py
@@ -12,9 +12,9 @@ from .quant_primitives import (
 import torch.nn.functional as F
 from .utils import TORCH_VERSION_AFTER_2_4
 if TORCH_VERSION_AFTER_2_4:
-    from torch._inductor.utils import do_bench
-else:
     from torch._inductor.runtime.runtime_utils import do_bench
+else:
+    from torch._inductor.utils import do_bench
 
 aten = torch.ops.aten
 
