894312ddf6418516a59544b7d839b5e111cd28ac
cpuhrsch <cpuhrsch@googlemail.com>
2024-04-30 14:25:16 -0700

    Re-enable test_8da4w_quantizer on 2.3 (#107)
    * Re-enable test_8da4w_quantizer on 2.3
    
    * Update test_quant_api.py
    
    * Update test_quant_api.py

--- test/quantization/test_quant_api.py
+++ test/quantization/test_quant_api.py
@@ -159,7 +159,7 @@ class TestQuantFlow(unittest.TestCase):
 
         torch.testing.assert_close(ref, res.cpu())
 
-    @unittest.skipIf(not TORCH_VERSION_AFTER_2_4, "skipping when torch verion is 2.4 or lower")
+    @unittest.skipIf(not TORCH_VERSION_AFTER_2_3, "skipping when torch verion is 2.3 or lower")
     def test_8da4w_quantizer(self):
         from torchao.quantization.quant_api import Int8DynActInt4WeightQuantizer
         from torchao.quantization.GPTQ import Int8DynActInt4WeightLinear
