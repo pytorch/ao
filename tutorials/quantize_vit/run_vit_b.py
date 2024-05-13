016c5f9f05f03d6ae8b78a1ca4a0a4dd19ead1bf
lancerts <tangshao28@gmail.com>
2024-05-13 07:22:15 -0700

    Update tutorials/quantize_vit/run_vit_b.py
    Co-authored-by: Mark Saroufim <marksaroufim@gmail.com>

--- tutorials/quantize_vit/run_vit_b.py
+++ tutorials/quantize_vit/run_vit_b.py
@@ -2,7 +2,7 @@ import torch
 import torchvision.models.vision_transformer as models
 
 from util import benchmark_model, profiler_runner
-
+torch.set_float32_matmul_precision("high")
 # Load Vision Transformer model
 model = models.vit_b_16(pretrained=True)
 
