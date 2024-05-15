20edc7c6c348b850735a1a4a04241769122573bc
Jesse Cai <jcjessecai@gmail.com>
2024-05-15 04:01:47 -0700

    wip

--- benchmarks/benchmark_gpu_sparsity.py
+++ benchmarks/benchmark_gpu_sparsity.py
@@ -49,15 +49,23 @@ def create_semi_structured_tensor(
 
 
 def benchmark_in_us(f, *args, **kwargs):
+    torch.cuda.synchronize()
+    torch.cuda.reset_peak_memory_stats()
+    mem_begin = torch.cuda.max_memory_allocated() / 2**20
     t0 = benchmark.Timer(
         stmt="f(*args, **kwargs)",
         globals={"args": args, "kwargs": kwargs, "f": f}
     )
-    return t0.blocked_autorange()
+    measurement = t0.timeit(number=100)
+    torch.cuda.synchronize()
+    name = measurement.task_spec.description
+    memory = torch.cuda.max_memory_allocated() / 2**20 - mem_begin
+    measurement.mem_use = memory
+    return measurement
 
 
 def run_gpu_sparse_benchmark(m, k, n, args):
-    dtype = DTYPE_LOOKUP[args.dtype]
+    dtype = getattr(torch, args.dtype)
 
     x = torch.randn(n, k).to(dtype).cuda()
     b = torch.randn(m, dtype=dtype).cuda()
@@ -77,51 +85,44 @@ def run_gpu_sparse_benchmark(m, k, n, args):
     if args.eval_fn == "linear":
         dense_output = F.linear(x, A, b)
         sparse_output = F.linear(x, A_sparse, b)
-        correct = torch.allclose(dense_output, sparse_output, rtol=1e-3, atol=1e-3)
         dense_time = benchmark_in_us(F.linear, x, A, b)
         sparse_time = benchmark_in_us(F.linear, x, A_sparse, b)
 
     elif args.eval_fn == "mm":
-        dense_output = torch.mm(x, A)
-        sparse_output = torch.mm(x, A_sparse)
-        correct = torch.allclose(dense_output, sparse_output, rtol=1e-3, atol=1e-3)
+        dense_output = torch.mm(A, x.t())
+        sparse_output = torch.mm(A_sparse, x.t())
 
-        dense_time = benchmark_in_us(torch.mm, x, A)
-        sparse_time = benchmark_in_us(torch.mm, x, A_sparse)
+        dense_time = benchmark_in_us(torch.mm, A, x.t())
+        sparse_time = benchmark_in_us(torch.mm, A_sparse, x.t())
     else:
         raise ValueError(f"Unknown eval_fn: {args.eval_fn}")
 
 
     return {
-        "test_function": "linear",
+        "test_function": args.eval_fn,
         "m": m,
         "k": k,
         "n": n,
         "dtype": args.dtype,
-        # "backend": args.backend,
         "sparse_latency (ms)": sparse_time.median * 1000,
-        "dense_latency (ms)": dense_time.median * 1000,
+        "sparse_latency iqr": sparse_time.iqr* 1000,
+        "sparse_mem": sparse_time.mem_use,
+        "dense_latency (ms)": dense_time.median* 1000,
+        "dense_latenct iqr": dense_time.iqr* 1000,
+        "dense_mem": dense_time.mem_use,
         "speedup (d/s)": dense_time.median / sparse_time.median,
-        # "correct": correct,
-        # "contiguous": sparse_output.is_contiguous(),
+        "contiguous": sparse_output.is_contiguous(),
     }
 
 
 if __name__ == "__main__":
-    DTYPE_LOOKUP = {
-        "int8": torch.int8,
-        "fp16": torch.float16,
-        "bf16": torch.bfloat16,
-        "fp32": torch.float32,
-    }
-
     parser = argparse.ArgumentParser(description="GPU Sparsity Microbenchmarks")
     parser.add_argument(
         "--mode",
         type=str,
         choices=[
-            "bert-large-shapes",
-            "sam-vitb-shapes",
+            "bert-large",
+            "sam-vit-b",
             "nvidia-fixed-k",
             "nvidia-fixed-mn",
         ],
@@ -150,13 +151,18 @@ if __name__ == "__main__":
     parser.add_argument(
         "--dtype",
         type=str,
-        choices=DTYPE_LOOKUP.keys(),
-        default="bf16",
+        choices=[
+            "int8",
+            "float16",
+            "bfloat16",
+            "float32",
+        ],
+        default="bfloat16",
     )
     parser.add_argument(
         "--backend", type=str, choices=["cutlass", "cusparselt"], default="cusparselt"
     )
-    parser.add_argument("-eval-fn", type=str, choices=["linear", "mm"], default="linear")
+    parser.add_argument("--eval-fn", type=str, choices=["linear", "mm"], default="linear")
     parser.add_argument("-contiguous", action="store_true")
     parser.add_argument("-save", action="store_true")
     args = parser.parse_args()
@@ -246,3 +252,55 @@ if __name__ == "__main__":
         df.to_csv(save_file)
         print(f"Finished benchmark: {args.mode} saved results to {save_file}")
     print(df)
+    import torch
+    print(torch.utils.collect_env.get_pretty_env_info())
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    plt.figure(figsize=(10, 5))
+
+    data = [
+            [ 0.971323, 1.114185,  1.103950,  0.963362],
+            [ 1.793014, 1.291936, 2.169129, 2.033742],
+            [ 1.851897, 1.656134,  2.020207, 1.884365],
+            [ 3.298112, 3.555021, 3.927926, 2.656046]]
+
+    columns = ('Attention QKV', 'Attention Output', 'MLP Lin1', 'MLP Lin2')
+    rows = ["sparsity_level=0.8, block_size=32", "sparsity_level=0.8, block_size=64", "sparsity_level=0.9, block_size=32", "sparsity_level=0.9, block_size=64"]
+
+    values = np.arange(0, 4, 0.5)
+
+    # Get some pastel shades for the colors
+    colors = plt.cm.Paired(np.linspace(0, 1, 7))
+    n_rows = len(data)
+
+    index = np.arange(len(columns))
+    bar_width = 0.15
+
+    # Initialize the vertical-offset for the grouped bar chart.
+    y_offset = np.zeros((n_rows, len(columns)))
+
+    # Plot bars and create text labels for the table
+    cell_text = []
+    for row in range(n_rows):
+        plt.bar(index + row * bar_width, data[row], bar_width, bottom=y_offset[row], color=colors[row])
+        y_offset[row] = y_offset[row] + data[row]
+        cell_text.append(['%1.2fx' % (x) for x in y_offset[row]])
+
+    # Add a table at the bottom of the axes
+    the_table = plt.table(cellText=cell_text,
+                        rowLabels=rows,
+                        rowColours=colors,
+                        colLabels=columns,
+                        loc='bottom')
+
+    # Adjust layout to make room for the table:
+    plt.subplots_adjust(left=0.2, bottom=0)
+    plt.axhline(y=1, linestyle='--', color='gray')
+    plt.ylabel(f"Speedup (Dense/Sparse)")
+    plt.yticks()
+    plt.xticks([], [])
+    plt.title(f'{args.mode} Microbenchmarks (dtype={args.dtype})')
+
+    plt.show()
+    plt.savefig('foo.png', bbox_inches='tight')
