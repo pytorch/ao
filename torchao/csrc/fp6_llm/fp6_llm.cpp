f32d09f82f2ce1cee544630ec4af3ae29c7d03fe
Thien Tran <gau.nernst@yahoo.com.sg>
2024-05-22 07:58:34 +0800

    remove weight dequant

--- torchao/csrc/fp6_llm/fp6_llm.cpp
+++ torchao/csrc/fp6_llm/fp6_llm.cpp
@@ -7,5 +7,4 @@ TORCH_LIBRARY_FRAGMENT(torchao, m) {
   m.def("fp16act_fp6weight_linear(Tensor _in_feats, Tensor _weights, Tensor _scales, int splitK) -> Tensor");
   m.def("prepack_fp6_weight(Tensor fp6_tensor) -> Tensor");
   m.def("fp16_to_fp6_original(Tensor fp16_tensor) -> Tensor");
-  m.def("fp6_weight_dequant(Tensor fp6_tensor, Tensor fp16_scale) -> Tensor");
 }
