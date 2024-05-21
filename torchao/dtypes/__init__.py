ee2310ce32c2c847f195b6257ee92724857a82d2
Thien Tran <gau.nernst@yahoo.com.sg>
2024-05-21 08:49:00 +0000

    add to_fp6 import

--- torchao/dtypes/__init__.py
+++ torchao/dtypes/__init__.py
@@ -1,8 +1,10 @@
 from .nf4tensor import NF4Tensor, to_nf4
 from .uint4 import UInt4Tensor
+from .fp6 import to_fp6
 
 __all__ = [
     "NF4Tensor",
     "to_nf4",
     "UInt4Tensor"
+    "to_fp6",
 ]
