2242477addbd8037a9dc15645f7d7f17c78dcbb3
Mark Saroufim <marksaroufim@meta.com>
2024-05-23 08:43:34 -0700

    yolo

--- test/dtypes/test_fp8.py
+++ test/dtypes/test_fp8.py
@@ -6,7 +6,7 @@ from torch.testing._internal.common_utils import (
     parametrize,
     run_tests,
 )
-from torchao.quantization.utils import TORCH_VERSION_AFTER_2_3
+from torchao.quantization.utils import TORCH_VERSION_AFTER_2_4
 try:
     from torchao.prototype.fp8 import gemm_split_k
     triton_available = True
@@ -28,8 +28,9 @@ class TestFP8Gemm(TestCase):
 
     # https://pytorch.org/tutorials/recipes/torch_compile_user_defined_triton_kernel_tutorial.html
 
-    @unittest.skipIf(not TORCH_VERSION_AFTER_2_3, "User defined triton functions are only supported in PyTorch 2.3 and above")
+    @unittest.skipIf(not TORCH_VERSION_AFTER_2_3, "User defined triton functions are only supported in PyTorch 2.4 and above")
     def test_user_defined_triton_function(self):
+        torch._inductor.config.force_disable_caches = True
         m, n, k = 256, 256, 512
 
         a = torch.randn((m, k), dtype=torch.float16, device="cuda")
