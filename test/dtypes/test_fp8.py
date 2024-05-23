1941a9d17ce60dfd972faefaf1e40a1b6140a5ac
Mark Saroufim <marksaroufim@meta.com>
2024-05-23 16:29:13 -0700

    Make fp8 test explicit

--- test/dtypes/test_fp8.py
+++ test/dtypes/test_fp8.py
@@ -9,7 +9,7 @@ from torch.testing._internal.common_utils import (
 )
 from torchao.quantization.utils import TORCH_VERSION_AFTER_2_4
 try:
-    from torchao.prototype.fp8 import gemm_split_k
+    from torchao.prototype.fp8 import gemm_split_k, to_float8
     triton_available = True
 except ImportError:
     triton_available = False
@@ -19,27 +19,40 @@ except ImportError:
 class TestFP8Gemm(TestCase):
     # @parametrize("dtype", [torch.bfloat16, torch.float16, torch.float32])
     def test_gemm_split_k(self):
-        m, n, k = 256, 256, 512
+        dtype = torch.float16
+        qdtype = torch.float8_e4m3fn
 
-        a = torch.randn((m, k), dtype=torch.float16, device="cuda")
-        b = torch.randn((k, n), dtype=torch.float16, device="cuda")
-        c = gemm_split_k(a, b)
-        c_expected = torch.matmul(a, b)
-        assert torch.allclose(c, c_expected, atol=0.07) # less than this and the accuracy check fails
+        torch.cuda.manual_seed(0)
+
+        m = 64
+        n = 4096
+        k = 4096
+
+        # create test inputs
+        x = torch.randn((m, k), dtype=dtype, device='cuda')
+        w = torch.randn((n, k), dtype=dtype, device='cuda')
+
+        x_fp8, x_inv_s = to_float8(x, dtype=qdtype)
+        w_fp8, w_inv_s = to_float8(w, dtype=qdtype)
+
+        y_torch, _ = torch._scaled_mm(x_fp8, w_fp8.t(), out_dtype=dtype, scale_a=x_inv_s, scale_b=w_inv_s)
+        y_triton = gemm_split_k(x_fp8, w_fp8.t(), scale_a=x_inv_s.item(), scale_b=w_inv_s.item())
+        y_fp16 = torch.nn.functional.linear(x, w)
+
+        cos_sim_torch = torch.nn.functional.cosine_similarity(y_fp16.reshape(-1), y_torch.reshape(-1), dim=0)
+        cos_sim_triton = torch.nn.functional.cosine_similarity(y_fp16.reshape(-1), y_triton.reshape(-1), dim=0)
+
+        assert cos_sim_torch > 0.99, f"fp16 vs torch cos_sim is too low: {cos_sim_torch}"
+        assert cos_sim_triton > 0.99, f"fp16 vs triton cos_sim is too low: {cos_sim_triton}"
 
     # https://pytorch.org/tutorials/recipes/torch_compile_user_defined_triton_kernel_tutorial.html
     @unittest.skip("fp8 kernel compilation does not work on a10g")
     def test_user_defined_triton_function(self):
-        import torch._inductor.config
-        torch._inductor.config.force_disable_caches = True
-        os.environ['TORCHINDUCTOR_COMPILE_THREADS'] = '1'
         m, n, k = 256, 256, 512
 
         a = torch.randn((m, k), dtype=torch.float16, device="cuda")
         b = torch.randn((k, n), dtype=torch.float16, device="cuda")
         compiled_function = torch.compile(gemm_split_k, fullgraph=True)(a,b)
-
-
 
 if __name__ == "__main__":
     run_tests()
