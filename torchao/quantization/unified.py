34af70a3b955121e9a6bdbbabfc339f2c4066a43
Mark Saroufim <marksaroufim@meta.com>
2024-05-24 12:13:04 -0700

    yolo

--- torchao/quantization/unified.py
+++ torchao/quantization/unified.py
@@ -1,8 +1,11 @@
 import torch
 from typing import Any
+from abc import ABC, abstractmethod
+
 
 # API 1, single quantize call to create a quantized model with quantized state_dict
-class Quantizer:
+class Quantizer(ABC):
+    @abstractmethod
     def quantize(
         self, model: torch.nn.Module, *args: Any, **kwargs: Any
     ) -> torch.nn.Module:
@@ -12,6 +15,7 @@ class Quantizer:
 
 # API 2, flow that needs calibration or training
 class TwoStepQuantizer:
+    @abstractmethod
     def prepare(
         self, model: torch.nn.Module, *args: Any, **kwargs: Any
     ) -> torch.nn.Module:
