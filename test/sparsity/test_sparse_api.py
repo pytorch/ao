37e5daa8253999560ee61aff18b330fc3f8924c6
Mark Saroufim <marksaroufim@meta.com>
2024-05-24 13:45:02 -0700

    yolo

--- test/sparsity/test_sparse_api.py
+++ test/sparsity/test_sparse_api.py
@@ -8,7 +8,7 @@ from torchao.sparsity import apply_fake_sparsity, apply_sparse_semi_structured
 from torchao.sparsity.prototype.dynamic_quant_sparse import Int8DynamicallyQuantizedSemiStructuredSparseLinearWeight
 from torchao.quantization.quant_api import (
     _replace_with_custom_fn_if_matches_filter,
-    _get_subclass_inserter,
+    get_subclass_inserter,
     _is_linear,
 )
 from torchao.quantization.utils import TORCH_VERSION_AFTER_2_3
@@ -61,7 +61,7 @@ class TestQuantSemiSparse(TestCase):
         apply_fake_sparsity(model)
         dense_result = model(input)
 
-        _replace_with_custom_fn_if_matches_filter(model, _get_subclass_inserter(Int8DynamicallyQuantizedSemiStructuredSparseLinearWeight), _is_linear)
+        _replace_with_custom_fn_if_matches_filter(model, get_subclass_inserter(Int8DynamicallyQuantizedSemiStructuredSparseLinearWeight), _is_linear)
         sparse_result = model(input)
 
         assert torch.allclose(dense_result, sparse_result, rtol=1e-1, atol=1e-1)
