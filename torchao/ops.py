dd2c9b69937728d982a08712ebd467930e7b55c2
drisspg <drisspguessous@gmail.com>
2024-05-24 10:35:06 -0700

    fix reamde and remove unused kernel

--- torchao/ops.py
+++ torchao/ops.py
@@ -17,20 +17,6 @@ def nms(boxes: Tensor, scores: Tensor, iou_threshold: float) -> Tensor:
     return torch.ops.torchao.nms.default(boxes, scores, iou_threshold)
 
 
-# Defines the meta kernel / fake kernel / abstract impl
-@register_custom_op("torchao::nms")
-def _(dets, scores, iou_threshold):
-    torch._check(dets.dim() == 2, lambda: f"boxes should be a 2d tensor, got {dets.dim()}D")
-    torch._check(dets.size(1) == 4, lambda: f"boxes should have 4 elements in dimension 1, got {dets.size(1)}")
-    torch._check(scores.dim() == 1, lambda: f"scores should be a 1d tensor, got {scores.dim()}")
-    torch._check(
-        dets.size(0) == scores.size(0),
-        lambda: f"boxes and scores should have same number of elements in dimension 0, got {dets.size(0)} and {scores.size(0)}",
-    )
-    ctx = torch._custom_ops.get_ctx()
-    num_to_keep = ctx.create_unbacked_symint()
-    return dets.new_empty(num_to_keep, dtype=torch.long)
-
 
 def prepack_fp6_weight(fp6_weight: Tensor) -> Tensor:
     """
@@ -45,6 +31,7 @@ def prepack_fp6_weight(fp6_weight: Tensor) -> Tensor:
     return torch.ops.torchao.prepack_fp6_weight.default(fp6_weight)
 
 
+# Defines the meta kernel / fake kernel / abstract impl
 @register_custom_op("torchao::prepack_fp6_weight")
 def _(fp6_weight):
     torch._check(fp6_weight.dim() == 2, lambda: f"weight should be a 2d tensor, got {fp6_weight.dim()}D")
