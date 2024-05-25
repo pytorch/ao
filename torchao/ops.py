d24dba8ae6a23414fb9f3aa240773bb509d7cf0b
Thien Tran <gau.nernst@yahoo.com.sg>
2024-05-25 13:51:40 +0800

    fix merge problem

--- torchao/ops.py
+++ torchao/ops.py
@@ -1,26 +1,14 @@
 import torch
 from torch import Tensor
+from torchao.quantization.utils import TORCH_VERSION_AFTER_2_4
 
-def nms(boxes: Tensor, scores: Tensor, iou_threshold: float) -> Tensor:
-    """
-    See https://pytorch.org/vision/main/generated/torchvision.ops.nms.html
-    """
-    return torch.ops.torchao.nms.default(boxes, scores, iou_threshold)
-
-
-# Defines the meta kernel / fake kernel / abstract impl
-@torch.library.impl_abstract("torchao::nms")
-def _(dets, scores, iou_threshold):
-    torch._check(dets.dim() == 2, lambda: f"boxes should be a 2d tensor, got {dets.dim()}D")
-    torch._check(dets.size(1) == 4, lambda: f"boxes should have 4 elements in dimension 1, got {dets.size(1)}")
-    torch._check(scores.dim() == 1, lambda: f"scores should be a 1d tensor, got {scores.dim()}")
-    torch._check(
-        dets.size(0) == scores.size(0),
-        lambda: f"boxes and scores should have same number of elements in dimension 0, got {dets.size(0)} and {scores.size(0)}",
-    )
-    ctx = torch._custom_ops.get_ctx()
-    num_to_keep = ctx.create_unbacked_symint()
-    return dets.new_empty(num_to_keep, dtype=torch.long)
+def register_custom_op(name):
+    def decorator(func):
+        if TORCH_VERSION_AFTER_2_4:
+            return torch.library.register_fake(f"{name}")(func)
+        else:
+            return torch.library.impl_abstract(f"{name}")(func)
+    return decorator
 
 
 def prepack_fp6_weight(fp6_weight: Tensor) -> Tensor:
@@ -36,7 +24,6 @@ def prepack_fp6_weight(fp6_weight: Tensor) -> Tensor:
     return torch.ops.torchao.prepack_fp6_weight.default(fp6_weight)
 
 
-# Defines the meta kernel / fake kernel / abstract impl
 @register_custom_op("torchao::prepack_fp6_weight")
 def _(fp6_weight):
     torch._check(fp6_weight.dim() == 2, lambda: f"weight should be a 2d tensor, got {fp6_weight.dim()}D")
@@ -56,7 +43,7 @@ def fp16_to_fp6_original(fp16_tensor: Tensor) -> Tensor:
     return torch.ops.torchao.fp16_to_fp6_original.default(fp16_tensor)
 
 
-@torch.library.impl_abstract("torchao::fp16_to_fp6")
+@register_custom_op("torchao::fp16_to_fp6")
 def _(fp16_tensor):
     torch._check(fp16_tensor.dim() == 2, lambda: f"weight should be a 2d tensor, got {fp16_tensor.dim()}D")
     torch._check(fp16_tensor.dtype is torch.float16, lambda: f"weight must be FP16, got {fp16_tensor.dtype}")
@@ -96,20 +83,3 @@ def _(_in_feats, _weights, _scales, splitK = 1):
     torch._check(OC == _scales.shape[0], lambda: "Dimensions mismatched")
 
     return _in_feats.new_empty((BS, OC))
-
-
-def fp6_weight_dequant(fp6_tensor: Tensor, fp16_scale: Tensor) -> Tensor:
-    return torch.ops.torchao.fp6_weight_dequant.default(fp6_tensor, fp16_scale)
-
-
-@torch.library.impl_abstract("torchao::fp6_weight_dequant")
-def _(fp6_tensor, fp16_scale):
-    torch._check(fp6_tensor.dim() == 2, lambda: f"weight should be a 2d tensor, got {fp6_tensor.dim()}D")
-    torch._check(fp6_tensor.dtype is torch.int32, lambda: f"weight must be INT32, got {fp6_tensor.dtype}")
-    torch._check(fp16_scale.dim() == 1, lambda: f"scale should be a 2d tensor, got {fp16_scale.dim()}D")
-    torch._check(fp16_scale.dtype is torch.float16, lambda: f"scale must be FP16, got {fp16_scale.dtype}")
-
-    OC, _IC = fp6_tensor.shape
-    torch._check(OC == fp16_scale.shape[0], lambda: "Dimensions mismatched")
-
-    return fp16_scale.new_empty((OC, _IC * 16 // 3))
