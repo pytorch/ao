5ec3deb92d0517e81b4aede3345b509e8d935721
vayuda <120random.things@gmail.com>
2024-05-29 14:57:06 -0400

    added full_graph=True  to compile

--- test/prototype/test_bitpacking.py
+++ test/prototype/test_bitpacking.py
@@ -2,7 +2,7 @@ import torch
 from torchao.prototype.common.bitpacking import pack, unpack
 import pytest
 
-
+from torchao.quantization.utils import TORCH_VERSION_AFTER_2_4
 if not TORCH_VERSION_AFTER_2_4:
     pytest.skip("Unsupported PyTorch version")
 
@@ -18,7 +18,7 @@ def test_uint4_to_uint8():
 def test_uint4_to_uint8_compile():
     torch._dynamo.config.specialize_int = True
     pack_compiled = torch.compile(pack)
-    unpack_compiled = torch.compile(unpack)
+    unpack_compiled = torch.compile(unpack, full_graph=True)
     test_tensor = torch.randint(0, 15, (3, 4), dtype=torch.uint8).cuda()
     packed = pack_compiled(test_tensor, 8, 4)
     unpacked = unpack_compiled(packed, 4)
@@ -37,7 +37,7 @@ def test_uint3_to_int16():
 def test_uint2_to_uint8_col_wise_compile():
     torch._dynamo.config.specialize_int = True
     pack_compiled = torch.compile(pack)
-    unpack_compiled = torch.compile(unpack)
+    unpack_compiled = torch.compile(unpack, full_graph=True)
     test_tensor = torch.randint(0, 3, (8, 8), dtype=torch.uint8).cuda()
     packed = pack_compiled(test_tensor, 8, 2, False)
     unpacked = unpack_compiled(packed,2, False)
