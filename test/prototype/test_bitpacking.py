889bd8f80e976ab69577c4b738e5af79bbb3b647
vayuda <120random.things@gmail.com>
2024-06-03 13:28:29 -0400

    tests written, issues fixed

--- test/prototype/test_bitpacking.py
+++ test/prototype/test_bitpacking.py
@@ -1,5 +1,5 @@
 import torch
-from torchao.prototype.common.bitpacking import pack, unpack
+from torchao.prototype.common.bitpacking import pack, unpack, dtype_to_bits
 import pytest
 from torch.utils._triton import has_triton
 from torchao.quantization.utils import TORCH_VERSION_AFTER_2_4
@@ -7,64 +7,35 @@ from torchao.quantization.utils import TORCH_VERSION_AFTER_2_4
 if not TORCH_VERSION_AFTER_2_4:
     pytest.skip("Unsupported PyTorch version", allow_module_level=True)
 
-def test_uint4_to_uint8_CPU():
-    test_tensor = torch.randint(0, 15, (4, 4), dtype=torch.uint8)
-    packed = pack(test_tensor, 8, 4, device='cpu')
-    unpacked = unpack(packed, 4, device='cpu')
-    unpadded = unpacked[:test_tensor.shape[0], ...]
-    assert(unpadded.allclose(test_tensor))
+def test_trinary_to_uint8_CPU():
+    test_tensor = torch.randint(-1, 1, (4, 4, 4), dtype=torch.int32)
+    for i in range(len(test_tensor.shape)):
+        packed = pack(test_tensor, "trinary", dimension = i, container_dtype = torch.uint8, device='cpu')
+        unpacked = unpack(packed, "trinary", dimension = i, device='cpu')
+        assert(unpacked.to(torch.int32).allclose(test_tensor))
 
-def test_uint3_to_int16_col_wise_cpu():
-    test_tensor = torch.randint(0, 7, (8, 5), dtype=torch.int16)
-    packed = pack(test_tensor,16, 3, False, device='cpu')
-    unpacked = unpack(packed, 3, False, device='cpu')
-    unpadded = unpacked[:test_tensor.shape[0], ...]
-    assert(unpadded.allclose(test_tensor))
-    
+def test_to_uint8_CPU():
+    for dtype in {torch.uint2, torch.uint3, torch.uint4, torch.uint5, torch.uint6, torch.uint7}:
+        test_tensor = torch.randint(0, 2**dtype_to_bits(dtype), (4, 4, 4), dtype=torch.uint8)
+        for i in range(len(test_tensor.shape)):
+            packed = pack(test_tensor, dtype, dimension = i, container_dtype = torch.uint8, device='cpu')
+            unpacked = unpack(packed, dtype, dimension = i, device='cpu')
+            assert unpacked.to(dtype).allclose(test_tensor), f"Failed for {dtype} on dim {i}"
+            
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
-def test_uint4_to_uint8():
-    test_tensor = torch.randint(0, 15, (4, 4), dtype=torch.uint8).cuda()
-    packed = pack(test_tensor, 8, 4)
-    unpacked = unpack(packed, 4)
-    unpadded = unpacked[:test_tensor.shape[0], ...]
-    assert(unpadded.allclose(test_tensor))
-     
-@pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
-@pytest.mark.skipif(not has_triton(), reason="unsupported without triton")
-def test_uint4_to_uint8_compile():
-    torch._dynamo.config.specialize_int = True
-    pack_compiled = torch.compile(pack, fullgraph=True)
-    unpack_compiled = torch.compile(unpack, fullgraph=True)
-    test_tensor = torch.randint(0, 15, (3, 4), dtype=torch.uint8).cuda()
-    packed = pack_compiled(test_tensor, 8, 4)
-    unpacked = unpack_compiled(packed, 4)
-    unpadded = unpacked[:test_tensor.shape[0], ...]
-    assert(unpadded.allclose(test_tensor))
-    
-@pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
-def test_uint3_to_int16():
-    test_tensor = torch.randint(0, 7, (5, 8), dtype=torch.int16).cuda()
-    packed = pack(test_tensor,16, 3)
-    unpacked = unpack(packed, 3)
-    unpadded = unpacked[:test_tensor.shape[0], ...]
-    assert(unpadded.allclose(test_tensor))
+def test_trinary_to_uint8():
+    test_tensor = torch.randint(-1, 1, (4, 4, 4), dtype=torch.int32).cuda()
+    for i in range(len(test_tensor.shape)):
+        packed = pack(test_tensor, "trinary", dimension = i, container_dtype = torch.uint8)
+        unpacked = unpack(packed, "trinary", dimension = i)
+        assert(unpacked.to(torch.int32).allclose(test_tensor))
+    print('trinary passed')
 
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
-@pytest.mark.skipif(not has_triton(), reason="unsupported without triton")
-def test_uint2_to_uint8_col_wise_compile():
-    torch._dynamo.config.specialize_int = True
-    pack_compiled = torch.compile(pack, fullgraph=True)
-    unpack_compiled = torch.compile(unpack, fullgraph=True)
-    test_tensor = torch.randint(0, 3, (8, 8), dtype=torch.uint8).cuda()
-    packed = pack_compiled(test_tensor, 8, 2, False)
-    unpacked = unpack_compiled(packed,2, False)
-    unpadded = unpacked[:test_tensor.shape[0], ...]
-    assert(unpadded.allclose(test_tensor))
-
-@pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
-def test_uint3_to_int16_col_wise():
-    test_tensor = torch.randint(0, 7, (8, 5), dtype=torch.int16).cuda()
-    packed = pack(test_tensor,16, 3, False)
-    unpacked = unpack(packed, 3, False)
-    unpadded = unpacked[:test_tensor.shape[0], ...]
-    assert(unpadded.allclose(test_tensor))
+def test_to_uint8():
+    for dtype in {torch.uint2, torch.uint3, torch.uint4, torch.uint5, torch.uint6, torch.uint7}:
+        test_tensor = torch.randint(0, 2**dtype_to_bits(dtype), (4, 4, 4), dtype=torch.uint8).cuda()
+        for i in range(len(test_tensor.shape)):
+            packed = pack(test_tensor, dtype, dimension = i, container_dtype = torch.uint8)
+            unpacked = unpack(packed, dtype, dimension = i)
+            assert unpacked.allclose(test_tensor), f"Failed for {dtype} on dim {i}"
--- torchao/prototype/common/bitpacking.py
+++ torchao/prototype/common/bitpacking.py
@@ -5,47 +5,72 @@ def mod_shape(shape, mod, dim):
     """changes a select dimension of the input shape to mod"""
     return (*shape[:dim], mod, *shape[dim+1:])
 
+
+def dtype_to_bits(dtype):
+    '''returns the number of bits in a dtype'''
+    if dtype in {torch.uint2, 'trinary'}:
+        return 2
+    elif dtype == torch.uint3:
+        return 3
+    elif dtype == torch.uint4:
+        return 4
+    elif dtype == torch.uint5:
+        return 5
+    elif dtype == torch.uint6:
+        return 6
+    elif dtype == torch.uint7:
+        return 7
+    elif dtype in {torch.uint8, torch.int8}:
+        return 8
+    elif dtype in {torch.uint16, torch.int16, torch.float16}:
+        return 16
+    elif dtype in {torch.uint32, torch.int32, torch.float32}:
+        return 32
+    elif dtype == {torch.uint64, torch.int64, torch.float64}:
+        return 64
+    else:
+        raise ValueError(f"dtype {dtype} not supported (yet)")
+    
 def unpack(data: torch.Tensor,
-           element_dtype: torch.dtype,
+           element_dtype: Union[torch.dtype, str], # accepting strings for trinary until thats added to torch
            dimension: Optional[int] = 0,
            device: Optional[str] ="cuda") -> torch.Tensor:
     """
     Unpacks small dtype elements from a larger dtype.
     
     Inputs:
-    data: - a tensor of packed elements of a small dtype within a larger dtype.
-    data_size: - the size of the small dtype in bits.
+    data: - a tensor of packed elements
+    element_dtype: - the dtype of the elements to unpack
     
     optional:
-    by_rows: bool - specifies whether to unpack... 
-        by rows: tensor(n,m) -> tensor(n*scale, m) 
-        or by columns: tensor(n,m) -> tensor(n,m*scale)
-        
-    defaults to rows because quantization is typically done by rows 
-    but choose the version which matches how you quantize as this improves memory accesses/performance
+    dimension: - the dimension to unpack along
+    
     
     Returns: torch.Tensor - a tensor of the unpacked elements.
     """
-    element_size = torch.iinfo(element_dtype).bits
-    container_size = torch.iinfo(data.dtype).bits
+    container_size = dtype_to_bits(data.dtype)
+    element_size = dtype_to_bits(element_dtype)
     scale = container_size // element_size
-    unpacked = _unpack(data, data_size, scale, dim, device)
+    
+    unpacked = _unpack(data, element_size, container_size, scale, dimension, device)
+    
     if element_dtype == "trinary":
         unpacked = unpacked.to(torch.int8) - 1
     return unpacked
 
-def _unpack(data, data_size, container_size, scale ,dim, device):
+def _unpack(data, element_size, container_size, scale ,dim, device):
     shape = data.shape
     unpacked_data = torch.zeros(mod_shape(shape, shape[dim]*scale, dim), dtype=data.dtype).to(device)
-    nbits = (1 << data_size) - 1 # mask for the last dtype_size bits
-    unpacked_data = []
+    nbits = (1 << element_size) - 1 # mask for the last dtype_size bits
     for i in range(scale):
-        # add the next nbits to the unpacked data
-        shift_amt = container_size - data_size * (i + 1)
-        unpacked_data.append(((data >> shift_amt) & (nbits)).to(data.dtype))
-        
+        shift_amt = container_size - element_size * (i + 1)
+        slices = [slice(None)] * unpacked_data.ndim
+        slices[dim] = slice(i, None, scale)
+        unpacked_data[slices] = ((data >> shift_amt) & (nbits)).to(data.dtype)
+
     # stack the unpacked data and reshape to the original shape
-    torch.stack(unpacked_data,dim=dim).view(mod_shape(shape,scale*shape[dim], dim)) 
+    return unpacked_data.view(mod_shape(shape,scale*shape[dim], dim)) 
+    
 
 def pack(data: torch.Tensor,
          element_dtype: Union[torch.dtype, str], # accepting strings for trinary until thats added to torch
@@ -58,54 +83,43 @@ def pack(data: torch.Tensor,
     TODO: support something like packing 8 uint 3s into 3 uint8s
     
     Inputs:
-    data: - a tensor of unpacked elements of a small dtype. The dtype used for the data will be used for the container.
-    dimension: - the dimension to pack along
-    element_dtype: specify the dtype of the elements to pack
+    data: a tensor of unpacked elements of a small dtype. The dtype used for the data will be used for the container.
+    dimension: the dimension to pack along
+    element_dtype: the dtype of the elements to pack
     
     optional:
     container_dtype: specify the dtype of the container if the data is not already inside a tensor of that dtype
-        
-    by_rows: specifies whether to pack values... 
-        by rows: tensor(n,m) -> tensor(n//scale, m) 
-        or by columns: tensor(n,m) -> tensor(n,m//scale)
+    
     
     defaults to rows because quantization is typically done by rows
     but choose the version which matches how you quantize as this improves memory accesses/performance
     
     Returns: torch.Tensor - a tensor of packed elements.
     """
+    if element_dtype == "trinary":
+        data =  data + 1
+        
     if container_dtype is not None:
         data = data.to(container_dtype)
-        
-    if type(element_dtype) == str:
-        if element_dtype == "trinary":
-            data = data+1
-        else:
-            raise ValueError(f"element_dtype {element_dtype} not supported")
-        
-    element_size = torch.iinfo(element_dtype).bits
-    container_size = torch.iinfo(data.dtype).bits
+    
+    container_size = dtype_to_bits(data.dtype)
+    element_size = dtype_to_bits(element_dtype)
     scale = container_size // element_size
+        
     assert data.shape[dimension] >= scale, f"not enough values to pack along dimension {dimension} ({data.shape[dimension]}) < scale ({scale})"
-    return _pack_uints(data, container_size, element_dtype, scale, dimension, device)
+    return _pack(data, container_size, element_size, scale, dimension, device)
 
 
 
-def _pack(data, container_size, data_size, scale, dim, device) -> torch.Tensor:
+def _pack(data, container_size, element_size, scale, dim, device) -> torch.Tensor:
     #pad dimension to be divisible by scale
-    if data.shape[dimension] % scale != 0:
+    if data.shape[dim] % scale != 0:
         padding = torch.zeros(mod_shape(data.shape, scale - data.shape[dim] % scale, dim), dtype=data.dtype).to(device)
-        data = torch.cat([data, padding], dim=dim).cuda()
+        data = torch.cat([data, padding], dim=dim).to(device)
         
     packed = torch.zeros(mod_shape(data.shape, data.shape[dim] // scale, dim), dtype=data.dtype).to(device)
     for i in range(scale):
-        torch.arange(start=i, stop=data.shape[k], step=scale)
-        packed |= torch.index_select(data, dim=k, index=indices) << container_size-data_size*(i+1)
+        slices = [slice(None)] * packed.ndim
+        slices[dim] = slice(i, None, scale)
+        packed |= data[slices] << container_size-element_size*(i+1)
     return packed
-
-
-test_tensor = torch.randint(0, 15, (4, 4), dtype=torch.uint8).cuda()
-packed = pack(test_tensor, torch.uint4)
-unpacked = unpack(packed, torch.uint4)
-unpadded = unpacked[:test_tensor.shape[0], ...]
-assert(unpadded.allclose(test_tensor))
