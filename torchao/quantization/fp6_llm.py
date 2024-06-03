cc56400426897c76a3e2243e8252758ef1285612
Thien Tran <gau.nernst@yahoo.com.sg>
2024-06-04 07:35:51 +0800

    update bench

--- torchao/quantization/fp6_llm.py
+++ torchao/quantization/fp6_llm.py
@@ -70,7 +70,6 @@ def _to_tc_float6_e3m2_original(tensor: Tensor) -> Tensor:
 
 # more optimized version of _to_tc_float6_e3m2_original() by merging ops
 # https://github.com/usyd-fsalab/fp6_llm/blob/ce76774bcfc26b325c1b558abcf1935026d9abbc/fp6_llm/csrc/utils/weight_prepacking.h
-@torch.compile
 def to_tc_float6_e3m2(tensor: Tensor) -> Tensor:
     assert tensor.ndim == 2
     M, N = tensor.shape
@@ -91,7 +90,6 @@ def to_tc_float6_e3m2(tensor: Tensor) -> Tensor:
     return torch.cat([tensor_2bit, tensor_4bit], dim=0)
 
 
-@torch.compile
 def from_tc_float6_e3m2(tensor: Tensor, M: int, N: int, dtype: torch.dtype = torch.float32) -> Tensor:
     assert tensor.ndim == 1
     assert (M % 64 == 0) and (N % 64 == 0)
