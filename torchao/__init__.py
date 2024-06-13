361bce60594eb49c06067dc1bfda473cfcf476c0
Mark Saroufim <marksaroufim@meta.com>
2024-06-13 11:44:20 -0700

    Add torchao.__version__

--- torchao/__init__.py
+++ torchao/__init__.py
@@ -1,11 +1,20 @@
 import torch
 import logging
 
+# We use this "hack" to set torchao.__version__ correctly
+# For local development this will default to whatever is version.txt
+# For release builds this will be set the version+architecture_postfix
+import pkg_resources
+try:
+    __version__ = pkg_resources.get_distribution('torchao').version
+except pkg_resources.DistributionNotFound:
+    __version__ = 'unknown'
+
+
 _IS_FBCODE = (
     hasattr(torch._utils_internal, "IS_FBSOURCE") and
     torch._utils_internal.IS_FBSOURCE
 )
-
 if not _IS_FBCODE:
     try:
         from . import _C
