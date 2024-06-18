8e93557ecef42226cdb3e744a494ab7c56c40a76
Thien Tran <gau.nernst@yahoo.com.sg>
2024-06-18 22:37:50 +0800

    fix indent

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ def get_extensions():
                 "-O3" if not debug_mode else "-O0",
                 "-t=0"
             ]
-       }
+        }
 
         if debug_mode:
             extra_compile_args["cxx"].append("/ZI")
