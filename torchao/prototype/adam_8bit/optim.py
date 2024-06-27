72e00f699dbb0539b879f7bc038ac7b1ca8dc793
Thien Tran <gau.nernst@yahoo.com.sg>
2024-06-27 08:17:21 +0000

    add adam int8

---
+++ torchao/prototype/adam_8bit/optim.py
@@ -0,0 +1,94 @@
+import torch
+from torch.optim import Adam
+from torch.optim.optimizer import _get_scalar_dtype
+
+from .subclass import DynamicInt8
+
+
+class AdamInt8(Adam):
+    def __init__(self, *args, group_size: int = 256, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.group_size = group_size
+
+    # override _init_group() to use INT8 for optim state
+    def _init_group(
+        self,
+        group,
+        params_with_grad,
+        grads,
+        exp_avgs,
+        exp_avg_sqs,
+        max_exp_avg_sqs,
+        state_steps,
+    ):
+        has_complex = False
+        for p in group["params"]:
+            if p.grad is not None:
+                has_complex |= torch.is_complex(p)
+                params_with_grad.append(p)
+                if p.grad.is_sparse:
+                    raise RuntimeError(
+                        "Adam does not support sparse gradients, please consider SparseAdam instead"
+                    )
+                grads.append(p.grad)
+
+                state = self.state[p]
+                # Lazy state initialization
+                if len(state) == 0:
+                    # note(crcrpar): [special device hosting for step]
+                    # Deliberately host `step` on CPU if both capturable and fused are off.
+                    # This is because kernel launches are costly on CUDA and XLA.
+                    state["step"] = (
+                        torch.zeros(
+                            (),
+                            dtype=_get_scalar_dtype(is_fused=group["fused"]),
+                            device=p.device,
+                        )
+                        if group["capturable"] or group["fused"]
+                        else torch.tensor(0.0, dtype=_get_scalar_dtype())
+                    )
+                    # Exponential moving average of gradient values
+                    state["exp_avg"] = torch.zeros_like(
+                        p, memory_format=torch.preserve_format
+                    )
+                    # Exponential moving average of squared gradient values
+                    state["exp_avg_sq"] = torch.zeros_like(
+                        p, memory_format=torch.preserve_format
+                    )
+
+                    if group["amsgrad"]:
+                        # Maintains max of all exp. moving avg. of sq. grad. values
+                        state["max_exp_avg_sq"] = torch.zeros_like(
+                            p, memory_format=torch.preserve_format
+                        )
+
+                    # added code for INT8 optim state
+                    # only apply for 2D params e.g. Linear.weight
+                    # TODO: apply for >2D params also e.g. Conv2d.weight
+                    # TODO: just flatten the weight internally?
+                    for k in ["exp_avg", "exp_avg_sq", "max_exp_avg_sq"]:
+                        if k in state and state[k].ndim == 2 and state[k].shape[1] % self.group_size == 0:
+                            state[k] = DynamicInt8.from_float(state[k], self.group_size)
+
+                exp_avgs.append(state["exp_avg"])
+                exp_avg_sqs.append(state["exp_avg_sq"])
+
+                if group["amsgrad"]:
+                    max_exp_avg_sqs.append(state["max_exp_avg_sq"])
+                if group["differentiable"] and state["step"].requires_grad:
+                    raise RuntimeError(
+                        "`requires_grad` is not supported for `step` in differentiable mode"
+                    )
+
+                # Foreach without capturable does not support a tensor lr
+                if (
+                    group["foreach"]
+                    and torch.is_tensor(group["lr"])
+                    and not group["capturable"]
+                ):
+                    raise RuntimeError(
+                        "lr as a Tensor is not supported for capturable=False and foreach=True"
+                    )
+
+                state_steps.append(state["step"])
+        return has_complex
