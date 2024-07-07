0ac59f75ae39dab64f0a09e341431c6ca7a5cab0
jeromeku <jerome.ku@gmail.com>
2024-07-07 21:10:23 +0000

    add more perf counter tools

--- torchao/profiler/performance_counter.py
+++ torchao/profiler/performance_counter.py
@@ -1,10 +1,17 @@
-# mypy: allow-untyped-defs
+import json
 import math
+import textwrap
+import time
 from collections import defaultdict
+from contextlib import contextmanager
+from copy import deepcopy
+from functools import partial
 
 import torch
 from torch.utils._pytree import tree_map
 from torch.utils.flop_counter import FlopCounterMode
+
+from .device_spec import DeviceSpec
 
 aten = torch.ops.aten
 
@@ -86,16 +93,222 @@ class PerformanceCounterMode(FlopCounterMode):
         
         return out
 
-if __name__ == "__main__":
-    import torch
-    from transformers import AutoModelForCausalLM, LlamaForCausalLM
-    
-    model_id = "/home/ubuntu/gpt-fast-dev/checkpoints/7B"
-    model: LlamaForCausalLM = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", torch_dtype=torch.float16, low_cpu_mem_usage=True)
-    input_ids = torch.randint(0, model.config.vocab_size, (1, 16), dtype=torch.int64, device="cuda")
-
-    with PerformanceCounterMode(display=False, depth=10) as perf_counter:
-        _ = model(input_ids)
+class PerformanceTimer:
+    def __init__(self, name, precision=1, display=False, depth=10):
+        self.name = name
+        self.precision = precision
+        self.display = display 
+        self.depth = depth
+        self.perf_counter = PerformanceCounterMode(display=display, depth=depth)
         
-    print(perf_counter.pretty_summary_counts(type="flops", depth=3))
-    print(perf_counter.pretty_summary_counts(type="io", depth=3))
+    def __enter__(self):
+        self.start = time.perf_counter()
+        self.perf_counter.__enter__()
+        return self
+
+    def _print_exit_msg(self):
+        gflops = round(self.total_flops / 1e9, self.precision)
+        ms = round(self.elapsed * 1e3, self.precision)
+        if self.display: 
+            print(f"{self.name.upper()}:  Elapsed = {ms} ms, FLOPS = {gflops} GFLOPs")
+
+    def __exit__(self, type, value, traceback):
+        self.end = time.perf_counter()
+        #Convert to ms
+        self.elapsed = (self.end - self.start)
+        self.perf_counter.__exit__(type, value, traceback)
+        if self.display:
+            self._print_exit_msg()        
+
+    @property
+    def total_flops(self):
+        return self.perf_counter.get_total_flops()
+    
+    @property
+    def total_io(self):
+        return self.perf_counter.get_total_io()
+    
+    @property
+    def flops_table(self):
+        return self.perf_counter.get_table()
+    
+    def get_summary_flop_counts(self):
+        return self.perf_counter.get_summary_flop_counts()
+    
+    def get_summary_io_counts(self):
+        return self.perf_counter.get_summary_io_counts()
+    
+    @property
+    def flop_counts(self):
+        return self.perf_counter.get_flop_counts()
+    
+    @property
+    def io_counts(self):
+        return self.perf_counter.get_io_counts()
+    
+    def get_pretty_summary(self, depth):
+        return self.perf_counter.pretty_summary_counts(depth=depth if depth is not None else self.depth)
+class CUDAPerformanceTimer(PerformanceTimer):
+        
+    def __enter__(self):
+        self.start = torch.cuda.Event(enable_timing=True)
+        self.end = torch.cuda.Event(enable_timing=True)
+        self.start.record()
+        self.perf_counter = PerformanceCounterMode(display=self.display, depth=self.depth)
+        self.perf_counter.__enter__()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.end.record()
+        torch.cuda.synchronize()
+        # Convert from ms to s
+        self.elapsed = self.start.elapsed_time(self.end) * 1e-3
+        self.perf_counter.__exit__(type, value, traceback)
+
+        if self.display:
+            self._print_exit_msg()        
+
+class PerformanceCounterManager:
+    COUNT_KEYS = ["label", "num_tokens", "elapsed", "throughput", "total_flops", "flops_table", "flop_counts"]
+    def __init__(self, depth=10, timer_cls=PerformanceTimer, verbose=False):
+        super().__init__()
+        self._counts = {}
+        self._depth = depth
+        self.timer_cls = timer_cls
+        self.verbose = verbose
+
+    @contextmanager
+    def count(self, label: str, num_tokens: int):
+        perf_timer = self.timer_cls(name=label, depth=self._depth)
+        perf_timer.__enter__()
+        try:
+            yield self
+        finally:
+            perf_timer.__exit__(None, None, None)
+            self._counts[label] = {"label": label,
+                                  "num_tokens": num_tokens,
+                                  "elapsed": perf_timer.elapsed,
+                                  "token_throughput": num_tokens / perf_timer.elapsed,
+                                  "total_flops": perf_timer.total_flops, 
+                                  "flops_throughput": perf_timer.total_flops / perf_timer.elapsed,
+                                  "total_io": perf_timer.total_io,
+                                  "io_throughput": perf_timer.total_io / perf_timer.elapsed,
+                                  "summary_flops": perf_timer.get_summary_flop_counts(),
+                                  "summary_io": perf_timer.get_summary_io_counts(),
+                                  "flop_counts": perf_timer.flop_counts,
+                                  "io_counts": perf_timer.io_counts,
+                                  "pretty_summary": perf_timer.get_pretty_summary(depth=self._depth),
+                                  }
+    @property
+    def counts(self):
+        return self._counts
+    def get_counts(self):
+        return self._counts            
+
+    @property
+    def total_flops(self):
+        return sum(count["total_flops"] for count in self._counts.values())
+    
+    @property
+    def total_io(self):
+        return sum(count["total_io"] for count in self._counts.values())
+    @property
+    def total_tokens(self):
+        return sum(count["num_tokens"] for count in self._counts.values())
+    
+    @property
+    def total_time(self):
+        return sum(count["elapsed"] for count in self._counts.values())
+    
+    def to_dict(self):
+        # Convert flop_counts from OpOverloadPackets to str
+        counts = deepcopy(self._counts)
+        for label,label_counts in counts.items():
+            counts[label]['flop_counts'] = {mod: {str(op): count for op, count in op_count.items()} for mod, op_count in label_counts['flop_counts'].items()}
+            counts[label]['io_counts'] = {mod: {str(op): count for op, count in op_count.items()} for mod, op_count in label_counts['io_counts'].items()}
+
+        return counts
+    
+    def to_json(self):
+        return json.dumps(self.to_dict(), indent=2)
+       
+    def get_summary(self, device_spec: DeviceSpec=None):
+        token_throughput = self.total_tokens / self.total_time
+        achieved_bandwidth = self.total_io / self.total_time
+        achieved_flops_per_s = self.total_flops / self.total_time
+        stats = { 
+                 "total_tokens": self.total_tokens,
+                 "total_time": self.total_time,
+                 "total_flops": self.total_flops,
+                 "total_io": self.total_io,
+                 "token_throughput": token_throughput,
+                 "achieved_bandwidth": achieved_bandwidth,
+                 "achieved_flops_per_s": achieved_flops_per_s,
+                 "arithmetic_intensity": self.total_flops / self.total_io
+             }
+        if device_spec is not None:
+            device_stats = {
+                "device_name": device_spec.name,
+                "theoretical_bandwidth": device_spec.bandwidth,
+                "theoretical_throughput": device_spec.flops,
+                "model_bandwidth_utilization": achieved_bandwidth / device_spec.bandwidth,
+                "model_flops_utilization": achieved_flops_per_s / device_spec.flops,
+            }
+            stats.update(device_stats)
+        return stats
+    def _format_single(self, label, counts, precision, verbose=False):
+        ms = round(counts['elapsed'] * 1e3, precision)
+        token_throughput = round(counts['token_throughput'], precision)
+        gflops = round(counts['total_flops'] / 1e9, precision)
+        gb = round(counts['total_io'] / 1e9, precision)
+        flop_throughput = round(gflops / counts['elapsed'], precision)
+        io_throughput = round(gb / counts['elapsed'], precision)
+        text = textwrap.dedent(f"""\
+            {label.title()}:
+              Elapsed = {ms:,} ms
+              Tokens:
+                Total {counts['num_tokens']}
+                Throughput {token_throughput} tokens/s
+              IO:
+                Total {gb:,} GB
+                Throughput {io_throughput} GB/s
+              FLOPs: 
+                Total {gflops:,} GFLOPs, 
+                Throughput {flop_throughput:,} GFLOP/s""")
+        if verbose:
+            counts_by_module = counts['pretty_summary']
+            text += textwrap.dedent(f"""\nCounts by Module:\n{counts_by_module}""")
+        
+        return text
+    
+    def _format_totals(self, precision=2):
+        ms = round(self.total_time * 1e3, precision)
+        token_throughput = round(self.total_tokens / self.total_time, precision)
+        gflops = round(self.total_flops / 1e9, precision)
+        gb = round(self.total_io / 1e9, precision)
+        flop_throughput = round(gflops / self.total_time, precision)
+        io_throughput = round(gb / self.total_time, precision)
+        text = textwrap.dedent(f"""\
+            FlopCounter Summary:
+              Total time = {ms:,} ms
+              Tokens:
+                Total {self.total_tokens}
+                Throughput {token_throughput:,} tokens/s
+              IO:
+                Total {gb:,} GB
+                Throughput {io_throughput:,} GB/s
+              FLOPs:
+                Total {gflops:,} GFLOPs
+                Throughput {flop_throughput:,} GFLOP/s""")
+        return text
+      
+    def print_summary(self, labels: list[str] = None, precision=2, verbose=None):
+        verbose = verbose if verbose is not None else self.verbose
+        _print = partial(print, flush=True, end='\n')
+        if labels is None:
+            text = self._format_totals(precision=precision)
+            _print(text)
+        else:
+            for label in labels:
+                text = self._format_single(label, self._counts[label], precision=precision, verbose=verbose)
+                _print(text)
