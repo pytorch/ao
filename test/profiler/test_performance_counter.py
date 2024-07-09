d295f536203c1b217263117e5bfb3a3cb6afbac1
jeromeku <jerome.ku@gmail.com>
2024-07-09 18:41:31 +0000

    refactor remaining tests

--- test/profiler/test_performance_counter.py
+++ test/profiler/test_performance_counter.py
@@ -18,11 +18,16 @@ from unittest.mock import patch
 import torch
 from parameterized import parameterized_class
 from utils import (
+    PerfCounterManagerTestConfig,
     PerfCounterResult,
     PerfCounterTestConfig,
     PerfStatsTestConfig,
+    attn_io_check,
+    ffn_io_check,
+    get_leaf_nodes,
     get_test_name,
     patch_device,
+    qkv_proj_io_check,
 )
 
 from torchao.profiler.device_spec import CUDADeviceSpec, DeviceSpec
@@ -35,115 +40,116 @@ from torchao.profiler.performance_counter import (
 )
 from torchao.utils import TORCH_VERSION_AFTER_2_5
 
+PERFCOUNTER_TEST_CONFIGS = [PerfCounterTestConfig(name="3.5B",
+                                                  batch_size=1,
+                                                  seqlen=128,
+                                                  dtype=torch.float16,
+                                                  num_hidden_layers=32 // 2,
+                                                  hidden_size=4096 // 2,
+                                                  intermediate_size=11008 // 2,
+                                                  num_attention_heads=32 // 2,
+                                                  vocab_size=32000 // 2),
+                            PerfCounterTestConfig(name="1.25B",
+                                                  batch_size=1,
+                                                  seqlen=128,
+                                                  dtype=torch.float16,
+                                                  num_hidden_layers=32 // 4,
+                                                  hidden_size=4096 // 4,
+                                                  intermediate_size=11008 // 4,
+                                                  num_attention_heads=32 // 4,
+                                                  vocab_size=32000 // 4),
+                            PerfCounterTestConfig(name="tiny",
+                                                  batch_size=1,
+                                                  seqlen=128,
+                                                  dtype=torch.float16,
+                                                  num_hidden_layers=1,
+                                                  hidden_size=4096 // 4,
+                                                  intermediate_size=11008 // 4,
+                                                  num_attention_heads=32 // 4,
+                                                  vocab_size=32000 // 4)]
 
-def get_leaf_nodes(count_keys, module_name):
-    return [k for k in count_keys if k.endswith(module_name)]
+@parameterized_class([asdict(cfg) for cfg in PERFCOUNTER_TEST_CONFIGS], class_name_func=get_test_name)
+class PerformanceCounterTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        model_cfg = LlamaConfig(num_hidden_layers=cls.num_hidden_layers, 
+                      hidden_size=cls.hidden_size,
+                      intermediate_size=cls.intermediate_size, 
+                      num_attention_heads=cls.num_attention_heads, 
+                      vocab_size=cls.vocab_size)
 
-def attn_proj_io_check(model_config, batch_size, seqlen, element_size):
-    input_size = batch_size * seqlen * model_config.hidden_size * element_size 
-    weight_size = model_config.hidden_size * model_config.hidden_size * element_size 
-    output_size = batch_size * seqlen * model_config.hidden_size * element_size
-    return input_size + weight_size + output_size
-def attn_io_check(model_config, batch_size, seqlen, element_size):
-    # queries, keys, values -> factor of 3
-    input_size = (batch_size * seqlen * model_config.hidden_size * 3) * element_size
-    output_size = (batch_size * seqlen * model_config.hidden_size) * element_size
-    return input_size + output_size 
-    
-def ffn_io_check(model_config, batch_size, seqlen, element_size, module_name):
-    assert module_name in ["up_proj", "gate_proj", "down_proj"]
-
-    if module_name == "down_proj":
-        input_size = batch_size * seqlen * model_config.intermediate_size * element_size
-    else:
-        input_size = batch_size * seqlen * model_config.hidden_size * element_size
-    weight_size = model_config.hidden_size * model_config.intermediate_size * element_size 
-    if module_name == "down_proj":
-        output_size = batch_size * seqlen * model_config.hidden_size * element_size
-    else:
-        output_size = batch_size * seqlen * model_config.intermediate_size * element_size 
-
-    return input_size + weight_size + output_size
-
-
-CONFIG_7B = (32, 4096, 11008, 32, 32000)
-MEDIUM_CONFIG = [p // 2 for p in CONFIG_7B]
-SMALL_CONFIG = [p // 4 for p in CONFIG_7B]
-
-@pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
-@pytest.mark.skipif(not TORCH_VERSION_AFTER_2_5, reason="requires torch >= 2.5")
-@pytest.mark.parametrize("num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, vocab_size", [MEDIUM_CONFIG, SMALL_CONFIG])
-@pytest.mark.parametrize("batch_size, seqlen", [(1, 128),])
-@pytest.mark.parametrize("dtype", [torch.float16], ids=lambda p: str(p))
-def test_performance_counter(num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, vocab_size, batch_size, seqlen, dtype):
-
-    cfg = LlamaConfig(num_hidden_layers=num_hidden_layers, 
-                      hidden_size=hidden_size,
-                      intermediate_size=intermediate_size, 
-                      num_attention_heads=num_attention_heads, 
-                      vocab_size=vocab_size)
-
-    # Note we set some options manually since the model doesn't seem to be initialized correctly
-    # when these options are set in LlamaConfig
-    cfg._attn_implementation = "sdpa"
-    model = LlamaForCausalLM(cfg).to(dtype).to("cuda")
-    model_config = model.config
-    element_size = dtype.itemsize
-    
-    input_ids = torch.randint(0, model_config.vocab_size, (batch_size, seqlen), device="cuda")
-    with torch.no_grad():
-        with torch.nn.attention.sdpa_kernel(torch.nn.attention.SDPBackend.EFFICIENT_ATTENTION):
-            with PerformanceCounterMode() as perf_counter:
-                _ = model(input_ids)
-    
-    summary_flops = perf_counter.get_summary_flop_counts()
-    summary_io = perf_counter.get_summary_io_counts()
-    flops_by_op = perf_counter.get_flop_counts()
-    io_by_op = perf_counter.get_io_counts()
-    assert len(summary_flops) == len(summary_io)
-    assert summary_flops.keys() == summary_io.keys()
-
-    # Attn Projections
-    for k in ["q_proj", "k_proj", "v_proj"]:
-        # Flops check
-        proj_keys = get_leaf_nodes(summary_flops.keys(), k)
-        assert len(proj_keys) == model.config.num_hidden_layers
-        expected_flops = 2 * batch_size * seqlen * model_config.hidden_size * model_config.hidden_size
-        assert expected_flops == summary_flops[proj_keys[0]]
+        # Note we set some options manually since the model doesn't seem to be initialized correctly
+        # when these options are set in LlamaConfig
+        model_cfg._attn_implementation = "sdpa"
+        cls.model = model = LlamaForCausalLM(model_cfg).to(cls.dtype).to("cuda")
+        cls.model_config = model.config
+        cls.element_size = cls.dtype.itemsize
         
-        # io check
-        expected_size = attn_proj_io_check(model_config, batch_size, seqlen, element_size)
-        assert expected_size == summary_io[proj_keys[0]]
+        input_ids = torch.randint(0, model.config.vocab_size, (cls.batch_size, cls.seqlen), device="cuda")
+        with torch.no_grad():
+            with torch.nn.attention.sdpa_kernel(torch.nn.attention.SDPBackend.EFFICIENT_ATTENTION):
+                with PerformanceCounterMode() as perf_counter:
+                    _ = model(input_ids)
+                    cls.perf_counter = perf_counter
+        cls.summary_flops = perf_counter.get_summary_flop_counts()
+        cls.summary_io = perf_counter.get_summary_io_counts()
+        cls.flops_by_op = perf_counter.get_flop_counts()
+        cls.io_by_op = perf_counter.get_io_counts()
 
-    # Attention
-    attention_keys = get_leaf_nodes(summary_flops.keys(), "self_attn")
-    for k in attention_keys:
-        flops = flops_by_op[k]
-        io_movement = io_by_op[k]
-        for op, count in flops.items():
-            if "attention" in op.__name__: 
-                expected_flops = 2 * 2 * batch_size * seqlen * seqlen * model_config.hidden_size
-                assert expected_flops == count
-        for op, count in io_movement.items():
-            if "attention" in op.__name__:
-                # Check approx equal due to other small artifacts returned by sdpa.mem_efficient_attention
-                # See #https://github.com/pytorch/pytorch/blob/main/aten/src/ATen/native/transformers/cuda/attention.cu#L867
-                # Check within 100 bytes
-                expected_size = attn_io_check(model_config, batch_size, seqlen, element_size)
-                assert abs(expected_size - count) < 100
-    # FFN
-    for k in ["up_proj", "gate_proj", "down_proj"]:
-        proj_keys = get_leaf_nodes(summary_flops.keys(), k)
-        assert len(proj_keys) == model.config.num_hidden_layers
-        expected_flops = 2 * batch_size * seqlen * model_config.hidden_size * model_config.intermediate_size
-        assert expected_flops == summary_flops[proj_keys[0]]
+    def test_qkv_proj(self):
+        batch_size, seqlen = self.batch_size, self.seqlen
+        element_size = self.element_size
         
-        # io check
-        expected_size = ffn_io_check(model_config, batch_size, seqlen, element_size, k)
-        assert expected_size == summary_io[proj_keys[0]]
+        assert len(self.summary_flops) == len(self.summary_io)
+        assert self.summary_flops.keys() == self.summary_io.keys()
 
-
-
+        # Attn Projections
+        for k in ["q_proj", "k_proj", "v_proj"]:
+            # Flops check
+            proj_keys = get_leaf_nodes(self.summary_flops.keys(), k)
+            assert len(proj_keys) == self.model.config.num_hidden_layers
+            expected_flops = 2 * batch_size * seqlen * self.model_config.hidden_size * self.model_config.hidden_size
+            assert expected_flops == self.summary_flops[proj_keys[0]]
+            
+            # io check
+            expected_size = qkv_proj_io_check(self.model_config, batch_size, seqlen, element_size)
+            assert expected_size == self.summary_io[proj_keys[0]]
+    
+    def test_attn(self):
+        batch_size, seqlen = self.batch_size, self.seqlen
+        element_size = self.element_size
+        model_config = self.model.config
+        
+        attention_keys = get_leaf_nodes(self.summary_flops.keys(), "self_attn")
+        for k in attention_keys:
+            flops = self.flops_by_op[k]
+            io_movement = self.io_by_op[k]
+            for op, count in flops.items():
+                if "attention" in op.__name__: 
+                    expected_flops = 2 * 2 * batch_size * seqlen * seqlen * model_config.hidden_size
+                    assert expected_flops == count
+            for op, count in io_movement.items():
+                if "attention" in op.__name__:
+                    # Check approx equal due to other small artifacts returned by sdpa.mem_efficient_attention
+                    # See #https://github.com/pytorch/pytorch/blob/main/aten/src/ATen/native/transformers/cuda/attention.cu#L867
+                    # Check within 100 bytes
+                    expected_size = attn_io_check(model_config, batch_size, seqlen, element_size)
+                    assert abs(expected_size - count) < 100
+                    
+    def test_ffn(self):
+        batch_size, seqlen = self.batch_size, self.seqlen
+        element_size = self.element_size
+        
+        for k in ["up_proj", "gate_proj", "down_proj"]:
+            proj_keys = get_leaf_nodes(self.summary_flops.keys(), k)
+            assert len(proj_keys) == self.model.config.num_hidden_layers
+            expected_flops = 2 * batch_size * seqlen * self.model_config.hidden_size * self.model_config.intermediate_size
+            assert expected_flops == self.summary_flops[proj_keys[0]]
+            
+            # io check
+            expected_size = ffn_io_check(self.model_config, batch_size, seqlen, element_size, k)
+            assert expected_size == self.summary_io[proj_keys[0]]
+                 
 PERFSTATS_TEST_CONFIGS = [PerfStatsTestConfig(label="with_device", 
                                               num_tokens=128, 
                                               duration=0.1, 
@@ -213,19 +219,8 @@ def test_performance_stats(cfg: PerfStatsTestConfig):
         assert expected_flops_utilization_str in stats_str
         
 
-# @pytest.fixture
-# def device_spec(request):
-#     device_name, bandwidth = request.param
-#     if device_name is not None:
-#         with patch_device(device_name):
-#             return CUDADeviceSpec(dtype=torch.bfloat16, bandwidth=bandwidth)
-#     else:
-#         return None
-    
-    
-PERFCOUNTERMANAGER_TEST_CONFIGS = [PerfCounterTestConfig("no_device", (1, 1024, 4096, 4096), PerformanceTimer, torch.bfloat16, (None, 0)),
-                PerfCounterTestConfig("a100", (1, 1024, 4096, 4096), CUDAPerformanceTimer, torch.bfloat16, ("A100", 2e12))]
-
+PERFCOUNTERMANAGER_TEST_CONFIGS = [PerfCounterManagerTestConfig("no_device", (1, 1024, 4096, 4096), PerformanceTimer, torch.bfloat16, (None, 0)),
+                                   PerfCounterManagerTestConfig("a100", (1, 1024, 4096, 4096), CUDAPerformanceTimer, torch.bfloat16, ("A100", 2e12))]
 
 @parameterized_class([asdict(cfg) for cfg in PERFCOUNTERMANAGER_TEST_CONFIGS], class_name_func=get_test_name)
 class TestPerformanceCounterManager(unittest.TestCase):
