25d2dec7382f03f157698f85b79682a8aa0b5022
Apurva Jain <apurvajain.kota@gmail.com>
2024-07-10 16:54:12 -0700

    Added support to benchmark_model for cpu and mps (#406)
    Added support to benchmark_model for cpu and mps.

--- test/integration/test_integration.py
+++ test/integration/test_integration.py
@@ -83,6 +83,7 @@ from torchao.utils import (
     TORCH_VERSION_AFTER_2_4,
     unwrap_tensor_subclass,
     is_fbcode,
+    benchmark_model
 )
 
 logger = logging.getLogger("INFO")
@@ -1496,6 +1497,40 @@ class TestUtils(unittest.TestCase):
         self.assertTrue(size2 < size)
 
 
+
+
+class TestBenchmarkModel(unittest.TestCase):
+    
+    class ToyLinearModel(torch.nn.Module):
+        def __init__(self, m=64, n=32, k=64):
+            super().__init__()
+            self.linear1 = torch.nn.Linear(m, n, bias=False)
+            self.linear2 = torch.nn.Linear(n, k, bias=False)
+
+        def example_inputs(self, batch_size=1, dtype=torch.float32, device="cpu"):
+            return (torch.randn(batch_size, self.linear1.in_features, dtype=dtype, device=device),)
+
+        def forward(self, x):
+            x = self.linear1(x)
+            x = self.linear2(x)
+            return x
+
+    def run_benchmark_model(self, device):
+        # params
+        dtype = torch.bfloat16
+        m = self.ToyLinearModel(1024, 1024, 1024).eval().to(dtype).to(device)
+        m_bf16 = copy.deepcopy(m)
+        example_inputs = m.example_inputs(dtype=dtype, device=device)
+        m_bf16 = torch.compile(m_bf16, mode='max-autotune')
+        num_runs = 1
+        return benchmark_model(m_bf16, num_runs, example_inputs[0])
+
+    @unittest.skipIf(not torch.cuda.is_available(), "Need CUDA available")
+    def test_benchmark_model_cuda(self):
+        assert self.run_benchmark_model("cuda") is not None
+
+    def test_benchmark_model_cpu(self):
+        assert self.run_benchmark_model("cpu") is not None
 
 
 if __name__ == "__main__":
