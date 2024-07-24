cf2c134a57ef00ae999c34c326ea5c0b8cbc5a48
Hanxian97 <hanxianhuang@meta.com>
2024-07-24 09:17:05 -0700

    remove exp files

--- torchao/quantization/prototype/mixed_precision/run_mp_quant_benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PYTHON_SCRIPT="scripts/generate.py"
-python $PYTHON_SCRIPT --checkpoint_path=checkpoints/meta-llama/Meta-Llama-3-8B/model.pth --compile --quantization=MP_llama3 --sensi_bit=5 --non_sensi_bit=4 --write_result mp_quant_benchmark_results.txt
-python $PYTHON_SCRIPT --checkpoint_path=checkpoints/meta-llama/Meta-Llama-3-8B/model.pth --compile --quantization=MP_llama3 --sensi_bit=4 --non_sensi_bit=3 --write_result mp_quant_benchmark_results.txt
-python $PYTHON_SCRIPT --checkpoint_path=checkpoints/meta-llama/Meta-Llama-3-8B/model.pth --compile --quantization=MP_llama3 --sensi_bit=5 --non_sensi_bit=3 --write_result mp_quant_benchmark_results.txt
-
-
-echo "All processes are complete."
