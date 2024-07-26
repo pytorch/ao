62834a22498a91218d72fe83ed7c7e59d5ac99b1
yiliu30 <yi4.liu@intel.com>
2024-07-26 04:54:10 -0400

    use int4 kernel
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- torchao/prototype/autoround/auto_round_flow.py
+++ torchao/prototype/autoround/auto_round_flow.py
@@ -1,3 +1,8 @@
+# ==------------------------------------------------------------------------------------------==
+# Utils
+# ==------------------------------------------------------------------------------------------==
+import os
+
 seed = 0
 import random
 
@@ -65,46 +70,67 @@ def create_qmodel_from_qdq_model(qdq_model):
     def _is_quantized_linear(model, fqn):
         return hasattr(model, "scale")
 
+    @torch.no_grad()
     def create_qlinear(linear):
         def _get_qdq_data(linear):
             # TODO: refine the impl, refer: https://github.com/yiliu30/auto-round/pull/2
-            qdq_weight = linear.weight
-            scales = linear.scale.reshape(-1, 1)
-            zeros = linear.zp.reshape(-1, 1)
+            # qdq_weight shape: (m, k)
+            qdq_weight = linear.weight.clone()
+            device = qdq_weight.device
+            # scales, zeros shape: (m, n_groups)
+            scales = linear.scale.to(device)
+            zeros = linear.zp.to(device)
+
+            # Requantize the qdqweight to get the int_data
             orig_shape = qdq_weight.shape
-            # breakpoint()
-            group_size = 128
-            gs_shape = (-1, group_size)
-            qweight_with_origin_shape = (qdq_weight.reshape(gs_shape) / scales + zeros).round().reshape(orig_shape)
-            qweight_with_origin_shape = qweight_with_origin_shape.to(torch.int32)
-            qweight = qweight_with_origin_shape
+            oc, ic = orig_shape
+            groupsize = linear.group_size
+            assert ic % groupsize == 0, f"expect k % groupsize == 0, but got {ic % groupsize}"
+            n_groups = ic // groupsize
 
-            scales = scales.reshape(qweight.shape[0], -1)
-            zp = zeros.reshape(qweight.shape[0], -1)
-
-            n, k = qweight.shape
+            # Check the shapes of scales and zeros with int_data
+            scales_zeros_expected_shape = torch.Size([oc, n_groups])
             assert (
-                scales.shape == torch.Size([qweight.shape[0], qweight.shape[1] // group_size])
-            ), f"expect scales shape {torch.Size([qweight.shape[0], qweight.shape[1] // group_size])}, but got {scales.shape}"
-            if zp is not None:
-                zp = zp.to(torch.bfloat16)
-                zp = zp.reshape(qweight.shape[0], -1)
-                assert (
-                    zp.shape == torch.Size([qweight.shape[0], qweight.shape[1] // group_size])
-                ), f"expect zp shape {torch.Size([qweight.shape[0], qweight.shape[1] // group_size])}, but got {zp.shape}"
-                zeros = (8 - zp) * scales
+                scales.shape == scales_zeros_expected_shape
+            ), f"expect scales shape {scales_zeros_expected_shape}, but got {scales.shape}"
 
+            assert (
+                zeros.shape == scales_zeros_expected_shape
+            ), f"expect zeros shape {scales_zeros_expected_shape}, but got {zeros.shape}"
+
+            flatten_scales = scales.reshape(-1, 1)
+            flatten_zeros = zeros.reshape(-1, 1)
+            gs_shape = (-1, groupsize)
+            int_data = (
+                qdq_weight.reshape(gs_shape)
+                .div(flatten_scales)
+                .add(flatten_zeros)
+                .round()
+                .reshape(orig_shape)
+                .to(torch.int32)
+            )
+
+            # Shift the zeros to align with tinnygemm
+            # TODO: more notes or discard this step
+            zeros = (8 - zeros) * scales
+
+            # Pack to tinygemm reqiured format
             # Hard code inner_k_tiles = 2
             inner_k_tiles = 2
-            # Pack to tinygemm reqiured format
-            packed_q = torch.ops.aten._convert_weight_to_int4pack(qweight, inner_k_tiles)
+
+            packed_int_data = torch.ops.aten._convert_weight_to_int4pack(int_data, inner_k_tiles)
             scales_and_zeros = ao_quant.utils.pack_tinygemm_scales_and_zeros(
                 scales.to(torch.bfloat16), zeros.to(torch.bfloat16)
             )
-            q_groups = k // group_size
-            return packed_q, scales_and_zeros
+            return packed_int_data, scales_and_zeros
 
+        # For Debug
         int_data, scales_and_zeros = _get_qdq_data(linear)
+        float_out = None
+        random_input = torch.randn((4, linear.weight.shape[1]), dtype=torch.bfloat16).to(int_data.device)
+        if os.environ.get("DEBUG", "0") == "1":
+            float_out = linear(random_input.to(linear.weight.dtype))
+
         # TODO: Double check below args
         woq_weight = ao_quant.Int4WeightOnlyQuantizedLinearWeight(
             int_data,
@@ -113,11 +139,18 @@ def create_qmodel_from_qdq_model(qdq_model):
             shape=linear.weight.shape,
             groupsize=128,
             inner_k_tiles=32,
-            dtype=linear.weight.dtype,
+            dtype=torch.bfloat16,
         )
         linear.weight = torch.nn.Parameter(woq_weight, requires_grad=False)
         del linear.scale
         del linear.zp
+        if os.environ.get("DEBUG", "0") == "1":
+            new_lin_int_out = linear(random_input)
+            # _max = (float_out - new_lin_int_out).abs().max()
+            # print(f"Max diff between float and int: {_max}")
+            assert torch.allclose(
+                float_out.to(new_lin_int_out.dtype), new_lin_int_out, atol=2e-1
+            ), f"The max diff between float and int is too large: {(float_out - new_lin_int_out).abs().max()}"
         return linear
 
     qmodel = ao_quant.quant_api._replace_with_custom_fn_if_matches_filter(
@@ -214,6 +247,7 @@ def apply_auto_round(observed_block: ObservedBlock):
     rounder = auto_round.AutoRound(
         model=block,
         tokenizer=None,
+        sym=False,  # Both True and False are OK
         bits=4,
         iters=2,
         use_quant_input=False,  # disable it for now
@@ -221,9 +255,10 @@ def apply_auto_round(observed_block: ObservedBlock):
         amp=False,
         seqlen=seq_len,
         batch_size=1,
+        low_gpu_mem_usage=False,
     )
     # TODO: rename the `quant_block` to `quant_block_`
-    rounder.quant_block(block, input_ids=_input_ids, input_others=_input_others)
+    rounder.quant_block(block, input_ids=_input_ids, input_others=_input_others, device=torch.device("cuda"))
     return create_qmodel_from_qdq_model(observed_block)
 
 
@@ -261,14 +296,16 @@ class TestFlow:
             new_output.append(out)
         assert_same(origin_output, new_output)
 
-    def test_with_gptj(self):
+    def test_with_opt(self):
         with torch.no_grad():
+            device = torch.device("cuda")
+
             # Step 0. Load the float model
             import transformers
 
             # pretrained_model_name_or_path = "hf-internal-testing/tiny-random-GPTJForCausalLM"
             pretrained_model_name_or_path = "facebook/opt-125m"
-            model = transformers.AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path)
+            model = transformers.AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path).to(device)
             tokenizer = transformers.AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
 
             # Step 1. replace the block with an observed block
@@ -288,7 +325,7 @@ class TestFlow:
             # "input_ids", "attention_mask"
             example_inputs = tokenizer(prompt, return_tensors="pt")
             for _ in range(iters):
-                model(**example_inputs)
+                model(**example_inputs.to(device))
 
             print(f"model with observer (after calibration): \n{model}")
 
--- torchao/quantization/subclass.py
+++ torchao/quantization/subclass.py
@@ -126,6 +126,7 @@ class QuantizedLinearWeightBase(torch.Tensor):
         kwargs = {} if kwargs is None else kwargs
 
         if func is torch.nn.functional.linear:
+            breakpoint()
             mat1, w_qtensor, bias = (
                 args[0],
                 args[1],
@@ -441,11 +442,11 @@ class Int4WeightOnlyQuantizedLinearWeight(QuantizedLinearWeightBase):
     def _quantized_op(act_mat, w_qtensor, bias):
         orig_act_size = act_mat.size()
         orig_dtype = act_mat.dtype
-
         # reshape and pad activation
         act_mat = act_mat.reshape(-1, act_mat.shape[-1]).to(torch.bfloat16)
-        pad_size = find_multiple(act_mat.shape[-1], 1024)
-        act_mat = torch.nn.functional.pad(act_mat, (0, pad_size - act_mat.shape[-1]))
+        # Any padding for weight? Otherwise it may cause the mismatch of the shape of the input and the weight
+        # pad_size = find_multiple(act_mat.shape[-1], 1024)
+        # act_mat = torch.nn.functional.pad(act_mat, (0, pad_size - act_mat.shape[-1]))
 
         # matmul
         y = aten._weight_int4pack_mm(
@@ -467,6 +468,7 @@ class Int4WeightOnlyQuantizedLinearWeight(QuantizedLinearWeightBase):
         return y.to(orig_dtype)
 
     def dequantize(self):
+        breakpoint()
         eye_shape = self.shape[1] if not self.transposed else self.shape[0]
         w_dq = self._quantized_op(
             torch.eye(eye_shape, device=self.device, dtype=self.dtype), self, None
