d175d1ac7756fac389179ee003358a3461ca97e2
Hanxian97 <hanxianhuang@meta.com>
2024-07-31 13:34:37 -0700

    rm mytestfunc

--- test/quantization/test_mixed_precision.py
+++ test/quantization/test_mixed_precision.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torchao.quantization import quantize_, int8_weight_only, int4_weight_only
 from torchao.quantization.utils import compute_error
 #from torchao.quantization.prototype.mixed_precision.naive_intNwo import intN_weight_only
-
+'''
 def test_weight_only_quant_naive(quantization_bit=2, symmetric=False):
     for x_shape in [[64, 32], [80, 80, 80, 32], [16, 64, 32]]:
         x = torch.randn(*x_shape, dtype=torch.bfloat16)
@@ -16,7 +16,7 @@ def test_weight_only_quant_naive(quantization_bit=2, symmetric=False):
         # e.g., we set sqnr threshold = 44 for 8-bit, so that 6.02 * 8= 48.16 fullfills
         assert sqnr > 44.0-(8-quantization_bit)*6.02, "sqnr: {} is too low".format(sqnr)
 
-'''
+
 # test if the asymmetric and symmetric quantization API works with different bit widths
 for i in [2, 3, 4, 5, 6, 8]:
     # test for asymmetric quantization
