f2f5bef1d26654716502708b2f7ec214e65d8136
Hanxian97 <hanxianhuang@meta.com>
2024-07-31 16:43:09 -0700

    add def in test

--- test/quantization/test_mixprecision.py
+++ test/quantization/test_mixprecision.py
@@ -3,3 +3,15 @@ import torch.nn as nn
 from torchao.quantization import quantize_, int8_weight_only, int4_weight_only
 from torchao.quantization.utils import compute_error
 from torchao.quantization.prototype.mixed_precision.naive_intNwo import intN_weight_only
+
+def test_weight_only_quant_naive(quantization_bit=2, symmetric=False):
+    for x_shape in [[64, 32], [80, 80, 80, 32], [16, 64, 32]]:
+        x = torch.randn(*x_shape, dtype=torch.bfloat16)
+        m = nn.Sequential(nn.Linear(32, 80)).bfloat16()
+        y_ref = m(x)
+        quantize_(m, intN_weight_only(n=quantization_bit, group_size=32, symmetric=symmetric))
+        y_wo = m(x)
+        sqnr = compute_error(y_ref, y_wo)
+        # SQNR_dB can be approximated by 6.02n, where n is the bit width of the quantization
+        # e.g., we set sqnr threshold = 44 for 8-bit, so that 6.02 * 8= 48.16 fullfills
+        assert sqnr > 44.0-(8-quantization_bit)*6.02, "sqnr: {} is too low".format(sqnr)
