aa9f330e18c421c8904a154d87180aa31c1e7deb
Hanxian97 <hanxianhuang@meta.com>
2024-07-31 00:19:59 -0700

    rename mytest

--- test/quantization/test_mixed_precision.py
+++ test/quantization/test_mixed_precision.py
@@ -4,7 +4,7 @@ from torchao.quantization import quantize_, int8_weight_only, int4_weight_only
 from torchao.quantization.utils import compute_error
 from torchao.quantization.prototype.mixed_precision.naive_intNwo import intN_weight_only
 
-def test_weight_only_quant(quantization_bit=2, symmetric=False):
+def test_weight_only_quant_naive(quantization_bit=2, symmetric=False):
     for x_shape in [[64, 32], [80, 80, 80, 32], [16, 64, 32]]:
         x = torch.randn(*x_shape, dtype=torch.bfloat16)
         m = nn.Sequential(nn.Linear(32, 80)).bfloat16()
@@ -21,14 +21,14 @@ def test_weight_only_quant(quantization_bit=2, symmetric=False):
 for i in [2, 3, 4, 5, 6, 8]:
     # test for asymmetric quantization
     try:
-        test_weight_only_quant(i, False)
+        test_weight_only_quant_naive(i, False)
         print(f"Test passed for {i}-bit using naive intNwo asymmetric quantization implementation")
     except Exception as e:
         print(f"Exception handled in test loop for {i}-bit asymmetric quantization. Details: {e}")
 
     #test for symmetric quantization
     try:
-        test_weight_only_quant(i, True)
+        test_weight_only_quant_naive(i, True)
         print(f"Test passed for {i}-bit using naive intNwo symmetric quantization implementation")
     except Exception as e:
         print(f"Exception handled in test loop for {i}-bit symmetric quantization. Details: {e}")
