fb73df5d5778cd63c167345d56fbce8d69c3d195
Hanxian97 <hanxianhuang@meta.com>
2024-07-31 11:25:35 -0700

    rm import and funccall

--- test/quantization/test_mixed_precision.py
+++ test/quantization/test_mixed_precision.py
@@ -9,7 +9,7 @@ def test_weight_only_quant_naive(quantization_bit=2, symmetric=False):
         x = torch.randn(*x_shape, dtype=torch.bfloat16)
         m = nn.Sequential(nn.Linear(32, 80)).bfloat16()
         y_ref = m(x)
-        quantize_(m, intN_weight_only(n=quantization_bit, group_size=32, symmetric=symmetric))
+        #quantize_(m, intN_weight_only(n=quantization_bit, group_size=32, symmetric=symmetric))
         y_wo = m(x)
         sqnr = compute_error(y_ref, y_wo)
         # SQNR_dB can be approximated by 6.02n, where n is the bit width of the quantization
