e559dc37433540cadd28cc68b67d0caf6f14d60f
Hanxian97 <hanxianhuang@meta.com>
2024-07-31 18:47:06 -0700

    skip cuda_not_avail for int4

--- test/quantization/test_mixprecision.py
+++ test/quantization/test_mixprecision.py
@@ -6,10 +6,12 @@ from torchao.quantization import quantize_, int8_weight_only, int4_weight_only
 from torchao.quantization.utils import compute_error
 from torchao.quantization.prototype.mixed_precision.naive_intNwo import intN_weight_only
 
+_CUDA_IS_AVAILABLE = torch.cuda.is_available()
+
 class TestWeightOnlyQuantNaive(unittest.TestCase):
 
-    def test_quantization(self):
-        for quantization_bit in [2, 3, 4, 5, 6, 8]:
+    def test_quantization_2_3_5_6_8_bit(self):
+        for quantization_bit in [2, 3, 5, 6, 8]:
             for symmetric in [False, True]:
                 with self.subTest(quantization_bit=quantization_bit, symmetric=symmetric):
                     for x_shape in [[64, 32], [80, 80, 80, 32], [16, 64, 32]]:
@@ -21,6 +23,18 @@ class TestWeightOnlyQuantNaive(unittest.TestCase):
                         sqnr = compute_error(y_ref, y_wo)
                         expected_sqnr_threshold = 44.0 - (8 - quantization_bit) * 6.02
                         self.assertGreater(sqnr, expected_sqnr_threshold, f"sqnr: {sqnr} is too low")
+
+    @unittest.skipIf(not _CUDA_IS_AVAILABLE, "skipping int4_wight_only when cuda is not available")
+    def test_quantization_4_bit(self):
+        for x_shape in [[64, 32], [80, 80, 80, 32], [16, 64, 32]]:
+            x = torch.randn(*x_shape, dtype=torch.bfloat16)
+            m = nn.Sequential(nn.Linear(32, 80)).bfloat16()
+            y_ref = m(x)
+            quantize_(m, intN_weight_only(n=4, group_size=32))
+            y_wo = m(x)
+            sqnr = compute_error(y_ref, y_wo)
+            expected_sqnr_threshold = 44.0 - (8 - 4) * 6.02
+            self.assertGreater(sqnr, expected_sqnr_threshold, f"sqnr: {sqnr} is too low")
 
 if __name__ == '__main__':
     unittest.main()
