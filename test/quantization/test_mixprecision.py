8cb228fb241f1db440305d9cafb79ec95f405cfd
Hanxian97 <hanxianhuang@meta.com>
2024-07-31 18:05:52 -0700

    make it a unittest

--- test/quantization/test_mixprecision.py
+++ test/quantization/test_mixprecision.py
@@ -1,9 +1,31 @@
+import unittest
+
 import torch
 import torch.nn as nn
 from torchao.quantization import quantize_, int8_weight_only, int4_weight_only
 from torchao.quantization.utils import compute_error
 from torchao.quantization.prototype.mixed_precision.naive_intNwo import intN_weight_only
 
+class TestWeightOnlyQuantNaive(unittest.TestCase):
+
+    def test_quantization(self):
+        for quantization_bit in [2, 3, 4, 5, 6, 8]:
+            for symmetric in [False, True]:
+                with self.subTest(quantization_bit=quantization_bit, symmetric=symmetric):
+                    for x_shape in [[64, 32], [80, 80, 80, 32], [16, 64, 32]]:
+                        x = torch.randn(*x_shape, dtype=torch.bfloat16)
+                        m = nn.Sequential(nn.Linear(32, 80)).bfloat16()
+                        y_ref = m(x)
+                        quantize_(m, intN_weight_only(n=quantization_bit, group_size=32, symmetric=symmetric))
+                        y_wo = m(x)
+                        sqnr = compute_error(y_ref, y_wo)
+                        expected_sqnr_threshold = 44.0 - (8 - quantization_bit) * 6.02
+                        self.assertGreater(sqnr, expected_sqnr_threshold, f"sqnr: {sqnr} is too low")
+
+if __name__ == '__main__':
+    unittest.main()
+    
+'''
 def test_weight_only_quant_naive(quantization_bit=2, symmetric=False):
     for x_shape in [[64, 32], [80, 80, 80, 32], [16, 64, 32]]:
         x = torch.randn(*x_shape, dtype=torch.bfloat16)
@@ -30,3 +52,4 @@ for i in [2, 3, 4, 5, 6, 8]:
         print(f"Test passed for {i}-bit using naive intNwo symmetric quantization implementation")
     except Exception as e:
         print(f"Exception handled in test loop for {i}-bit symmetric quantization. Details: {e}")
+'''
