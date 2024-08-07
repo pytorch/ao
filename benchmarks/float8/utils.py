c81ae5d1627d0e2a610d457f78419efaf0213141
vasiliy <vasiliy@fb.com>
2024-08-07 10:49:03 -0700

    float8 profiling script: filter out microbenchmarking overhead
    Summary:
    
    Our microbenchmarks have a lot of overhead. This PR attempts to get a
    cleaner measurement of only the kernels in the fwd+bwd, and subtracts
    the kernels unrelated to fwd+bwd code. This makes the kernel summary
    tables more reflective of GPU bound real use cases.
    
    Test Plan:
    
    profiling ln -> linear:
    
    ```
    python benchmarks/float8/profile_linear_float8.py --dtype_filter both ~/local/tmp --model_type ln_linear
    ```
    
    new output, note that only kernels relevant to ln and linear are
    displayed
    
    ```
    Summary of time (ms) by kernel category
    
     experiment     0_ref  1_float8  f8_div_ref  ref_div_f8
     category
     0_gemm        10.045     5.194       0.517       1.934
     1_f8_overhead  0.000     1.778         inf       0.000
     2_other        0.592     0.073       0.124       8.066
     All           10.637     7.045       0.662       1.510
    ```
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 305b51588ef241846b5c9dded1c699c8d648aa9a
    ghstack-comment-id: 2274007593
    Pull Request resolved: https://github.com/pytorch/ao/pull/629

--- benchmarks/float8/utils.py
+++ benchmarks/float8/utils.py
@@ -9,10 +9,44 @@ import re
 from typing import Optional
 
 
-def profiler_output_to_time_by_kernel_name(prof):
+def profiler_output_to_filtered_time_by_kernel_name(
+    prof, 
+    num_iter: int,
+    num_leaf_tensors: int,
+):
     """
-    Input: a profiler with captured events.
-    Output: a deduplicated list of GPU time in nanoseconds grouped by CPU kernel name
+    Input: 
+      * `prof`: a profiler with captured events
+      * `num_iter`: number of iterations used to capture `prof`
+      * `num_leaf_tensors`: number of leaf tensors to accumulate gradients to
+    Output: a deduplicated list of GPU time in nanoseconds grouped by CPU kernel name,
+      with the microbenchmark overhead filtered out
+
+    Currently assumes that `prof` captured events from a microbenchmark which was
+    set up as follows:
+
+        #
+        # Forward pass 
+        #
+
+        # Expected GPU kernel overhead: none
+        y = func(...)
+
+        # Convenient way to set up the backward pass without caring about shapes
+        y_sum = y.sum()
+
+        # Expected GPU kernel overhead:
+        # * the call to `sum`
+
+        #
+        # Backward pass
+        #
+        y_sum.backward()
+
+        # Expected GPU kernel overhead:
+        # * the call to `aten.fill_` to put a tensor with a single 1.0 value as the input to the backward
+        # * the call to `aten.copy_` to fill the first `grad_output` tensor with 1.0
+        # * the call to `aten.add_` to accumulate grads, once per leaf tensor
 
     Note that if there are user_annotations in the captured events, `torch.profiler`
     will include their time in the total GPU time displayed at the bottom of
@@ -23,13 +57,35 @@ def profiler_output_to_time_by_kernel_name(prof):
     thresh = 1e-10
     kernel_name_to_gpu_time_us = collections.defaultdict(float)
     for e in key_averages:
+
         # manually filter top-level CPU events with attributed CUDA time
-        # example CPU event row:
+        # example CPU event row from printing `key_averages`:
         #                                               aten::addmm         0.83%      76.554us         0.98%      90.846us      90.846us       1.022ms        31.82%       1.022ms       1.022ms             1
         # and it maps to this CUDA event:
         #   sm80_xmma_gemm_f32f32_f32f32_f32_tn_n_tilesize256x64...         0.00%       0.000us         0.00%       0.000us       0.000us       1.022ms        31.82%       1.022ms       1.022ms             1
         if not (e.self_cpu_time_total > thresh and e.self_device_time_total > thresh):
             continue
+
+        # manually filter expected microbenchmarking overhead, in order of execution
+        if e.key == 'aten::sum':
+            # forward pass sum
+            assert e.count == num_iter, f'unexpected number of iter for {e.key}'
+            continue
+        elif e.key == 'aten::fill_':
+            # filling the forward pass sum with 1.0
+            assert e.count == num_iter, f'unexpected number of iter for {e.key}'
+            continue
+        elif e.key == 'aten::copy_':
+            # copying 1.0 from grad_out of `sum` to grad_out of next op
+            assert e.count == num_iter, f'unexpected number of iter for {e.key}'
+            continue
+        elif e.key == 'aten::add_':
+            # accumulating gradients into leaf tensors
+            assert e.count == (num_iter * num_leaf_tensors), f'unexpected number of iter for {e.key}'
+            continue
+        elif e.key == 'cudaDeviceSynchronize':
+            continue
+
         kernel_name_to_gpu_time_us[e.key] = e.self_device_time_total
     return kernel_name_to_gpu_time_us
 
