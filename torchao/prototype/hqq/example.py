f7a9e503aeb112e60bd603003bb310a73ee8c32d
mobicham <hicham@mobiuslabs.com>
2024-08-07 09:09:45 +0000

    update example with mutliple nbits

--- torchao/prototype/hqq/example.py
+++ torchao/prototype/hqq/example.py
@@ -12,7 +12,7 @@ from torchao.dtypes.affine_quantized_tensor import (
 
 #Parameters
 device, compute_dtype = "cuda:0", torch.bfloat16
-nbits, group_size, axis = 4, 64, 1
+group_size, axis = 64, 1
 
 linear_layer = torch.nn.Linear(4096, 11800, bias=False, device=device)
 x = torch.randn((1, linear_layer.in_features), dtype=torch.float, device=device)/20.
@@ -21,40 +21,42 @@ W = linear_layer.weight.data.clone().to(device=device, dtype=compute_dtype)
 del linear_layer.weight 
 ################################################################################################
 
-q_tensor_default = AffineQuantizedTensor.from_float(
-        input_float=W,
-        mapping_type=MappingType.ASYMMETRIC,
-        block_size=[1, group_size],
-        target_dtype=torch.uint8,
-        quant_min=0,
-        quant_max=2**nbits - 1,
-        preserve_zero=False,#Important
-        zero_point_domain= ZeroPointDomain.FLOAT,
-        layout_type=PlainLayoutType(),
-        )
+for nbits in list(range(2, 9))[::-1]:
+    print('------------------------------------------------------------------------------')
+    q_tensor_default = AffineQuantizedTensor.from_float(
+            input_float=W,
+            mapping_type=MappingType.ASYMMETRIC,
+            block_size=[1, group_size],
+            target_dtype=torch.uint8,
+            quant_min=0,
+            quant_max=2**nbits - 1,
+            preserve_zero=False,#Important
+            zero_point_domain= ZeroPointDomain.FLOAT,
+            layout_type=PlainLayoutType(),
+            )
 
-linear_layer.weight = q_tensor_default
-print("Default dequantization error", (W - q_tensor_default.dequantize()).abs().mean().item())
-print('Default Dot product error', (y_ref - linear_layer(x.to(compute_dtype))).abs().mean().item())
-# Default dequantization error 0.001953125
-# Default Dot product error 0.0057801781222224236
+    linear_layer.weight = q_tensor_default
+    print("nbits", nbits, "| Default dequantization error", (W - q_tensor_default.dequantize()).abs().mean().item())
+    print("nbits", nbits, '| Default Dot product error', (y_ref - linear_layer(x.to(compute_dtype))).abs().mean().item())
+    # 4-bit Default dequantization error 0.001953125
+    # 4-bit Default Dot product error 0.0057801781222224236
 
 
-q_tensor_hqq = AffineQuantizedTensor.from_float(
-        input_float=W,
-        mapping_type=MappingType.ASYMMETRIC,
-        block_size=[1, group_size],
-        target_dtype=torch.uint8,
-        quant_min=0,
-        quant_max=2**nbits - 1,
-        preserve_zero=False,#Important
-        zero_point_domain= ZeroPointDomain.FLOAT,
-        layout_type=PlainLayoutType(),
-        use_hqq=True,
-        )
+    q_tensor_hqq = AffineQuantizedTensor.from_float(
+            input_float=W,
+            mapping_type=MappingType.ASYMMETRIC,
+            block_size=[1, group_size],
+            target_dtype=torch.uint8,
+            quant_min=0,
+            quant_max=2**nbits - 1,
+            preserve_zero=False,#Important
+            zero_point_domain= ZeroPointDomain.FLOAT,
+            layout_type=PlainLayoutType(),
+            use_hqq=True,
+            )
 
-linear_layer.weight = q_tensor_hqq
-print("HQQ dequantization error", (W - q_tensor_hqq.dequantize()).abs().mean().item())
-print('HQQ Dot product error', (y_ref - linear_layer(x.to(compute_dtype))).abs().mean().item())
-# HQQ dequantization error 0.0004863739013671875
-# HQQ Dot product error 0.0014263123739510775
+    linear_layer.weight = q_tensor_hqq
+    print("nbits", nbits, "| HQQ dequantization error", (W - q_tensor_hqq.dequantize()).abs().mean().item())
+    print("nbits", nbits, '| HQQ Dot product error', (y_ref - linear_layer(x.to(compute_dtype))).abs().mean().item())
+    # 4-bit HQQ dequantization error 0.0004863739013671875
+    # 4-bit HQQ Dot product error 0.0014263123739510775
