a898df9e4f7be5677ecc6438c9113c6ff066f7e9
Jerry Zhang <jerryzh168@gmail.com>
2024-08-07 19:23:50 -0700

    Move Uintx out of prototype for future extension
    Summary:
    Thanks @vayuda for adding the initial version of Uintx tensor subclass
    we can now integrate this with `torch.uint1` to `torch.uint7` dtypes with some helpers
    to unblock the benefit of bitpacking (model size saving) to people first, and then
    we can gradually optimize the performance.
    
    Also executorch is planning to integrate their low bit kernels with us, more native experience with
    these lower bit types will be required / useful there as well
    
    Test Plan:
    python test/dtypes/test_uintx.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- test/dtypes/test_bitpacking.py
+++ test/dtypes/test_bitpacking.py
@@ -1,9 +1,9 @@
 import torch
-from torchao.prototype.uintx import pack, unpack, pack_cpu, unpack_cpu
+from torchao.dtypes.uintx.bitpacking import pack, unpack, pack_cpu, unpack_cpu
 import pytest
 from torch.utils._triton import has_triton
 
-element_bit_width = (1,2,3,4,5,6,7)
+bit_widths = (1,2,3,4,5,6,7)
 dimensions = (0, -1, 1)
 
 @pytest.fixture(autouse=True)
@@ -11,36 +11,36 @@ def run_before_and_after_tests():
     yield
     torch._dynamo.reset() # reset cache between tests
 
-@pytest.mark.parametrize("element_bit_width", element_bit_width)
+@pytest.mark.parametrize("bit_width", bit_widths)
 @pytest.mark.parametrize("dim", dimensions)
-def test_CPU(element_bit_width, dim):
-    test_tensor = torch.randint(0, 2**element_bit_width, (32,32,32), dtype=torch.uint8, device='cpu')
-    packed = pack_cpu(test_tensor, element_bit_width, dim = dim)
-    unpacked = unpack_cpu(packed, element_bit_width, dim = dim)
+def test_CPU(bit_width, dim):
+    test_tensor = torch.randint(0, 2**bit_width, (32,32,32), dtype=torch.uint8, device='cpu')
+    packed = pack_cpu(test_tensor, bit_width, dim = dim)
+    unpacked = unpack_cpu(packed, bit_width, dim = dim)
     assert(unpacked.allclose(test_tensor))
 
 
-@pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")          
-@pytest.mark.parametrize("element_bit_width", element_bit_width)
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
+@pytest.mark.parametrize("bit_width", bit_widths)
 @pytest.mark.parametrize("dim", dimensions)
-def test_GPU(element_bit_width, dim):
-    test_tensor = torch.randint(0, 2**element_bit_width, (32,32,32), dtype=torch.uint8).cuda()
-    packed = pack(test_tensor, element_bit_width, dim = dim)
-    unpacked = unpack(packed, element_bit_width, dim = dim)
+def test_GPU(bit_width, dim):
+    test_tensor = torch.randint(0, 2**bit_width, (32,32,32), dtype=torch.uint8).cuda()
+    packed = pack(test_tensor, bit_width, dim = dim)
+    unpacked = unpack(packed, bit_width, dim = dim)
     assert(unpacked.allclose(test_tensor))
 
 
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
 @pytest.mark.skipif(not has_triton(), reason="unsupported without triton")
-@pytest.mark.parametrize("element_bit_width", element_bit_width)
+@pytest.mark.parametrize("bit_width", bit_widths)
 @pytest.mark.parametrize("dim", dimensions)
-def test_compile(element_bit_width, dim):
+def test_compile(bit_width, dim):
     torch._dynamo.config.specialize_int = True
     pack_compile = torch.compile(pack, fullgraph=True)
     unpack_compile = torch.compile(unpack, fullgraph=True)
-    test_tensor = torch.randint(0, 2**element_bit_width, (32,32,32), dtype=torch.uint8).cuda()
-    packed = pack(test_tensor, element_bit_width, dim = dim)
-    unpacked = unpack(packed, element_bit_width, dim = dim)
+    test_tensor = torch.randint(0, 2**bit_width, (32,32,32), dtype=torch.uint8).cuda()
+    packed = pack(test_tensor, bit_width, dim = dim)
+    unpacked = unpack(packed, bit_width, dim = dim)
     assert(unpacked.allclose(test_tensor))
 
 # these test cases are for the example pack walk through in the bitpacking.py file
@@ -62,5 +62,3 @@ def test_pack_example_CPU():
     assert torch.tensor([39, 146], dtype=torch.uint8).allclose(shard_2)
     unpacked = unpack([shard_4, shard_2], 6)
     assert unpacked.allclose(test_tensor)
-    
-    
