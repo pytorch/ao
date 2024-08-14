93ca471d0bf131635271065988a71a8fc311c3e6
root <hicham@mobiuslabs.com>
2024-08-14 08:23:53 +0000

    separate xnbit tests + check device

--- test/hqq/test_hqq_affine.py
+++ test/hqq/test_hqq_affine.py
@@ -11,7 +11,7 @@ from torchao.dtypes.affine_quantized_tensor import (
     MappingType,
 )
 
-torch.random.manual_seed(100)
+cuda_available = torch.cuda.is_available()
 
 #Parameters
 device            = 'cuda:0'
@@ -23,15 +23,20 @@ preserve_zero     = False
 zero_point_domain = ZeroPointDomain.FLOAT
 zero_point_dtype  = compute_dtype
 inner_k_tiles     = 8
+in_features       = 4096
+out_features      = 11800
+torch_seed        = 100
 
+def _init_data(in_features, out_features, compute_dtype, device, torch_seed):
+    torch.random.manual_seed(torch_seed)
+    linear_layer = torch.nn.Linear(in_features, out_features, bias=False, device=device)
+    x = torch.randn((1, linear_layer.in_features), dtype=torch.float, device=device)/20.
+    y_ref = linear_layer(x)
+    W = linear_layer.weight.data.clone().to(device=device, dtype=compute_dtype)
+    return W, x, y_ref
 
-in_features, out_features = 4096, 11800
-linear_layer = torch.nn.Linear(in_features, out_features, bias=False, device=device)
-x = torch.randn((1, linear_layer.in_features), dtype=torch.float, device=device)/20.
-y_ref = linear_layer(x)
-W = linear_layer.weight.data.clone().to(device=device, dtype=compute_dtype)
-
-def _eval_hqq(nbits, W, y_ref, layout_type):
+def _eval_hqq(nbits, layout_type):
+    W, x, y_ref  = _init_data(in_features, out_features, compute_dtype, device, torch_seed)
     q_tensor_hqq = to_affine_quantized(
             input_float=W,
             mapping_type=mapping_type,
@@ -53,46 +58,45 @@ def _eval_hqq(nbits, W, y_ref, layout_type):
 
     return dequantize_error, dot_product_error
 
-class TestHQQ(unittest.TestCase):
+
+class TestHQQBase(unittest.TestCase):
+    @unittest.skipIf(not cuda_available, "Need CUDA available")
+    def test_hqq(self, nbits=None, layout_type=None, ref_dequantize_error=None, ref_dot_product_error=None):
+        if(nbits is None): return
+        dequantize_error, dot_product_error = _eval_hqq(nbits=nbits, layout_type=layout_type)
+        self.assertTrue(dequantize_error < ref_dequantize_error)
+        self.assertTrue(dot_product_error < ref_dot_product_error)
+
+class TestHQQ8Bit(TestHQQBase):
     def test_hqq_plain_8bit(self):
-        dequantize_error, dot_product_error = _eval_hqq(8, W, y_ref, PlainLayoutType())
-        self.assertTrue(dequantize_error < 5e-5)
-        self.assertTrue(dot_product_error < 0.00013)
+        self.test_hqq(nbits=8, layout_type=PlainLayoutType(), ref_dequantize_error=5e-5, ref_dot_product_error=0.00013)
 
+class TestHQQ7Bit(TestHQQBase):
     def test_hqq_plain_7bit(self):
-        dequantize_error, dot_product_error = _eval_hqq(7, W, y_ref, PlainLayoutType())
-        self.assertTrue(dequantize_error < 6e-05)
-        self.assertTrue(dot_product_error < 0.000193)
+        self.test_hqq(nbits=7, layout_type=PlainLayoutType(), ref_dequantize_error=6e-05, ref_dot_product_error=0.000193)
 
+class TestHQQ6Bit(TestHQQBase):
     def test_hqq_plain_6bit(self):
-        dequantize_error, dot_product_error = _eval_hqq(6, W, y_ref, PlainLayoutType())
-        self.assertTrue(dequantize_error < 0.0001131)
-        self.assertTrue(dot_product_error < 0.000353)
+        self.test_hqq(nbits=6, layout_type=PlainLayoutType(), ref_dequantize_error=0.0001131, ref_dot_product_error=0.000353)
 
+class TestHQQ5Bit(TestHQQBase):
     def test_hqq_plain_5bit(self):
-        dequantize_error, dot_product_error = _eval_hqq(5, W, y_ref, PlainLayoutType())
-        self.assertTrue(dequantize_error < 0.00023)
-        self.assertTrue(dot_product_error < 0.000704)
+        self.test_hqq(nbits=5, layout_type=PlainLayoutType(), ref_dequantize_error=0.00023, ref_dot_product_error=0.000704)
 
+class TestHQQ4bit(TestHQQBase):
     def test_hqq_plain_4bit(self):
-        dequantize_error, dot_product_error = _eval_hqq(4, W, y_ref, PlainLayoutType())
-        self.assertTrue(dequantize_error < 0.000487)
-        self.assertTrue(dot_product_error < 0.001472)
-
+        self.test_hqq(nbits=4, layout_type=PlainLayoutType(), ref_dequantize_error=0.000487, ref_dot_product_error=0.001472)
+    
     def test_hqq_tensorcore_4bit(self):
-        dequantize_error, dot_product_error = _eval_hqq(4, W, y_ref, TensorCoreTiledLayoutType(inner_k_tiles=inner_k_tiles))
-        self.assertTrue(dequantize_error < 0.000487)
-        self.assertTrue(dot_product_error < 0.00147)
+        self.test_hqq(nbits=4, layout_type=TensorCoreTiledLayoutType(inner_k_tiles=inner_k_tiles), ref_dequantize_error=0.000487, ref_dot_product_error=0.00147)
 
+class TestHQQ3Bit(TestHQQBase):
     def test_hqq_plain_3bit(self):
-        dequantize_error, dot_product_error = _eval_hqq(3, W, y_ref, PlainLayoutType())
-        self.assertTrue(dequantize_error < 0.00101)
-        self.assertTrue(dot_product_error < 0.003047)
+        self.test_hqq(nbits=3, layout_type=PlainLayoutType(), ref_dequantize_error=0.00101, ref_dot_product_error=0.003047)
 
+class TestHQQ2Bit(TestHQQBase):
     def test_hqq_plain_2bit(self):
-        dequantize_error, dot_product_error = _eval_hqq(2, W, y_ref, PlainLayoutType())
-        self.assertTrue(dequantize_error < 0.002366)
-        self.assertTrue(dot_product_error < 0.007255)
+        self.test_hqq(nbits=2, layout_type=PlainLayoutType(), ref_dequantize_error=0.002366, ref_dot_product_error=0.007255)
 
 if __name__ == "__main__":
     unittest.main()
