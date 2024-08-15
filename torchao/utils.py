a8acdcd9ffaf04dbff9d7aafed12992296cd8188
Jesse Cai <jcjessecai@gmail.com>
2024-08-15 08:07:46 -0700

    update

--- torchao/utils.py
+++ torchao/utils.py
@@ -42,7 +42,7 @@ def _assert_and_get_unique_device(module: torch.nn.Module) -> Any:
     return device
 
 
-def benchmark_model(model, num_runs, args=(), kwargs=None, device_type=None):
+def benchmark_model(model, num_runs, args=(), kwargs=None, device_type=None, num_warmup=10):
     """Benchmark model runs with `args` and `kwargs` both are optional
     """
     if kwargs is None:
@@ -51,6 +51,9 @@ def benchmark_model(model, num_runs, args=(), kwargs=None, device_type=None):
     if device_type is None:
         assert isinstance(model, torch.nn.Module), "Expecting `model` to be torch.nn.Module if device_type is not provided"
         device_type = _assert_and_get_unique_device(model).type
+
+    for _ in range(num_warmup):
+        model(*args, **kwargs)
 
     if device_type == "cuda":
         torch.cuda.synchronize()
