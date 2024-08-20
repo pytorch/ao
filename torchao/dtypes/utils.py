6644bb66ea3cb0c1ecdfb974a891df60ffc9fc08
jainapurva <apurvajain.kota@gmail.com>
2024-08-20 13:27:06 -0700

    Float8 updates

--- torchao/dtypes/utils.py
+++ torchao/dtypes/utils.py
@@ -25,6 +25,11 @@ class LayoutType:
     def extra_repr(self) -> str:
         return ""
 
+@dataclass(frozen=True)
+class FpxLayoutType(LayoutType):
+    pass
+
+
 """
 Plain LayoutType, the most basic LayoutType, also has no extra metadata, will typically be the default
 """
