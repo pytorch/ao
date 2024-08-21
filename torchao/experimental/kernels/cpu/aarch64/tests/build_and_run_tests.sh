d683ba33e466107a2f33a8784199b319f1314423
Scott Roy <161522778+metascroy@users.noreply.github.com>
2024-08-21 10:33:50 -0700

    Update licenses in torchao/experimental
    Differential Revision: D61548961
    
    Pull Request resolved: https://github.com/pytorch/ao/pull/720

--- torchao/experimental/kernels/cpu/aarch64/tests/build_and_run_tests.sh
+++ torchao/experimental/kernels/cpu/aarch64/tests/build_and_run_tests.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 export TORCHAO_LIBRARIES=${SCRIPT_DIR}/../../../../../..
 export CMAKE_OUT=/tmp/cmake-out/torch_ao/tests
