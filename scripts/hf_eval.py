1fb4a2bb261bf9a612e5d263af449673bff6ff54
jainapurva <apurvajain.kota@gmail.com>
2024-08-22 11:35:13 -0700

    revert changes to hf_eval

--- scripts/hf_eval.py
+++ scripts/hf_eval.py
@@ -18,7 +18,6 @@ from torchao.quantization import (
     int4_weight_only,
     int8_weight_only,
     int8_dynamic_activation_int8_weight,
-    float8_weight_only,
     quantize_,
     autoquant,
 )
@@ -56,8 +55,6 @@ def run_evaluation(repo_id, tasks, limit, device, precision, quantization, compi
     elif quantization == "int4wo":
         # note cannot quantize this model on cpu and run it on cuda at this time
         quantize_(model.to(device=device), int4_weight_only())
-    elif quantization == "float8wo":
-        quantize_(model.to(device=device), float8_weight_only())
     elif quantization == "autoquant":
         model = autoquant(model.to(device=device))
 
@@ -92,7 +89,7 @@ if __name__ == '__main__':
     parser.add_argument('--limit', type=int, default=None, help='Number of eval samples to evaluate')
     parser.add_argument('--precision', type=lambda x: getattr(torch, x.split(".")[-1]), default=torch.bfloat16, help='dtype precision to use')
     parser.add_argument('--device', type=str, default="cuda", help='Device to use for evaluation')
-    parser.add_argument('-q', '--quantization', default = "None", choices=["int8dq", "int8wo", "int4wo","autoquant", "None", "float8wo"], help='Which quantization technique to apply')
+    parser.add_argument('-q', '--quantization', default = "None", choices=["int8dq", "int8wo", "int4wo","autoquant", "None"], help='Which quantization technique to apply')
     parser.add_argument('--compile', action='store_true', help='Whether to compile the model.')
     parser.add_argument('--save', action='store_true', help='Whether to save the model.')
     parser.add_argument('--batch_size', type=int, default=1, help='Batch size to use for evaluation, note int8wo and int4wo work best with small batchsizes, int8dq works better with large batchsizes')
