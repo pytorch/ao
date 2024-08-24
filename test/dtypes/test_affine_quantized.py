ff96cba07a42ffcdd75f720401e35af9349c2499
jainapurva <apurvajain.kota@gmail.com>
2024-08-23 19:48:35 -0700

    typos

--- test/dtypes/test_affine_quantized.py
+++ test/dtypes/test_affine_quantized.py
@@ -17,9 +17,6 @@ import torch
 import unittest
 import tempfile
 
-from torchao.utils import (
-    TORCH_VERSION_AT_LEAST_2_5,
-)
 
 class TestAffineQuantized(TestCase):
     @unittest.skipIf(not torch.cuda.is_available(), "Need CUDA available")
