86c7b0d1d79bfc9ee5125db61b18da48e257e205
HDCharles <39544797+HDCharles@users.noreply.github.com>
2024-08-27 13:59:53 -0400

    Llama3.1 and kv_cache quantization (#738)
    * Support for Llama 3.1 and kv_cache_quantization
    
    Summary:
    
    this PR has support for llama 3.1 and some improvements to kv_cache quantization and general peak memory performance for llama
    
    # summary of changes
    
    1) add 3.1 support for llama
    2) change quantized_kv_cache init so it doesn't create a full precision peak: see below
    2) reorder causal mask init: see below
    3) add option for linear causal mask: see below
    4) add option for cache_size: the default generate.py behavior requires you do generate 32k tokens if you want to haev a size 32k kv_cache/causal_mask, the cache_size option lets you simply set the cache size but generate a smaller number of tokens to make it easier to benchmark
    5) add option to generate memory profile: used to generate the images below
    
    | context length (tokens) | normal peak (GB) | kv_quant peak (GB) | kv quant+causal fix peak (GB) |
    |-------------------------|------------------|--------------------|-------------------------------|
    |                    8192 |            17.86 |              17.52 |                         17.47 |
    |                   16384 |            19.81 |              18.75 |                         18.48 |
    |                   32768 |            23.83 |              21.72 |                         20.64 |
    |                   65536 |             33.5 |              29.54 |                         25.24 |
    |                  131072 |            59.27 |              52.62 |                         34.18 |
    
    tests:
    
    see benchmarks.sh
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    * further kv_cache investigation
    
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    * memory fixes for kv_cache quant
    
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    * fix benchmarks
    
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,4 +1,6 @@
 python scripts/download.py --repo_id meta-llama/Llama-2-7b-chat-hf
 python scripts/download.py --repo_id meta-llama/Meta-Llama-3-8B
+python scripts/download.py --repo_id meta-llama/Meta-Llama-3.1-8B
 python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/meta-llama/Llama-2-7b-chat-hf
 python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/meta-llama/Meta-Llama-3-8B
+python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/meta-llama/Meta-Llama-3.1-8B
