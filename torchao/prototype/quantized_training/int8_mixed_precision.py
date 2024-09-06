d367f772c99cc7915669f8553809fab55cb98b8b
Thien Tran <gau.nernst@yahoo.com.sg>
2024-09-06 08:58:57 +0800

    use FP8 impl of __torch_dispatch__

--- torchao/prototype/quantized_training/int8_mixed_precision.py
+++ torchao/prototype/quantized_training/int8_mixed_precision.py
@@ -1,11 +1,9 @@
 from typing import Any, NamedTuple, Optional, Tuple
 
 import torch
+import torch.utils._pytree as pytree
 from torch import Tensor, nn
-from torch.utils._python_dispatch import return_and_correct_aliasing
 from torch.utils._triton import has_triton
-
-from torchao.utils import TorchAOBaseTensor
 
 from .int8 import quantize_int8_rowwise
 
@@ -20,11 +18,6 @@ else:
         return A_scaled @ B_scaled
 
 
-aten = torch.ops.aten
-c10d_functional = torch.ops.c10d_functional
-_c10d_functional = torch.ops._c10d_functional
-
-
 class Int8MixedPrecisionConfig(NamedTuple):
     output: bool = True
     grad_input: bool = True
@@ -34,7 +27,24 @@ class Int8MixedPrecisionConfig(NamedTuple):
 _DEFAULT_CONFIG = Int8MixedPrecisionConfig()
 
 
-class Int8MixedPrecisionLinearWeight(TorchAOBaseTensor):
+# adapated from FP8 implementation of WeightWithDynamicFloat8CastTensor
+aten = torch.ops.aten
+_ops_to_preserve_subclass = {
+    aten.detach.default,
+    aten.empty_like.default,
+    aten.new_zeros.default,
+    aten.slice.Tensor,
+    aten.copy_.default,
+    aten.view.default,
+    aten.as_strided.default,
+    aten._to_copy.default,
+    aten._pin_memory.default,
+    aten.split.Tensor,
+    aten.clone.default,
+}
+
+
+class Int8MixedPrecisionLinearWeight(Tensor):
     @staticmethod
     @torch._dynamo.disable
     def __new__(cls, data: Tensor, config: Int8MixedPrecisionConfig):
@@ -43,6 +53,9 @@ class Int8MixedPrecisionLinearWeight(TorchAOBaseTensor):
             data.shape,
             dtype=data.dtype,
             device=data.device,
+            strides=data.stride(),
+            storage_offset=data.storage_offset(),
+            pin_memory=data.is_pinned(),
         )
 
     @torch._dynamo.disable
@@ -63,6 +76,38 @@ class Int8MixedPrecisionLinearWeight(TorchAOBaseTensor):
     def to_original(self):
         return self._data.clone()
 
+    @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = dict()
+        
+        if func is torch.nn.functional.linear:
+            act, weight = args[:2]
+            bias = args[2] if len(args) > 2 else None
+            return _Int8MixedPrecisionLinear.apply(act, weight._data, bias, weight.config)
+
+        with torch._C.DisableTorchFunctionSubclass():
+            return func(*args, **kwargs)
+
+    @classmethod
+    def __torch_dispatch__(cls, func, types, args, kwargs):
+        config = None
+
+        def unwrap(x: cls):
+            nonlocal config
+            if config is None:
+                config = x.config
+            else:
+                assert x.config == config
+            return x._data
+
+        args, kwargs = pytree.tree_map_only(cls, unwrap, (args, kwargs))
+        out = func(*args, **kwargs)
+        if func not in _ops_to_preserve_subclass:
+            return out
+        else:
+            return pytree.tree_map_only(Tensor, lambda x: cls(x, config), out)
+
     def fsdp_pre_all_gather(self, mesh):
         # TODO: pre-quantize weight here -> reduce comm bandwidth.
         # we will need another tensor subclass to hold the quantized weight.
@@ -79,82 +124,6 @@ class Int8MixedPrecisionLinearWeight(TorchAOBaseTensor):
         (data,) = all_gather_outputs
         (config,) = metadata
         return Int8MixedPrecisionLinearWeight(data.to(param_dtype), config), all_gather_outputs
-
-
-implements = Int8MixedPrecisionLinearWeight.implements
-
-
-@implements(torch.nn.functional.linear)
-def _(func, types, args, kwargs):
-    act, weight = args[:2]
-    bias = args[2] if len(args) > 2 else None
-    return _Int8MixedPrecisionLinear.apply(act, weight._data, bias, weight.config)
-
-
-@implements(
-    [
-        aten.detach.default,
-        aten.clone.default,
-        aten._to_copy.default,
-        aten.empty_like.default,
-        # FSDP ops
-        aten.slice.Tensor,
-        aten.new_zeros.default,
-        aten.view.default,
-        aten.as_strided.default,
-        c10d_functional.all_gather_into_tensor.default,
-        _c10d_functional.all_gather_into_tensor.default,
-        c10d_functional.wait_tensor.default,
-        _c10d_functional.wait_tensor.default,
-    ]
-)
-def _(func, types, args, kwargs):
-    out = Int8MixedPrecisionLinearWeight(func(args[0]._data, *args[1:], **kwargs), args[0].config)
-    return return_and_correct_aliasing(func, args, kwargs, out)
-
-
-@implements(
-    [
-        aten.copy_.default,
-        aten.addcdiv_.default,
-        aten.add_.Tensor,
-        aten.mul_.Tensor,
-        # param init functions
-        aten.uniform_.default,
-        aten.erfinv_.default,
-        aten.clamp_.default,
-    ]
-)
-def _(func, types, args, kwargs):
-    unpacked_args = [x._data if isinstance(x, Int8MixedPrecisionLinearWeight) else x for x in args]
-    func(*unpacked_args, **kwargs)
-    return args[0]
-
-
-@implements([aten._fused_adam_.default, aten._fused_adamw_.default])
-def _(func, types, args, kwargs):
-    params = [x._data if isinstance(x, Int8MixedPrecisionLinearWeight) else x for x in args[0]]
-    func(params, *args[1:], **kwargs)
-
-
-# return normal tensor
-@implements(
-    [
-        aten.zeros_like.default,  # called by optimizers
-        aten.add.Tensor,
-        aten.mul.Tensor,
-    ]
-)
-def _(func, types, args, kwargs):
-    unpacked_args = [x._data if isinstance(x, Int8MixedPrecisionLinearWeight) else x for x in args]
-    return func(*unpacked_args, **kwargs)
-
-
-# FSDP op
-@implements(aten.split.Tensor)
-def _(func, types, args, kwargs):
-    data_list = func(args[0]._data, *args[1:], **kwargs)
-    return [Int8MixedPrecisionLinearWeight(x, args[0].config) for x in data_list]
 
 
 # alternative UX
@@ -180,6 +149,7 @@ class Int8MixedPrecisionLinear(nn.Linear):
 
 
 def _dynamic_int8_linear(input: Tensor, weight: Tensor) -> Tensor:
+    # TODO: check if we need to enforce .contiguous() for input_i8 and weight_i8
     input_i8, input_scale = quantize_int8_rowwise(input)
     weight_i8, weight_scale = quantize_int8_rowwise(weight)
     return int8_mm_dequant(input_i8, weight_i8.T, input_scale, weight_scale)
