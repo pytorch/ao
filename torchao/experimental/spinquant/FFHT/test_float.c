04026e662495ba7c9863ca5c5429a26e90caec37
Scott Wolchok <swolchok@fb.com>
2024-09-06 14:09:49 -0700

    FFHT enhancements to fast hadamard transform kernels
    Use FFHT to speed up Fast Hadamard Transform on
    CPU. fast_hadamard_test was delayed to here becuase it was a source
    for a reference implementation.
    
    Differential Revision: [D61029709](https://our.internmc.facebook.com/intern/diff/D61029709/)
    
    [ghstack-poisoned]

--- torchao/experimental/spinquant/FFHT/test_float.c
+++ torchao/experimental/spinquant/FFHT/test_float.c
@@ -3,24 +3,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "dumb_fht.h"
 #include "fht.h"
-
-void dumb_fht(float *buf, int log_n);
-void dumb_fht(float *buf, int log_n) {
-    int n = 1 << log_n;
-    for (int i = 0; i < log_n; ++i) {
-        int s1 = 1 << i;
-        int s2 = s1 << 1;
-        for (int j = 0; j < n; j += s2) {
-            for (int k = 0; k < s1; ++k) {
-                float u = buf[j + k];
-                float v = buf[j + k + s1];
-                buf[j + k] = u + v;
-                buf[j + k + s1] = u - v;
-            }
-        }
-    }
-}
 
 int main(void) {
     srand(4057218);
