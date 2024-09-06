b0b8ab37cf016dedc6bf30b6dfb8e6ab797943c8
Scott Wolchok <swolchok@fb.com>
2024-09-06 14:09:46 -0700

    [torchao] FFHT: ARM NEON port
    Patch the code generator to be capable of generating NEON
    code and leave it configured to do that since we already have the
    checked-in generated AVX and SSE code (and no immediate plans to use
    it). Generated code size was a potential issue so I also patched the
    generator to 1) reuse generated code for previous smaller sizes where
    applicable and 2) choose the smallest code that isn't more than 10%
    slower than the very fastest code.
    
    Differential Revision: [D60194970](https://our.internmc.facebook.com/intern/diff/D60194970/)
    
    [ghstack-poisoned]

--- torchao/experimental/spinquant/FFHT/fht.h
+++ torchao/experimental/spinquant/FFHT/fht.h
@@ -8,9 +8,13 @@ extern "C" {
 #endif
 
 int fht_float(float *buf, int log_n);
+#ifndef __aarch64__
 int fht_double(double *buf, int log_n);
+#endif
 int fht_float_oop(float *in, float *out, int log_n);
+#ifndef __aarch64__
 int fht_double_oop(double *in, double *out, int log_n);
+#endif
 
 
 #ifdef __cplusplus
@@ -21,17 +25,21 @@ static inline int fht(float *buf, int log_n) {
     return fht_float(buf, log_n);
 }
 
+#ifndef __aarch64__
 static inline int fht(double *buf, int log_n) {
     return fht_double(buf, log_n);
 }
+#endif
 
 static inline int fht(float *buf, float *out, int log_n) {
     return fht_float_oop(buf, out, log_n);
 }
 
+#ifndef __aarch64__
 static inline int fht(double *buf, double *out, int log_n) {
     return fht_double_oop(buf, out, log_n);
 }
+#endif
 
 #endif
 
--- torchao/experimental/spinquant/FFHT/gen.py
+++ torchao/experimental/spinquant/FFHT/gen.py
@@ -1,3 +1,4 @@
+# Portions Copyright (c) Meta Platforms, Inc. and affiliates.
 import csv
 import os
 import sys
@@ -18,14 +19,19 @@ def float_avx_0(register, aux_registers, ident=''):
             'the main register can\'t be one of the auxiliary ones')
     if len(aux_registers) < 4:
         raise Exception('float_avx_0 needs at least four auxiliary registers')
+    # given source ABCDEFGH, destination register gets AACCEEGG
     res = ident + '"vpermilps $160, %%%%%s, %%%%%s\\n"\n' % (register,
                                                              aux_registers[0])
+    # given source ABCDEFGH, destination register gets BBDDFFHH
     res += ident + '"vpermilps $245, %%%%%s, %%%%%s\\n"\n' % (register,
                                                               aux_registers[1])
+    # aux2 <- 0
     res += ident + '"vxorps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[2], aux_registers[2], aux_registers[2])
+    # aux3 <- -B -B -D -D -F -F -H -H
     res += ident + '"vsubps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[1], aux_registers[2], aux_registers[3])
+    # reg <- (A+B)(A-B)(C+D)(C-D)(E+F)(E-F)(G+H)(G-H)
     res += ident + '"vaddsubps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[3], aux_registers[0], register)
     return res
@@ -39,16 +45,22 @@ def float_avx_1(register, aux_registers, ident=''):
             'the main register can\'t be one of the auxiliary ones')
     if len(aux_registers) < 5:
         raise Exception('float_avx_1 needs at least five auxiliary registers')
+    # Given source ABCDEFGH, r0 <- ABABEFEF
     res = ident + '"vpermilps $68, %%%%%s, %%%%%s\\n"\n' % (register,
                                                             aux_registers[0])
+    # Given source ABCD, r1 <- CDCDGHGH
     res += ident + '"vpermilps $238, %%%%%s, %%%%%s\\n"\n' % (register,
                                                               aux_registers[1])
+    # r2 <- 0
     res += ident + '"vxorps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[2], aux_registers[2], aux_registers[2])
+    # r3 <- -C -D -C -D -G -H -G -H
     res += ident + '"vsubps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[1], aux_registers[2], aux_registers[3])
+    # r4 <- C D -C -D G H -G -H
     res += ident + '"vblendps $204, %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[3], aux_registers[1], aux_registers[4])
+    # reg <- (A + C) (B + D) (A - C) (B - D) etc.
     res += ident + '"vaddps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[0], aux_registers[4], register)
     return res
@@ -62,14 +74,19 @@ def float_avx_2(register, aux_registers, ident=''):
             'the main register can\'t be one of the auxiliary ones')
     if len(aux_registers) < 4:
         raise Exception('float_avx_2 needs at least four auxiliary registers')
+    # r0 <- 0
     res = ident + '"vxorps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[0], aux_registers[0], aux_registers[0])
+    # r1 <- -A -B -C -D -E -F -G -H
     res += ident + '"vsubps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         register, aux_registers[0], aux_registers[1])
+    # r2 <- ABABEFEF
     res += ident + '"vperm2f128 $0, %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         register, register, aux_registers[2])
+    # r3 <- C D -C -D G H -G -H
     res += ident + '"vperm2f128 $49, %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[1], register, aux_registers[3])
+    # reg <- (A + C) (B + D)(A - C) (B - D) etc.
     res += ident + '"vaddps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[2], aux_registers[3], register)
     return res
@@ -98,14 +115,19 @@ def double_avx_0(register, aux_registers, ident=''):
             'the main register can\'t be one of the auxiliary ones')
     if len(aux_registers) < 4:
         raise Exception('double_avx_0 needs at least four auxiliary registers')
+    # r0 <- AACC
     res = ident + '"vpermilpd $0, %%%%%s, %%%%%s\\n"\n' % (register,
                                                            aux_registers[0])
+    # r1 <- BBDD
     res += ident + '"vpermilpd $15, %%%%%s, %%%%%s\\n"\n' % (register,
                                                              aux_registers[1])
+    # r2 <- 0
     res += ident + '"vxorpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[2], aux_registers[2], aux_registers[2])
+    # r3 <- -B -B -D -D
     res += ident + '"vsubpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[1], aux_registers[2], aux_registers[3])
+    # reg <- (A + B)(A - B)(C + D)(C - D)
     res += ident + '"vaddsubpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[3], aux_registers[0], register)
     return res
@@ -119,14 +141,19 @@ def double_avx_1(register, aux_registers, ident=''):
             'the main register can\'t be one of the auxiliary ones')
     if len(aux_registers) < 4:
         raise Exception('double_avx_1 needs at least four auxiliary registers')
+    # r0 <- ABAB
     res = ident + '"vperm2f128 $0, %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         register, register, aux_registers[0])
+    # r1 <- 0
     res += ident + '"vxorpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[1], aux_registers[1], aux_registers[1])
+    # r2 <- -A -B -C -D
     res += ident + '"vsubpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         register, aux_registers[1], aux_registers[2])
+    # r3 <- C D -C -D
     res += ident + '"vperm2f128 $49, %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[2], register, aux_registers[3])
+    # reg <- (A + C)(B + D)(A - C)(B - D)
     res += ident + '"vaddpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (
         aux_registers[3], aux_registers[0], register)
     return res
@@ -259,6 +286,60 @@ def double_sse_1_etc(from_register_0,
     return res
 
 
+# Given reg = ABCD, return (A+B)(A-B)(C+D)(C-D)
+def float_neon_0(register, aux_registers, ident=''):
+    if not is_distinct(aux_registers):
+        raise Exception('auxiliary registers must be distinct')
+    if register in aux_registers:
+        raise Exception(
+            'the main register can\'t be one of the auxiliary ones')
+    if len(aux_registers) < 2:
+        raise Exception('float_neon_0 needs at least two auxiliary registers')
+    # r0 <- AACC
+    res = f'{ident}"TRN1 {aux_registers[0]}.4S, {register}.4S, {register}.4S\\n"\n'
+    # r1 <- -A -B -C -D
+    res += f'{ident}"FNEG {aux_registers[1]}.4S, {register}.4S\\n"\n'
+    # r2 <- B (-B) D -D
+    res += f'{ident}"TRN2 {aux_registers[1]}.4S, {register}.4S, {aux_registers[1]}.4S\\n"\n'
+    # reg <- (A+B)(A-B)(C+D)(C-D)
+    res += f'{ident}"FADD {register}.4S, {aux_registers[0]}.4S, {aux_registers[1]}.4S\\n"\n'
+
+    return res
+
+# Given reg = ABCD, return (A + C)(B + D)(A - C)(B - D)
+def float_neon_1(register, aux_registers, ident=''):
+    if not is_distinct(aux_registers):
+        raise Exception('auxiliary registers must be distinct')
+    if register in aux_registers:
+        raise Exception(
+            'the main register can\'t be one of the auxiliary ones')
+    if len(aux_registers) < 2:
+        raise Exception('float_neon_1 needs at least two auxiliary registers')
+    # r0 <- ABAB
+    res = f'{ident}"DUP {aux_registers[0]}.2D, {register}.D[0]\\n"\n'
+    # r1 <- -A -B -C -D
+    res += f'{ident}"FNEG {aux_registers[1]}.4S, {register}.4S\\n"\n'
+    # r1 <- C D -C -D
+    res += f'{ident}"INS {aux_registers[1]}.D[0], {register}.D[1]\\n"\n'
+    # reg <- (A + C)(B + D)(A - C)(B - D)
+    res += f'{ident}"FADD {register}.4S, {aux_registers[0]}.4S, {aux_registers[1]}.4S\\n"\n'
+
+    return res;
+
+
+def float_neon_2_etc(from_register_0,
+                     from_register_1,
+                     to_register_0,
+                     to_register_1,
+                     ident=''):
+    if not is_distinct(
+        [from_register_0, from_register_1, to_register_0, to_register_1]):
+        raise Exception('four registers must be distinct')
+    res = f'{ident}"FADD {to_register_0}.4S, {from_register_0}.4S, {from_register_1}.4S\\n"\n'
+    res += f'{ident}"FSUB {to_register_1}.4S, {from_register_0}.4S, {from_register_1}.4S\\n"\n'
+    return res
+
+
 def plain_step(type_name, buf_name, log_n, it, ident=''):
     if log_n <= 0:
         raise Exception("log_n must be positive")
@@ -279,6 +360,9 @@ def plain_step(type_name, buf_name, log_n, it, ident=''):
     return res
 
 
+MOVE_INSTRUCTION_USE_NEON = "NEON MOV"
+
+
 def composite_step(buf_name,
                    log_n,
                    from_it,
@@ -289,6 +373,8 @@ def composite_step(buf_name,
                    special_steps,
                    main_step,
                    ident=''):
+    # HACK: NEON needs different syntax for loads and stores.
+    use_neon_movs = move_instruction == MOVE_INSTRUCTION_USE_NEON
     if log_n < log_w:
         raise Exception('need at least %d elements' % (1 << log_w))
     num_registers = len(registers)
@@ -312,13 +398,19 @@ def composite_step(buf_name,
     if num_nontrivial_levels == 0:
         res = ident + 'for (int j = 0; j < %d; j += %d) {\n' % (n, 1 << log_w)
         res += ident + '  __asm__ volatile (\n'
-        res += ident + '    "%s (%%0), %%%%%s\\n"\n' % (move_instruction,
-                                                        input_registers[0])
+        if use_neon_movs:
+            res += f'{ident}    "LD1 {{{input_registers[0]}.4S}}, [%0]\\n"\n'
+        else:
+            res += ident + '    "%s (%%0), %%%%%s\\n"\n' % (move_instruction,
+                                                            input_registers[0])
         for it in range(from_it, to_it):
             res += special_steps[it](input_registers[0], output_registers,
                                      ident + '    ')
-        res += ident + '    "%s %%%%%s, (%%0)\\n"\n' % (move_instruction,
-                                                        input_registers[0])
+        if use_neon_movs:
+            res += f'{ident}    "ST1 {{{input_registers[0]}.4S}}, [%0]\\n"\n'
+        else:
+            res += ident + '    "%s %%%%%s, (%%0)\\n"\n' % (move_instruction,
+                                                            input_registers[0])
         res += ident + '    :: "r"(%s + j) : %s, "memory"\n' % (buf_name,
                                                                 clobber)
         res += ident + '  );\n'
@@ -333,8 +425,11 @@ def composite_step(buf_name,
                                              (to_it - num_nontrivial_levels))))
     res += ident + '    __asm__ volatile (\n'
     for l in range(1 << num_nontrivial_levels):
-        res += ident + '      "%s (%%%d), %%%%%s\\n"\n' % (move_instruction, l,
-                                                           input_registers[l])
+        if use_neon_movs:
+            res += f'{ident}      "LD1 {{{input_registers[l]}.4S}}, [%{l}]\\n"\n'
+        else:
+            res += ident + '      "%s (%%%d), %%%%%s\\n"\n' % (move_instruction, l,
+                                                               input_registers[l])
     for it in range(from_it, log_w):
         for ii in range(1 << num_nontrivial_levels):
             res += special_steps[it](input_registers[ii], output_registers,
@@ -351,8 +446,11 @@ def composite_step(buf_name,
         input_registers = output_registers
         output_registers = tmp
     for l in range(1 << num_nontrivial_levels):
-        res += ident + '      "%s %%%%%s, (%%%d)\\n"\n' % (
-            move_instruction, input_registers[l], l)
+        if use_neon_movs:
+            res += f'{ident}      "ST1 {{{input_registers[l]}.4S}}, [%{l}]\\n"\n'
+        else:
+            res += ident + '      "%s %%%%%s, (%%%d)\\n"\n' % (
+                move_instruction, input_registers[l], l)
     res += ident + '      :: %s : %s, "memory"\n' % (
         ', '.join(['"r"(%s + %s)' % (buf_name, x) for x in subcube]), clobber)
     res += ident + '    );\n'
@@ -386,6 +484,15 @@ def double_sse_composite_step(buf_name, log_n, from_it, to_it, ident=''):
                           [double_sse_0], double_sse_1_etc, ident)
 
 
+NEON_VECTOR_REGS = [f"v{x}" for x in range(0, 32)]
+
+
+def float_neon_composite_step(buf_name, log_n, from_it, to_it, ident=''):
+    return composite_step(buf_name, log_n, from_it, to_it, 2,
+                          NEON_VECTOR_REGS, MOVE_INSTRUCTION_USE_NEON,
+                          [float_neon_0, float_neon_1], float_neon_2_etc, ident)
+
+
 def plain_unmerged(type_name, log_n):
     signature = "static inline void helper_%s_%d(%s *buf)" % (type_name, log_n,
                                                        type_name)
@@ -413,7 +520,8 @@ def greedy_merged(type_name, log_n, composite_step):
             try:
                 composite_step('buf', log_n, cur_it, cur_to_it)
                 break
-            except Exception:
+            except Exception as e:
+                print(f"warning: {e}")
                 cur_to_it -= 1
                 continue
         res += composite_step('buf', log_n, cur_it, cur_to_it, '  ')
@@ -435,18 +543,22 @@ def greedy_merged_recursive(type_name, log_n, threshold_step, composite_step):
     res = '%s;\n' % signature
     res += '%s {\n' % signature
     res += '  if (depth == %d) {\n' % threshold_step
-    cur_it = 0
-    while cur_it < threshold_step:
-        cur_to_it = threshold_step
-        while True:
-            try:
-                composite_step('buf', threshold_step, cur_it, cur_to_it)
-                break
-            except Exception:
-                cur_to_it -= 1
-                continue
-        res += composite_step('buf', threshold_step, cur_it, cur_to_it, '    ')
-        cur_it = cur_to_it
+    if threshold_step == log_n:
+        cur_it = 0
+        while cur_it < threshold_step:
+            cur_to_it = threshold_step
+            while True:
+                try:
+                    composite_step('buf', threshold_step, cur_it, cur_to_it)
+                    break
+                except Exception:
+                    cur_to_it -= 1
+                    continue
+            res += composite_step('buf', threshold_step, cur_it, cur_to_it, '    ')
+            cur_it = cur_to_it
+    else:
+        res += '    helper_%s_%d(buf);\n' % (type_name, threshold_step)
+
     res += '    return;\n'
     res += '  }\n'
     cur_it = threshold_step
@@ -463,7 +575,10 @@ def greedy_merged_recursive(type_name, log_n, threshold_step, composite_step):
         for i in range(1 << (cur_to_it - cur_it)):
             res += '    helper_%s_%d_recursive(buf + %d, %d);\n' % (
                 type_name, log_n, i * (1 << cur_it), cur_it)
-        res += composite_step('buf', cur_to_it, cur_it, cur_to_it, '    ')
+        if cur_to_it < log_n:
+            res += '    helper_%s_%d(buf);' % (type_name, cur_to_it)
+        else:
+            res += composite_step('buf', cur_to_it, cur_it, cur_to_it, '    ')
         res += '    return;\n'
         res += '  }\n'
         cur_it = cur_to_it
@@ -531,12 +646,20 @@ def measure_time(code, log_n, type_name, method_name, num_it=3):
     return get_mean_stddev()
 
 
+# Configuration parameter; set to False if you want the absolute fastest code without regard to size.
+CARE_ABOUT_CODE_SIZE = True
+
+# When CARE_ABOUT_CODE_SIZE, accept the smallest code that is not slower than
+# MAX_PERFORMANCE_PENALTY_FOR_REDUCED_SIZE * the fastest time.
+MAX_PERFORMANCE_PENALTY_FOR_REDUCED_SIZE = 1.1
+
+
 if __name__ == '__main__':
     final_code = '#include "fht.h"\n'
+    code_so_far = ''
     hall_of_fame = []
     for (type_name,
-         composite_step_generator) in [('float', float_avx_composite_step),
-                                       ('double', double_avx_composite_step)]:
+         composite_step_generator) in [('float', float_neon_composite_step)]:
         for log_n in range(1, max_log_n + 1):
             sys.stdout.write('log_n = %d\n' % log_n)
             times = []
@@ -547,29 +670,41 @@ if __name__ == '__main__':
             except Exception:
                 (res, desc) = (plain_unmerged(type_name, log_n),
                                'plain_unmerged')
-            time = measure_time(res, log_n, type_name,
+            time = measure_time(code_so_far + res, log_n, type_name,
                                 'helper_%s_%d' % (type_name, log_n))
-            times.append((time, res, desc))
-            sys.stdout.write('log_n = %d; iterative; time = %.10e\n' % (log_n,
-                                                                        time))
+            code_size = res.count('\n')
+            times.append((time, res, code_size, desc))
+            sys.stdout.write('log_n = %d; iterative; code_size = %d; time = %.10e\n' % (log_n,
+                                                                                        code_size, time))
             for threshold_step in range(1, log_n + 1):
                 try:
                     res = greedy_merged_recursive(type_name, log_n,
                                                   threshold_step,
                                                   composite_step_generator)
-                    time = measure_time(res, log_n, type_name,
+                    time = measure_time(code_so_far + res, log_n, type_name,
                                         'helper_%s_%d' % (type_name, log_n))
+                    code_size = res.count('\n')
                     times.append(
-                        (time, res,
+                        (time, res, code_size,
                          'greedy_merged_recursive %d' % threshold_step))
                     sys.stdout.write(
-                        'log_n = %d; threshold_step = %d; time = %.10e\n' %
-                        (log_n, threshold_step, time))
-                except Exception:
-                    sys.stdout.write('FAIL: %d\n' % threshold_step)
-            (best_time, best_code, best_desc) = min(times)
+                        'log_n = %d; threshold_step = %d; code_size = %d; time = %.10e\n' %
+                        (log_n, threshold_step, code_size, time))
+                except Exception as e:
+                    sys.stdout.write(f'FAIL: {threshold_step} ({e})\n')
+            if CARE_ABOUT_CODE_SIZE:
+                fastest_time = min(times)[0]
+                times_by_size = sorted(times, key=lambda x: x[2])
+                for x in times_by_size:
+                    if x[0] <= fastest_time * MAX_PERFORMANCE_PENALTY_FOR_REDUCED_SIZE:
+                        smallest_acceptable = x
+                        break
+                (best_time, best_code, best_code_size, best_desc) = smallest_acceptable
+            else:
+                (best_time, best_code, best_code_size, best_desc) = min(times)
             hall_of_fame.append((type_name, log_n, best_time, best_desc))
             final_code += best_code
+            code_so_far += best_code
             sys.stdout.write('log_n = %d; best_time = %.10e; %s\n' %
                              (log_n, best_time, best_desc))
         final_code += 'int fht_%s(%s *buf, int log_n) {\n' % (type_name,
@@ -584,10 +719,10 @@ if __name__ == '__main__':
             final_code += '  }\n'
         final_code += '  return 1;\n'
         final_code += '}\n'
-    with open('fht_avx.c', 'w') as output:
+    with open('fht_neon.c', 'w') as output:
         output.write(final_code)
     sys.stdout.write('hall of fame\n')
-    with open('hall_of_fame_avx.txt', 'w') as hof:
+    with open('hall_of_fame_neon.txt', 'w') as hof:
         for (type_name, log_n, best_time, best_desc) in hall_of_fame:
             s = 'type_name = %s; log_n = %d; best_time = %.10e; best_desc = %s\n' % (
                 type_name, log_n, best_time, best_desc)
