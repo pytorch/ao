d6e891e353321b78277105fc9e5febf4ea81cd95
Scott Roy <161522778+metascroy@users.noreply.github.com>
2024-09-09 15:05:01 -0700

    Add 2, 3, 4, 5 bit custom ops
    Differential Revision: D62248716
    
    Pull Request resolved: https://github.com/pytorch/ao/pull/828

--- torchao/experimental/kernels/cpu/linear/examples/torch_custom_op/run_custom_op.py
+++ torchao/experimental/kernels/cpu/linear/examples/torch_custom_op/run_custom_op.py
@@ -4,16 +4,21 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from torch_custom_op import quantize, replace_linear_with_quantized_linear
-import torch
 import copy
 
-group_size = 16
+import torch
+from torch_custom_op import (
+    linear_a8sz_w_lowbit_reference_impl,
+    replace_linear_with_quantized_linear,
+)
+
+group_size = 256
 m = 1
 n = 4096
 k = 4096
-nbit = 4
-n_layers = 10
+nbit = 5
+has_weight_zeros = True
+n_layers = 5
 
 print("Creating random model")
 layers = [torch.nn.Linear(k, n, bias=False) for _ in range(n_layers)]
@@ -22,8 +27,15 @@ model = model.eval()
 
 print("Quantizing random model")
 quantized_model = copy.deepcopy(model)
-quantized_model =  quantized_model.eval()
-replace_linear_with_quantized_linear(quantized_model, kwargs={"group_size": group_size, "nbit": nbit})
+quantized_model = quantized_model.eval()
+replace_linear_with_quantized_linear(
+    quantized_model,
+    kwargs={
+        "group_size": group_size,
+        "nbit": nbit,
+        "has_weight_zeros": has_weight_zeros,
+    },
+)
 
 print("Creating random activations")
 activations = torch.randn(m, k, dtype=torch.float32)
@@ -48,36 +60,42 @@ fn = torch._export.aot_load("/tmp/torch_custom_op_example_model.so", "cpu")
 fn(activations)
 
 
-print("Checking correctness on layer 0")
-
-rtol=1e-05
-
-# default is 1e-8, but PyTorch and C++ (and ARM neon) have different rounding
-# conventions for ties (PyTorch rounds half to even and C++ rounds half to odd)
-# TODO(T200109708): address this
-atol=1e-05 
-
+print("\nChecking correctness on layer 0")
 linear = model[0]
 quantized_linear = quantized_model[0]
-weight_qvals, weight_scales = quantize(linear.weight, group_size, quantized_linear.nbit, scale_only=True)
-
-activation_qvals, activations_scales, activations_zeros = quantize(activations, k, 8, False)
-activations_dequantized = activations_scales * (activation_qvals - activations_zeros)
-weights_dequantized = (weight_qvals.reshape(-1, group_size) * weight_scales.reshape(-1, 1)).reshape(n, k)
 
 with torch.no_grad():
     result = quantized_linear(activations)
-    expected_result = torch.matmul(activations_dequantized, weights_dequantized.transpose(1, 0))
+    expected_result = linear_a8sz_w_lowbit_reference_impl(
+        linear.weight, activations, group_size, nbit, has_weight_zeros
+    )
     non_quantized_result = linear(activations)
 
-if not (torch.allclose(result, expected_result, rtol=rtol, atol=atol)):
-    rand_idxs = torch.randint(0, result.shape[1], (5,))
-    print("rand_idxs: ", rand_idxs)
-    print("kernel_result[rand_idxs]: ", result[0][rand_idxs])
-    print("expected_result[rand_idxs]: ", expected_result[0][rand_idxs])
-    assert False
-else:
-    print("Correctness check passed")
 
-print("kernel_result[0:5]: ", result[0][0:5])
-print("non_quantized_result[0:5]: ", non_quantized_result[0][0:5])
+# Check that entries in result match entries in expected_result
+num_mismatch_at_low_tol = 0
+num_total = result.reshape(-1).shape[0]
+for i in range(num_total):
+    actual_val = result.reshape(-1)[i]
+    expected_val = expected_result.reshape(-1)[i]
+    if not torch.allclose(actual_val, expected_val):
+        num_mismatch_at_low_tol += 1
+
+        # If results are not close at a relaxed tolerance, exit with failure
+        if not torch.allclose(actual_val, expected_val, atol=1e-6):
+            assert False, "Correctness check failed"
+
+# Assert at most 5% of entries are not close at a low tolerance
+assert num_mismatch_at_low_tol / num_total <= 0.05, "Correctness check failed"
+print(
+    "Correctness check passed.  All results are close, and ",
+    (num_total - num_mismatch_at_low_tol),
+    "/",
+    num_total,
+    " entries are close at a low tolerance.",
+)
+print("Quantization errors:")
+print("\tL1 error: ", torch.mean(torch.abs(result - non_quantized_result)).item())
+print("\tL2 error: ", torch.mean((result - non_quantized_result) ** 2).item())
+print("\tquantized_result[0:5]: ", result[0][0:5])
+print("\tnon_quantized_result[0:5]: ", non_quantized_result[0][0:5])
