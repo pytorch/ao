9d169a8534d5ee4dca3b9720ae251bfb3fdc0446
Scott Roy <161522778+metascroy@users.noreply.github.com>
2024-09-09 15:05:01 -0700

    Add 2, 3, 4, 5 bit custom ops
    Differential Revision: D62248716
    
    Pull Request resolved: https://github.com/pytorch/ao/pull/828

--- torchao/experimental/kernels/cpu/linear/examples/torch_custom_op/torch_custom_op.py
+++ torchao/experimental/kernels/cpu/linear/examples/torch_custom_op/torch_custom_op.py
@@ -40,14 +40,57 @@ def quantize(vals: torch.Tensor, group_size: int, nbit: int, scale_only: bool):
     return group_qvals, group_scales, group_zeros
 
 
-class Chn8ActGrp3WgtQuantizedLinear(nn.Module):
-    nbit = 3
+def linear_a8sz_w_lowbit_reference_impl(
+    weights, activations, group_size, nbit, has_weight_zeros
+):
+    n, k = weights.shape
+    m, k = activations.shape
+    assert m == 1
+    assert k % group_size == 0
 
-    def __init__(self, squeeze_unsqueeze_dim0=False):
+    if has_weight_zeros:
+        weight_qvals, weight_scales, weight_zeros = quantize(
+            weights, group_size, nbit, scale_only=False
+        )
+        weights_dequantized = (
+            weight_scales.reshape(-1, 1)
+            * (weight_qvals.reshape(-1, group_size) - weight_zeros.reshape(-1, 1))
+        ).reshape(n, k)
+    else:
+        weight_qvals, weight_scales = quantize(
+            weights, group_size, nbit, scale_only=True
+        )
+        weights_dequantized = (
+            weight_scales.reshape(-1, 1) * (weight_qvals.reshape(-1, group_size))
+        ).reshape(n, k)
+
+    activation_qvals, activations_scales, activations_zeros = quantize(
+        activations, k, 8, False
+    )
+    activations_dequantized = activations_scales * (
+        activation_qvals - activations_zeros
+    ).reshape(m, k)
+    return torch.matmul(activations_dequantized, weights_dequantized.transpose(1, 0))
+
+
+class _quantized_linear(nn.Module):
+    def __init__(
+        self,
+        nbit,
+        has_weight_zeros,
+        pack_weight_op,
+        linear_op,
+        squeeze_unsqueeze_dim0=False,
+    ):
         super().__init__()
         self.squeeze_unsqueeze_dim0 = squeeze_unsqueeze_dim0
+        self.nbit = nbit
 
-    def initialize_from_unpacked_weights(self, weight_qvals, weight_scales, group_size):
+        self._has_weight_zeros = has_weight_zeros
+        self._pack_weights_op = pack_weight_op
+        self._linear_op = linear_op
+
+    def pack_weights(self, weight_qvals, weight_scales_and_zeros, group_size):
         n, k = weight_qvals.shape
 
         # TODO(T200095131): convert self.n, self.k, self.group_size to
@@ -55,65 +98,23 @@ class Chn8ActGrp3WgtQuantizedLinear(nn.Module):
         self.n = torch.empty(n)
         self.k = torch.empty(k)
         self.group_size = torch.empty(group_size)
-        self.packed_weights = torch.ops.torchao._pack_weights_3bit(
-            weight_qvals, weight_scales, self.group_size
-        )
 
-    def initialize_from_packed_weights(self, packed_weights, n, k, group_size):
-        # TODO(T200095131): convert self.n, self.k, self.group_size to
-        # int when supported by AOTI
-        self.n = torch.empty(n)
-        self.k = torch.empty(k)
-        self.group_size = torch.empty(group_size)
-        self.packed_weights = packed_weights
-
-    def forward(self, x):
-        if self.squeeze_unsqueeze_dim0:
-            x = x.squeeze(0)
-
-        res = torch.ops.torchao._linear_3bit(
-            self.packed_weights, self.n, self.k, self.group_size, x
-        )
-
-        if self.squeeze_unsqueeze_dim0:
-            res = res.unsqueeze(0)
-        return res
-
-
-class Chn8ActGrp4WgtQuantizedLinear(nn.Module):
-    nbit = 4
-
-    def __init__(self, squeeze_unsqueeze_dim0=False):
-        super().__init__()
-        self.squeeze_unsqueeze_dim0 = squeeze_unsqueeze_dim0
-
-    def initialize_from_unpacked_weights(self, weight_qvals, weight_scales, group_size):
-        n, k = weight_qvals.shape
-
-        # TODO(T200095131): convert self.n, self.k, self.group_size to
-        # int when supported by AOTI
-        self.n = torch.empty(n)
-        self.k = torch.empty(k)
-        self.group_size = torch.empty(group_size)
-        self.packed_weights = torch.ops.torchao._pack_weights_4bit(
-            weight_qvals, weight_scales, self.group_size
-        )
-
-    def initialize_from_packed_weights(self, packed_weights, n, k, group_size):
-        # TODO(T200095131): convert self.n, self.k, self.group_size to
-        # int when supported by AOTI
-        self.n = torch.empty(n)
-        self.k = torch.empty(k)
-        self.group_size = torch.empty(group_size)
-        self.packed_weights = packed_weights
+        if self._has_weight_zeros:
+            weight_scales, weight_zeros = weight_scales_and_zeros
+            self.packed_weights = self._pack_weights_op(
+                weight_qvals, weight_scales, weight_zeros, self.group_size
+            )
+        else:
+            weight_scales = weight_scales_and_zeros
+            self.packed_weights = self._pack_weights_op(
+                weight_qvals, weight_scales, self.group_size
+            )
 
     def forward(self, x):
         if self.squeeze_unsqueeze_dim0:
             x = x.squeeze(0)
 
-        res = torch.ops.torchao._linear_4bit(
-            self.packed_weights, self.n, self.k, self.group_size, x
-        )
+        res = self._linear_op(self.packed_weights, self.n, self.k, self.group_size, x)
 
         if self.squeeze_unsqueeze_dim0:
             res = res.unsqueeze(0)
@@ -123,6 +124,7 @@ class Chn8ActGrp4WgtQuantizedLinear(nn.Module):
 def replace_linear_with_quantized_linear(module: nn.Module, kwargs={}):
     group_size = kwargs["group_size"]
     nbit = kwargs["nbit"]
+    has_weight_zeros = kwargs["has_weight_zeros"]
     squeeze_unsqueeze_dim0 = (
         kwargs["squeeze_unsqueeze_dim0"]
         if "squeeze_unsqueeze_dim0" in kwargs
@@ -132,30 +134,98 @@ def replace_linear_with_quantized_linear(module: nn.Module, kwargs={}):
     for name, child in module.named_children():
         if isinstance(child, nn.Linear):
             assert child.bias is None
-            n, k = child.weight.shape
-            weight_qvals, weight_scales = quantize(
-                child.weight, group_size=group_size, nbit=nbit, scale_only=True
-            )
 
-            if nbit == 3:
-                setattr(
-                    module, name, Chn8ActGrp3WgtQuantizedLinear(squeeze_unsqueeze_dim0)
+            if not has_weight_zeros:
+                weight_qvals, weight_scales = quantize(
+                    child.weight, group_size=group_size, nbit=nbit, scale_only=True
                 )
-                getattr(module, name).initialize_from_unpacked_weights(
-                    weight_qvals,
-                    weight_scales,
-                    group_size,
-                )
-            elif nbit == 4:
-                setattr(
-                    module, name, Chn8ActGrp4WgtQuantizedLinear(squeeze_unsqueeze_dim0)
-                )
-                getattr(module, name).initialize_from_unpacked_weights(
-                    weight_qvals,
-                    weight_scales,
-                    group_size,
-                )
+                weight_scales_and_zeros = weight_scales
             else:
-                raise ValueError(f"Unsupported nbit: {nbit}")
+                weight_qvals, weight_scales, weight_zeros = quantize(
+                    child.weight, group_size=group_size, nbit=nbit, scale_only=False
+                )
+                weight_scales_and_zeros = (weight_scales, weight_zeros.to(torch.int8))
+
+            qlinear = None
+            if nbit == 2:
+                if has_weight_zeros:
+                    qlinear = _quantized_linear(
+                        nbit=nbit,
+                        has_weight_zeros=has_weight_zeros,
+                        pack_weight_op=torch.ops.torchao._pack_weights_a8sz_w2sz,
+                        linear_op=torch.ops.torchao._linear_a8sz_w2sz,
+                        squeeze_unsqueeze_dim0=squeeze_unsqueeze_dim0,
+                    )
+                else:
+                    qlinear = _quantized_linear(
+                        nbit=nbit,
+                        has_weight_zeros=has_weight_zeros,
+                        pack_weight_op=torch.ops.torchao._pack_weights_a8sz_w2s,
+                        linear_op=torch.ops.torchao._linear_a8sz_w2s,
+                        squeeze_unsqueeze_dim0=squeeze_unsqueeze_dim0,
+                    )
+            elif nbit == 3:
+                if has_weight_zeros:
+                    qlinear = _quantized_linear(
+                        nbit=nbit,
+                        has_weight_zeros=has_weight_zeros,
+                        pack_weight_op=torch.ops.torchao._pack_weights_a8sz_w3sz,
+                        linear_op=torch.ops.torchao._linear_a8sz_w3sz,
+                        squeeze_unsqueeze_dim0=squeeze_unsqueeze_dim0,
+                    )
+                else:
+                    qlinear = _quantized_linear(
+                        nbit=nbit,
+                        has_weight_zeros=has_weight_zeros,
+                        pack_weight_op=torch.ops.torchao._pack_weights_a8sz_w3s,
+                        linear_op=torch.ops.torchao._linear_a8sz_w3s,
+                        squeeze_unsqueeze_dim0=squeeze_unsqueeze_dim0,
+                    )
+            elif nbit == 4:
+                if has_weight_zeros:
+                    qlinear = _quantized_linear(
+                        nbit=nbit,
+                        has_weight_zeros=has_weight_zeros,
+                        pack_weight_op=torch.ops.torchao._pack_weights_a8sz_w4sz,
+                        linear_op=torch.ops.torchao._linear_a8sz_w4sz,
+                        squeeze_unsqueeze_dim0=squeeze_unsqueeze_dim0,
+                    )
+                else:
+                    qlinear = _quantized_linear(
+                        nbit=nbit,
+                        has_weight_zeros=has_weight_zeros,
+                        pack_weight_op=torch.ops.torchao._pack_weights_a8sz_w4s,
+                        linear_op=torch.ops.torchao._linear_a8sz_w4s,
+                        squeeze_unsqueeze_dim0=squeeze_unsqueeze_dim0,
+                    )
+            elif nbit == 5:
+                if has_weight_zeros:
+                    qlinear = _quantized_linear(
+                        nbit=nbit,
+                        has_weight_zeros=has_weight_zeros,
+                        pack_weight_op=torch.ops.torchao._pack_weights_a8sz_w5sz,
+                        linear_op=torch.ops.torchao._linear_a8sz_w5sz,
+                        squeeze_unsqueeze_dim0=squeeze_unsqueeze_dim0,
+                    )
+                else:
+                    qlinear = _quantized_linear(
+                        nbit=nbit,
+                        has_weight_zeros=has_weight_zeros,
+                        pack_weight_op=torch.ops.torchao._pack_weights_a8sz_w5s,
+                        linear_op=torch.ops.torchao._linear_a8sz_w5s,
+                        squeeze_unsqueeze_dim0=squeeze_unsqueeze_dim0,
+                    )
+            else:
+                raise ValueError(
+                    f"Unsupported nbit ({nbit}) and has_weight_zeros ({has_weight_zeros}) combination"
+                )
+
+            assert qlinear is not None
+            setattr(module, name, qlinear)
+            getattr(module, name).pack_weights(
+                weight_qvals,
+                weight_scales_and_zeros,
+                group_size,
+            )
         else:
             replace_linear_with_quantized_linear(child, kwargs)
