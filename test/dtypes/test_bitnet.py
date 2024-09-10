45969b958eb2a42f689b9dd65f0a299c60bde968
Mark Saroufim <marksaroufim@meta.com>
2024-09-10 03:37:42 -0700

    yolo

--- test/dtypes/test_bitnet.py
+++ test/dtypes/test_bitnet.py
@@ -58,7 +58,7 @@ def _apply_weight_only_uint2_quant(model):
         lambda mod, fqn: isinstance(mod, torch.nn.Linear),
     )
 
-@pytest.skip(TORCH_VERSION_AT_LEAST_2_5)
+@pytest.mark.skipif(TORCH_VERSION_AT_LEAST_2_5, reason="Regression introdued in nightlies")
 @pytest.mark.parametrize("input_shape", [[2, 4], [5, 5, 5, 4], [1, 4, 4]])
 def test_uint2_quant(input_shape):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
