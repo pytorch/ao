3fa38aaf1276e36845a82fb399e5054718a441c4
Driss Guessous <32754868+drisspg@users.noreply.github.com>
2024-09-13 13:55:17 -0700

    [Float8] Add static constructor that will be used in Observer workflow (#869)

--- torchao/float8/__init__.py
+++ torchao/float8/__init__.py
@@ -23,6 +23,7 @@ from torchao.float8.float8_tensor import (
     LinearMMConfig,
     ScaledMMConfig,
 )
+from torchao.float8.inference import Float8MMConfig
 from torchao.float8.fsdp_utils import precompute_float8_dynamic_scale_for_fsdp
 
 from torchao.utils import TORCH_VERSION_AT_LEAST_2_5
@@ -31,7 +32,7 @@ from torchao.utils import TORCH_VERSION_AT_LEAST_2_5
 if TORCH_VERSION_AT_LEAST_2_5:
     # Needed to load Float8Tensor with weights_only = True
     from torch.serialization import add_safe_globals
-    add_safe_globals([Float8Tensor, ScaledMMConfig, GemmInputRole, LinearMMConfig])
+    add_safe_globals([Float8Tensor, ScaledMMConfig, GemmInputRole, LinearMMConfig, Float8MMConfig])
 
 __all__ = [
     # configuration
