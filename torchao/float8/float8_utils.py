f1b9c1dde4fd228addac8c5b573aadafcb8cccff
Wei Feng <weif@meta.com>
2024-09-24 13:42:55 -0700

    liner
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- torchao/float8/float8_utils.py
+++ torchao/float8/float8_utils.py
@@ -98,7 +98,7 @@ def amax_history_to_scale_stack(
 
 
 @torch.no_grad()
-def tensor_to_amax(x: torch.Tensor, reduce_amax: bool = False, device_mesh: "DeviceMesh" = None) -> torch.Tensor:
+def tensor_to_amax(x: torch.Tensor, reduce_amax: bool = False, device_mesh: dist.DeviceMesh = None) -> torch.Tensor:
     amax = torch.max(torch.abs(x))
 
     # If the user asked for distributed reduction, do it.
@@ -116,7 +116,7 @@ def tensor_to_scale(
     x: torch.Tensor,
     float8_dtype: torch.dtype,
     reduce_amax: bool = False,
-    device_mesh: "DeviceMesh" = None,
+    device_mesh: dist.DeviceMesh = None,
 ) -> torch.Tensor:
     amax = tensor_to_amax(x, reduce_amax=reduce_amax, device_mesh=device_mesh)
     return amax_to_scale(amax, float8_dtype, x.dtype)
