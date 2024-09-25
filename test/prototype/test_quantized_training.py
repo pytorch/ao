62218eff72661ceb0e5068f1901c3fd7fddd704c
Thien Tran <gau.nernst@yahoo.com.sg>
2024-09-25 11:43:36 +0800

    adjust tolerance

--- test/prototype/test_quantized_training.py
+++ test/prototype/test_quantized_training.py
@@ -281,9 +281,9 @@ class TestFSDP2(FSDPTest):
         # quantize_fn, mp_policy, tolerance
         test_args = [
             # high tolerance due to stochastic rounding
-            (int8_weight_only_quantized_training(), mp_policy, 0.05),
-            (int8_mixed_precision_training(), mp_policy, 1e-6),
-            (bitnet_training(), mp_policy, 1e-6),
+            (int8_weight_only_quantized_training, mp_policy, 0.05),
+            (int8_mixed_precision_training, mp_policy, 1e-6),
+            (bitnet_training, mp_policy, 1e-5),
         ]
 
         # FSDP2 mixed-precision requires https://github.com/pytorch/pytorch/pull/136129
@@ -296,9 +296,9 @@ class TestFSDP2(FSDPTest):
             bf16_mp_policy = MixedPrecisionPolicy(param_dtype=torch.bfloat16)
 
             extra_args = [
-                (int8_weight_only_quantized_training(), bf16_mp_policy, 1e-2),
-                (int8_mixed_precision_training(), bf16_mp_policy, 1e-2),
-                (bitnet_training(), bf16_mp_policy, 1e-2),
+                (int8_weight_only_quantized_training, bf16_mp_policy, 1e-2),
+                (int8_mixed_precision_training, bf16_mp_policy, 1e-2),
+                (bitnet_training, bf16_mp_policy, 1e-2),
             ]
             test_args.extend(extra_args)
 
@@ -324,8 +324,8 @@ class TestFSDP2(FSDPTest):
         base_model = Transformer(model_args).cuda()
         fsdp_model = copy.deepcopy(base_model)
 
-        quantize_(base_model.layers, quantize_fn, set_inductor_config=False)
-        quantize_(fsdp_model.layers, quantize_fn, set_inductor_config=False)
+        quantize_(base_model.layers, quantize_fn(), set_inductor_config=False)
+        quantize_(fsdp_model.layers, quantize_fn(), set_inductor_config=False)
 
         for layer in fsdp_model.layers:
             fully_shard(layer, mp_policy=mp_policy)
@@ -354,7 +354,7 @@ class TestFSDP2(FSDPTest):
             base_optim.step()
 
             rel_error = (fsdp_loss - base_loss).abs() / base_loss.abs()
-            assert rel_error < tolerance, (args, iter_idx, rel_error)
+            assert rel_error < tolerance, (quantize_fn.__name__, mp_policy, iter_idx, rel_error)
 
 
 instantiate_parametrized_tests(TestQuantizedTraining)
