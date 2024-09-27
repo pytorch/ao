b113edae23ae9e7a53228ede02e699d048555ab5
Jerry Zhang <jerryzh168@gmail.com>
2024-09-26 18:17:54 -0700

    fix version

--- torchao/testing/utils.py
+++ torchao/testing/utils.py
@@ -320,7 +320,7 @@ class TorchAOTensorParallelTestCase(DTensorTestBase):
 
         y_d = dn_dist(up_dist(input_dtensor))
 
-        if TORCH_VERSION_AT_LEAST_2_5:
+        if not TORCH_VERSION_AT_LEAST_2_5:
             # Need torch 2.5 to support compiled tensor parallelism
             return
 
