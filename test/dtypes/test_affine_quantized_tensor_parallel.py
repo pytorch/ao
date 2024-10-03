2b8397226e3c6c0e1fba75375a8f4f487307ba6e
jainapurva <apurvajain.kota@gmail.com>
2024-10-02 18:46:14 -0700

    Temporary Fix: Skip on H100

--- test/dtypes/test_affine_quantized_tensor_parallel.py
+++ test/dtypes/test_affine_quantized_tensor_parallel.py
@@ -5,8 +5,12 @@ from torchao.quantization import int8_weight_only
 class TestAffineQuantizedTensorParallel(TorchAOTensorParallelTestCase):
     pass
 
+is_H100 = torch.cuda.is_available() and torch.cuda.get_device_capability() >= (9, 0)
 
-copy_tests(TorchAOTensorParallelTestCase, TestAffineQuantizedTensorParallel, "aqt_tp")
+if not is_H100:
+    copy_tests(TorchAOTensorParallelTestCase, TestAffineQuantizedTensorParallel, "aqt_tp")
+else:
+    print("Skipping TestAffineQuantizedTensorParallel because it doesn't run on H100")
 
 if __name__ == "__main__":
     run_tests()
