9e0a59f5866a02e756c9478db5a5b28c4e7b342a
andrewor14 <andrewor14@gmail.com>
2024-10-09 17:40:24 -0400

    Make module swap the main QAT flow again (#1037)
    Summary: Following https://github.com/pytorch/ao/issues/987, this
    commit makes module swap the main QAT flow today. We remove all
    tensor subclass fake quantize injection logic since this is not
    needed in both the long term and the short term plans for QAT.
    In the short term, we will continue to use a full module swap
    flow, and only migrate to the long term flow once there is
    general distributed support for tensor subclasses and when
    tensor subclass composability provides meaningful benefits.
    
    Test Plan:
    python test/quantization/test_qat.py
    
    [ghstack-poisoned]

--- torchao/quantization/prototype/qat/__init__.py
+++ torchao/quantization/prototype/qat/__init__.py
@@ -1,17 +1,14 @@
 from .api import (
+    ComposableQATQuantizer,
+)
+from .linear import (
     disable_4w_fake_quant,
     disable_8da4w_fake_quant,
     enable_4w_fake_quant,
     enable_8da4w_fake_quant,
-    int4_weight_only_fake_quantize,
-    int8_dynamic_activation_int4_weight_fake_quantize,
-    ComposableQATQuantizer,
     Int4WeightOnlyQATQuantizer,
-    Int8DynActInt4WeightQATQuantizer,
-)
-
-from ._module_swap_api import (
     Int8DynActInt4WeightQATLinear,
+    Int8DynActInt4WeightQATQuantizer,
 )
 from .embedding import (
     Int4WeightOnlyEmbeddingQATQuantizer,
@@ -22,8 +19,6 @@ __all__ = [
     "disable_8da4w_fake_quant",
     "enable_4w_fake_quant",
     "enable_8da4w_fake_quant",
-    "int4_weight_only_fake_quantize",
-    "int8_dynamic_activation_int4_weight_fake_quantize",
     "ComposableQATQuantizer",
     "Int4WeightOnlyQATQuantizer",
     "Int4WeightOnlyEmbeddingQATQuantizer"
