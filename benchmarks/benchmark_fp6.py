f50d8d745e4dfa8c5d0c8ceed2e7cfa7f27d87c7
Tobias van der Werff <33268192+tobiasvanderwerff@users.noreply.github.com>
2024-10-10 11:04:21 +0200

    Add FP6 benchmark option to use BF16

--- benchmarks/benchmark_fp6.py
+++ benchmarks/benchmark_fp6.py
@@ -7,12 +7,13 @@ from torchao.utils import benchmark_torch_function_in_microseconds
 from tqdm import tqdm
 
 
-def benchmark(m: int, k: int, n: int):
-    float_data = torch.randn(n, k, dtype=torch.half, device="cuda")
+def benchmark(m: int, k: int, n: int, use_bf16=False):
+    dtype = torch.bfloat16 if use_bf16 else torch.half
+    float_data = torch.randn(n, k, dtype=dtype, device="cuda")
     fp6_weight = to_affine_quantized_fpx(float_data, FloatxTensorCoreLayout(3, 2))
-    fp16_weight = fp6_weight.dequantize(torch.half)
+    fp16_weight = fp6_weight.dequantize(dtype)
 
-    fp16_act = torch.randn(m, k, dtype=torch.half, device="cuda")
+    fp16_act = torch.randn(m, k, dtype=dtype, device="cuda")
     fp6_output = F.linear(fp16_act, fp6_weight)
     fp16_output = F.linear(fp16_act, fp16_weight)
 
@@ -28,7 +29,7 @@ def benchmark(m: int, k: int, n: int):
         "k": k,
         "n": n,
         "fp6_latency (ms)": fp6_time,
-        "fp16_latency (ms)": fp16_time,
+        f"{'bf16' if use_bf16 else 'fp16'}_latency (ms)": fp16_time,
         "speedup (d/s)": fp16_time / fp6_time,
         "correct": correct,
     }
@@ -39,11 +40,13 @@ if __name__ == "__main__":
     k_vals = (8192, 8192, 8192, 28672)
     n_vals = (8192, 10240, 57344, 8192)
 
+    use_bf16 = True
+
     results = []
 
     for m in tqdm([1 << i for i in range(10)]):
         for n, k in zip(n_vals, k_vals):
-            results.append(benchmark(m, k, n))
+            results.append(benchmark(m, k, n, use_bf16=use_bf16))
 
     df = pd.DataFrame(results)
     df.to_csv("fp6_llm_benchmark_results.csv", index=False)
