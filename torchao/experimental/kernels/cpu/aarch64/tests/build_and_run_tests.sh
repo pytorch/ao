f28e556f3085c8c41af5618e5ea371439edc25e7
Digant Desai <digantdesai@meta.com>
2024-10-10 10:19:13 -0500

    [experimental][kleidi] Build kernel tests in debug mode

--- torchao/experimental/kernels/cpu/aarch64/tests/build_and_run_tests.sh
+++ torchao/experimental/kernels/cpu/aarch64/tests/build_and_run_tests.sh
@@ -9,9 +9,9 @@ set -eu
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 export TORCHAO_LIBRARIES=${SCRIPT_DIR}/../../../../../..
 export CMAKE_OUT=/tmp/cmake-out/torch_ao/tests
-cmake -DTORCHAO_LIBRARIES=${TORCHAO_LIBRARIES} -S ${TORCHAO_LIBRARIES}/torchao/experimental/kernels/cpu/aarch64/tests -B ${CMAKE_OUT}
+cmake -DCMAKE_BUILD_TYPE=Debug -DTORCHAO_LIBRARIES=${TORCHAO_LIBRARIES} -S ${TORCHAO_LIBRARIES}/torchao/experimental/kernels/cpu/aarch64/tests -B ${CMAKE_OUT}
 
-cmake --build  ${CMAKE_OUT}
+cmake --build ${CMAKE_OUT}
 
 # Run
 ${CMAKE_OUT}/test_quantization
