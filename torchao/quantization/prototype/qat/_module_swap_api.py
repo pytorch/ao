661b04cd8b0dd168ae15132c746409140a52d757
andrewor14 <andrewor14@gmail.com>
2024-10-11 07:56:37 -0700

    Fix QAT module swap BC
    Add back _module_swap_api.Int8DynActInt4WeightQATLinear.
    
    [ghstack-poisoned]

--- torchao/quantization/prototype/qat/_module_swap_api.py
+++ torchao/quantization/prototype/qat/_module_swap_api.py
@@ -2,6 +2,7 @@
 # These will be removed in the future
 
 from .linear import (
+    Int8DynActInt4WeightQATLinear,
     Int8DynActInt4WeightQATQuantizer as Int8DynActInt4WeightQATQuantizerModuleSwap,
     Int4WeightOnlyQATQuantizer as Int4WeightOnlyQATQuantizerModuleSwap,
     enable_8da4w_fake_quant as enable_8da4w_fake_quant_module_swap,
