b53694a4cabb1f02a4aa814c4c882bd07fcb515c
Scott Roy <161522778+metascroy@users.noreply.github.com>
2024-10-14 18:12:48 -0700

    Update cmake files (#1070)
    * update cmake files
    
    * remove config file

--- torchao/experimental/build_torchao_ops.sh
+++ torchao/experimental/build_torchao_ops.sh
@@ -13,14 +13,14 @@ TARGET="${1}"
 export CMAKE_PREFIX_PATH=$(python -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')
 echo "CMAKE_PREFIX_PATH: ${CMAKE_PREFIX_PATH}"
 if [[ $TARGET == "executorch" ]]; then
-    TORCHAO_OP_EXECUTORCH_BUILD=ON
+    TORCHAO_BUILD_EXECUTORCH_OPS=ON
 else
-    TORCHAO_OP_EXECUTORCH_BUILD=OFF
+    TORCHAO_BUILD_EXECUTORCH_OPS=OFF
 fi
-export CMAKE_OUT=cmake-out/torchao
+export CMAKE_OUT=cmake-out
 cmake -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} \
     -DCMAKE_INSTALL_PREFIX=${CMAKE_OUT} \
-    -DTORCHAO_OP_EXECUTORCH_BUILD="${TORCHAO_OP_EXECUTORCH_BUILD}" \
+    -DTORCHAO_BUILD_EXECUTORCH_OPS="${TORCHAO_BUILD_EXECUTORCH_OPS}" \
     -S . \
     -B ${CMAKE_OUT}
-cmake --build  ${CMAKE_OUT} --target install --config Release
+cmake --build  ${CMAKE_OUT} -j 16 --target install --config Release
