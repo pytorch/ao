d2aadf29ae251ee924e081018a29f1b694382c27
Peter Yeh <pyeh@amd.com>
2024-10-17 14:49:30 -0500

    add sparse_marlin kernel to the build

--- setup.py
+++ setup.py
@@ -108,8 +108,9 @@ def get_extensions():
     extensions_cuda_dir = os.path.join(extensions_dir, "cuda")
     cuda_sources = list(glob.glob(os.path.join(extensions_cuda_dir, "**/*.cu"), recursive=True))
 
-    extensions_hip_dir = os.path.join(extensions_dir, "cuda", "fp6_llm")
+    extensions_hip_dir = os.path.join(extensions_dir, "cuda", "sparse_marlin")
     hip_sources = list(glob.glob(os.path.join(extensions_hip_dir, "*.cu"), recursive=True))
+    hip_sources = list(glob.glob(os.path.join(extensions_hip_dir, "*.h"), recursive=True))
 
     if not IS_ROCM and use_cuda:
         sources += cuda_sources
