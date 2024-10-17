f817edf135c14e39e94546bde7fb871dbf3bf27d
Peter Yeh <pyeh@amd.com>
2024-10-17 15:29:21 -0500

    drop .h from conversion

--- setup.py
+++ setup.py
@@ -117,7 +117,6 @@ def get_extensions():
 
     extensions_hip_dir = os.path.join(extensions_dir, "cuda", "tensor_core_tiled_layout", "sparse_marlin")
     hip_sources = list(glob.glob(os.path.join(extensions_hip_dir, "*.cu"), recursive=True))
-    hip_sources = list(glob.glob(os.path.join(extensions_hip_dir, "*.h"), recursive=True))
 
     if not IS_ROCM and use_cuda:
         sources += cuda_sources
