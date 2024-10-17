00cfadd2e89b1fa114055f1672c6875d8d1d5b82
Xia, Weiwen <weiwen.xia@intel.com>
2024-10-16 21:35:54 -0400

    Update readme.md

--- torchao/prototype/smoothquant/example.py
+++ torchao/prototype/smoothquant/example.py
@@ -13,9 +13,8 @@ from torchao.prototype.smoothquant import (
 from torchao.quantization import quantize_
 
 
-# adapted from the AWQ example
 def get_calib_dataset(tokenizer=None, n_samples=100, block_size=512):
-    dataset = load_dataset("mit-han-lab/pile-val-backup", split="validation")
+    dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split='validation')
     samples = []
     n_tokens = n_samples * block_size
     n_run = n_tokens
@@ -36,11 +35,11 @@ def get_calib_dataset(tokenizer=None, n_samples=100, block_size=512):
     cat_samples = torch.cat(samples, dim=1)
     return [cat_samples[:, i * block_size : (i + 1) * block_size] for i in range(n_samples)]
 
-# adapted from the AWQ example
+
 def wiki2_eval(model, tokenizer, sequence_length, stride=512, verbose=True, device="cuda"):
     model.eval()
-    tokenizer.pad_token     = tokenizer.eos_token 
-    tokenizer.padding_side  = "right" 
+    tokenizer.pad_token     = tokenizer.eos_token
+    tokenizer.padding_side  = "right"
     tokenizer.add_eos_token = False
 
     print("Loading dataset")
@@ -56,10 +55,10 @@ def wiki2_eval(model, tokenizer, sequence_length, stride=512, verbose=True, devi
     for i in tqdm(range(0, encodings['input_ids'].size(1), stride), disable=not verbose):
         begin_loc  = max(i + stride - sequence_length, 0)
         end_loc    = min(i + stride, encodings['input_ids'].size(1))
-        trg_len    = end_loc - i  
+        trg_len    = end_loc - i
         input_ids  = encodings['input_ids'][:,begin_loc:end_loc]
         target_ids = input_ids.clone()
-        target_ids[:,:-trg_len] = -100 #ignore context 
+        target_ids[:,:-trg_len] = -100 # ignore context
 
         t1 = time.time()
         with torch.no_grad():
@@ -79,8 +78,8 @@ def wiki2_eval(model, tokenizer, sequence_length, stride=512, verbose=True, devi
         print('time', str(pred_time) + ' sec/it')
 
     return {'perplexity':ppl, 'prediction_time':pred_time}
-    
-# adapted from the AWQ example
+
+
 def benchmark(model, tokenizer, max_length, tasks=None, device="cuda"):
     model.eval()
     model.config.use_cache = False
@@ -94,18 +93,18 @@ def benchmark(model, tokenizer, max_length, tasks=None, device="cuda"):
 
 def wikitext2_ppl(
         model_id: str,
+        alpha: float,
         quant_mode: str,
-        calibration_size: int, 
-        device: str, 
-        precision:torch.dtype, 
-        sequence_length: int, 
+        calibration_size: int,
+        device: str,
+        precision:torch.dtype,
+        sequence_length: int,
         compile: bool,
         model_load_path: str,
         model_save_path: str):
     print(f"Loading model on {device}...")
     torch.manual_seed(34)
     t0 = time.time()
-    # load any model with torch.nn.linear layers
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     if model_load_path is not None and os.path.exists(model_load_path):
         print(f"Loading quantized model from {model_load_path}")
@@ -118,7 +117,7 @@ def wikitext2_ppl(
         print(f"running calibration")
         t0 = time.time()
         # insert observers to find average magnitude and calculate scales
-        insert_smooth_quant_observer(model, 0.5, quant_mode, calibration_size)
+        insert_smooth_quant_observer(model, alpha, quant_mode, calibration_size)
         calibration_data = get_calib_dataset(tokenizer=tokenizer, n_samples=calibration_size, block_size=sequence_length)
         for batch in calibration_data:
             model(batch.to(device))
@@ -136,16 +135,17 @@ def wikitext2_ppl(
             torch.save(model, model_save_path)
             print(f"Time to save quantized model: {time.time() - t0:.02f} seconds")
     if compile:
-        model = torch.compile(model)
+        model = torch.compile(model, dynamic=True)
 
     return benchmark(model, tokenizer, sequence_length, tasks=["PPL"], device=device)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate a model with the specified parameters.")
-        
 
     # Optional arguments with default values
     parser.add_argument("--model-id", "-m", type=str, help="Repository ID of the model.")
+    parser.add_argument("--alpha", type=float, default=0.5, help="The alpha hyperparameter for SmoothQuant.")
     parser.add_argument("--quant-mode", type=str, help="Quantization mode, either static or dynamic.")
     parser.add_argument("--calibration-samples", type=int, default=10, help="Number of samples to use for calibration. Default is 10.")
     parser.add_argument("--device", type=str, default="cuda", help="Device to run the evaluation on. Default is 'cuda'.")
@@ -163,6 +163,7 @@ if __name__ == "__main__":
     precision_dtype = getattr(torch, args.precision, torch.bfloat16)
     ppl = wikitext2_ppl(
         args.model_id,
+        args.alpha,
         args.quant_mode,
         args.calibration_samples,
         args.device,
