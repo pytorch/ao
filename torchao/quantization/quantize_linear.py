f6f990e76636ed0c50415e50ced9dbac421722aa
dhug <danielpatrickhug@gmail.com>
2024-10-17 18:30:06 +0000

    Updated imports and migrated Quantizer based quantizers to quantize_linear from gptq

--- torchao/quantization/quantize_linear.py
+++ torchao/quantization/quantize_linear.py
@@ -1,9 +1,14 @@
 import torch
 import torch.nn as nn
+import logging
+from torchao.quantization.quant_primitives import MappingType
 import torch.nn.functional as F
-from typing import Optional, Callable, Type
+from torchao.quantization.unified import Quantizer
+from typing import Optional, Callable, Type, Dict, Any
 from torchao.quantization.utils import (
     per_token_dynamic_quant,
+    groupwise_affine_quantize_tensor,
+    group_quantize_tensor_symmetric,
 )
 def _check_linear_int4_k(k, groupsize = 1, inner_k_tiles = None):
     k_divisible_by_groupsize = k % groupsize == 0
@@ -69,6 +74,71 @@ class WeightOnlyInt4Linear(torch.nn.Module):
             self.scales_precision,
         )
 
+class Int4WeightOnlyQuantizer(Quantizer):
+    def __init__(
+        self,
+        groupsize: int = 256,
+        padding_allowed: bool = True,
+        inner_k_tiles: Optional[int] = 8,
+        device: torch.device = torch.device("cuda"),
+        precision: torch.dtype = torch.bfloat16,
+    ) -> None:
+        super().__init__()
+        assert inner_k_tiles in [2, 4, 8]
+        assert groupsize in [32, 64, 128, 256]
+
+        self.inner_k_tiles = inner_k_tiles
+        self.groupsize: int = groupsize
+        self.padding_allowed: bool = padding_allowed
+        self.device: torch.device = device
+        # precision and dtype are being used interchangeably here
+        self.precision: torch.dtype = precision
+
+    @torch.no_grad()
+    def _create_quantized_state_dict(
+        self, model: torch.nn.Module
+    ) -> Dict[str, torch.Tensor]:
+        cur_state_dict = model.state_dict()
+        for fqn, mod in model.named_modules():
+            if isinstance(mod, torch.nn.Linear):
+                assert not mod.bias
+                out_features = mod.out_features
+                in_features = mod.in_features
+                # assert out_features % 8 == 0, "require out_features % 8 == 0"
+                logging.info(f"linear: {fqn}, in={in_features}, out={out_features}")
+
+                assert (
+                    in_features % self.groupsize == 0
+                ), f"require in_features:{in_features} % self.groupsize:{self.groupsize} == 0"
+
+                weight = mod.weight.data
+                if not _check_linear_int4_k(
+                    in_features, self.groupsize, self.inner_k_tiles
+                ):
+                    if self.padding_allowed:
+                        from .utils import find_multiple
+                        import torch.nn.functional as F
+                        logging.warn(f"warning: {fqn} is padded to satisfy in_features % 1024 == 0")
+                        padded_in_features = find_multiple(in_features, 1024)
+                        weight = F.pad(weight, pad=(0, padded_in_features - in_features))
+                    else:
+                        logging.warn(f"warning: {fqn} is skipped, int4 requires that in_features is 32, 64, or is divisible by 1024, " +
+                                "and that groupsize and inner_k_tiles*16 evenly divide into it")
+                        continue
+                (
+                    w_int4x8,
+                    scales_and_zeros
+                ) = groupwise_affine_quantize_tensor(
+                    weight,
+                    4,  # n_bit
+                    self.groupsize,
+                    self.precision, # dtype for scales_and_zeros
+                )
+                # TODO: just get the device from mod.weight.device?
+                weight_int4pack = torch.ops.aten._convert_weight_to_int4pack(w_int4x8.to(self.device), self.inner_k_tiles)
+                cur_state_dict[f"{fqn}.weight"] = weight_int4pack.to(self.device)
+                cur_state_dict[f"{fqn}.scales_and_zeros"] = scales_and_zeros.to(self.device)
+        return cur_state_dict
 
 def _replace_linear_int4(
     module: torch.nn.Module,
@@ -114,6 +184,27 @@ def _replace_linear_int4(
                 copy_weights,
             )
 
+def linear_forward_int4(
+    x: torch.Tensor,
+    weight_int4pack: torch.Tensor,
+    scales_and_zeros: torch.Tensor,
+    out_features: int,
+    groupsize: int,
+    precision: torch.dtype = torch.bfloat16,
+    scales_precision: torch.dtype = torch.bfloat16,
+):
+    origin_x_size = x.size()
+    x = x.reshape(-1, origin_x_size[-1])
+    c = torch.ops.aten._weight_int4pack_mm(
+        x.to(precision),
+        weight_int4pack,
+        groupsize,
+        scales_and_zeros.to(scales_precision)
+    ).to(dtype=x.dtype)
+    new_shape = origin_x_size[:-1] + (out_features,)
+    c = c.reshape(new_shape)
+    return c
+
 def _replace_linear_8da4w(
     module: torch.nn.Module,
     groupsize: int,
@@ -148,6 +239,22 @@ def _replace_linear_8da4w(
         return new_linear
 
     _replace_with_custom_fn_if_matches_filter(module, replacement_fn, filter_fn)
+
+def replace_linear_8da4w(
+    module: torch.nn.Module,
+    groupsize: int,
+    padding_allowed: bool,
+    precision: torch.dtype,
+    scales_precision: torch.dtype,
+):
+    _replace_linear_8da4w(
+        module,
+        groupsize,
+        padding_allowed,
+        precision,
+        scales_precision,
+        Int8DynActInt4WeightLinear,
+    )
 
 def linear_forward_8da4w(
     x,
@@ -187,27 +294,6 @@ def linear_forward_8da4w(
     # new_shape = origin_x_size[:-1] + (out_features,)
     # c = c.reshape(new_shape)
 
-    return c
-
-def linear_forward_int4(
-    x: torch.Tensor,
-    weight_int4pack: torch.Tensor,
-    scales_and_zeros: torch.Tensor,
-    out_features: int,
-    groupsize: int,
-    precision: torch.dtype = torch.bfloat16,
-    scales_precision: torch.dtype = torch.bfloat16,
-):
-    origin_x_size = x.size()
-    x = x.reshape(-1, origin_x_size[-1])
-    c = torch.ops.aten._weight_int4pack_mm(
-        x.to(precision),
-        weight_int4pack,
-        groupsize,
-        scales_and_zeros.to(scales_precision)
-    ).to(dtype=x.dtype)
-    new_shape = origin_x_size[:-1] + (out_features,)
-    c = c.reshape(new_shape)
     return c
 
 
@@ -294,3 +380,88 @@ class Int8DynActInt4WeightLinear(torch.nn.Module):
             self.groupsize,
             self.precision,
         )
+
+class Int8DynActInt4WeightQuantizer(Quantizer):
+    def __init__(
+        self,
+        groupsize: int = 256,
+        padding_allowed: bool = False,
+        precision: torch.dtype = torch.float32,
+        scales_precision: torch.dtype = torch.float32,
+        device: torch.device = torch.device("cpu"),
+        mapping_type: MappingType = MappingType.SYMMETRIC
+    ) -> None:
+        super().__init__()
+        self.groupsize: int = groupsize
+        self.padding_allowed: bool = padding_allowed
+        self.precision: torch.dtype = precision
+        self.scales_precision: torch.dtype = scales_precision
+        self.device: torch.device = device
+        self.mapping_type: MappingType = mapping_type
+
+    @torch.no_grad()
+    def _create_quantized_state_dict(
+        self, model: torch.nn.Module
+    ) -> Dict[str, torch.Tensor]:
+        cur_state_dict = model.state_dict()
+        for fqn, mod in model.named_modules():
+            if isinstance(mod, torch.nn.Linear):
+                assert not mod.bias
+                out_features = mod.out_features
+                in_features = mod.in_features
+                # assert out_features % 8 == 0, "require out_features % 8 == 0"
+                logging.info(f"linear: {fqn}, in={in_features}, out={out_features}")
+
+                assert (
+                    in_features % self.groupsize == 0
+                ), f"require in_features:{in_features} % self.groupsize:{self.groupsize} == 0"
+
+                weight = mod.weight.data
+                if not _check_linear_int4_k(in_features, self.groupsize):
+                    if self.padding_allowed:
+                        from .utils import find_multiple
+                        import torch.nn.functional as F
+                        logging.warn(f"warning: {fqn} is padded to satisfy in_features % 1024 == 0")
+                        padded_in_features = find_multiple(in_features, 1024)
+                        weight = F.pad(weight, pad=(0, padded_in_features - in_features))
+                    else:
+                        logging.warn(f"warning: {fqn} is skipped, int4 requires that in_features is 32, 64, or is divisible by 1024, " +
+                              "and that groupsize and inner_k_tiles*16 evenly divide into it")
+                        continue
+                (
+                    weight_int8,
+                    scales,
+                    zeros,
+                ) = group_quantize_tensor_symmetric(
+                    weight.to(self.precision),
+                    4,  # n_bit
+                    self.groupsize,
+                    self.scales_precision,
+                    mapping_type=self.mapping_type
+                )
+                cur_state_dict[f"{fqn}.weight"] = weight_int8.to(self.device)
+                cur_state_dict[f"{fqn}.scales"] = scales.to(self.device)
+                cur_state_dict[f"{fqn}.zeros"] = zeros.to(self.device)
+                # TODO: support bias?
+
+        return cur_state_dict
+
+    def _convert_for_runtime(self, model: torch.nn.Module) -> torch.nn.Module:
+        replace_linear_8da4w(
+            model,
+            self.groupsize,
+            self.padding_allowed,
+            self.precision,
+            # TODO: this should be self.scales_precision?
+            self.precision,
+        )
+        return model
+
+    def quantize(
+        self, model: torch.nn.Module, *args: Any, **kwargs: Any
+    ) -> torch.nn.Module:
+        state_dict = self._create_quantized_state_dict(model)
+        model = self._convert_for_runtime(model)
+        # TODO: make it strict
+        model.load_state_dict(state_dict, strict=False)
+        return model
