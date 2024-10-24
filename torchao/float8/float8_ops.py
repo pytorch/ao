e0f3fffa60abd2142d262eb09539ba0010913356
jainapurva <apurvajain.kota@gmail.com>
2024-10-24 10:37:31 -0700

    Call inline

--- torchao/float8/float8_ops.py
+++ torchao/float8/float8_ops.py
@@ -289,8 +289,8 @@ def float8_mm(aten_op, args, kwargs=None):
         b._linear_mm_config,
     )
     if scaled_mm_config.emulate:
-        return torch.ops.aten.mm_float8_emulated(
-            a._data, a._scale, b._data, b._scale, output_dtype
+        return torch.mm(a._data.float() / a._scale, b._data.float() / b._scale).to(
+            output_dtype
         )
     tensor_out = addmm_float8_unwrapped(
         a_data,
@@ -325,8 +325,8 @@ def float8_addmm(aten_op, args, kwargs=None):
         b._linear_mm_config,
     )
     if scaled_mm_config.emulate:
-        out = torch.ops.aten.mm_float8_emulated(
-            a._data, a._scale, b._data, b._scale, output_dtype
+        out = torch.mm(a._data.float() / a._scale, b._data.float() / b._scale).to(
+            output_dtype
         )
         return out + bias
     tensor_out = addmm_float8_unwrapped(
@@ -483,36 +483,3 @@ def copy_fp8(aten_op, args, kwargs=None):
         )
     else:
         raise RuntimeError("Unsupported semantics for copy_ in Float8Tensor")
-
-
-def mm_float8_emulated(
-    m1,  # input 1 data
-    s1,  # input 1 scale
-    m2,  # input 2 data
-    s2,  # input 2 scale
-    dtype3,  # output dtype
-):
-    # naive implementation: dq -> op -> q
-    m1_fp32 = m1.float() / s1
-    m2_fp32 = m2.float() / s2
-    m3_fp32 = torch.mm(m1_fp32, m2_fp32)
-
-    return m3_fp32.to(dtype3)
-
-
-# ATen op placeholders
-# Register the aten level functions we need.
-# These are mostly placeholder and might need to be implemented in c++ as needed
-lib = Library("aten", "FRAGMENT")
-
-lib.define(
-    "mm_float8_emulated(Tensor m1, Tensor s1, Tensor m2, Tensor s2, ScalarType dtype3) -> Tensor"
-)
-lib.impl("mm_float8_emulated", mm_float8_emulated, "CPU")
-lib.impl("mm_float8_emulated", mm_float8_emulated, "CUDA")
-
-
-@torch.library.impl(lib, "mm_float8_emulated", "Meta")
-def _mm_float8_emulated_meta(m1, s1, m2, s2, dtype3):
-    out = torch.mm(m1.float(), m2.float()).to(dtype3)
-    return out
