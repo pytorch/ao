cc56c94b4d37cc8d54311d8b96224e5bc19355f4
drisspg <drisspguessous@gmail.com>
2024-10-25 10:01:20 -0700

    add to safe globals
    stack-info: PR: https://github.com/pytorch/ao/pull/1171, branch: drisspg/stack/15

--- torchao/float8/__init__.py
+++ torchao/float8/__init__.py
@@ -11,7 +11,7 @@ from torchao.float8.config import (
     Float8LinearConfig,
     ScalingType,
 )
-from torchao.float8.float8_linear import Float8Linear
+from torchao.float8.float8_linear import Float8Linear, WeightWithDelayedFloat8CastTensor
 from torchao.float8.float8_linear_utils import (
     convert_to_float8_training,
     linear_requires_sync,
@@ -32,7 +32,17 @@ from torchao.utils import TORCH_VERSION_AT_LEAST_2_5
 if TORCH_VERSION_AT_LEAST_2_5:
     # Needed to load Float8Tensor with weights_only = True
     from torch.serialization import add_safe_globals
-    add_safe_globals([Float8Tensor, ScaledMMConfig, GemmInputRole, LinearMMConfig, Float8MMConfig])
+
+    add_safe_globals(
+        [
+            Float8Tensor,
+            ScaledMMConfig,
+            GemmInputRole,
+            LinearMMConfig,
+            Float8MMConfig,
+            WeightWithDelayedFloat8CastTensor,
+        ]
+    )
 
 __all__ = [
     # configuration
