4b563f28324b50da6fa95caa6c085441bcd7638f
andrewor14 <andrewor14@gmail.com>
2024-10-24 20:08:49 -0400

    Fix int8_weight_only group_size (#1165)
    group_size was never read.

--- torchao/quantization/quant_api.py
+++ torchao/quantization/quant_api.py
@@ -578,8 +578,7 @@ def int8_weight_only(group_size=None):
         zero_point_dtype = torch.int64
         if group_size is None:
             group_size = weight.shape[1]
-
-        block_size = (1, weight.shape[1])
+        block_size = (1, group_size)
         return to_affine_quantized_intx(weight, mapping_type, block_size, target_dtype, eps=eps, zero_point_dtype=zero_point_dtype)
 
     return _get_linear_subclass_inserter(apply_int8wo_quant, group_size=group_size)
