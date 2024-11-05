c5c562287bbeb2bf8d34d979a2405484f3759117
Christian Puhrsch <cpuhrsch@fb.com>
2024-11-05 11:48:23 -0800

    revert another file

--- torchao/_models/sam2/modeling/sam2_base.py
+++ torchao/_models/sam2/modeling/sam2_base.py
@@ -467,7 +467,6 @@ class SAM2Base(torch.nn.Module):
     def forward_image(self, img_batch: torch.Tensor):
         """Get the image feature on the input batch."""
         backbone_out = self.image_encoder(img_batch)
-        # import pdb; pdb.set_trace()
         if self.use_high_res_features_in_sam:
             # precompute projected level 0 and level 1 features in SAM decoder
             # to avoid running it again on every SAM click
