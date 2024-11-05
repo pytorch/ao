c2bf92f667ca78430b34c46643d9b5648be3533f
Christian Puhrsch <cpuhrsch@fb.com>
2024-11-05 11:47:20 -0800

    revert two files

--- torchao/_models/sam2/modeling/sam/prompt_encoder.py
+++ torchao/_models/sam2/modeling/sam/prompt_encoder.py
@@ -13,6 +13,7 @@ from torchao._models.sam2.modeling.position_encoding import PositionEmbeddingRan
 
 from torchao._models.sam2.modeling.sam2_utils import LayerNorm2d
 
+
 class PromptEncoder(nn.Module):
     def __init__(
         self,
