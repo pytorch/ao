10fc46092858d9d1ea0be8f53c1153fe2d25cd0e
Diogo-V <diogo.venancio00@gmail.com>
2024-11-07 14:16:36 +0000

    feat: wip - update eval script

--- eval_marlin.py
+++ eval_marlin.py
@@ -8,150 +8,16 @@ from safetensors.torch import load_file  # Import safetensors loader
 
 from torchao._models.llama.model import prepare_inputs_for_model
 from torchao.quantization import int4_weight_only, quantize_
+from torchao._models._eval import MultiTensorInputRecorder
 from torchao.dtypes import MarlinSparseLayout
 from torchao.sparsity.sparse_api import apply_fake_sparsity
 from torchao.quantization.GPTQ_MT import Int4WeightOnlyGPTQQuantizer, MultiTensor
-
-import lm_eval
-from lm_eval.evaluator import evaluate
-from lm_eval.models.huggingface import HFLM as eval_wrapper
-from lm_eval.tasks import get_task_dict
-
 
 # ENVS
 model_name = 'meta-llama/Meta-Llama-3-8B'
 model_name = 'TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T'
 device = "cuda"
 calibration_limit = 4
-
-class InputRecorder(eval_wrapper):
-    def __init__(
-        self,
-        tokenizer,
-        calibration_seq_length,
-        input_prep_func=None,
-        pad_calibration_inputs=False,
-        vocab_size=32000,
-        pad_token=0,
-        device="cpu",
-    ):
-        try:
-            super().__init__()
-        except TypeError:
-            # lm_eval 0.4.2 removed the default init
-            super().__init__("gpt2", device="cpu")
-
-        self.tokenizer = tokenizer
-        self._device = torch.device(device)
-        self.vocab_size = vocab_size
-        self._max_seq_length = calibration_seq_length
-        self.calibration_seq_length = calibration_seq_length
-
-        self.input_prep_func = (
-            input_prep_func if input_prep_func is not None
-            else lambda x: (x,)
-        )
-
-        self.pad_calibration_inputs = pad_calibration_inputs
-        self.pad_token = pad_token
-
-        self.inputs = []
-
-    @property
-    def eot_token_id(self):
-        try:
-            return self.tokenizer.eos_id()
-        except:
-            return self.tokenizer.eos_id
-
-    @property
-    def max_length(self):
-        return self._max_seq_length
-
-    @property
-    def max_gen_toks(self):
-        return 50
-
-    @property
-    def batch_size(self):
-        return 1
-
-    @property
-    def device(self):
-        return self._device
-
-    def tok_encode(self, string: str, **kwargs):
-        tokens = self.tokenizer.encode(string)
-        if hasattr(self.tokenizer, "bos_id"):
-            try:
-                tokens = [self.tokenizer.bos_id()] + tokens
-            except:
-                tokens = [self.tokenizer.bos_id] + tokens
-        return tokens
-
-    def tok_decode(self, tokens):
-        decoded = self.tokenizer.decode(tokens)
-        return decoded
-
-    def add_input(self, args):
-        self.inputs.append(args)
-
-    def record_inputs(
-        self,
-        calibration_tasks,
-        calibration_limit,
-    ):
-        try:
-            lm_eval.tasks.initialize_tasks()
-        except:
-            pass
-
-        task_dict = get_task_dict(calibration_tasks)
-        print("Obtaining GPTQ calibration inputs on: ", calibration_tasks)
-
-        evaluate(
-            self,
-            task_dict,
-            limit=calibration_limit,
-        )
-        return self
-
-    def get_inputs(self):
-        return self.inputs
-
-    def _model_call(self, inps):
-        inps = inps.squeeze(0)
-        T = len(inps)
-        if (
-            # can't use inputs that are too short when padding disabled
-            (T < self.calibration_seq_length and not self.pad_calibration_inputs)
-            or
-            # can't use inputs that actually use token we use for padding
-            (self.pad_calibration_inputs and self.pad_token in inps)
-        ):
-            # give random output
-            return torch.randn(
-                (1, T, self.vocab_size), dtype=torch.bfloat16, device=self._device
-            )
-
-        # pad or truncate to the right size
-        if T >= self.calibration_seq_length:
-            inps = inps[: self.calibration_seq_length]
-        else:
-            inps = F.pad(inps, (self.pad_token, self.calibration_seq_length - T))
-
-        inps = inps.unsqueeze(0)
-        model_in = self.input_prep_func(inps)
-
-        self.add_input(model_in)
-
-        # output `something` with correct shape to keep eval going
-        return torch.randn(
-            (1, T, self.vocab_size), dtype=torch.bfloat16, device=self._device
-        )
-
-    def _model_generate(self, context, max_length, eos_token_id):
-        raise Exception("unimplemented")
 
 
 def get_model_and_tokenizer(precision: torch.dtype = torch.bfloat16):
@@ -203,35 +69,26 @@ def baseline(model, tokenizer):
 
 
 def int4wo_gptq(model, tokenizer):
-    blocksize = 128
-    percdamp = 0.01
-    groupsize = 64
+    groupsize = 128
     calibration_tasks = ["wikitext"]
     calibration_seq_length = 100
-    input_prep_func = prepare_inputs_for_model
     pad_calibration_inputs = False
     print("Recording inputs")
-    inputs = InputRecorder(
-            tokenizer,
-            calibration_seq_length,
-            input_prep_func,
-            pad_calibration_inputs,
-            model.config.vocab_size,
-            device="cpu",
-        ).record_inputs(
-            calibration_tasks,
-            calibration_limit,
-        ).get_inputs()
-    print("Inputs recorded")
-    quantizer = Int4WeightOnlyGPTQQuantizer(
-            blocksize,
-            percdamp,
-            groupsize,
-        )
+    inputs = MultiTensorInputRecorder(
+        tokenizer,
+        calibration_seq_length,
+        prepare_inputs_for_model,
+        pad_calibration_inputs,
+        model.config.vocab_size,
+        device="cpu"
+    ).record_inputs(
+        calibration_tasks,
+        calibration_limit,
+    ).get_inputs()
     
-    multi = [MultiTensor([ inp for inp, _ in inputs]), MultiTensor([ inds for _, inds in inputs])]
     print("Quantizing model")
-    model = quantizer.quantize(model, multi).cuda()
+    quantizer = Int4WeightOnlyGPTQQuantizer(group_size=groupsize, device=device)
+    model = quantizer.quantize(model, inputs).to(device)
     print("Model quantized")
     
     return model
