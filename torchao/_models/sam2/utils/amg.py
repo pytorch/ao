d2d44614cf629cbf6c093074f0bd0ca7174095c7
Christian Puhrsch <cpuhrsch@fb.com>
2024-11-13 20:19:15 -0800

    Use compile for calculate_stability_score

--- torchao/_models/sam2/utils/amg.py
+++ torchao/_models/sam2/utils/amg.py
@@ -212,6 +212,7 @@ def area_from_rle(rle: Dict[str, Any]) -> int:
     return sum(rle["counts"][1::2])
 
 
+@torch.compile(fullgraph=True, dynamic=True)
 def calculate_stability_score(
     masks: torch.Tensor, mask_threshold: float, threshold_offset: float
 ) -> torch.Tensor:
