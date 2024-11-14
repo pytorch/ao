06e69f6a9337b0a984c41a36001b7f58ab594ff3
HandH1998 <1335248067@qq.com>
2024-11-14 12:15:55 +0800

    support W4A8 Marlin kernel (#1113)
    support Marlin W4A8 kernel

--- torchao/dtypes/__init__.py
+++ torchao/dtypes/__init__.py
@@ -3,6 +3,7 @@ from .affine_quantized_tensor import (
     Float8AQTTensorImpl,
     Float8Layout,
     Layout,
+    MarlinQQQLayout,
     MarlinSparseLayout,
     PlainLayout,
     SemiSparseLayout,
@@ -13,6 +14,7 @@ from .affine_quantized_tensor import (
     to_affine_quantized_fpx,
     to_affine_quantized_intx,
     to_affine_quantized_intx_static,
+    to_marlinqqq_quantized_intx,
 )
 from .nf4tensor import NF4Tensor, to_nf4
 
@@ -29,6 +31,7 @@ __all__ = [
     "to_affine_quantized_fpx",
     "to_affine_quantized_floatx",
     "to_affine_quantized_floatx_static",
+    "to_marlinqqq_quantized_intx",
     "Layout",
     "PlainLayout",
     "SemiSparseLayout",
@@ -36,4 +39,5 @@ __all__ = [
     "Float8Layout",
     "Float8AQTTensorImpl",
     "MarlinSparseLayout",
+    "MarlinQQQLayout",
 ]
