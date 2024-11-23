06fc16c97d540eb6143465e72e1c78017fc54329
Christian Puhrsch <cpuhrsch@fb.com>
2024-11-22 22:36:20 -0800

    cli, automatic downlaods and other QoL improvements

--- examples/sam2_amg_server/cli.py
+++ examples/sam2_amg_server/cli.py
@@ -3,21 +3,32 @@ import logging
 import matplotlib.pyplot as plt
 from server import file_bytes_to_image_tensor
 from server import show_anns
+from server import model_type_to_paths
+from server import MODEL_TYPES_TO_MODEL
 from torchao._models.sam2.build_sam import build_sam2
 from torchao._models.sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
 from torchao._models.sam2.utils.amg import rle_to_mask
 from io import BytesIO
 
-def main(checkpoint_path, input_path, output_path, points_per_batch=1024):
+def main_docstring():
+    return f"""
+    Args:
+        checkpoint_path (str): Path to folder containing checkpoints from https://github.com/facebookresearch/sam2?tab=readme-ov-file#download-checkpoints
+        model_type (str): Choose from one of {", ".join(MODEL_TYPES_TO_MODEL.keys())}
+        input_path (str): Path to input image
+        output_path (str): Path to output image
+    """
+
+def main(checkpoint_path, model_type, input_path, output_path, points_per_batch=1024, output_format='png', verbose=False):
     device = "cuda"
-    from pathlib import Path
-    sam2_checkpoint = Path(checkpoint_path) / Path("sam2.1_hiera_large.pt")
-    model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
-    
-    logging.info(f"Loading model {sam2_checkpoint} with config {model_cfg}")
+    sam2_checkpoint, model_cfg = model_type_to_paths(checkpoint_path, model_type)
+    if verbose:
+        print(f"Loading model {sam2_checkpoint} with config {model_cfg}")
     sam2 = build_sam2(model_cfg, sam2_checkpoint, device=device, apply_postprocessing=False)
     mask_generator = SAM2AutomaticMaskGenerator(sam2, points_per_batch=points_per_batch, output_mode="uncompressed_rle")
     image_tensor = file_bytes_to_image_tensor(bytearray(open(input_path, 'rb').read()))
+    if verbose:
+        print(f"Loaded image of size {tuple(image_tensor.shape)} and generating mask.")
     masks = mask_generator.generate(image_tensor)
     
     # Save an example
@@ -27,11 +38,11 @@ def main(checkpoint_path, input_path, output_path, points_per_batch=1024):
     plt.axis('off')
     plt.tight_layout()
     buf = BytesIO()
-    plt.savefig(buf, format='png')
+    plt.savefig(buf, format=output_format)
     buf.seek(0)
     with open(output_path, "wb") as file:
         file.write(buf.getvalue())
-    print(f"Wrote output image to {output_path}")
 
+main.__doc__ = main_docstring()
 if __name__ == "__main__":
     fire.Fire(main)
