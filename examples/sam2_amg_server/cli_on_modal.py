81d7afa6589c9a7a829c8ff59960086379f4d0de
Christian Puhrsch <cpuhrsch@meta.com>
2024-11-25 20:51:17 -0800

    cleanup

--- examples/sam2_amg_server/cli_on_modal.py
+++ examples/sam2_amg_server/cli_on_modal.py
@@ -2,17 +2,9 @@ from pathlib import Path
 
 import modal
 
-
 app = modal.App("torchao-sam-2-cli")
 
-
-REPO_ROOT = Path(__file__).parent
 TARGET = "/root/"
-
-N_H100 = 8
-
-COMMIT_SHA = "cbc099dd73291fbd51f08b7b6f9360420f511890"
-SCRIPT_URL = f"https://raw.githubusercontent.com/KellerJordan/modded-nanogpt/{COMMIT_SHA}/train_gpt2.py"
 
 image = (
     modal.Image.debian_slim(python_version="3.12.7")
@@ -60,16 +52,10 @@ checkpoints = modal.Volume.from_name("checkpoints", create_if_missing=True)
     },
     timeout=60 * 60,
 )
-def train(input_bytes):
+def eval(input_bytes, fast, furious):
     import torch
     import torchao
     import os
-
-    # module_path = torchao.__file__
-    # print(f"The path to the torchao module is: {module_path}")
-    # for root, dirs, files in os.walk(os.path.dirname(module_path)):
-    #     for file in files:
-    #         print(os.path.join(root, file))
 
     import subprocess
     from pathlib import Path
@@ -84,20 +70,6 @@ def train(input_bytes):
     download_file("https://raw.githubusercontent.com/pytorch/ao/refs/heads/climodal1/examples/sam2_amg_server/server.py", "server.py")
     # Create a Path object for the current directory
     current_directory = Path('.')
-    # List all files and directories in the current directory
-    current_directory_contents = current_directory.iterdir()
-    # Print the contents
-    for item in current_directory_contents:
-        print(item)
-
-    from torchao import _models
-    print(dir(_models))
-    from torchao._models import sam2
-    print(dir(sam2))
-    # from torchao._models.sam2 import configs
-    # print(dir(configs))
-    print("torch.__version__")
-    print(torch.__version__)
 
     with open('/tmp/dog.jpg', 'wb') as file:
         file.write(input_bytes)
@@ -110,13 +82,13 @@ def train(input_bytes):
              input_path="/tmp/dog.jpg",
              output_path="/tmp/dog_masked_2.png",
              verbose=True,
-             fast=False,
-             furious=True)
+             fast=fast,
+             furious=furious)
           
     return bytearray(open('/tmp/dog_masked_2.png', 'rb').read())
 
 @app.local_entrypoint()
-def main(input_path, output_path):
-    bytes = train.remote(open(input_path, 'rb').read())
+def main(input_path, output_path, fast=False, furious=False):
+    bytes = eval.remote(open(input_path, 'rb').read(), fast, furious)
     with open(output_path, "wb") as file:
         file.write(bytes)
