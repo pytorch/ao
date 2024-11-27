db05adf4b3c6d72721eaf152aa41c9d35dcecc3c
HDCharles <39544797+HDCharles@users.noreply.github.com>
2024-11-27 00:31:36 -0500

    testing build linux wheels
    https://github.com/pytorch/test-infra/pull/5986 and https://github.com/pytorch/pytorch/pull/141565 changed back to manylinux 2014 for versions of cuda/cpu, testing that this fixes the issue since hte actual error seems to be occuring in the Build the wheel step before hte post script gets run:
    
    https://github.com/pytorch/ao/actions/runs/12041427944/job/33573281510

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -13,7 +13,7 @@ if [[ "$CU_VERSION" == cu* ]]; then
     WHEEL_NAME=$(ls dist/)
 
     pushd dist
-    manylinux_plat=manylinux_2_28_x86_64
+    manylinux_plat=manylinux2014_x86_64
     auditwheel repair --plat "$manylinux_plat" -w . \
     --exclude libtorch.so \
     --exclude libtorch_python.so \
