b92baa172e4675ea9f223147092eec023ae4ab53
DerekLiu35 <derekliu2021@gmail.com>
2024-12-13 18:13:09 -0500

    Update __init__.py

--- torchao/prototype/quantization/codebook/__init__.py
+++ torchao/prototype/quantization/codebook/__init__.py
@@ -3,10 +3,11 @@ from .codebook_ops import (
     dequantize_codebook,
     quantize_codebook,
 )
-from .codebook_quantized_tensor import CodebookQuantizedTensor
+from .codebook_quantized_tensor import CodebookQuantizedTensor, codebook_weight_only
 
 __all__ = [
     "CodebookQuantizedTensor",
+    "codebook_weight_only",
     "quantize_codebook",
     "dequantize_codebook",
     "choose_qparams_codebook",
