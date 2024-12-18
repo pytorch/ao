590ea1ee62e88a9d0a77d77510981748e4907e7d
Daniel Vega-Myhre <danvm@fb.com>
2024-12-18 09:53:37 -0800

    unit test for float8nocompile

---
+++ torchao/prototype/float8nocompile/test/test.py
@@ -0,0 +1,74 @@
+import pytest
+import torch
+import torch.nn as nn
+
+from torchao.float8.float8_linear_utils import convert_to_float8_training
+from torchao.prototype.float8nocompile.float8nocompile_linear_utils import (
+    convert_to_float8_nocompile_training,
+)
+from torchao.utils import TORCH_VERSION_AT_LEAST_2_5
+
+if not TORCH_VERSION_AT_LEAST_2_5:
+    raise AssertionError("torchao.float8 requires PyTorch version 2.5 or greater")
+
+
+class TestModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(32, 64, bias=False),
+            nn.Linear(64, 32, bias=False),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.layers(x)
+
+
+@pytest.fixture
+def model1():
+    torch.manual_seed(0)
+    return TestModel()
+
+
+@pytest.fixture
+def model2():
+    torch.manual_seed(0)
+    return TestModel()
+
+
+def test_model_weights_and_gradients(model1, model2):
+    assert torch.cuda.is_available()
+    device = torch.device("cuda")
+
+    model1.to(torch.bfloat16).to(device)
+    model2.to(torch.bfloat16).to(device)
+
+    # swap nn.Linear layers with Float8LinearNoCompile layers in model1 only
+    convert_to_float8_nocompile_training(model1)
+
+    input_data = torch.randn(16, 32, dtype=torch.bfloat16).to(device)
+    optim1 = torch.optim.SGD(model1.parameters(), lr=0.01)
+    optim2 = torch.optim.SGD(model2.parameters(), lr=0.01)
+    loss_fn = nn.MSELoss()
+
+    for i in range(1):
+        print(f"step {i}")
+        optim1.zero_grad()
+        optim2.zero_grad()
+
+        output1 = model1(input_data)
+        output2 = model2(input_data)
+
+        loss1 = loss_fn(output1, torch.zeros_like(output1))
+        loss2 = loss_fn(output2, torch.zeros_like(output2))
+
+        loss1.backward()
+        loss2.backward()
+
+        optim1.step()
+        optim2.step()
+
+        # compare the weights and gradients of both models
+        for param1, param2 in zip(model1.parameters(), model2.parameters()):
+            assert torch.allclose(param1.data, param2.data, atol=1e-3, rtol=1e-3)
+            assert torch.allclose(param1.grad, param2.grad, atol=1e-3, rtol=1e-3)
