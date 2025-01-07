43e20616870dd3b3491a71b5e4a3b3c176e57544
jainapurva <apurvajain.kota@gmail.com>
2025-01-07 13:48:36 -0800

    Torchao folder linted

--- torchao/__init__.py
+++ torchao/__init__.py
@@ -1,28 +1,33 @@
-import torch
 import logging
 
 # torch/nested/_internal/nested_tensor.py:417: UserWarning: Failed to initialize NumPy: No module named 'numpy'
 import warnings
-warnings.filterwarnings("ignore", message="Failed to initialize NumPy: No module named 'numpy'")
+
+import torch
+
+warnings.filterwarnings(
+    "ignore", message="Failed to initialize NumPy: No module named 'numpy'"
+)
 
 
 # We use this "hack" to set torchao.__version__ correctly
 # the version of ao is dependent on environment variables for multiple architectures
 # For local development this will default to whatever is version.txt
 # For release builds this will be set the version+architecture_postfix
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import PackageNotFoundError, version
+
 try:
     __version__ = version("torchao")
 except PackageNotFoundError:
-    __version__ = 'unknown'  # In case this logic breaks don't break the build
+    __version__ = "unknown"  # In case this logic breaks don't break the build
 
 _IS_FBCODE = (
-    hasattr(torch._utils_internal, "IS_FBSOURCE") and
-    torch._utils_internal.IS_FBSOURCE
+    hasattr(torch._utils_internal, "IS_FBSOURCE") and torch._utils_internal.IS_FBSOURCE
 )
 if not _IS_FBCODE:
     try:
         from pathlib import Path
+
         so_files = list(Path(__file__).parent.glob("_C*.so"))
         assert len(so_files) == 1, f"Expected one _C*.so file, found {len(so_files)}"
         torch.ops.load_library(so_files[0])
@@ -34,14 +39,15 @@ from torchao.quantization import (
     autoquant,
     quantize_,
 )
-from . import dtypes
-from . import testing
+
+from . import dtypes, testing
 
 __all__ = [
     "dtypes",
     "autoquant",
     "quantize_",
     "testing",
+    "ops",
 ]
 
 # test-pytorchbot
