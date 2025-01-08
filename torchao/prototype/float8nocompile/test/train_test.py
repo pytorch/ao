7a44bd942e21702dfc769b76847e8b8851c6856c
Daniel Vega-Myhre <danvm@meta.com>
2025-01-08 07:51:27 -0800

    Update
    [ghstack-poisoned]

--- torchao/prototype/float8nocompile/test/train_test.py
+++ torchao/prototype/float8nocompile/test/train_test.py
@@ -36,7 +36,9 @@ def model2():
     return TestModel()
 
 
-@pytest.mark.parametrize("input_shape", [(16, 32), (1, 16, 32), (2, 16, 32)])
+@pytest.mark.parametrize(
+    "input_shape", [(16, 32), (1, 16, 32), (2, 16, 32), (128, 8192, 32)]
+)
 def test_model_weights_and_gradients(model1, model2, input_shape: tuple[int, int]):
     assert torch.cuda.is_available()
     device = torch.device("cuda")
