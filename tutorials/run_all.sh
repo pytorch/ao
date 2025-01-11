6e9e0c241f8a2cc1b5bc69ef15d0c8751e3fbde4
Jerry Zhang <jerryzh168@gmail.com>
2025-01-10 19:09:04 -0800

    tensor paralell check

--- tutorials/run_all.sh
+++ tutorials/run_all.sh
@@ -11,7 +11,7 @@ find . -type d | while read dir; do
     find "$dir" -maxdepth 1 -name "*.py" | while read file; do
       filename=$(basename "$file")
       echo "filename: $filename"
-      if [ "$filename" = *"tensor_parallel"* ]; then
+      if [[ "$filename" == *"tensor_parallel"* ]]; then
         echo "Running: torchrun --standalone --nnodes=1 --nproc-per-node=1 $file"
         torchrun --standalone --nnodes=1 --nproc-per-node=4 "$file"
         STATUS=$?
@@ -32,7 +32,7 @@ find . -type d | while read dir; do
 done
 
 echo "Failed: $FAILED"
-if (( FAILED == 1 )); then
+if (( $FAILED == 1 )); then
   echo "One or more tests failed"
   exit 1
 else
