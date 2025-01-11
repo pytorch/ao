fbe06b1f2e6c8d19a0bca9b7a7b2b365f57cdf33
Jerry Zhang <jerryzh168@gmail.com>
2025-01-10 19:19:48 -0800

    change tp file check condition

--- tutorials/run_all.sh
+++ tutorials/run_all.sh
@@ -11,7 +11,7 @@ find . -type d | while read dir; do
     find "$dir" -maxdepth 1 -name "*.py" | while read file; do
       filename=$(basename "$file")
       echo "filename: $filename"
-      if [[ "$filename" == *"tensor_parallel"* ]]; then
+      if echo "$filename" | grep -q "tensor_parallel"; then
         echo "Running: torchrun --standalone --nnodes=1 --nproc-per-node=1 $file"
         torchrun --standalone --nnodes=1 --nproc-per-node=4 "$file"
         STATUS=$?
