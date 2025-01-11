2cb6f61d4450ab2ab87d62ed2323912118232397
Jerry Zhang <jerryzh168@gmail.com>
2025-01-10 18:44:54 -0800

    checking status code

--- tutorials/run_all.sh
+++ tutorials/run_all.sh
@@ -3,20 +3,23 @@ FAILED=0
 find . -type d | while read dir; do
   if [ -f "$dir/run.sh" ]; then
     echo "Running: $dir/run.sh"
-    pushd "$dir"
+    CURRENT_DIR=$(pwd)
+    cd "$dir"
     bash run.sh
-    popd
+    cd "$CURRENT_DIR"
   else
     find "$dir" -maxdepth 1 -name "*.py" | while read file; do
       filename=$(basename "$file")
-      if [[ "$filename" == *"tensor_parallel"* ]]; then
+      if [ "$filename" == *"tensor_parallel"* ]; then
         echo "Running: torchrun --standalone --nnodes=1 --nproc-per-node=1 $file"
         torchrun --standalone --nnodes=1 --nproc-per-node=4 "$file"
         STATUS=$?
+        echo "Status: $STATUS"
       else
         echo "Running: python $file"
         python "$file"
         STATUS=$?
+        echo "Status: $STATUS"
       fi
 
       if [ $STATUS -ne 0 ]; then
