eb21c3255a53aabf256ab794a2ee7ba59cda4e21
Jerry Zhang <jerryzh168@gmail.com>
2025-01-10 18:25:44 -0800

    Fix run_tutorials code
    Summary:
    Last script actually has some errors but didn't error out, this PR
    added the logic for the CI job to show error when some job fails
    and also fixed remaining code
    
    Test Plan:
    CI
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- tutorials/run_all.sh
+++ tutorials/run_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+FAILED=0
 find . -type d | while read dir; do
   if [ -f "$dir/run.sh" ]; then
     echo "Running: $dir/run.sh"
@@ -7,13 +8,29 @@ find . -type d | while read dir; do
     popd
   else
     find "$dir" -maxdepth 1 -name "*.py" | while read file; do
-      if [[ "$file" == *"tensor_parallel"* ]]; then
+      filename=$(basename "$file")
+      if [[ "$filename" == *"tensor_parallel"* ]]; then
         echo "Running: torchrun --standalone --nnodes=1 --nproc-per-node=1 $file"
         torchrun --standalone --nnodes=1 --nproc-per-node=4 "$file"
+        STATUS=$?
       else
         echo "Running: python $file"
         python "$file"
+        STATUS=$?
+      fi
+
+      if [ $STATUS -ne 0 ]; then
+        FAILED=1
+        echo "Test failed: $file"
       fi
     done
   fi
 done
+
+if [ $FAILED -eq 1 ]; then
+  echo "One or more tests failed"
+  exit 1
+else
+  echo "All tests passed"
+  exit 0
+fi
