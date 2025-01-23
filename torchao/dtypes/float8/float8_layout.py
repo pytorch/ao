dc97eba708425aaf2d6887472591e9326168c667
Daniel Vega-Myhre <danvm@meta.com>
2025-01-23 10:17:04 -0800

    Update
    [ghstack-poisoned]

--- torchao/dtypes/float8/float8_layout.py
+++ torchao/dtypes/float8/float8_layout.py
@@ -11,17 +11,17 @@ from torchao.dtypes.affine_quantized_tensor import (
     AffineQuantizedTensor,
     register_layout,
 )
-from torchao.dtypes.utils import AQTTensorImpl, Layout, get_out_shape
+from torchao.dtypes.utils import AQTTensorImpl, get_out_shape, Layout
 from torchao.float8.inference import (
-    Float8MMConfig,
     _is_rowwise_scaled,
     addmm_float8_unwrapped_inference,
+    Float8MMConfig,
     preprocess_data,
 )
 from torchao.quantization.quant_primitives import (
-    FP8_TYPES,
     choose_qparams_affine_float8,
     dequantize_affine_float8,
+    FP8_TYPES,
     quantize_affine_float8,
 )
 from torchao.utils import _is_float8_type, fill_defaults
@@ -223,7 +223,7 @@ class Float8QuantizedTensor(AffineQuantizedTensor):
     def dequantize(self, output_dtype: Optional[torch.dtype] = None) -> torch.Tensor:
         if output_dtype is None:
             output_dtype = self.dtype
-        int_data, scale = self.tensor_impl.get_plain()
+        int_data, scale, _ = self.tensor_impl.get_plain()
         return dequantize_affine_float8(
             int_data,
             scale,
