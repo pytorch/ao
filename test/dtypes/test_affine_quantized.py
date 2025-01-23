004014cba4c37e9235fac2c217a626d554088fcc
vasiliy <vasiliy@fb.com>
2025-01-23 10:11:54 -0800

    [wip] configs configs configs!
    Summary:
    
    POC for:
    
    * decoupling configuration from transformation
    * stop passing obscure stateful callables around
    * enable printing of configuration
    * reduce amount of context switching to navigate the logic from `quantize_` to
      quantizing a single module
    
    TODO more polish before wider discussion.
    
    Test Plan:
    
    ```
    pytest test/quantization/test_quant_api.py -s -x -k test_int4_weight_only_numerics
    pytest test/quantization/test_qat.py -s -x -k test_quantize_api_standalone
    pytest test/quantization/test_qat.py -s -x -k test_quantize_api_convert_path
    ```
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 56720183d4530d718a44257ec61110f7a3ffee9f
    ghstack-comment-id: 2607756510
    Pull Request resolved: https://github.com/pytorch/ao/pull/1595

--- test/dtypes/test_affine_quantized.py
+++ test/dtypes/test_affine_quantized.py
@@ -8,6 +8,7 @@ from torch.testing._internal.common_utils import (
     run_tests,
 )
 
+from torchao.core.config import AOBaseConfig
 from torchao.dtypes import CutlassInt4PackedLayout, Int4CPULayout, SemiSparseLayout
 from torchao.quantization import (
     float8_weight_only,
@@ -15,6 +16,7 @@ from torchao.quantization import (
     int8_dynamic_activation_int4_weight,
     int8_dynamic_activation_int8_weight,
     int8_weight_only,
+    quantize_,
 )
 from torchao.quantization.quant_primitives import MappingType, ZeroPointDomain
 from torchao.utils import (
@@ -80,7 +82,8 @@ class TestAffineQuantized(TestCase):
         t = linear.weight
         shape = t.shape
         apply_int4_weight_only_quant = int4_weight_only(group_size=32)
-        ql = apply_int4_weight_only_quant(linear)
+        quantize_(linear, apply_int4_weight_only_quant)
+        ql = linear
         aqt = ql.weight
         aqt_shape = aqt.shape
         self.assertEqual(aqt_shape, shape)
@@ -100,7 +103,11 @@ class TestAffineQuantized(TestCase):
     )
     def test_weights_only(self, apply_quant):
         linear = torch.nn.Linear(128, 256, dtype=torch.bfloat16, device="cuda")
-        ql = apply_quant(linear)
+        if isinstance(apply_quant, AOBaseConfig):
+            quantize_(linear, apply_quant)
+            ql = linear
+        else:
+            ql = apply_quant(linear)
         with tempfile.NamedTemporaryFile() as f:
             torch.save(ql.state_dict(), f)
             f.seek(0)
@@ -178,8 +185,13 @@ class TestAffineQuantized(TestCase):
     )
     @unittest.skipIf(not torch.cuda.is_available(), "Need CUDA available")
     def test_print_quantized_module(self, apply_quant):
+        print(apply_quant)
         linear = torch.nn.Linear(128, 256, dtype=torch.bfloat16, device="cuda")
-        ql = apply_quant(linear)
+        if isinstance(apply_quant, AOBaseConfig):
+            quantize_(linear, apply_quant)
+            ql = linear
+        else:
+            ql = apply_quant(linear)
         assert "AffineQuantizedTensor" in str(ql)
 
 
@@ -193,7 +205,10 @@ class TestAffineQuantizedBasic(TestCase):
         apply_quant_list = get_quantization_functions(False, True, device)
         for apply_quant in apply_quant_list:
             linear = torch.nn.Linear(128, 256, dtype=dtype, device=device)
-            ql = apply_quant(linear)
+            if isinstance(apply_quant, AOBaseConfig):
+                quantize_(linear, apply_quant)
+            else:
+                ql = apply_quant(linear)
             lp_tensor = ql.weight
             tensor_data_name_dict, tensor_attributes = lp_tensor.__tensor_flatten__()
             tensor_data_dict = {
