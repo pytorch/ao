befa425fc4f261c2a6b5f459386d291771f26281
Apurva Jain <apurvajain.kota@gmail.com>
2025-01-24 11:55:13 -0800

    Revert "Move fpx to tensor subclass (#1603)"
    This reverts commit 70be2452f3ae4fbd13ab61609732878baa990c84.

--- torchao/dtypes/__init__.py
+++ torchao/dtypes/__init__.py
@@ -4,14 +4,12 @@ from .affine_quantized_tensor import (
     to_affine_quantized_floatx,
     to_affine_quantized_floatx_static,
     # experimental, will be merged into floatx in the future
+    to_affine_quantized_fpx,
     to_affine_quantized_intx,
     to_affine_quantized_intx_static,
 )
 from .floatx import (
     Float8Layout,
-    FloatxTensor,
-    FloatxTensorCoreLayout,
-    to_affine_quantized_fpx,
 )
 from .nf4tensor import NF4Tensor, to_nf4
 from .uintx import (
@@ -54,6 +52,4 @@ __all__ = [
     "MarlinQQQLayout",
     "Int4CPULayout",
     "CutlassInt4PackedLayout",
-    "FloatxTensor",
-    "FloatxTensorCoreLayout",
 ]
