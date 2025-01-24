70be2452f3ae4fbd13ab61609732878baa990c84
Apurva Jain <apurvajain.kota@gmail.com>
2025-01-24 11:27:48 -0800

    Move fpx to tensor subclass (#1603)

--- torchao/dtypes/floatx/__init__.py
+++ torchao/dtypes/floatx/__init__.py
@@ -1,7 +1,9 @@
 from .float8_layout import Float8Layout
 from .floatx_tensor_core_layout import (
+    FloatxTensor,
     FloatxTensorCoreLayout,
     from_scaled_tc_floatx,
+    to_affine_quantized_fpx,
     to_scaled_tc_floatx,
 )
 
@@ -10,4 +12,6 @@ __all__ = [
     "to_scaled_tc_floatx",
     "from_scaled_tc_floatx",
     "Float8Layout",
+    "to_affine_quantized_fpx",
+    "FloatxTensor",
 ]
