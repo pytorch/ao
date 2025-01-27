09dd63677a071d88ffbf064f4b79130853768cef
Jane (Yuan) Xu <31798555+janeyx99@users.noreply.github.com>
2025-01-27 17:31:24 -0500

    Prepare for -DPy_LIMITED_API flag in pytorch #145764 (#1627)
    * Prepare for enforcement of -DPy_LIMITED_API flag pytorch #145764
    
    * Add the flag now to not regress
    
    * format

--- torchao/csrc/cuda/tensor_core_tiled_layout/tensor_core_tiled_layout.cu
+++ torchao/csrc/cuda/tensor_core_tiled_layout/tensor_core_tiled_layout.cu
@@ -5,7 +5,7 @@
 #include <ATen/cuda/CUDAContext.h>
 #include <ATen/DeviceGuard.h>
 #include <c10/cuda/CUDAGuard.h>
-#include <torch/extension.h>
+#include <torch/library.h>
 
 template <typename U, typename V>
 constexpr __host__ __device__ auto divUp(U a, V b) -> decltype(a + b) {
