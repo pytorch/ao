eaa21f2338fcfa369709422d7a5d8ef3734b37a2
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-02-03 15:16:15 -0800

    up

--- torchao/experimental/ops/linear_8bit_act_xbit_weight/kernel_selector.h
+++ torchao/experimental/ops/linear_8bit_act_xbit_weight/kernel_selector.h
@@ -17,21 +17,68 @@
 #include <optional>
 #include <unordered_map>
 
+#if defined(TORCHAO_ENABLE_KLEIDI)
+#include <torchao/experimental/kernels/cpu/aarch64/kleidi/kai_matmul_clamp_f32_qai8dxp1x8_qsi4c32p4x8_1x4x32_neon_dotprod.h>
+#include <torchao/experimental/kernels/cpu/aarch64/kleidi/kai_matmul_clamp_f32_qai8dxp1x8_qsi4c32p8x8_1x8x32_neon_dotprod.h>
+#if defined (TORCHAO_ENABLE_ARM_I8MM)
+#include <torchao/experimental/kernels/cpu/aarch64/kleidi/kai_matmul_clamp_f32_qai8dxp4x8_qsi4c32p4x8_8x4x32_neon_i8mm.h>
+#include <torchao/experimental/kernels/cpu/aarch64/kleidi/kai_matmul_clamp_f32_qai8dxp4x8_qsi4c32p8x8_4x8x32_neon_i8mm.h>
+#endif  // TORCHAO_ENABLE_ARM_I8MM
+#endif // TORCHAO_ENABLE_KLEIDI
+
+
+// #if defined(TORCHAO_ENABLE_KLEIDI)
+
+// enum kai_kernel_id {
+//     dotprod_1x4x32 = 0,
+//     dotprod_1x8x32,
+//     i8mm_4x8x32,
+//     i8mm_8x4x32
+// };
+
+// #define KAI_GEN_UKERNEL(kernel_ns)                                                       \
+//         namespace kernel = kernel_ns;                                                    \
+//         auto uk = kernel::get_ukernel();                                                 \
+//         config.mr = uk.get_m_step();                                                     \
+//         config.nr = uk.get_n_step();                                                     \
+//         config.activation_data_size_fn = &kernel::activation_data_size;                  \
+//         config.weight_data_size_fn = &kernel::weight_data_size;                          \
+//         config.preferred_activation_data_alignment = kernel::get_preferred_alignement(); \
+//         config.preferred_weight_data_alignment = kernel::get_preferred_alignement();     \
+//         config.prepare_activation_data_fn = &kernel::prepare_activation_data;            \
+//         config.prepare_weight_data_fn = &kernel::prepare_weight_data;                    \
+//         config.kernel_fn = &kernel::kernel;                                              \
+
+// template <kai_kernel_id kernel_id>
+// UKernelConfig get_ukernel_config_kleidi() {
+//     UKernelConfig config;
+// #if defined (TORCHAO_ENABLE_ARM_I8MM)
+//     if constexpr (kernel_id == i8mm_4x8x32) {
+//         KAI_GEN_UKERNEL(torchao::kernels::cpu::aarch64::kleidi::kai_matmul_clamp_f32_qai8dxp_qsi4c32p::neon_i8mm_4x8x32);
+//         return config;
+//     }
+//     if constexpr (kernel_id == i8mm_8x4x32) {
+//         KAI_GEN_UKERNEL(torchao::kernels::cpu::aarch64::kleidi::kai_matmul_clamp_f32_qai8dxp_qsi4c32p::neon_i8mm_8x4x32);
+//         return config;
+//     }
+// #endif // TORCHAO_ENABLE_ARM_I8MM
+//     if constexpr (kernel_id == dotprod_1x8x32) {
+//         KAI_GEN_UKERNEL(torchao::kernels::cpu::aarch64::kleidi::kai_matmul_clamp_f32_qai8dxp_qsi4c32p::neon_dotprod_1x8x32);
+//         return config;
+//     }
+//     KAI_GEN_UKERNEL(torchao::kernels::cpu::aarch64::kleidi::kai_matmul_clamp_f32_qai8dxp_qsi4c32p::neon_dotprod_1x4x32);
+//     return config;
+// }
+
+// #endif // TORCHAO_ENABLE_KLEIDI
+
+
+
 namespace torchao::ops::linear_8bit_act_xbit_weight {
 
 namespace {
-// If needed in future, we can add uarch to the UKernelConfigCacheKey if needed
-using UKernelConfigCacheKey = torchao::ops::PackedWeightsHeader;
-struct UKernelConfigCacheKeyHash {
-  std::size_t operator()(const UKernelConfigCacheKey& k) const {
-    std::size_t hash =  std::hash<int>()(static_cast<int>(k.format));
-    for (int i = 0; i < k.params.size(); i++) {
-      hash ^= std::hash<int>()(k.params[i]);
-    }
-    return hash;
-  }
-};
-using UKernelConfigCacheType = std::unordered_map<UKernelConfigCacheKey, torchao::ops::linear_8bit_act_xbit_weight::UKernelConfig, UKernelConfigCacheKeyHash>;
+using UKernelConfigCacheKey = torchao::ops::PackedWeightsFormat;
+using UKernelConfigCacheType = std::unordered_map<UKernelConfigCacheKey, torchao::ops::linear_8bit_act_xbit_weight::UKernelConfig>;
 }
 
 template <int weight_nbit, bool has_weight_zeros, bool has_bias, bool has_clamp>
@@ -39,19 +86,20 @@ void register_ukernel_config_universal(UKernelConfigCacheType& ukernel_config_ca
   if (!cpuinfo_initialize()) {
     throw std::runtime_error("Failed to initialize cpuinfo!");
   }
-  UKernelConfigCacheKey key = torchao::ops::linear_8bit_act_xbit_weight::get_packed_weights_header_universal(weight_nbit, has_weight_zeros, has_bias, nr, kr);
+  UKernelConfigCacheKey key = torchao::ops::linear_8bit_act_xbit_weight::get_packed_weights_format_universal(weight_nbit, has_weight_zeros, has_bias, nr, kr);
 
   if (cpuinfo_has_arm_neon_dot()) {
     if (nr == 8 && kr == 16) {
+      namespace kernel = torchao::kernels::cpu::aarch64::linear::channelwise_8bit_activation_groupwise_lowbit_weight_1x8x16_f32_neondot;
   ukernel_config_cache[key] = torchao::ops::linear_8bit_act_xbit_weight::UKernelConfig{
-    &torchao::kernels::cpu::aarch64::linear::channelwise_8bit_activation_groupwise_lowbit_weight_1x8x16_f32_neondot::activation_data_size<has_weight_zeros>,
+    &kernel::activation_data_size<has_weight_zeros>,
     /*preferred_activation_data_alignment*/16,
-    &torchao::kernels::cpu::aarch64::linear::channelwise_8bit_activation_groupwise_lowbit_weight_1x8x16_f32_neondot::prepare_activation_data<has_weight_zeros>,
-    &torchao::kernels::cpu::aarch64::linear::channelwise_8bit_activation_groupwise_lowbit_weight_1x8x16_f32_neondot::weight_data_size<weight_nbit, has_weight_zeros, has_bias>,
+    &kernel::prepare_activation_data<has_weight_zeros>,
+    &kernel::weight_data_size<weight_nbit, has_weight_zeros, has_bias>,
     /*preferred_weight_data_alignment*/16,
-    &torchao::kernels::cpu::aarch64::linear::channelwise_8bit_activation_groupwise_lowbit_weight_1x8x16_f32_neondot::prepare_weight_data<weight_nbit, has_weight_zeros, has_bias>,
+    &kernel::prepare_weight_data<weight_nbit, has_weight_zeros, has_bias>,
     /*nr*/8,
-    {{{/*mr*/1, &torchao::kernels::cpu::aarch64::linear::channelwise_8bit_activation_groupwise_lowbit_weight_1x8x16_f32_neondot::kernel<weight_nbit, has_weight_zeros, has_bias, has_clamp>}}}
+    {{{/*mr*/1, &kernel::kernel<weight_nbit, has_weight_zeros, has_bias, has_clamp>}}}
   };
   return;
   }
@@ -60,17 +108,90 @@ void register_ukernel_config_universal(UKernelConfigCacheType& ukernel_config_ca
   throw std::runtime_error("Cannot register ukernel_config for packing format ukernel because no implementation is available on this platform");
 }
 
+template <int weight_nbit, bool has_weight_zeros, bool has_bias>
+void register_ukernel_config_kleidi_ai(UKernelConfigCacheType& ukernel_config_cache, int nr, int kr, int sr) {
+  std::cout << "register_ukernel_config_kleidi_ai" << std::endl;
+  if (!cpuinfo_initialize()) {
+    throw std::runtime_error("Failed to initialize cpuinfo!");
+  }
+
+  // TODO: make better
+  UKernelConfigCacheKey key = torchao::ops::linear_8bit_act_xbit_weight::get_packed_weights_format_kleidi_ai(weight_nbit, has_weight_zeros, has_bias, nr, kr, sr);
+
+  #if defined (TORCHAO_ENABLE_ARM_I8MM)
+  if (cpuinfo_has_arm_i8mm()) {
+    namespace kernel = torchao::kernels::cpu::aarch64::kleidi::kai_matmul_clamp_f32_qai8dxp_qsi4c32p::neon_i8mm_4x8x32;                                                    \
+        auto uk = kernel::get_ukernel();
+        ukernel_config_cache[key] = torchao::ops::linear_8bit_act_xbit_weight::UKernelConfig{
+          &kernel::activation_data_size,
+          kernel::get_preferred_alignement(),
+          &kernel::prepare_activation_data,
+          &kernel::weight_data_size,
+          kernel::get_preferred_alignement(),
+          &kernel::prepare_weight_data,
+          /*nr*/static_cast<int>(uk.get_n_step()),
+          {{{/*mr*/static_cast<int>(uk.get_m_step()), &kernel::kernel}}}
+        };
+    return;
+  }
+  #endif // TORCHAO_ENABLE_ARM_I8MM
+
+
+  if (cpuinfo_has_arm_neon_dot()) {
+        if (nr == 8 && kr == 16 && sr == 2) {
+          namespace kernel = torchao::kernels::cpu::aarch64::kleidi::kai_matmul_clamp_f32_qai8dxp_qsi4c32p::neon_dotprod_1x8x32;
+          auto uk = kernel::get_ukernel();
+          assert (nr == uk.get_nr());
+          assert (kr == uk.get_kr());
+          assert (sr == uk.get_sr());
+          ukernel_config_cache[key] = torchao::ops::linear_8bit_act_xbit_weight::UKernelConfig{
+            &kernel::activation_data_size,
+            kernel::get_preferred_alignement(),
+            &kernel::prepare_activation_data,
+            &kernel::weight_data_size,
+            kernel::get_preferred_alignement(),
+            &kernel::prepare_weight_data,
+            /*nr*/static_cast<int>(uk.get_n_step()),
+            {{{/*mr*/static_cast<int>(uk.get_m_step()), &kernel::kernel}}}
+          };
+          return;
+        }
+
+        if (nr == 4 && kr == 8 && sr == 2) {
+          namespace kernel = torchao::kernels::cpu::aarch64::kleidi::kai_matmul_clamp_f32_qai8dxp_qsi4c32p::neon_dotprod_1x4x32;
+          auto uk = kernel::get_ukernel();
+          assert (nr == uk.get_nr());
+          assert (kr == uk.get_kr());
+          assert (sr == uk.get_sr());
+          ukernel_config_cache[key] = torchao::ops::linear_8bit_act_xbit_weight::UKernelConfig{
+            &kernel::activation_data_size,
+            kernel::get_preferred_alignement(),
+            &kernel::prepare_activation_data,
+            &kernel::weight_data_size,
+            kernel::get_preferred_alignement(),
+            &kernel::prepare_weight_data,
+            /*nr*/static_cast<int>(uk.get_n_step()),
+            {{{/*mr*/static_cast<int>(uk.get_m_step()), &kernel::kernel}}}
+          };
+          return;
+        }
+  }
+
+
+throw std::runtime_error("Cannot register ukernel_config for packing format kleidi_ai because no implementation is available on this platform");
+}
+
 
 template <int weight_nbit, bool has_weight_zeros>
-void register_ukernel_config(UKernelConfigCacheType& ukernel_config_cache, torchao::ops::PackedWeightsHeader header) {
-  auto it = ukernel_config_cache.find(header);
+void register_ukernel_config(UKernelConfigCacheType& ukernel_config_cache, torchao::ops::PackedWeightsFormat format) {
+  auto it = ukernel_config_cache.find(format);
   if (it != ukernel_config_cache.end()) {
     throw std::runtime_error("UKernel config already registered");
   }
   
-  switch (header.format) {
-    case torchao::ops::PackedWeightsFormat::linear_8bit_act_xbit_weight_universal: {
-      auto packing_params = torchao::ops::linear_8bit_act_xbit_weight::get_universal_packing_params(header);
+  switch (format.type) {
+    case torchao::ops::PackedWeightsType::linear_8bit_act_xbit_weight_universal: {
+      auto packing_params = torchao::ops::linear_8bit_act_xbit_weight::get_universal_packing_params(format);
       if (packing_params.weight_nbit != weight_nbit) {
         throw std::runtime_error("Packed weights are not in the expected format");
       }
@@ -84,11 +205,17 @@ void register_ukernel_config(UKernelConfigCacheType& ukernel_config_cache, torch
       }
       break;
     }
+    case torchao::ops::PackedWeightsType::kleidi_ai: {
+      auto packing_params = torchao::ops::linear_8bit_act_xbit_weight::get_kleidi_ai_packing_params(format);
+      assert (packing_params.has_bias == true);
+      register_ukernel_config_kleidi_ai<weight_nbit, has_weight_zeros, /*has_bias*/true>(ukernel_config_cache, packing_params.nr, packing_params.kr, packing_params.sr);
+      break;
+    }
     default:
       throw std::runtime_error("No implementation for packed weights format");
   }
 
-  it = ukernel_config_cache.find(header);
+  it = ukernel_config_cache.find(format);
   if (it == ukernel_config_cache.end()) {
     throw std::runtime_error("UKernel config did not register");
   }
@@ -96,46 +223,49 @@ void register_ukernel_config(UKernelConfigCacheType& ukernel_config_cache, torch
 
 
 template <int weight_nbit, bool has_weight_zeros>
-torchao::ops::linear_8bit_act_xbit_weight::UKernelConfig select_ukernel_config(torchao::ops::PackedWeightsHeader header) {
+torchao::ops::linear_8bit_act_xbit_weight::UKernelConfig select_ukernel_config(torchao::ops::PackedWeightsFormat format) {
   static UKernelConfigCacheType ukernel_config_cache;
 
   // Check cache
-  auto it = ukernel_config_cache.find(header);
+  auto it = ukernel_config_cache.find(format);
   if (it != ukernel_config_cache.end()) {
     std::cout << "UKERNEL CONFIG FROM CACHE: " << std::endl;
     return it->second;
   }
 
   std::cout << "REGISTERING UKERNEL CONFIG: " << std::endl;
-  register_ukernel_config<weight_nbit, has_weight_zeros>(ukernel_config_cache, header);
-  it = ukernel_config_cache.find(header);
+  register_ukernel_config<weight_nbit, has_weight_zeros>(ukernel_config_cache, format);
+  it = ukernel_config_cache.find(format);
   assert(it != ukernel_config_cache.end());  
   auto config = it->second;
   return config;
 }
 
-// TODO: make packing format and header separate concepts
-// Header is a serialized packing format
+// TODO: make packing format and format separate concepts
+// format is a serialized packing format
 template <int weight_nbit, bool has_weight_zeros, bool has_bias>
-torchao::ops::PackedWeightsHeader select_header(std::optional<std::string> target = std::nullopt) {
+torchao::ops::PackedWeightsFormat select_packed_weights_format(std::optional<std::string> target = std::nullopt) {
   if (!cpuinfo_initialize()) {
     throw std::runtime_error("Failed to initialize cpuinfo!");
   }
 
-  // Select KleidiAI header
+  // Select KleidiAI format
+  #if defined(TORCHAO_ENABLE_KLEIDI)
   if (!target || *target == "kleidi_ai") {
     if (weight_nbit == 4 && !has_weight_zeros) {
+      return torchao::ops::linear_8bit_act_xbit_weight::get_packed_weights_format_kleidi_ai(weight_nbit, has_weight_zeros, /*has_bias*/true, /*nr*/8, /*kr*/16, /*sr*/2);
     }
   }
+  #endif // defined(TORCHAO_ENABLE_KLEIDI)
   
-  // Select universal header
+  // Select universal format
   if (!target || *target == "universal") {
     if (cpuinfo_has_arm_neon_dot()) {
-      return torchao::ops::linear_8bit_act_xbit_weight::get_packed_weights_header_universal(weight_nbit, has_weight_zeros, has_bias, /*nr*/8, /*kr*/16, /*version*/1);
+      return torchao::ops::linear_8bit_act_xbit_weight::get_packed_weights_format_universal(weight_nbit, has_weight_zeros, has_bias, /*nr*/8, /*kr*/16, /*version*/1);
     }
   }
 
-  throw std::runtime_error("No header was selected");
+  throw std::runtime_error("No format was selected");
 }
 
 } // namespace torchao::ops::linear_8bit_act_xbit_weight
--- torchao/experimental/ops/linear_8bit_act_xbit_weight/op_linear_8bit_act_xbit_weight-impl.h
+++ torchao/experimental/ops/linear_8bit_act_xbit_weight/op_linear_8bit_act_xbit_weight-impl.h
@@ -69,30 +69,39 @@ Tensor pack_weights_cpu(
   using namespace torchao::ops::linear_8bit_act_xbit_weight;
 
 
-auto header = select_header<weight_nbit, has_weight_zeros, has_bias>();
-auto ukernel_config = select_ukernel_config<weight_nbit, has_weight_zeros>(header);
+auto packed_weights_format = select_packed_weights_format<weight_nbit, has_weight_zeros, has_bias>();
+auto ukernel_config = select_ukernel_config<weight_nbit, has_weight_zeros>(packed_weights_format);
 
   auto pack_weight_tiling_params = get_default_pack_weight_data_tiling_params(
       ukernel_config, n, /*target_panels_per_thread=*/1);
 
-  auto packed_weight_data_size = torchao::ops::PackedWeightsHeader::size() +
+  auto packed_weight_data_size = torchao::ops::PackedWeightsFormat::serialized_size() +
       get_packed_weight_data_size(ukernel_config, n, k, group_size);
   Tensor packed_weights = torch::empty(
       {static_cast<int64_t>(packed_weight_data_size)}, torch::kInt8);
-  header.write(
+  packed_weights_format.serialize(
       packed_weights.mutable_data_ptr<int8_t>());
+
+      // TODO: hack to call Kleidi AI kernel
+      // Need to pipe bias through pack_weight_data_operator
+      float* bias_ptr = nullptr;
+      std::vector<float> bias(n, 0.0);
+      if (packed_weights_format.type == torchao::ops::PackedWeightsType::kleidi_ai) {
+        bias_ptr = bias.data();
+      }
+
   pack_weight_data_operator(
       ukernel_config,
       pack_weight_tiling_params,
       packed_weights.mutable_data_ptr<int8_t>() +
-          torchao::ops::PackedWeightsHeader::size(),
+          torchao::ops::PackedWeightsFormat::serialized_size(),
       n,
       k,
       group_size,
       weight_qvals.const_data_ptr<int8_t>(),
       weight_scales.const_data_ptr<float>(),
       weight_zeros_ptr,
-      /*bias*/ nullptr);
+      bias_ptr);
 
   return packed_weights;
 }
@@ -147,11 +156,11 @@ Tensor pack_weights_meta(
   using namespace torchao::ops::linear_8bit_act_xbit_weight;
 
 
-  auto header = select_header<weight_nbit, has_weight_zeros, has_bias>();
-    auto ukernel_config = select_ukernel_config<weight_nbit, has_weight_zeros>(header);
+  auto packed_weights_format = select_packed_weights_format<weight_nbit, has_weight_zeros, has_bias>();
+    auto ukernel_config = select_ukernel_config<weight_nbit, has_weight_zeros>(packed_weights_format);
 
 
-  auto packed_weight_data_size = torchao::ops::PackedWeightsHeader::size() +
+  auto packed_weight_data_size = torchao::ops::PackedWeightsFormat::serialized_size() +
       get_packed_weight_data_size(ukernel_config, n, k, group_size);
   return torch::empty({static_cast<int64_t>(packed_weight_data_size)})
       .to("meta");
@@ -245,12 +254,12 @@ Tensor linear_out_cpu(
       packed_weights.dtype() == torch::kInt8, "packed_weights must be int8");
 #endif // USE_ATEN
   TORCHAO_CHECK(
-      packed_weights.size(0) >= torchao::ops::PackedWeightsHeader::size(),
-      "packed_weights is not big enough to read the header.");
-  auto header =
-      torchao::ops::PackedWeightsHeader::read(packed_weights.const_data_ptr());
+      packed_weights.size(0) >= torchao::ops::PackedWeightsFormat::serialized_size(),
+      "packed_weights is not big enough to read the packed_weights_format.");
+  auto packed_weights_format =
+      torchao::ops::PackedWeightsFormat::deserialize(packed_weights.const_data_ptr());
 
-  auto ukernel_config = select_ukernel_config<weight_nbit, has_weight_zeros>(header);
+  auto ukernel_config = select_ukernel_config<weight_nbit, has_weight_zeros>(packed_weights_format);
 
   auto linear_tiling_params = get_default_linear_tiling_params(
       ukernel_config,
@@ -282,7 +291,7 @@ Tensor linear_out_cpu(
       k,
       group_size,
       packed_weights.const_data_ptr<int8_t>() +
-          torchao::ops::PackedWeightsHeader::size(),
+          torchao::ops::PackedWeightsFormat::serialized_size(),
       activations.const_data_ptr<float>(),
       // Clamp parameters are ignored because config is created from
       // has_clamp = false
