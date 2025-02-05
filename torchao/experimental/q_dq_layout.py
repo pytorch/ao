b0167de58a15381121bb71d921574681f75ebf0e
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-02-05 15:11:41 -0800

    up

--- torchao/experimental/q_dq_layout.py
+++ torchao/experimental/q_dq_layout.py
@@ -12,7 +12,9 @@ from torchao.dtypes.affine_quantized_tensor import register_layout
 from torchao.dtypes.affine_quantized_tensor_ops import (
     register_aqt_quantized_linear_dispatch,
 )
-
+from torchao.dtypes.affine_quantized_tensor import (
+    AffineQuantizedTensor,
+)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 
@@ -49,27 +51,42 @@ def _linear_check(input_tensor, weight_tensor, bias):
 
 
 def _linear_impl(input_tensor, weight_tensor, bias):
-    x = per_token_dynamic_quant(input_tensor)
+    if isinstance(input_tensor, AffineQuantizedTensor):
+        input_tensor = input_tensor.dequantize()
+    if isinstance(weight_tensor, AffineQuantizedTensor):
+        weight_tensor = weight_tensor.dequantize()
+    return torch.nn.functional.linear(input_tensor, weight_tensor, bias)
 
-    w_int_data = weight_tensor.tensor_impl.int_data
-    w_scale = weight_tensor.tensor_impl.scale
-    w_zero_point = weight_tensor.tensor_impl.zero_point
-    assert len(weight_tensor.block_size) == 2
-    assert weight_tensor.block_size[0] == 1
-    group_size = weight_tensor.block_size[1]
+    # assert isinstance(input_tensor, )
+    # if isinstance(input_tensor, AffineQuantizedTensor):
 
-    w_dq = _quantized_decomposed_dequantize_per_channel_group_wrapper(
-        w_int_data,
-        w_scale,
-        w_zero_point,
-        weight_tensor.quant_min,
-        weight_tensor.quant_max,
-        torch.int8,
-        group_size,
-        torch.float32,
-    )
 
-    return torch.nn.functional.linear(x, w_dq, bias)
+    #         input_tensor = input_tensor.dequantize()
+    #     if isinstance(weight_tensor, AffineQuantizedTensor):
+    #         weight_tensor = weight_tensor.dequantize()
+    #     return torch.nn.functional.linear(input_tensor, weight_tensor, bias)
+
+    # x = per_token_dynamic_quant(input_tensor)
+
+    # w_int_data = weight_tensor.tensor_impl.int_data
+    # w_scale = weight_tensor.tensor_impl.scale
+    # w_zero_point = weight_tensor.tensor_impl.zero_point
+    # assert len(weight_tensor.block_size) == 2
+    # assert weight_tensor.block_size[0] == 1
+    # group_size = weight_tensor.block_size[1]
+
+    # w_dq = _quantized_decomposed_dequantize_per_channel_group_wrapper(
+    #     w_int_data,
+    #     w_scale,
+    #     w_zero_point,
+    #     weight_tensor.quant_min,
+    #     weight_tensor.quant_max,
+    #     torch.int8,
+    #     group_size,
+    #     torch.float32,
+    # )
+
+    # return torch.nn.functional.linear(x, w_dq, bias)
 
 
 register_aqt_quantized_linear_dispatch(
