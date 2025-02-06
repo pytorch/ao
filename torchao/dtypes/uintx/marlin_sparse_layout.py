438f2c5af5e0102027159b54656ce5f273245637
Peter Y. Yeh <pyeh@amd.com>
2025-02-06 10:31:43 -0800

    lint

--- torchao/dtypes/uintx/marlin_sparse_layout.py
+++ torchao/dtypes/uintx/marlin_sparse_layout.py
@@ -195,7 +195,7 @@ class MarlinSparseAQTTensorImpl(AQTTensorImpl):
     def get_plain(self):
         from torchao.sparsity.marlin import (
             unpack_from_marlin_24,
-        )  # avoid circular import
+        )
 
         int_data_expanded, scales_expanded = unpack_from_marlin_24(
             self.int_data,
@@ -220,7 +220,7 @@ class MarlinSparseAQTTensorImpl(AQTTensorImpl):
         from torchao.sparsity.marlin import (
             const,
             pack_to_marlin_24,
-        )  # avoid circular import
+        )
 
         assert isinstance(_layout, MarlinSparseLayout)
 
