19afc8e5ddc4685e26df134e69e0536526e949cc
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-02-07 14:51:13 -0800

    up

--- torchao/experimental/ops/mps/test/test_lowbit.py
+++ torchao/experimental/ops/mps/test/test_lowbit.py
@@ -10,20 +10,19 @@ import unittest
 import torch
 from parameterized import parameterized
 
-libname = "libtorchao_ops_mps_aten.dylib"
-libpath = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "../cmake-out/lib/", libname)
-)
-
 try:
     for nbit in range(1, 8):
         getattr(torch.ops.torchao, f"_linear_fp_act_{nbit}bit_weight")
         getattr(torch.ops.torchao, f"_pack_weight_{nbit}bit")
 except AttributeError:
     try:
+        libname = "libtorchao_ops_mps_aten.dylib"
+        libpath = os.path.abspath(os.path.join(os.path.dirname(__file__), "../cmake-out/lib/", libname))
         torch.ops.load_library(libpath)
-    except:
-        raise RuntimeError(f"Failed to load library {libpath}")
+    except Exception as e:
+        print("FAILED TO LOAD")
+        raise e
+        # raise RuntimeError(f"Failed to load library {libpath}")
     else:
         try:
             for nbit in range(1, 8):
