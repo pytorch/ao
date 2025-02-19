82fa92cc9d2d9c07a0c345e07d8a0c1725131928
Jesse Cai <jcjessecai@gmail.com>
2025-02-18 16:50:34 -0800

    wip

--- torchao/kernel/bsr_triton_ops.py
+++ torchao/kernel/bsr_triton_ops.py
@@ -1,263 +1,29 @@
 # mypy: allow-untyped-decorators
 # mypy: allow-untyped-defs
-import math
 import os
-import weakref
-from functools import lru_cache
 from typing import Optional
 
 import torch
 from torch._dynamo.utils import warn_once
+from torch.sparse._triton_ops_meta import (
+    broadcast_batch_dims,
+    get_meta,
+    launch_kernel,
+    optimize_bsr_dense_addmm,
+    prepare_inputs,
+    ptr_stride_extractor,
+    tile_to_blocksize,
+)
 from torch.utils._triton import has_triton
-
-from torch.sparse._triton_ops_meta import get_meta
-
 
 TORCH_SPARSE_BSR_SCATTER_MM_LRU_CACHE_SIZE = int(
     os.getenv("TORCH_SPARSE_BSR_SCATTER_MM_LRU_CACHE_SIZE", 2)
 )
 
-
-def check(cond, msg):
-    if not cond:
-        raise ValueError(msg)
-
-
-def check_bsr_layout(f_name, t):
-    check(
-        t.layout == torch.sparse_bsr,
-        f"{f_name}(): only BSR sparse format is supported for the sparse argument.",
-    )
-
-
-def check_device(f_name, t, device):
-    check(
-        t.device == device and t.device.type == "cuda",
-        f"{f_name}(): all inputs are expected to be on the same GPU device.",
-    )
-
-
-def check_mm_compatible_shapes(f_name, lhs, rhs):
-    check(
-        lhs.dim() >= 2 and rhs.dim() >= 2,
-        f"{f_name}(): all inputs involved in the matrix product are expected to be at least 2D, "
-        f"but got lhs.dim() == {lhs.dim()} and rhs.dim() == {rhs.dim()}.",
-    )
-
-    _m, kl = lhs.shape[-2:]
-    kr, _n = rhs.shape[-2:]
-
-    check(
-        kl == kr,
-        f"{f_name}(): arguments' sizes involved in the matrix product are not compatible for matrix multiplication, "
-        f"got lhs.shape[-1] == {kl} which is not equal to rhs.shape[-2] == {kr}.",
-    )
-
-
-def check_dtype(f_name, t, dtype, *additional_dtypes):
-    check(
-        t.dtype == dtype
-        and t.dtype
-        in ((torch.half, torch.bfloat16, torch.float) + tuple(*additional_dtypes)),
-        f"{f_name}(): all inputs are expected to be of the same dtype "
-        f"and one of (half, bfloat16, float32) or {additional_dtypes}, "
-        f"but got dtype == {t.dtype}.",
-    )
-
-
-def check_blocksize(f_name, blocksize):
-    assert len(blocksize) == 2
-
-    def is_power_of_two(v):
-        return not (v & (v - 1))
-
-    def is_compatible_blocksize(b):
-        res = True
-        for blocksize in b:
-            # Triton loads only blocks which are at least 16 and powers of 2.
-            res = (blocksize >= 16 and is_power_of_two(blocksize)) and res
-        return res
-
-    check(
-        is_compatible_blocksize(blocksize),
-        f"{f_name}(): sparse inputs' blocksize ({blocksize[0]}, {blocksize[1]}) "
-        "should be at least 16 and a power of 2 in each dimension.",
-    )
-
-
-def make_triton_contiguous(t):
-    """Return input as a triton-contiguous tensor.
-
-    A triton-contiguous tensor is defined as a tensor that has strides
-    with minimal value smaller than or equal to 1.
-
-    While triton kernels support triton-non-contiguous tensors (all
-    strides being greater than 1) arguments, a considerable slow-down
-    occurs because tensor data is copied element-wise rather than
-    chunk-wise. Zero strides is assumed to not have this defect.
-    """
-    if min(t.stride()) > 1:
-        # TODO: investigate if contiguity along other axes than the
-        # last one can be beneficial for performance
-        return t.contiguous()
-    else:
-        return t
-
-
-def broadcast_batch_dims(f_name, *tensors):
-    try:
-        return torch.broadcast_shapes(*(t.shape[:-2] for t in tensors))
-    except Exception:
-        check(False, f"{f_name}(): inputs' batch dimensions are not broadcastable!")
-
-
-def slicer(dim, slice_range, *tensors):
-    for t in tensors:
-        slices = [slice(None)] * t.dim()
-        slices[dim] = slice_range
-        yield t[slices]
-
-
-def multidim_slicer(dims, slices, *tensors):
-    for t in tensors:
-        s = [slice(None)] * t.dim()
-        for d, d_slice in zip(dims, slices):
-            if d is not None:
-                s[d] = d_slice
-        yield t[s]
-
-
-def ptr_stride_extractor(*tensors):
-    for t in tensors:
-        yield t
-        yield from t.stride()
-
-
-def grid_partitioner(full_grid, grid_blocks, tensor_dims_map):
-    assert 0 <= len(full_grid) <= 3
-    assert 0 <= len(grid_blocks) <= 3
-
-    import itertools
-
-    def generate_grid_points():
-        for fg, mg in zip(full_grid, grid_blocks):
-            yield range(0, fg, mg)
-
-    def generate_sliced_tensors(slices):
-        for t, t_dims in tensor_dims_map.items():
-            yield next(multidim_slicer(t_dims, slices, t))
-
-    for grid_point in itertools.product(*generate_grid_points()):
-        grid = [
-            min(fg - gp, mg) for fg, gp, mg in zip(full_grid, grid_point, grid_blocks)
-        ]
-        slices = [slice(gp, gp + g) for gp, g in zip(grid_point, grid)]
-        # grid_points are iterated in a "contiguous" order, i.e.
-        # left dimensions traversed slower than right dimensions.
-        # This order is reversed for CUDA grids.
-        yield grid[::-1], *generate_sliced_tensors(slices)
-
-
-def launch_kernel(kernel, tensor_dims_map, full_grid, grid_blocks=None):
-    # cuda_max_grid = (2 ** 31 - 1, 2 ** 16 - 1, 2 ** 16 - 1)
-    cuda_max_grid = (2147483647, 65535, 65535)[::-1]
-    if grid_blocks is None:
-        grid_blocks = cuda_max_grid
-    else:
-
-        def valid_grid_dim(g, mg):
-            if g is None:
-                return mg
-            else:
-                # grid must be at least 1 and no greater than mg
-                return max(1, min(g, mg))
-
-        grid_blocks = tuple(
-            valid_grid_dim(g, mg) for g, mg in zip(grid_blocks, cuda_max_grid)
-        )  # type: ignore[assignment]
-
-    for grid, *sliced_tensors in grid_partitioner(
-        full_grid, grid_blocks, tensor_dims_map
-    ):
-        kernel(grid, *sliced_tensors)
-
-
-def prepare_inputs(bsr, *dense_tensors):
-    # Introduce fake batch dimension if not present for convenience.
-    crow_indices = bsr.crow_indices().unsqueeze(0)
-    col_indices = bsr.col_indices().unsqueeze(0)
-    values = make_triton_contiguous(bsr.values().unsqueeze(0))
-    tensors = [make_triton_contiguous(t.unsqueeze(0)) for t in dense_tensors]
-
-    # Compute broadcasted batch dimension
-    batch_dims_broadcasted = torch.broadcast_shapes(
-        values.shape[:-3], *(t.shape[:-2] for t in tensors)
-    )
-
-    # Broadcast batch dimensions and squash.
-    # The result can be either a view or a copy.
-    def batch_broadcast_and_squash(t, batch_dims, invariant_dims):
-        return t.broadcast_to(batch_dims + invariant_dims).flatten(
-            0, len(batch_dims) - 1
-        )
-
-    crow_indices = batch_broadcast_and_squash(
-        crow_indices, batch_dims_broadcasted, (-1,)
-    )
-
-    col_indices = batch_broadcast_and_squash(col_indices, batch_dims_broadcasted, (-1,))
-    values = batch_broadcast_and_squash(
-        values, batch_dims_broadcasted, values.shape[-3:]
-    )
-    tensors = [
-        batch_broadcast_and_squash(t, batch_dims_broadcasted, t.shape[-2:])
-        for t in tensors
-    ]
-
-    return crow_indices, col_indices, values, *tensors
-
-
-def broadcast_batch_dims_bsr(f_name, bsr, *tensors):
-    batch_shape = broadcast_batch_dims(f_name, bsr, *tensors)
-
-    crow_indices = bsr.crow_indices().broadcast_to(batch_shape + (-1,))
-    col_indices = bsr.col_indices().broadcast_to(batch_shape + (-1,))
-    values = bsr.values().broadcast_to(batch_shape + bsr.values().shape[-3:])
-    size = batch_shape + bsr.shape[-2:]
-    return torch.sparse_compressed_tensor(
-        crow_indices, col_indices, values, size=size, layout=bsr.layout
-    )
-
-
-# NOTE: this function will ALWAYS create a view
-def tile_to_blocksize(t, blocksize):
-    *rest, m, n = t.shape
-    new_shape = rest + [
-        m // blocksize[0],
-        blocksize[0],
-        n // blocksize[1],
-        blocksize[1],
-    ]
-    # using .view instead of .reshape to ensure that the result is
-    # indeed a view:
-    return t.view(new_shape).transpose(-3, -2)
-
-
-def as1Dbatch(tensor):
-    """Return tensor as 3D tensor by either prepending new dimensions to
-    the tensor shape (when ``tensor.ndim < 3``), or by collapsing
-    starting dimensions into the first dimension (when ``tensor.ndim >
-    3``).
-    """
-    while tensor.ndim < 3:
-        tensor = tensor.unsqueeze(0)
-    if tensor.ndim > 3:
-        tensor = tensor.flatten(0, tensor.ndim - 3)
-    assert tensor.ndim == 3, tensor.shape
-    return tensor
-
+AUTOTUNE = True
 
 ## addmm functionality
+
 
 def bsr_dense_addmm_meta(
     M,
@@ -336,39 +102,42 @@ def bsr_dense_addmm_meta(
         if meta is not None:
             meta.update(**extra)
             return meta
+        elif AUTOTUNE:
+            optimize_bsr_dense_addmm(
+                M,
+                K,
+                N if N >= 16 else 16,
+                Ms,
+                Ks,
+                beta=beta,
+                alpha=alpha,
+                sparsity=sparsity,
+                dtype=dtype,
+                opname="bsr_dense_addmm",
+                verbose=True,
+            )
+            padded_key = (
+                M,
+                K,
+                N if N >= 16 else 16,
+                Ms,
+                Ks,
+                beta == 0,
+                beta == 1,
+                alpha == 1,
+            )
+            meta = get_meta(
+                "bsr_dense_addmm",
+                padded_key,
+                device_name,
+                version=(_version, version_dtype, sparsity),
+            )
+            return meta
         else:
-            # see [Computing optimal kernel parameters] in
-            # _triton_ops_meta.py for ways to avoid this warning
-            # optimize_bsr_dense_addmm(
-            #     M,
-            #     K,
-            #     16,
-            #     64,
-            #     64,
-            #     beta=beta,
-            #     alpha=alpha,
-            #     sparsity=sparsity,
-            #     dtype=dtype,
-            #     opname="bsr_dense_addmm",
-            #     verbose=True,
-            # )
-            # get padded key
-            # padded_key = (M, K, 16, Ms, Ks, beta == 0, beta == 1, alpha == 1)
-            # meta = get_meta(
-            #     "bsr_dense_addmm",
-            #     padded_key,
-            #     device_name,
-            #     version=(_version, version_dtype, sparsity),
-            # )
-            pass
-            # breakpoint()
-            # return meta
-            # message
-            # breakpoint()
-            # warn_once(
-            #     "bsr_dense_addmm uses non-optimal triton kernel parameters"
-            #     f" for {M=} {K=} {N=} {Ms=}, {Ks=} {beta=} {alpha=} {dtype=} {out_dtype=}"
-            # )
+            warn_once(
+                "bsr_dense_addmm uses non-optimal triton kernel parameters"
+                f" for {M=} {K=} {N=} {Ms=}, {Ks=} {beta=} {alpha=} {dtype=} {out_dtype=}"
+            )
 
     SPLIT_N = SPLIT_N or max(N // Ms, 1)
     GROUP_SIZE_ROW = GROUP_SIZE_ROW or 4
@@ -380,125 +149,6 @@ def bsr_dense_addmm_meta(
         num_stages=num_stages,
         num_warps=num_warps,
         **extra,
-    )
-
-
-class TensorAsKey:
-    """A light-weight wrapper of a tensor that enables storing tensors as
-    keys with efficient memory reference based comparision as an
-    approximation to data equality based keys.
-
-    Motivation: the hash value of a torch tensor is tensor instance
-    based that does not use data equality and makes the usage of
-    tensors as keys less useful. For instance, the result of
-    ``len({a.crow_indices(), a.crow_indices()})`` is `2`, although,
-    the tensor results from `crow_indices` method call are equal, in
-    fact, these share the same data storage.
-    On the other hand, for efficient caching of tensors we want to
-    avoid calling torch.equal that compares tensors item-wise.
-
-    TensorAsKey offers a compromise in that it guarantees key equality
-    of tensors that references data in the same storage in the same
-    manner and without accessing underlying data. However, this
-    approach does not always guarantee correctness. For instance, for
-    a complex tensor ``x``, we have ``TensorAsKey(x) ==
-    TensorAsKey(x.conj())`` while ``torch.equal(x, x.conj())`` would
-    return False.
-    """
-
-    def __init__(self, obj):
-        def get_tensor_key(obj):
-            # Warning: TensorAsKey does not track negative nor
-            # conjugate bits of its input object because in the use
-            # case of wrapping compressed/plain indices of compressed
-            # sparse tensors (that are always integer tensors with
-            # non-negative items) these bits are never set. However,
-            # when extending the use of TensorAsKey to float or
-            # complex tensors, the values of these bits (see is_neg
-            # and is_conj methods) must be included in the key as
-            # well.
-            assert not (obj.dtype.is_floating_point or obj.dtype.is_complex), obj.dtype
-            return (
-                obj.data_ptr(),
-                obj.storage_offset(),
-                obj.shape,
-                obj.stride(),
-                obj.dtype,
-            )
-
-        self._obj_ref = weakref.ref(obj)
-        if obj.layout is torch.strided:
-            self.key = get_tensor_key(obj)
-        elif obj.layout in {torch.sparse_csr, torch.sparse_bsr}:
-            self.key = (
-                get_tensor_key(obj.crow_indices()),
-                get_tensor_key(obj.col_indices()),
-            )
-        elif obj.layout in {torch.sparse_csc, torch.sparse_bsc}:
-            self.key = (
-                get_tensor_key(obj.ccol_indices()),
-                get_tensor_key(obj.row_indices()),
-            )
-        else:
-            raise NotImplementedError(obj.layout)
-        self._hash = hash(self.key)
-
-    def __hash__(self):
-        return self._hash
-
-    def __eq__(self, other):
-        if not isinstance(other, TensorAsKey):
-            return False
-        if self.obj is None or other.obj is None:
-            # dead objects always compare unequal unless these are
-            # same objects
-            return self is other
-        return self.key == other.key
-
-    @property
-    def obj(self):
-        """Return object if alive, otherwise None."""
-        return self._obj_ref()
-
-
-def _int_bsr_dense_addmm(
-    input: torch.Tensor,
-    bsr: torch.Tensor,
-    dense: torch.Tensor,
-    *,
-    beta=1,
-    alpha=1,
-    left_alpha: Optional[torch.Tensor] = None,
-    right_alpha: Optional[torch.Tensor] = None,
-    out: Optional[torch.Tensor] = None,
-    skip_checks: bool = False,
-    max_grid: Optional[tuple[Optional[int], Optional[int], Optional[int]]] = None,
-    meta: Optional[dict] = None,
-):
-    if out is None and dense.dtype is torch.int8:
-        f_name = "_int_bsr_dense_addmm"
-        crow_indices = bsr.crow_indices()
-        batch_ndim = crow_indices.dim() - 1
-        M = bsr.shape[batch_ndim]
-        N = dense.shape[-1]
-        original_batch_dims_broadcasted = broadcast_batch_dims(f_name, bsr, dense)
-        out = torch.empty(
-            original_batch_dims_broadcasted + (M, N),
-            dtype=torch.int32,
-            device=dense.device,
-        )
-    return bsr_dense_addmm(
-        input,
-        bsr,
-        dense,
-        beta=beta,
-        alpha=alpha,
-        left_alpha=left_alpha,
-        right_alpha=right_alpha,
-        out=out,
-        skip_checks=skip_checks,
-        max_grid=max_grid,
-        meta=meta,
     )
 
 
@@ -532,8 +182,6 @@ def bsr_dense_addmm(
     M, K = bsr.shape[batch_ndim : batch_ndim + 2]
     blocksize = values.shape[batch_ndim + 1 : batch_ndim + 3]
     N = dense.shape[-1]
-
-    # todo: implement checks
 
     original_batch_dims_broadcasted = broadcast_batch_dims(f_name, bsr, dense)
     if out is None:
@@ -780,10 +428,10 @@ if has_triton():
         inner_block_arange = tl.arange(0, BLOCKSIZE_INNER)
 
         if BLOCKSIZE_COL < 16 or BLOCKSIZE_COL % 16 != 0:
-            PADDED_BLOCKSIZE_COL : tl.constexpr = 16
+            PADDED_BLOCKSIZE_COL: tl.constexpr = 16
         else:
             PADDED_BLOCKSIZE_COL: tl.constexpr = BLOCKSIZE_COL
-        
+
         col_block_arange = tl.arange(0, PADDED_BLOCKSIZE_COL)
 
         # Pointers are set to the first block of the current row.
@@ -822,8 +470,9 @@ if has_triton():
             + col_indices_stride * nnz_offset
         )
 
-        output_acc_block = tl.zeros((BLOCKSIZE_ROW, PADDED_BLOCKSIZE_COL), dtype=acc_dtype)
-        # offsets = tl.arange(0, PADDED_BLOCKSIZE_COL)[None, :]
+        output_acc_block = tl.zeros(
+            (BLOCKSIZE_ROW, PADDED_BLOCKSIZE_COL), dtype=acc_dtype
+        )
         for _ in range(row_nnz):
             values_block = tl.load(values_block_ptrs)
 
@@ -884,7 +533,11 @@ if has_triton():
                 output_acc_block += beta * tl.load(input_ptrs)
 
         # write back the result
-        tl.store(output_ptrs, output_acc_block.to(output_ptr.dtype.element_ty), mask=col_block_arange[None, :]< BLOCKSIZE_COL)
+        tl.store(
+            output_ptrs,
+            output_acc_block.to(output_ptr.dtype.element_ty),
+            mask=col_block_arange[None, :] < BLOCKSIZE_COL,
+        )
 
 else:
     _bsr_strided_addmm_kernel = None  # type: ignore[assignment]
