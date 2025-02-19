cc5e13cd0470ad0fe83ebf6ed6057254a6363b08
Jesse Cai <jcjessecai@gmail.com>
2025-02-18 18:38:54 -0800

    autotuning working properly now

--- torchao/kernel/__init__.py
+++ torchao/kernel/__init__.py
@@ -1,9 +1,8 @@
 from torchao.kernel.intmm import int_scaled_matmul, safe_int_mm
-from torchao.kernel.bsr_triton_ops import bsr_dense_addmm, broadcast_batch_dims
+from torchao.kernel.bsr_triton_ops import bsr_dense_addmm
 
 __all__ = [
     "bsr_dense_addmm",
-    "broadcast_batch_dims"
     "safe_int_mm",
     "int_scaled_matmul",
 ]
--- torchao/kernel/bsr_triton_ops.py
+++ torchao/kernel/bsr_triton_ops.py
@@ -5,24 +5,153 @@ from typing import Optional
 
 import torch
 from torch._dynamo.utils import warn_once
-from torch.sparse._triton_ops_meta import (
+from torch.sparse._triton_ops_meta import get_meta, minimize, update
+from torch.sparse._triton_ops import (
     broadcast_batch_dims,
-    get_meta,
-    launch_kernel,
-    optimize_bsr_dense_addmm,
     prepare_inputs,
     ptr_stride_extractor,
     tile_to_blocksize,
+    launch_kernel,
 )
 from torch.utils._triton import has_triton
 
-TORCH_SPARSE_BSR_SCATTER_MM_LRU_CACHE_SIZE = int(
-    os.getenv("TORCH_SPARSE_BSR_SCATTER_MM_LRU_CACHE_SIZE", 2)
-)
+AUTOTUNE = os.getenv("BSR_AUTOTUNE", False)
 
-AUTOTUNE = True
 
-## addmm functionality
+def tune_bsr_dense_addmm(
+    input,
+    bsr,
+    dense,
+    *,
+    beta=1,
+    alpha=1,
+    left_alpha=None,
+    right_alpha=None,
+    out=None,
+    store=False,
+    verbose=False,
+    force=False,
+    opname=None,
+):
+    """Tune bsr_dense_addmm kernel parameters against the given inputs.
+
+    When store is True, the tuning results will be stored in the
+    database of kernel parameters.
+    """
+    import triton
+
+    if opname is None:
+        opname = "bsr_dense_addmm"
+
+    N = dense.shape[-1]
+    values = bsr.values()
+    crow_indices = bsr.crow_indices()
+    batch_ndim = crow_indices.dim() - 1
+    M, K = bsr.shape[batch_ndim : batch_ndim + 2]
+    BM, BK = values.shape[batch_ndim + 1 : batch_ndim + 3]
+
+    # Reference parameters is a set of parameters that leads to a
+    # successful kernel call and the corresponding timing is used as a
+    # reference for computing speedups. Avoid changing the reference
+    # parameters when possible.
+    reference_meta = dict(
+        GROUP_SIZE_ROW=1, num_stages=4, num_warps=4, SPLIT_N=max(N // BM, 1)
+    )
+
+    # Compute the key of parameters:
+    sparsity = round(1 - bsr._nnz() * BM * BK / (M * K), 2)
+    dtype = bsr.dtype
+    if out is None:
+        out_dtype = dtype
+    else:
+        out_dtype = out.dtype
+    if out_dtype is dtype:
+        version_dtype = dtype
+    else:
+        version_dtype = (dtype, out_dtype)
+    version = (0, version_dtype, sparsity)
+    key = (M, K, N, BM, BK, beta == 0, beta == 1, alpha == 1)
+
+    # For tuning, for an initial state, use parameters from the
+    # database if available, otherwise, use the reference parameters.
+    initial_meta = get_meta(opname, key, version=version, exact=True)
+    if initial_meta is None:
+        may_skip_update = False
+        initial_meta = get_meta(opname, key, version=(0, dtype, 0.5), exact=True)
+        if initial_meta is None:
+            initial_meta = reference_meta
+    elif not force:
+        return initial_meta
+    else:
+        may_skip_update = True
+
+    # The target function that is minimized in the tuning process:
+    def bench(meta, input=input, bsr=bsr, dense=dense, alpha=alpha, out=out):
+        def test_func():
+            return bsr_dense_addmm(
+                input,
+                bsr,
+                dense,
+                beta=beta,
+                alpha=alpha,
+                left_alpha=left_alpha,
+                right_alpha=right_alpha,
+                meta=meta,
+                out=out,
+            )
+
+        return triton.testing.do_bench(test_func, warmup=500, rep=100)
+
+    # The step function that increments a specified meta parameter:
+    def step_meta_parameter(name, value, direction, meta, M=M, N=N, K=K, BM=BM, BK=BK):
+        # return next value in positive or negative direction, or
+        # input value if the step will result an invalid
+        # value. The input value is assumed to be valid.
+        is_log = name in {"SPLIT_N", "num_warps"}
+        min_value = dict(SPLIT_N=1, num_warps=1, num_stages=1, GROUP_SIZE_ROW=1)[name]
+        max_value = dict(SPLIT_N=max(N // BM, 1)).get(name)
+        value_step = dict(SPLIT_N=2, num_warps=2, num_stages=1, GROUP_SIZE_ROW=1)[name]
+        if is_log:
+            next_value = (
+                value * value_step**direction
+                if direction > 0
+                else value // (value_step ** abs(direction))
+            )
+        else:
+            next_value = value + value_step * direction
+        if min_value is not None:
+            next_value = max(next_value, min_value)
+        if max_value is not None:
+            next_value = min(next_value, max_value)
+        if name == "SPLIT_N" and N % next_value != 0:
+            return value
+        return next_value
+
+    # Tune:
+    meta, speedup, timing, sensitivity_message = minimize(
+        bench,
+        initial_meta,
+        reference_meta,
+        step_meta_parameter,
+        max_step=2,
+        verbose=verbose,
+    )
+    if verbose:
+        print(f"-> {sensitivity_message}, {speedup=:.1f} %, {timing=:.3f} ms")
+
+    if store and not (
+        may_skip_update and meta == initial_meta and initial_meta is not reference_meta
+    ):
+        device_name = torch.cuda.get_device_name()
+        update(
+            opname,
+            device_name,
+            version,
+            key,
+            tuple(meta[k] for k in sorted(meta)),
+        )
+
+    return meta
 
 
 def bsr_dense_addmm_meta(
@@ -102,37 +231,6 @@ def bsr_dense_addmm_meta(
         if meta is not None:
             meta.update(**extra)
             return meta
-        elif AUTOTUNE:
-            optimize_bsr_dense_addmm(
-                M,
-                K,
-                N if N >= 16 else 16,
-                Ms,
-                Ks,
-                beta=beta,
-                alpha=alpha,
-                sparsity=sparsity,
-                dtype=dtype,
-                opname="bsr_dense_addmm",
-                verbose=True,
-            )
-            padded_key = (
-                M,
-                K,
-                N if N >= 16 else 16,
-                Ms,
-                Ks,
-                beta == 0,
-                beta == 1,
-                alpha == 1,
-            )
-            meta = get_meta(
-                "bsr_dense_addmm",
-                padded_key,
-                device_name,
-                version=(_version, version_dtype, sparsity),
-            )
-            return meta
         else:
             warn_once(
                 "bsr_dense_addmm uses non-optimal triton kernel parameters"
@@ -196,6 +294,37 @@ def bsr_dense_addmm(
                 out.mul_(beta)
         return out
 
+    if meta is None:
+        sparsity = round(1 - bsr._nnz() * blocksize[0] * blocksize[1] / (M * K), 2)
+        if AUTOTUNE:
+            meta = tune_bsr_dense_addmm(
+                input,
+                bsr,
+                dense,
+                beta=beta,
+                alpha=alpha,
+                left_alpha=left_alpha,
+                right_alpha=right_alpha,
+                out=out,
+                store=True,
+                force=False,
+                verbose=True,
+                opname="bsr_dense_addmm",
+            )
+        else:
+            meta = bsr_dense_addmm_meta(
+                M,
+                K,
+                N,
+                blocksize[0],
+                blocksize[1],
+                beta,
+                alpha,
+                sparsity=sparsity,
+                dtype=dense.dtype,
+                out_dtype=out.dtype,
+            )
+
     left_alpha_is_one = False
     right_alpha_is_one = False
     if left_alpha is None:
@@ -220,20 +349,6 @@ def bsr_dense_addmm(
     assert left_alpha.stride()[-1] == 0
     assert right_alpha.stride()[-2] == 0
 
-    if meta is None:
-        sparsity = round(1 - bsr._nnz() * blocksize[0] * blocksize[1] / (M * K), 2)
-        meta = bsr_dense_addmm_meta(
-            M,
-            K,
-            N,
-            blocksize[0],
-            blocksize[1],
-            beta,
-            alpha,
-            sparsity=sparsity,
-            dtype=dense.dtype,
-            out_dtype=out.dtype,
-        )
     out_backup = out
 
     (
@@ -248,7 +363,7 @@ def bsr_dense_addmm(
     ) = prepare_inputs(bsr, input, dense, left_alpha, right_alpha, out)
 
     BM, BK = blocksize
-    SPLIT_N = meta.get("SPLIT_N", N // BM)
+    SPLIT_N = meta.get("SPLIT_N", max(N // BM, 1))
     BN = N // SPLIT_N
 
     out_untiled = out
