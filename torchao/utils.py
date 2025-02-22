287836a573ff1da149714566d4a7ab25ad0607ad
Jesse Cai <jessecai@meta.com>
2025-02-21 16:13:02 -0800

    Move import into function definition

--- torchao/utils.py
+++ torchao/utils.py
@@ -7,7 +7,6 @@ from importlib.metadata import version
 from math import gcd
 from typing import Any, Callable, Tuple
 
-import pytest
 import torch
 import torch.nn.utils.parametrize as parametrize
 
@@ -168,6 +167,7 @@ def skip_if_rocm(message=None):
     Args:
         message (str, optional): Additional information about why the test is skipped.
     """
+    import pytest
 
     def decorator(func):
         @functools.wraps(func)
