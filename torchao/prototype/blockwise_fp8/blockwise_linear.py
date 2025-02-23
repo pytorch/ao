0caf9def9882c75f91364e28a936a510f409410c
Degnel <bigarnaque@gmail.com>
2025-02-23 16:29:22 +0100

    Doc: init + linting + readme

--- torchao/prototype/blockwise_fp8/blockwise_linear.py
+++ torchao/prototype/blockwise_fp8/blockwise_linear.py
@@ -17,15 +17,25 @@ class BlockwiseQuantLinear(nn.Module):
         bias (bool): Whether to include a bias term. Defaults to False.
         block_size (int): Block size for quantization. Defaults to 128.
     """
+
     dtype = torch.bfloat16
 
-    def __init__(self, in_features: int, out_features: int, bias: bool = False, block_size:int = 128, dtype = torch.float8_e4m3fn):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bias: bool = False,
+        block_size: int = 128,
+        dtype=torch.float8_e4m3fn,
+    ):
         super().__init__()
         assert dtype is torch.float8_e4m3fn, "Only float8_e4m3fn is supported for now."
         scale_in_features = (in_features + block_size - 1) // block_size
         scale_out_features = (out_features + block_size - 1) // block_size
         self.weight = nn.Parameter(torch.empty(out_features, in_features, dtype=dtype))
-        self.weight.scale = self.scale = nn.Parameter(torch.empty(scale_out_features, scale_in_features, dtype=torch.float32))
+        self.weight.scale = self.scale = nn.Parameter(
+            torch.empty(scale_out_features, scale_in_features, dtype=torch.float32)
+        )
         self.block_size = block_size
 
         if bias:
@@ -45,7 +55,7 @@ class BlockwiseQuantLinear(nn.Module):
         """
         x, scale = fp8_blockwise_act_quant(x, self.block_size)
         y = blockwise_fp8_gemm(x, scale, self.weight, self.weight.scale)
-        
+
         if self.bias is not None:
             y += self.bias
         return y
