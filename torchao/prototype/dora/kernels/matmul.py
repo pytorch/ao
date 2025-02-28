44c3fd792c710666408eca474cf56a3c9c8825fc
HDCharles <charlesdavidhernandez@gmail.com>
2025-02-28 07:32:33 -0800

    Fixing DORA imports
    Summary: these imports were pointing at nothing
    
    Test Plan: python test/dora/test_dora_fusion.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- torchao/prototype/dora/kernels/matmul.py
+++ torchao/prototype/dora/kernels/matmul.py
@@ -9,13 +9,16 @@ from .common import (
     TRITON_SUPPORTED_ACC_TYPES,
     SwizzleType,
     TritonInputPrecision,
-    early_config_prune,
-    estimate_matmul_time,
     get_compute_bound_configs,
-    get_configs_io_bound,
-    get_higher_dtype,
     swizzle_tile,
     to_tl_type,
+)
+from torchao.prototype.common.triton.matmul import (
+    get_configs_io_bound, 
+    get_higher_dtype,
+    early_config_prune,
+    estimate_matmul_time,
+    get_higher_dtype,
 )
 from .custom_autotune import autotune
 
