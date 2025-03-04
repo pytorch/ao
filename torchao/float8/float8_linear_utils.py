b84bc5100e02b6980924f619728015106ae64cf2
Mark Saroufim <marksaroufim@gmail.com>
2025-03-03 19:58:42 -0800

    Update float8_linear_utils.py

--- torchao/float8/float8_linear_utils.py
+++ torchao/float8/float8_linear_utils.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD 3-Clause license found in the
 # LICENSE file in the root directory of this source tree.
 import logging
-from typing import Callable, List, Optional
+from typing import Callable, Optional
 
 import torch.nn as nn
 
