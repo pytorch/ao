b01214c6a39bcf4a355fff2bc54fd55ccb4a82a0
Apurva Jain <appy@meta.com>
2025-03-14 14:09:28 -0700

    Update (base update)
    [ghstack-poisoned]

--- scripts/run_ruff_fix.sh
+++ scripts/run_ruff_fix.sh
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
 ruff check . --fix
 # --isolated is used to skip the allowlist at all so this applies to all files
 # please be careful when using this large changes means everyone needs to rebase
