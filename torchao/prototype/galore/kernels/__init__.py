7a9c71fa3d86d4ea4cbdcc56bc946f23c588c8f3
Apurva Jain <appy@meta.com>
2025-03-14 14:09:27 -0700

    Add copyright header to files in torchao folder
    ghstack-source-id: 2010242548c76fb26d9538963d78e9fd8f2a2026
    Pull Request resolved: https://github.com/pytorch/ao/pull/1897

--- torchao/prototype/galore/kernels/__init__.py
+++ torchao/prototype/galore/kernels/__init__.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
 from .adam_downproj_fused import fused_adam_mm_launcher
 from .adam_step import triton_adam_launcher
 from .matmul import triton_mm_launcher
