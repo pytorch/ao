94f49f452d6b58628dae79dcdf64615e268c8704
jainapurva <apurvajain.kota@gmail.com>
2025-03-17 09:53:29 -0700

    Copyright header

--- benchmarks/microbenchmarks/benchmark_inference.py
+++ benchmarks/microbenchmarks/benchmark_inference.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 """
 Inference benchmark runner
 
