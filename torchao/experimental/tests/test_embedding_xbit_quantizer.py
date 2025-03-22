7b9cbceb247513a7d8f2b3e51afa87f77e00ba1a
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-03-22 15:26:25 -0700

    Add quant api + python test for shared embedding (#1937)
    Add quant api + python test for shared embedding (#1937)
    
    Summary:
    
    Adds shared embedding to quantizer and adds unit test for it in python
    
    Reviewed By: digantdesai
    
    Differential Revision: D71234378

--- torchao/experimental/tests/test_embedding_xbit_quantizer.py
+++ torchao/experimental/tests/test_embedding_xbit_quantizer.py
@@ -9,16 +9,23 @@ import tempfile
 import unittest
 
 import torch
+from torch.testing import FileCheck
 
-from torchao.experimental.quant_api import (
-    IntxWeightEmbeddingQuantizer,
-    _IntxWeightQuantizedEmbeddingFallback,
+from torchao.experimental.packed_linear_int8_dynamic_activation_intx_weight_layout import (
+    PackedLinearInt8DynamicActivationIntxWeightLayout,
 )
+from torchao.experimental.quant_api import (
+    EmbeddingQuantizer,
+    Int8DynamicActivationIntxWeightConfig,
+    SharedEmbeddingQuantizer,
+)
+from torchao.quantization.granularity import PerGroup, PerRow
+from torchao.quantization.quant_api import quantize_
 
 
 class TestEmbeddingQuantizer(unittest.TestCase):
     def test_accuracy(self):
-        group_size = 128
+        granularity = PerGroup(128)
         embedding_dim = 4096
         num_embeddings = 131
         model = torch.nn.Sequential(
@@ -26,27 +33,42 @@ class TestEmbeddingQuantizer(unittest.TestCase):
         )
         indices = torch.randint(0, num_embeddings, (7,), dtype=torch.int32)
 
-        for nbit in [1, 2, 3, 4, 5, 6, 7, 8]:
-            print(f"Testing nbit={nbit}")
+        for weight_dtype in [
+            torch.int1,
+            torch.int2,
+            torch.int3,
+            torch.int4,
+            torch.int5,
+            torch.int6,
+            torch.int7,
+            torch.int8,
+        ]:
+            print(f"Testing weight_dtype={weight_dtype}")
             quantized_model = copy.deepcopy(model)
-            quantizer = IntxWeightEmbeddingQuantizer(
-                device="cpu",
-                precision=torch.float32,
-                bitwidth=nbit,
-                groupsize=group_size,
+            quantizer = EmbeddingQuantizer(
+                weight_dtype=weight_dtype,
+                granularity=granularity,
+                has_weight_zeros=True,
+                use_fallback=False,
             )
             quantized_model = quantizer.quantize(quantized_model)
 
             with torch.no_grad():
+                reference_quantizer = EmbeddingQuantizer(
+                    weight_dtype=weight_dtype,
+                    granularity=granularity,
+                    has_weight_zeros=True,
+                    use_fallback=True,
+                )
+                reference_model = copy.deepcopy(model)
+                reference_model = reference_quantizer.quantize(reference_model)
                 result = quantized_model(indices)
-                reference_impl = _IntxWeightQuantizedEmbeddingFallback(nbit)
-                reference_impl.quantize_and_pack_weights(model[0].weight, group_size)
-                expected_result = reference_impl(indices)
+                expected_result = reference_model(indices)
             self.assertTrue(torch.allclose(result, expected_result))
 
     def test_export_compile_aoti(self):
-        nbit = 4
-        group_size = 128
+        weight_dtype = torch.int4
+        granularity = PerRow()
         embedding_dim = 4096
         num_embeddings = 131
         model = torch.nn.Sequential(
@@ -55,33 +77,112 @@ class TestEmbeddingQuantizer(unittest.TestCase):
         indices = torch.randint(0, num_embeddings, (42,), dtype=torch.int32)
 
         print("Quantizing model")
-        quantizer = IntxWeightEmbeddingQuantizer(
-            device="cpu",
-            precision=torch.float32,
-            bitwidth=nbit,
-            groupsize=group_size,
+        quantizer = EmbeddingQuantizer(
+            weight_dtype=weight_dtype,
+            granularity=granularity,
+            has_weight_zeros=True,
+            use_fallback=False,
         )
         quantized_model = quantizer.quantize(model)
+        eager_results = model(indices)
 
         print("Exporting quantized model")
-        torch.export.export(quantized_model, (indices,), strict=True)
+        with torch.no_grad():
+            exported_model = torch.export.export(
+                quantized_model, (indices,), strict=True
+            )
+            exported_results = exported_model.module()(indices)
+            self.assertTrue(torch.allclose(eager_results, exported_results))
 
         print("Compiling quantized model")
         quantized_model_compiled = torch.compile(quantized_model)
         with torch.no_grad():
             quantized_model_compiled(indices)
+            compiled_results = quantized_model_compiled(indices)
+            self.assertTrue(torch.allclose(eager_results, compiled_results))
 
         with tempfile.TemporaryDirectory() as tmpdirname:
             print("Exporting quantized model with AOTI")
-            torch._export.aot_compile(
-                quantized_model,
-                (indices,),
-                options={"aot_inductor.output_path": f"{tmpdirname}/model.so"},
+            package_path = f"{tmpdirname}/model.pt2"
+            torch._inductor.aoti_compile_and_package(
+                exported_model, package_path=package_path
             )
+            fn = torch._inductor.aoti_load_package(package_path)
+            aoti_results = fn(indices)
+            self.assertTrue(torch.allclose(eager_results, aoti_results))
 
-            print("Running quantized model in AOTI")
-            fn = torch._export.aot_load(f"{tmpdirname}/model.so", "cpu")
-            fn(indices)
+    def test_shared_embedding(self):
+        weight_dtype = torch.int4
+        granularity = PerRow()
+        has_weight_zeros = True
+        embedding_dim = 4096
+        num_embeddings = 131
+        embedding = torch.nn.Embedding(num_embeddings, embedding_dim)
+        unembedding = torch.nn.Linear(embedding_dim, num_embeddings)
+        unembedding.weight = copy.deepcopy(embedding.weight)
+        model = torch.nn.Sequential(
+            *[
+                embedding,
+                torch.nn.Linear(embedding_dim, embedding_dim),
+                unembedding,
+            ]
+        )
+        indices = torch.randint(0, num_embeddings, (42,), dtype=torch.int32)
+
+        # Reference implementation quantizes the embedding and unembedding
+        # layers separately
+        quantized_model_reference = copy.deepcopy(model)
+        EmbeddingQuantizer(
+            weight_dtype=weight_dtype,
+            granularity=granularity,
+            has_weight_zeros=has_weight_zeros,
+        ).quantize(quantized_model_reference)
+        quantize_(
+            quantized_model_reference,
+            Int8DynamicActivationIntxWeightConfig(
+                weight_dtype=weight_dtype,
+                granularity=granularity,
+                has_weight_zeros=has_weight_zeros,
+                layout=PackedLinearInt8DynamicActivationIntxWeightLayout(
+                    target="universal"
+                ),
+            ),
+            filter_fn=lambda m, fqn: fqn == "2",
+        )
+
+        # Do shared embedding quantization
+        quantized_model = copy.deepcopy(model)
+        SharedEmbeddingQuantizer(
+            weight_dtype=weight_dtype,
+            granularity=granularity,
+            has_weight_zeros=has_weight_zeros,
+        ).quantize(quantized_model)
+
+        # Check results are same and weights share the same id
+        with torch.no_grad():
+            result = quantized_model(indices)
+            expected_result = quantized_model_reference(indices)
+        self.assertTrue(torch.allclose(result, expected_result))
+        self.assertTrue(
+            id(quantized_model[0].unembedding_packed_weights)
+            == id(quantized_model[2].packed_weight)
+        )
+
+        # Test export
+        exported_program = torch.export.export(quantized_model, (indices,))
+        exported_result = exported_program.module()(indices)
+        self.assertTrue(torch.allclose(result, exported_result))
+
+        # Check the shared_embedding and linear ops use the same lifted weight
+        weight = "b_getattr_l__fn_____0___unembedding_packed_weights"
+        expected_lines = [
+            f"torch.ops.torchao._shared_embedding_4bit.default({weight}, 4096, 131, 4096, reshape)",
+            f"torch.ops.torchao._linear_8bit_act_4bit_weight.default(linear, {weight}, 4096, 131, 4096)",
+        ]
+        for line in expected_lines:
+            FileCheck().check_count(line, 1, exactly=True).run(
+                exported_program.graph_module.code
+            )
 
 
 if __name__ == "__main__":
