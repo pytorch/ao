61f0ee464957b634e359777974ebd61f34cec800
Daniel Vega-Myhre <danvm@meta.com>
2025-03-27 11:50:35 -0700

    add todo

--- torchao/prototype/grouped_mm/__init__.py
+++ torchao/prototype/grouped_mm/__init__.py
@@ -23,8 +23,8 @@ def _grouped_scaled_mm(
     then performs a scaled grouped GEMM and returns the results.
 
     Args:
-        A (torch.Tensor): The first input tensor (non-tranposed, row-major).
-        B (torch.Tensor): The second input tensor (non-transposed, row-major).
+        A (torch.Tensor): The first input tensor, which can be 2D or 3D.
+        B_t (torch.Tensor): The second input tensor which must be 3D.
         float8_recipe (Float8LinearRecipeName): The recipe to use for dynamic float8 quantization.
         offs (Optional[torch.Tensor]): The offsets to use to mark the starting index of each group. This
             is required when 2D A tensor is used, otherwise it should be None.
@@ -61,10 +61,14 @@ class _Float8GroupedMM(torch.autograd.Function):
         # perform dynamic float8 quantization using the given recipe, if specified
         assert 2 <= A.ndim <= 3, "A must be 2D or 3D"
         assert B_t.ndim == 3, "B must be 3D"
+
+        # offsets are required for 2D A tensor, otherwise it should be None.
         if A.ndim == 2:
             assert offs is not None, "offs must be specified for 2D A tensor"
         else:
             assert offs is None, "offs must not be specified for 3D A tensor"
+
+        # TODO: pad dims to be multiples of 16, as required by torch._scaled_grouped_mm.
 
         # Fetch float8 config from specified recipe name.
         float8_config = Float8LinearConfig.from_recipe_name(float8_recipe_name)
