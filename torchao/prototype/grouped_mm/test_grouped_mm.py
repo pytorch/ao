5d367df64b3f1ed7114bdab37f14dae1735b9b24
Daniel Vega-Myhre <danvm@meta.com>
2025-03-31 20:37:05 -0700

    forward matches but _scaled_mm has no backward

--- torchao/prototype/grouped_mm/test_grouped_mm.py
+++ torchao/prototype/grouped_mm/test_grouped_mm.py
@@ -1,4 +1,5 @@
-from typing import List
+from re import I
+from typing import List, Tuple
 import pytest
 import torch
 from torch import nn
@@ -68,10 +69,12 @@ def test_grouped_gemm_2d_3d(use_fast_accum, strided):
     )
 
     # Validate result.
-    validate_grouped_mm(
+    ref_a = a.detach().clone().requires_grad_(True)
+    ref_b = b.detach().clone().requires_grad_(True)
+    ref_out = compute_reference_forward(
         out,
-        a,
-        b,
+        ref_a,
+        ref_b,
         n_groups,
         out_dtype,
         use_fast_accum,
@@ -81,94 +84,14 @@ def test_grouped_gemm_2d_3d(use_fast_accum, strided):
 
     # Run backward pass.
     out.sum().backward()
+    ref_out.sum().backward()
+
+    # Validate gradients.
+    assert torch.equal(a.grad, ref_a.grad)
+    assert torch.equal(b.grad, ref_b.grad)
 
 
-@pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
-def test_gradients():
-    # define a fake W1 for a MoE layer
-    device = "cuda"
-    m, k, n, n_groups = 16, 16, 32, 4
-
-    x = torch.randn(m * n_groups, k, device=device, dtype=torch.bfloat16)
-    params = nn.Parameter(torch.randn(n_groups, k, n, device=device, dtype=torch.bfloat16))
-    offs = torch.arange(m, m * n_groups + 1, m, device="cuda", dtype=torch.int32)
-
-    # clone the inputs and params for computing the reference gradients
-    ref_x, ref_params = x.detach().clone().requires_grad_(True), params.detach().clone().requires_grad_(True)
-
-    # compute the output
-    out = _grouped_scaled_mm(
-        x,
-        params,
-        offs=offs,
-        float8_recipe=Float8LinearRecipeName.ROWWISE,
-        out_dtype=torch.bfloat16,
-        use_fast_accum=False,
-    )
-
-    # compute the gradients
-    out.sum().backward()
-
-    # check the gradients exist
-    assert params.grad is not None
-
-    # check the gradients are not all nan
-    assert not params.grad.isnan().any()
-
-    # compare with reference gradients
-    ref_out, ref_grad = compute_reference_output_and_gradient(ref_x, ref_params, offs)
-    assert torch.allclose(out, ref_out, atol=1e-2, rtol=1e-2), "outputs not close"
-    assert torch.allclose(params.grad, ref_grad, atol=1e-2, rtol=1e-2), "gradients not close"
-
-
-def compute_reference_output_and_gradient(x: torch.Tensor, params: torch.Tensor, offs: torch.Tensor):
-    assert len(offs) == params.size(0), "len(offs) != params.size(0); expected same number of offs/groups as params"
-    assert x.ndim == 2, "expected 2D input"
-    assert params.ndim == 3, "expected 3D params"
-
-    # convert params to column-major memory layout
-    params_col_major = params.transpose(-2, -1).contiguous().transpose(-2, -1)
-
-    # determine group sizes based on offsets
-    group_sizes = offs_to_group_sizes(offs.tolist())
-    
-    # use group sizes to split x into groups
-    x_groups = torch.split(x, group_sizes, dim=0)
-
-    # compute a separate _scaled_mm for each group, with the weight (i.e., expert) it's assigned to
-    outputs = []
-    for group_idx, group in enumerate(x_groups):
-        group_output = matmul_with_hp_or_float8_args.apply(
-            group,
-            params_col_major[group_idx],
-            LinearMMConfig(),
-            Float8LinearConfig.from_recipe_name(Float8LinearRecipeName.ROWWISE),
-        )
-        outputs.append(group_output)
-        
-    # compute the param gradients and return them
-    ref_output = torch.cat(outputs, dim=0)
-    assert ref_output.shape[0] == x.shape[0] and ref_output.shape[-1] == params.shape[-1], "invalid output shape"
-
-    # perform backward pass
-    ref_output.sum().backward()
-    assert params.grad is not None, "params don't have gradients"
-
-    # return reference gradient
-    ref_grad = params.grad
-    return ref_output, ref_grad
-
-
-def offs_to_group_sizes(offs: List[int]) -> List[int]:
-    group_sizes = []
-    prev_off = 0
-    for off in offs:
-        group_sizes.append(off - prev_off)
-        prev_off = off
-    return group_sizes
-
-
-def validate_grouped_mm(
+def compute_reference_forward(
     result: torch.Tensor,
     A: torch.Tensor,
     B: torch.Tensor,
@@ -180,59 +103,59 @@ def validate_grouped_mm(
 ):
     assert result.dtype == out_dtype
 
-    # Validate output by comparing the partition of the grouped scaled mm output
-    # with a corresponding individual scaled mm.
-    # We need the scales used for A and B to do this, so convert to Float8Tensors here.
+    # Convert A to fp8.
     float8_config = Float8LinearConfig.from_recipe_name(float8_recipe_name)
-    A_fp8 = hp_tensor_to_float8_dynamic(
+    A_scales = tensor_to_scale(
         A,
-        float8_dtype=float8_config.cast_config_input.target_dtype,
-        linear_mm_config=LinearMMConfig(),
-        gemm_input_role=GemmInputRole.INPUT,
-        scaling_granularity=float8_config.cast_config_input.scaling_granularity,
+        float8_config.cast_config_input.target_dtype, 
+        scaling_granularity=float8_config.cast_config_input.scaling_granularity, 
         axiswise_dim=-1,
         round_scales_to_power_of_2=float8_config.round_scales_to_power_of_2,
     )
+    A_scaled = A.to(torch.float32) * A_scales
+    A_fp8 = to_fp8_saturated(A_scaled, float8_config.cast_config_input.target_dtype)
 
-    B_t_fp8 = hp_tensor_to_float8_dynamic(
-        B.transpose(-2, -1),
-        float8_config.cast_config_input.target_dtype,
-        linear_mm_config=LinearMMConfig(),
-        gemm_input_role=GemmInputRole.WEIGHT,
-        scaling_granularity=float8_config.cast_config_weight.scaling_granularity,
+    # Convert B^t to fp8.
+    B_t = B.transpose(-2, -1)
+    B_t_scales = tensor_to_scale(
+        B_t,
+        float8_config.cast_config_weight.target_dtype, 
+        scaling_granularity=float8_config.cast_config_input.scaling_granularity, 
         axiswise_dim=-2,
         round_scales_to_power_of_2=float8_config.round_scales_to_power_of_2,
     )
+    B_t_scaled = B_t.to(torch.float32) * B_t_scales
+    B_t_fp8 = to_fp8_saturated(B_t_scaled, float8_config.cast_config_weight.target_dtype)
 
-    # grouped_scaled_mm doesn't support empty dims
-    scale_A = A_fp8._scale.squeeze()
-    scale_B = B_t_fp8._scale.squeeze()
-
-    A_list, B_list, A_scale_list, B_scale_list, result_list = [], [], [], [], []
-    start = 0
-
-    # Since A 2D, we need to split it into parts based on offs, so we can perform
-    # separate _scaled_mm calls for each part.
+    # Split A and result into chunks, one for each group.
     offs_cpu = offs.cpu()
+    A_list, A_scale_list, result_list = [], [], []
+    start = 0
     for i in range(n_groups):
-        A_list.append(A_fp8._data[start : offs_cpu[i]])
-        A_scale_list.append(scale_A[start : offs_cpu[i]])
+        A_list.append(A_fp8[start : offs_cpu[i]])
+        A_scale_list.append(A_scales[start : offs_cpu[i]])
         result_list.append(result[start : offs_cpu[i]])
         start = offs_cpu[i]
 
-    A_scale_list = scale_A
-    B_scale_list = scale_B
-
-    # Validate result of each part of the grouped mm is equal to the corresponding indiviudal scaled mm.
-    for a, b, ascale, bscale, result in zip(
-        A_list, B_list, A_scale_list, B_scale_list, result_list
+    # Validate result of each part of the grouped mm is equal to the separate scaled mm.
+    outputs = []
+    for a, b, ascale, bscale, group_result in zip(
+        A_list, B_t_fp8, A_scale_list, B_t_scales, result_list
     ):
-        result_ref = torch._scaled_mm(
+        ref_group_result = torch._scaled_mm(
             a,
-            b.t(),
-            ascale.view(-1, 1),
-            bscale.view(1, -1),
+            b,
+            ascale,
+            bscale,
             out_dtype=torch.bfloat16,
             use_fast_accum=use_fast_accum,
         )
-        assert result == result_ref
+
+        # Verify group result is accurate.
+        assert torch.equal(group_result, ref_group_result)
+        outputs.append(ref_group_result)
+
+    # Concatenate the outputs and verify the full result is correct.
+    output_ref = torch.cat(outputs, dim=0)
+    assert torch.equal(result, output_ref)
+    return output_ref
