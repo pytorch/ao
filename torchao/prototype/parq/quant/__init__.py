f4ee2d73479b01bc5d9ed6fc91f602b72090b099
lisjin <lvj@fb.com>
2025-04-04 09:33:00 -0700

    Replace torchao.prototype.parq with facebookresearch/parq submodule

--- torchao/prototype/parq/quant/__init__.py
+++ torchao/prototype/parq/quant/__init__.py
@@ -1,3 +1,13 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
+
 from .lsbq import LSBQuantizer  # noqa: F401
 from .quantizer import Quantizer  # noqa: F401
-from .uniform import UnifQuantizer  # noqa: F401
+from .uniform import (  # noqa: F401
+    MaxUnifQuantizer,
+    TernaryUnifQuantizer,
+    UnifQuantizer,
+)
