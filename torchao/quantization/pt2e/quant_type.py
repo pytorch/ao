88cd9c71e2ea1913b6921eeec6c850e39e17c455
Jerry Zhang <jerryzh168@gmail.com>
2025-04-14 18:16:15 -0700

    Copy and adapt pt2e quantization code to torchao (#2048)
    * Copy and adapt pt2e quantization code to torchao
    
    Summary:
    First step of dev-discuss.pytorch.org/t/torch-ao-quantization-migration-plan/2810
    
    core logic of pt2e are duplicated, and also ported test here
    
    previous unfinished migration: https://github.com/pytorch/ao/pull/1916
    
    Next:
    
    move meta internal callsites to depend on torchao
    Docs will be migrated separately
    
    Test Plan:
    pytest test/quantization/pt2e_flow
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    * fix import
    
    * update
    
    * import
    
    * import
    
    * import version check
    
    * import
    
    * guard pytorch version for op
    
    * torchao_quant namespace
    
    * rename namespace to torchao
    
    * trigger CI
    
    * working around 'test_int4.quantize_per_tensor_int4' has no overload name 'default' issue
    
    * fix
    
    * debug why the test failed
    
    * skip test due to out dated nightly

---
+++ torchao/quantization/pt2e/quant_type.py
@@ -0,0 +1,40 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
+
+import enum
+
+__all__ = [
+    "QuantType",
+]
+
+
+# Quantization type (dynamic quantization, static quantization).
+# Should match the c++ enum in quantization_type.h
+class QuantType(enum.IntEnum):
+    DYNAMIC = 0
+    STATIC = 1
+    QAT = 2
+    WEIGHT_ONLY = 3
+
+
+_quant_type_to_str = {
+    QuantType.STATIC: "static",
+    QuantType.DYNAMIC: "dynamic",
+    QuantType.QAT: "qat",
+    QuantType.WEIGHT_ONLY: "weight_only",
+}
+
+
+# TODO: make this private
+def _get_quant_type_to_str(quant_type: QuantType) -> str:
+    return _quant_type_to_str[quant_type]
+
+
+def _quant_type_from_str(name: str) -> QuantType:
+    for quant_type, s in _quant_type_to_str.items():
+        if name == s:
+            return quant_type
+    raise ValueError(f"Unknown QuantType name '{name}'")
