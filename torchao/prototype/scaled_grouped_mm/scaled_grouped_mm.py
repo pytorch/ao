22e34bdf5cfab760e294d3d4a9ce1db9c822cfed
Daniel Vega-Myhre <danvm@meta.com>
2025-04-21 14:12:30 -0700

    lint

--- torchao/prototype/scaled_grouped_mm/scaled_grouped_mm.py
+++ torchao/prototype/scaled_grouped_mm/scaled_grouped_mm.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD 3-Clause license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Optional, Tuple
+from typing import Optional
 
 import torch
 
