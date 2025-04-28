e9076fe20f6db2900b4c3e6ceebdea5baebcdc80
Xia, Weiwen <weiwen.xia@intel.com>
2025-04-28 01:47:11 -0700

    [PT2E][X86] Migrate fusion passes in Inductor to torchao

--- torchao/quantization/pt2e/pt2e/lowering.py
+++ torchao/quantization/pt2e/pt2e/lowering.py
@@ -7,25 +7,52 @@
 import torch
 from torch._inductor.constant_folding import constant_fold
 from torch._inductor.fx_passes.freezing_patterns import freezing_passes
+from typing import Optional
 
 __all__ = [
     "lower_pt2e_quantized_to_x86",
 ]
 
+FUSION_PATH_REGISTERED = False
+
 
 def lower_pt2e_quantized_to_x86(
     model: torch.fx.GraphModule,
-    example_inputs: tuple[torch.Tensor, ...],
+    example_inputs: Optional[tuple[torch.Tensor, ...]] = None,
+    compile: bool = True,
+    **compile_options: Optional[dict],
 ) -> torch.fx.GraphModule:
     """Lower a PT2E-qantized model to x86 backend.
 
     Args:
     * `model` (torch.fx.GraphModule): a model quantized by PT2E quantization flow.
     * `example_inputs` (tuple[torch.Tensor, ...]): example inputs for the model.
+    * `compile` (bool): whether to torch.compile the model. Default is True.
+        Torch.compile brings more performance improvement.
+    * `compile_options` (dict): options for torch.compile.
 
     Return:
-    A GraphModule lowered to x86 backend.
+    A module lowered to x86 backend.
     """
+
+    if compile:
+        global FUSION_PATH_REGISTERED
+        if not FUSION_PATH_REGISTERED:
+            global torch
+            from torchao.prototype.inductor.fx_passes.quantization import (
+                _register_quantization_weight_pack_pass,
+                quant_lift_up,
+            )
+            import torch._inductor.config
+
+            torch._inductor.config.pre_grad_custom_pass = quant_lift_up
+            _register_quantization_weight_pack_pass()
+            FUSION_PATH_REGISTERED = True
+        return torch.compile(model, **compile_options)
+
+    assert example_inputs is not None, (
+        "example_inputs should not be None when compile is False"
+    )
 
     def _post_autograd_decomp_table():  # type: ignore[no-untyped-def]
         decomp_table = torch.export.default_decompositions()
