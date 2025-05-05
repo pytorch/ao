df064d00970e24782fb0fcda500ac4724e3388e9
Jerry Zhang <jerryzh168@gmail.com>
2025-05-05 16:05:15 -0700

    format

--- test/dtypes/test_affine_quantized.py
+++ test/dtypes/test_affine_quantized.py
@@ -393,13 +393,21 @@ class TestAffineQuantizedBasic(TestCase):
     @skip_if_rocm("ROCm enablement in progress")
     def test_slice_and_copy_int4wo(self, device, dtype):
         l = torch.nn.Linear(1024, 1024).to("cuda").to(torch.bfloat16)
-        l.weight = torch.nn.Parameter(torch.zeros(1024, 1024, dtype=torch.bfloat16, device="cuda"))
+        l.weight = torch.nn.Parameter(
+            torch.zeros(1024, 1024, dtype=torch.bfloat16, device="cuda")
+        )
         quantize_(l, Int4WeightOnlyConfig())
         param = l.weight
         param_data = param.data
         param_data = param_data.narrow(0, 0, 512)
-        assert param.data.tensor_impl.packed_weight.data_ptr() == param_data.tensor_impl.packed_weight.data_ptr()
-        assert param.data.tensor_impl.scale_and_zero.data_ptr() == param_data.tensor_impl.scale_and_zero.data_ptr()
+        assert (
+            param.data.tensor_impl.packed_weight.data_ptr()
+            == param_data.tensor_impl.packed_weight.data_ptr()
+        )
+        assert (
+            param.data.tensor_impl.scale_and_zero.data_ptr()
+            == param_data.tensor_impl.scale_and_zero.data_ptr()
+        )
         assert param.data.dequantize()[0][0] == 0
 
         # dummy_l has random input (shouldn't be 0)
@@ -412,7 +420,6 @@ class TestAffineQuantizedBasic(TestCase):
 
         # making sure param.data is updated
         assert param.data.dequantize()[0][0] != 0
-
 
 
 common_utils.instantiate_parametrized_tests(TestAffineQuantized)
--- torchao/dtypes/uintx/tensor_core_tiled_layout.py
+++ torchao/dtypes/uintx/tensor_core_tiled_layout.py
@@ -353,7 +353,9 @@ class TensorCoreTiledAQTTensorImpl(AQTTensorImpl):
             n_by_8, k_by_inner_tiles, _, _ = self.packed_weight.shape
             sz_dim1, sz_dim0, _ = self.scale_and_zero.shape
             data_len = self.shape[dim]
-            assert dim in [0, 1], f"TensorCoreTiledAQTTensorImpl dispatch: attempting to run {func}, with dim={dim}, that is not supported"
+            assert dim in [0, 1], (
+                f"TensorCoreTiledAQTTensorImpl dispatch: attempting to run {func}, with dim={dim}, that is not supported"
+            )
 
             if dim == 0:
                 pw_len = n_by_8
@@ -371,8 +373,17 @@ class TensorCoreTiledAQTTensorImpl(AQTTensorImpl):
             end_sz = int(end / sz_ratio)
 
             packed_weight = aten.slice(self.packed_weight, dim, start_pw, end_pw, step)
-            scale_and_zero = aten.slice(self.scale_and_zero, 1-dim, start_sz, end_sz, step)
-            return return_and_correct_aliasing(func, args, kwargs, TensorCoreTiledAQTTensorImpl(packed_weight, scale_and_zero, self.transposed, self._layout))
+            scale_and_zero = aten.slice(
+                self.scale_and_zero, 1 - dim, start_sz, end_sz, step
+            )
+            return return_and_correct_aliasing(
+                func,
+                args,
+                kwargs,
+                TensorCoreTiledAQTTensorImpl(
+                    packed_weight, scale_and_zero, self.transposed, self._layout
+                ),
+            )
 
         raise NotImplementedError(
             f"TensorCoreTiledAQTTensorImpl dispatch: attempting to run {func}, this is not supported"
