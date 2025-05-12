9ff58a4d1c36c33ebb68dbf18b9dc3e97b6ab9c2
Jesse Cai <jcjessecai@gmail.com>
2025-05-12 04:59:04 -0700

    fix ruff for utils

--- benchmarks/benchmark_e2e_fp8_sparse_linear.py
+++ benchmarks/benchmark_e2e_fp8_sparse_linear.py
@@ -125,7 +125,7 @@ def benchmark(num_tokens, hidden_size=8192, intermediate_size=8192):
 if __name__ == "__main__":
     with torch.no_grad():
         results = []
-        for num_tokens in tqdm([512, 1024, 2048, 4096, 8192]):
+        for num_tokens in tqdm([64, 128, 256, 512, 1024, 2048, 4096]):
             results.append(benchmark(num_tokens))
             torch.compiler.reset()
 
--- torchao/prototype/sparsity/activation/utils.py
+++ torchao/prototype/sparsity/activation/utils.py
@@ -1,61 +1,59 @@
 import torch
 import torch.nn.functional as F
+from torchao.ops import to_sparse_semi_structured_cutlass_sm9x_f8
 
 
-def dump_metadata_format_cutlass():
-    r, c = 128, 256
-    # 238 in binary
-    W_ref_asdf = (
+def _dump_metadata_format_cutlass(rows=128, cols=256, device=torch.device("cuda"), dtype=torch.float8_e4m3fn):
+    """
+    This is a helper function to dump the metadata packing format for 2:4 sparse GEMMS.
+
+    We create a 2:4 sparse tensor by tiling the same pattern and then changing a singular 1x4 strip of the metadata at a time. 
+    This will allow us to find the corresponding location in the metadata that changes. 
+    """
+
+    # We tile the same pattern [0, 0, 1, 1] which yields 238 for all metadata values. 
+    dense_reference_tensor = (
         torch.Tensor([0, 0, 1, 1])
-        .to(device=device, dtype=torch.float8_e4m3fn)
-        .tile((r, c // 4))
+        .to(device=device, dtype=dtype)
+        .tile((rows, cols // 4))
         .contiguous()
     )
-    packed_reference, meta_reference = to_sparse_semi_structured_cutlass_sm9x_f8(
-        W_ref_asdf
+    _, meta_reference = to_sparse_semi_structured_cutlass_sm9x_f8(
+        dense_reference_tensor
     )
-    # W_quant_func = _float8_cutlass_quant_sparse
-    # W_aqt = W_quant_func(W_ref_asdf, dtypeq_W)
-    # W_meta = W_aqt.tensor_impl.meta
     print("INITIAL")
     print(meta_reference)
     print(meta_reference.shape, meta_reference.is_contiguous(), meta_reference.dtype)
-    breakpoint()
-    garbanzo_beans = meta_reference.tolist()
+    
+    metadata_list = meta_reference.tolist()
 
-    pattern = [1, 1, 0, 0]  # 68
-    for i in range(r):
+    # The probe pattern yields the value 68 in the metadata
+    probe_pattern = [1, 1, 0, 0]
+    for i in range(rows):
         num_per_tb = 8
-        for j in range(c // num_per_tb):
-            W_ref = W_ref_asdf.clone()
-            W_ref[i, j * num_per_tb : (j + 1) * num_per_tb] = (
-                torch.Tensor(pattern)
+        for j in range(cols // num_per_tb):
+            dense_reference_tensor_c = dense_reference_tensor.clone()
+            dense_reference_tensor_c[i, j * num_per_tb : (j + 1) * num_per_tb] = (
+                torch.Tensor(probe_pattern)
                 .to(device=device, dtype=dtype)
                 .tile((1, 2))
                 .contiguous()
             )
-            _, W_meta = to_sparse_semi_structured_cutlass_sm9x_f8(W_ref)
+            # use the reference cutlass function to pack metadata
+            _, meta_refernece_probe = to_sparse_semi_structured_cutlass_sm9x_f8(dense_reference_tensor_c)
 
-            indicies = (W_meta == 68).nonzero()
-
-            # print(indicies, i, j, W_meta)
-            # breakpoint()
+            # find where the reference packed metadata is equal to 68
+            indicies = (meta_refernece_probe== 68).nonzero()
 
             for r_i, c_i in indicies:
-                garbanzo_beans[r_i][c_i] = (
+                metadata_list[r_i][c_i] = (
                     f"a[{i:2d}, {j*num_per_tb:2d}:{(j+1)*num_per_tb:2d}]"
                 )
 
-    # from pprint import pprint
     print("METADATA FORMAT")
-    for line in garbanzo_beans:
+    for line in metadata_list:
         print(line)
         print()
-        # print(line[:4])
-        # print(line[4:])
-
-    assert False
-    # torch.testing.assert_close(W_meta, W_subclass_sparse.meta.view(torch.uint8))
 
 
 class SquaredReLU(torch.nn.Module):
