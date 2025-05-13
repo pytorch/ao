dc0d4e093ec301c5c9d3325ed3999958e3ff2a2b
Jeff Daily <jeff.daily@amd.com>
2025-05-13 22:03:14 +0000

    [reland2][ROCm] preshuffled weight mm

--- torchao/ops.py
+++ torchao/ops.py
@@ -7,7 +7,7 @@ import functools
 from typing import Optional
 
 import torch
-from torch import Tensor
+from torch import Tensor, dtype
 
 from torchao.utils import TORCH_VERSION_AT_LEAST_2_4
 
@@ -41,6 +41,12 @@ lib.define(
 )
 lib.define(
     "sparse24_sm90_sparsify(Tensor input, str metadata_fmt, str activation, str sp_selection_algo, *, ScalarType? dtype = None, Tensor? scale=None) -> (Tensor, Tensor)"
+)
+lib.define(
+    "swizzle_mm(Tensor mat1, Tensor mat2, bool mat1_is_swizzled, bool mat2_is_swizzled) -> Tensor"
+)
+lib.define(
+    "swizzle_scaled_mm(Tensor mat1, Tensor mat2, bool mat1_is_swizzled, bool mat2_is_swizzled, Tensor scale_a, Tensor scale_b, Tensor? bias=None, Tensor? scale_result=None, ScalarType? out_dtype=None) -> Tensor"
 )
 lib.define(
     "swizzle_mm(Tensor mat1, Tensor mat2, bool mat1_is_swizzled, bool mat2_is_swizzled) -> Tensor"
@@ -809,6 +815,68 @@ def _(
     bias: Optional[Tensor],
     scale_result: Optional[Tensor],
     out_dtype: Optional[torch.dtype],
+) -> Tensor:
+    return mat1.new_empty(mat1.shape[0], mat2.shape[1])
+
+
+def swizzle_mm(
+    mat1: Tensor, mat2: Tensor, mat1_is_swizzled: bool, mat2_is_swizzled: bool
+) -> Tensor:
+    """
+    Similar to torch.mm but Tensor inputs can be SwizzleTensor instances.
+
+    """
+    return torch.ops.torchao.swizzle_mm.default(
+        mat1, mat2, mat1_is_swizzled, mat2_is_swizzled
+    )
+
+
+@register_custom_op("torchao::swizzle_mm")
+def _(
+    mat1: Tensor, mat2: Tensor, mat1_is_swizzled: bool, mat2_is_swizzled: bool
+) -> Tensor:
+    return mat1.new_empty(mat1.shape[0], mat2.shape[1])
+
+
+def swizzle_scaled_mm(
+    mat1: Tensor,
+    mat2: Tensor,
+    mat1_is_swizzled: bool,
+    mat2_is_swizzled: bool,
+    scale_a: Tensor,
+    scale_b: Tensor,
+    bias: Optional[Tensor],
+    scale_result: Optional[Tensor],
+    out_dtype: Optional[dtype],
+) -> Tensor:
+    """
+    Similar to torch.mm but Tensor inputs can be SwizzleTensor instances.
+
+    """
+    return torch.ops.torchao.swizzle_scaled_mm.default(
+        mat1,
+        mat2,
+        mat1_is_swizzled,
+        mat2_is_swizzled,
+        scale_a,
+        scale_b,
+        bias,
+        scale_result,
+        out_dtype,
+    )
+
+
+@register_custom_op("torchao::swizzle_scaled_mm")
+def _(
+    mat1: Tensor,
+    mat2: Tensor,
+    mat1_is_swizzled: bool,
+    mat2_is_swizzled: bool,
+    scale_a: Tensor,
+    scale_b: Tensor,
+    bias: Optional[Tensor],
+    scale_result: Optional[Tensor],
+    out_dtype: Optional[dtype],
 ) -> Tensor:
     return mat1.new_empty(mat1.shape[0], mat2.shape[1])
 
