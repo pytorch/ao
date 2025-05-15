17c953130ac5aee9c161087a992a06ff271a612f
Jesse Cai <jcjessecai@gmail.com>
2025-05-15 14:40:19 -0700

    update

--- torchao/kernel/splitk_sparse_gemv.py
+++ torchao/kernel/splitk_sparse_gemv.py
@@ -1,45 +1,48 @@
-# adapted from deja vu
+"""
+This code is adapted from https://github.com/FasterDecoding/TEAL/blob/main/kernels/sparse_gemv.py
+
+Since we already have sparse activations from ReLU, we can get rid of the thresholding step and just use the sparse tensor directly.
+"""
 
 from typing import Optional
 
 import torch
 import triton
 import triton.language as tl
-def init_to_zero(*names):
-    def init_func(nargs):
-        for name in names:
-            nargs[name].zero_()
-    return init_func
+from torch.library import triton_op, wrap_triton
+
+def init_to_zero(*args, **kwargs):
+    # print(type)
+    args[0]["Y"].zero_()
 
 # NOTE: will need to warm up kernels each time, triton autotune caching isn't a thing right now
 
 configs=[
-    triton.Config({"BLOCK_M": 64, "BLOCK_N": 128}, num_warps=2, pre_hook=init_to_zero("Y")), 
+    triton.Config({"BLOCK_M": 64, "BLOCK_N": 128}, num_warps=2, pre_hook=init_to_zero), 
 
-    triton.Config({"BLOCK_M": 64, "BLOCK_N": 64}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 8, "BLOCK_N": 128}, num_warps=2, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 16, "BLOCK_N": 256}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 16, "BLOCK_N": 256}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 32, "BLOCK_N": 256}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 64, "BLOCK_N": 256}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 128, "BLOCK_N": 16}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 128, "BLOCK_N": 32}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 128, "BLOCK_N": 64}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 128, "BLOCK_N": 128}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 128, "BLOCK_N": 256}, num_warps=4, pre_hook=init_to_zero("Y")),
+    triton.Config({"BLOCK_M": 64, "BLOCK_N": 64}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 8, "BLOCK_N": 128}, num_warps=2, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 16, "BLOCK_N": 256}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 16, "BLOCK_N": 256}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 32, "BLOCK_N": 256}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 64, "BLOCK_N": 256}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 128, "BLOCK_N": 16}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 128, "BLOCK_N": 32}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 128, "BLOCK_N": 64}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 128, "BLOCK_N": 128}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 128, "BLOCK_N": 256}, num_warps=4, pre_hook=init_to_zero),
 
-    triton.Config({"BLOCK_M": 128, "BLOCK_N": 512}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 64, "BLOCK_N": 512}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 32, "BLOCK_N": 512}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 16, "BLOCK_N": 512}, num_warps=4, pre_hook=init_to_zero("Y")),
+    triton.Config({"BLOCK_M": 128, "BLOCK_N": 512}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 64, "BLOCK_N": 512}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 32, "BLOCK_N": 512}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 16, "BLOCK_N": 512}, num_warps=4, pre_hook=init_to_zero),
 
 
-    # Llama 3 variants can use BLOCK_N >= 1024
-    triton.Config({"BLOCK_M": 128, "BLOCK_N": 1024}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 16, "BLOCK_N": 1024}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 64, "BLOCK_N": 1024}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 32, "BLOCK_N": 1024}, num_warps=4, pre_hook=init_to_zero("Y")),
-    triton.Config({"BLOCK_M": 16, "BLOCK_N": 1024}, num_warps=4, pre_hook=init_to_zero("Y")),
+    # # Llama 3 variants can use BLOCK_N >= 1024
+    triton.Config({"BLOCK_M": 128, "BLOCK_N": 1024}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 64, "BLOCK_N": 1024}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 32, "BLOCK_N": 1024}, num_warps=4, pre_hook=init_to_zero),
+    triton.Config({"BLOCK_M": 16, "BLOCK_N": 1024}, num_warps=4, pre_hook=init_to_zero),
 ]
 
 @triton.autotune(
@@ -71,19 +74,18 @@ def splitk_sparse_gemv_kernel(
     
     # eviction policy go brrr
     if BATCHSIZE == 1:
-        x0 = tl.load(X_ptr, mask=rm < M, other=0.0, eviction_policy='evict_last') # reuse x across threadblocks
-        idx = x0 != 0 
+        x0 = tl.load(X_ptr, mask=rm < M, other=0, eviction_policy='evict_last') # reuse x across threadblocks
+        idx = (x0 != 0)
         # selectively load weight rows
-        a = tl.load(A_ptr, mask=idx[:, None], other=0.0, eviction_policy='evict_first') # only load weights once per threadblock
+        a = tl.load(A_ptr, mask=idx[:, None], other=0, eviction_policy='evict_first') # only load weights once per threadblock
         acc0 = tl.sum(a.to(tl.float32) * x0.to(tl.float32)[:, None], 0)
 
     # rematerialize rm and rn to save registers
     rn = start_n * BLOCK_N + tl.arange(0, BLOCK_N)
 
-    tl.atomic_add(Y_ptr, acc0, mask=rn < N)
+    tl.atomic_add(Y_ptr, acc0, mask=rn < N, sem="relaxed")
 
 
-from torch.library import triton_op, wrap_triton
 
 # NOTE: assumes that weight is column major
 @triton_op("torchao::splitk_sparse_gemv", mutates_args={})
@@ -100,7 +102,7 @@ def splitk_sparse_gemv(
     N, Z = weight.shape
     beam_width, seq_len, _ = x.shape
     assert x.shape[2] == Z
-    x = x.contiguous()
+    assert x.is_contiguous()
     
     assert weight.stride(1) > 1, "weight should be column major"
 
@@ -108,7 +110,7 @@ def splitk_sparse_gemv(
     grid = lambda META: (
         triton.cdiv(N, META["BLOCK_N"]),
         triton.cdiv(Z, META["BLOCK_M"]),
-    )  # noqa
+    )
 
     output = torch.empty(
         beam_width,
