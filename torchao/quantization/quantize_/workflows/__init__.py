c36c8ee6f023b875ae07fa0cc4d3872a266f8c2e
Jerry Zhang <jerryzh168@gmail.com>
2025-07-01 18:56:11 -0700

    Add all fbgemm kernel Tensors into Int4WeightOnlyConfig and Float8DynamicActivationInt4WeightConfig
    Summary:
    we will
    * deprecate FbgemmConfig since it's a single kernel (later).
    * we'd like to categorize things to derived dtype + packed format, e.g. int4 preshuffled, float8 plain
    * Added PackingFormat that has preshuffled, plain and _legacy for legacy implementation
    
    Test Plan:
    python test/quantization/quantize_/workflows/int4/test_int4_tensor.py
    python test/quantization/quantize_/workflows/int4/test_int4_preshuffled_tensor.py
    python test/quantization/quantize_/workflows/float8/test_float8_tensor.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    stack-info: PR: https://github.com/pytorch/ao/pull/2474, branch: jerryzh168/stack/10

--- torchao/quantization/quantize_/workflows/__init__.py
+++ torchao/quantization/quantize_/workflows/__init__.py
@@ -5,8 +5,12 @@ from .float8.float8_tensor import (
 from .int4.int4_preshuffled_tensor import (
     Int4PreshuffledTensor,
 )
+from .int4.int4_tensor import (
+    Int4Tensor,
+)
 
 __all__ = [
+    "Int4Tensor",
     "Int4PreshuffledTensor",
     "Float8Tensor",
     "QuantizeTensorToFloat8Kwargs",
