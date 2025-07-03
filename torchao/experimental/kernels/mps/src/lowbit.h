0aefecb1e0e7c7e5550fde7405ab4476e394ee44
angelayi <yiangela7@gmail.com>
2025-07-03 10:09:17 -0700

    Update
    [ghstack-poisoned]

--- torchao/experimental/kernels/mps/src/lowbit.h
+++ torchao/experimental/kernels/mps/src/lowbit.h
@@ -73,16 +73,11 @@ using DispatchFn =
     void (*)(id<MTLComputeCommandEncoder>, int32_t, int32_t, int32_t, int32_t);
 
 inline void linear_lowbit_quant_weights_mps_impl(
-    id<MTLBuffer> a_buf,
-    size_t a_offset,
-    id<MTLBuffer> b_buf,
-    size_t b_offset,
-    id<MTLBuffer> s_buf,
-    size_t s_offset,
-    id<MTLBuffer> z_buf,
-    size_t z_offset,
-    id<MTLBuffer> out_buf,
-    size_t out_offset,
+    std::pair<id<MTLBuffer>, size_t> a_buf_offset,
+    std::pair<id<MTLBuffer>, size_t> b_buf_offset,
+    std::pair<id<MTLBuffer>, size_t> s_buf_offset,
+    std::pair<id<MTLBuffer>, size_t> z_buf_offset,
+    std::pair<id<MTLBuffer>, size_t> out_buf_offset,
     int32_t M,
     int32_t K,
     int32_t N,
@@ -102,11 +97,11 @@ inline void linear_lowbit_quant_weights_mps_impl(
           metal_lowbit_quantized_lib.getPipelineStateForFunc(shader_func);
       const auto maxThreadsPerGroup = [cpl maxTotalThreadsPerThreadgroup];
       [computeEncoder setComputePipelineState:cpl];
-      [computeEncoder setBuffer:a_buf offset:a_offset atIndex:0];
-      [computeEncoder setBuffer:b_buf offset:b_offset atIndex:1];
-      [computeEncoder setBuffer:s_buf offset:s_offset atIndex:2];
-      [computeEncoder setBuffer:z_buf offset:z_offset atIndex:3];
-      [computeEncoder setBuffer:out_buf offset:out_offset atIndex:4];
+      [computeEncoder setBuffer:a_buf_offset.first offset:a_buf_offset.second atIndex:0];
+      [computeEncoder setBuffer:b_buf_offset.first offset:b_buf_offset.second atIndex:1];
+      [computeEncoder setBuffer:s_buf_offset.first offset:s_buf_offset.second atIndex:2];
+      [computeEncoder setBuffer:z_buf_offset.first offset:z_buf_offset.second atIndex:3];
+      [computeEncoder setBuffer:out_buf_offset.first offset:out_buf_offset.second atIndex:4];
       [computeEncoder setBytes:sizes.data()
                         length:sizeof(uint32_t) * sizes.size()
                        atIndex:5];
@@ -138,17 +133,12 @@ std::tuple<const std::string, DispatchFn> get_shader_func_and_dispatch(
 // LowBit Quantized Weights Linear on Metal
 template <int nbit>
 void linear_lowbit_quant_weights_mps(
-    id<MTLBuffer> a_buf,
-    size_t a_offset,
-    id<MTLBuffer> b_buf,
-    size_t b_offset,
+    std::pair<id<MTLBuffer>, size_t> a_buf_offset,
+    std::pair<id<MTLBuffer>, size_t> b_buf_offset,
     int64_t qGroupSize,
-    id<MTLBuffer> s_buf,
-    size_t s_offset,
-    id<MTLBuffer> z_buf,
-    size_t z_offset,
-    id<MTLBuffer> out_buf,
-    size_t out_offset,
+    std::pair<id<MTLBuffer>, size_t> s_buf_offset,
+    std::pair<id<MTLBuffer>, size_t> z_buf_offset,
+    std::pair<id<MTLBuffer>, size_t> out_buf_offset,
     int32_t M,
     int32_t K,
     int32_t N,
@@ -164,16 +154,11 @@ void linear_lowbit_quant_weights_mps(
   const DispatchFn dispatch_fn = std::get<1>(shader_func_and_dispatch);
 
   return linear_lowbit_quant_weights_mps_impl(
-      a_buf,
-      a_offset,
-      b_buf,
-      b_offset,
-      s_buf,
-      s_offset,
-      z_buf,
-      z_offset,
-      out_buf,
-      out_offset,
+      a_buf_offset,
+      b_buf_offset,
+      s_buf_offset,
+      z_buf_offset,
+      out_buf_offset,
       M,
       K,
       N,
