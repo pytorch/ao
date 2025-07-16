d2f63e95d27b150befeafb31431c38a7b5250c36
Daniel Vega-Myhre <danvm@meta.com>
2025-07-16 09:44:53 -0700

    single and multi device float8 test workflows in ci

---
+++ test/float8/test_everything_single_gpu.sh
@@ -0,0 +1,18 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
+#!/bin/bash
+
+# terminate script on first error
+set -e
+
+pytest test/float8/test_base.py --verbose -s
+pytest test/float8/test_compile.py --verbose -s
+pytest test/float8/test_numerics_integration.py --verbose -s
+pytest test/float8/test_auto_filter.py --verbose -s
+pytest test/integration --verbose -s
+pytest test/dtypes/test_affine_quantized_float.py --verbose -s
+
+echo "all single gpu tests successful"
