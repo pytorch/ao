ed26e9bac651e69e1d561ae4bca187880c08b4fe
Huy Do <huydhn@gmail.com>
2025-07-25 19:04:24 -0700

    Just use pip
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- .github/scripts/run_vllm_tests.sh
+++ .github/scripts/run_vllm_tests.sh
@@ -4,10 +4,10 @@ set -eux
 
 
 # vLLM docker image is using CUDA 12.8 and python 3.12
-uv pip install --pre fbgemm-gpu-genai --index-url https://download.pytorch.org/whl/nightly/cu128
-uv pip install -r dev-requirements.txt
+pip install --pre fbgemm-gpu-genai --index-url https://download.pytorch.org/whl/nightly/cu128
+pip install -r dev-requirements.txt
 # Build and install ao
-uv pip install .
+pip install .
 
 pushd vllm
 pytest test/integration --verbose -s
