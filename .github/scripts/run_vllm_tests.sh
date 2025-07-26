8665c733d0208e8b157a663dbed7619df9c4af76
Huy Do <huydhn@gmail.com>
2025-07-25 19:46:14 -0700

    Use fbgemm stable
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- .github/scripts/run_vllm_tests.sh
+++ .github/scripts/run_vllm_tests.sh
@@ -2,9 +2,8 @@
 
 set -eux
 
-
 # vLLM docker image is using CUDA 12.8 and python 3.12
-pip install --pre fbgemm-gpu-genai --index-url https://download.pytorch.org/whl/nightly/cu128
+pip install --pre fbgemm-gpu-genai --index-url https://download.pytorch.org/whl/cu128
 pip install -r dev-requirements.txt
 # Build and install ao
 pip install .
