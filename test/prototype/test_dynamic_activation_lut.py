be994bff76960e824f662ce1a947f9412b124f6e
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-07-29 09:37:42 -0700

    up

--- test/prototype/test_dynamic_activation_lut.py
+++ test/prototype/test_dynamic_activation_lut.py
@@ -3,6 +3,7 @@
 #
 # This source code is licensed under the BSD 3-Clause license found in the
 # LICENSE file in the root directory of this source tree.
+
 import platform
 import sys
 from copy import deepcopy
