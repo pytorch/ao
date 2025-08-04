6c817e207cbdc766e2c9f8c2623c2208aa577bf1
vasiliy <vasiliy@fb.com>
2025-08-04 05:22:40 -0700

    [not for land] float8 debug recipe for float8 rowwise fwd and hp bwd
    Summary:
    
    Test Plan:
    
    ```bash
    with-proxy
    CONFIG_FILE="./torchtitan/models/llama3/train_configs/debug_model.toml"
    ./run_train.sh --model.print_after_conversion --model.converters float8
    --training.compile --float8.recipe_name fwd_float8_bwd_hp
    ```
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- torchao/float8/config.py
+++ torchao/float8/config.py
@@ -134,6 +134,9 @@ class Float8LinearRecipeName(enum.Enum):
     #   * the e4m3 dtype is used across the board, including for gradients
     ROWWISE_WITH_GW_HP = "rowwise_with_gw_hp"
 
+    # debug only, not for land
+    FWD_FLOAT8_BWD_HP = "fwd_float8_bwd_hp"
+
 
 @dataclass(frozen=True)
 class Float8LinearConfig:
@@ -325,6 +328,28 @@ class Float8LinearConfig:
             cc_go_gw = CastConfig(
                 scaling_type=ScalingType.DISABLED, target_dtype=e4m3_dtype
             )
+
+            return Float8LinearConfig(
+                cast_config_input=cc_i,
+                cast_config_weight=cc_w,
+                cast_config_grad_output=cc_go,
+                cast_config_input_for_grad_weight=cc_i_gw,
+                cast_config_weight_for_grad_input=cc_w_gi,
+                cast_config_grad_output_for_grad_weight=cc_go_gw,
+            )
+
+        elif recipe_name is Float8LinearRecipeName.FWD_FLOAT8_BWD_HP:
+            # output_hp = input_fp8_axiswise_dim0 @ weight_t_axiswise_dim1
+            cc_i = CastConfig(scaling_granularity=ScalingGranularity.AXISWISE)
+            cc_w = CastConfig(scaling_granularity=ScalingGranularity.AXISWISE)
+
+            # grad_input_hp = grad_output_hp @ weight_hp
+            cc_go = CastConfig(scaling_type=ScalingType.DISABLED)
+            cc_w_gi = CastConfig(scaling_type=ScalingType.DISABLED)
+
+            # grad_weight_hp = input_t_hp @ grad_output_hp
+            cc_i_gw = CastConfig(scaling_type=ScalingType.DISABLED)
+            cc_go_gw = CastConfig(scaling_type=ScalingType.DISABLED)
 
             return Float8LinearConfig(
                 cast_config_input=cc_i,
