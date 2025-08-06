0043879de8db0fd4dac06e08bcb35899142b9af6
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-08-05 21:58:20 -0700

    Update test_quant_passes.py
    Reduce tolerance to make tests less flakey

--- torchao/experimental/tests/test_quant_passes.py
+++ torchao/experimental/tests/test_quant_passes.py
@@ -91,7 +91,7 @@ class TestQuantPasses(unittest.TestCase):
 
         # Numerics should match
         exported_results = exported.module()(activations)
-        self.assertTrue(torch.allclose(exported_results, eager_results))
+        self.assertTrue(torch.allclose(exported_results, eager_results, atol=1e-3, rtol=1e-3))
 
     @parameterized.expand(
         [
@@ -148,7 +148,7 @@ class TestQuantPasses(unittest.TestCase):
 
         # Numerics should match
         exported_results = exported.module()(indices)
-        self.assertTrue(torch.allclose(exported_results, eager_results))
+        self.assertTrue(torch.allclose(exported_results, eager_results, atol=1e-3, rtol=1e-3))
 
 
 if __name__ == "__main__":
