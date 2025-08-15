b40fd97d477ba734f83ebfea6380b3f90476a50b
liangel-02 <liangel@meta.com>
2025-08-15 15:47:19 -0700

    Int4 sparse marlin tensor (#2771)
    * added marlin sparse to packing format, inital commit
    
    * deleting unnecessary functions
    
    * packing
    
    * linear
    
    * add call to from_hp
    
    * unit test
    
    * fix test_linear
    
    * formatting
    
    * remove comments
    
    * update VERSION to version
    
    * fix module path unit test
    
    * adding sizes to linear unit test
    
    * move pre_process and from_plain to from_hp
    
    * compile test

--- torchao/quantization/quantize_/workflows/__init__.py
+++ torchao/quantization/quantize_/workflows/__init__.py
@@ -2,6 +2,9 @@ from .float8.float8_tensor import (
     Float8Tensor,
     QuantizeTensorToFloat8Kwargs,
 )
+from .int4.int4_marlin_sparse_tensor import (
+    Int4MarlinSparseTensor,
+)
 from .int4.int4_preshuffled_tensor import (
     Int4PreshuffledTensor,
 )
@@ -12,6 +15,7 @@ from .int4.int4_tensor import (
 __all__ = [
     "Int4Tensor",
     "Int4PreshuffledTensor",
+    "Int4MarlinSparseTensor",
     "Float8Tensor",
     "QuantizeTensorToFloat8Kwargs",
 ]
