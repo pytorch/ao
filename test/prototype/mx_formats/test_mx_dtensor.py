91927994f7c9045c11014af4178c60295eeb7e50
Vasiliy Kuznetsov <vkuzo@users.noreply.github.com>
2025-08-15 14:39:15 -0400

    mx_formats: make emulated tests pass on H100, and add to CI (#2773)
    Update
    
    [ghstack-poisoned]

--- test/prototype/mx_formats/test_mx_dtensor.py
+++ test/prototype/mx_formats/test_mx_dtensor.py
@@ -15,7 +15,7 @@ import os
 import pytest
 import torch
 
-from torchao.utils import TORCH_VERSION_AT_LEAST_2_7
+from torchao.utils import TORCH_VERSION_AT_LEAST_2_7, is_sm_at_least_100
 
 if not TORCH_VERSION_AT_LEAST_2_7:
     pytest.skip("Unsupported PyTorch version", allow_module_level=True)
@@ -109,8 +109,9 @@ if __name__ == "__main__":
         _test_dtensor_cast_to_mxfp8,
         _test_mxfp8_mlp_tensor_parallelism,
         _test_mxfp8_mlp_tensor_parallelism_dim1_triton,
-        _test_mxfp8_mlp_tensor_parallelism_dim1_cuda,
     ]
+    if is_sm_at_least_100():
+        tests.append(_test_mxfp8_mlp_tensor_parallelism_dim1_cuda)
 
     for test in tqdm(tests, desc="Running tests"):
         try:
