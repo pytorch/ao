5c0d6a3fa6b86f7711165fb1c4a4bdf6bd771944
Vasiliy Kuznetsov <vkuzo@users.noreply.github.com>
2025-08-18 18:54:15 -0400

    nvfp4 tensor: refactor weight-only vs dynamic quant (#2790)
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]

--- test/prototype/mx_formats/test_mx_tensor.py
+++ test/prototype/mx_formats/test_mx_tensor.py
@@ -916,8 +916,8 @@ def test_nvfp4_swizzled_scales_serialization():
     tensor_list, ctx = original_tensor.__tensor_flatten__()
 
     # Verify swizzled flag is preserved in context
-    assert NVFP4Tensor.tensor_attribute_names[3] == "_is_swizzled_scales"
-    assert ctx[3] == True
+    assert NVFP4Tensor.tensor_attribute_names[2] == "_is_swizzled_scales"
+    assert ctx[2] == True
 
     # Test deserialization
     inner_tensors = {}
