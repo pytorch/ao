75e67fbc9f13000f4c9b82ad928f6f9167ea630a
Zhang, Liangang <liangang.zhang@intel.com>
2025-08-19 09:12:09 +0800

    Update int4_xpu_layout.py

--- torchao/dtypes/uintx/int4_xpu_layout.py
+++ torchao/dtypes/uintx/int4_xpu_layout.py
@@ -81,8 +81,7 @@ def _linear_bf16_act_uint4_weight_float_zero_impl(input_tensor, weight_tensor, b
 
     # groupwise int4 quantization
     groupsize = weight_tensor.block_size[1]
-    import pdb 
-    pdb.set_trace()
+    
     y = torch.ops.aten._weight_int4pack_mm(
         act_mat, packed_weight, groupsize, scales_and_zeros
     )
