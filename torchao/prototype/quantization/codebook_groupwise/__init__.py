7f7f626c089f0a588771a11ae1cb41b8502797f4
Zeyu Song <87307087+szyszyzys@users.noreply.github.com>
2025-08-23 02:38:47 -0400

    Add lut quantized embedding.
    Differential Revision: D79750002
    
    Pull Request resolved: https://github.com/pytorch/ao/pull/2824

--- torchao/prototype/quantization/codebook_groupwise/__init__.py
+++ torchao/prototype/quantization/codebook_groupwise/__init__.py
@@ -1,4 +1,9 @@
 from .api import GroupwiseLutWeightConfig
 from .codebook_quantized_tensor import CodebookQuantizedPackedTensor
 
-__all__ = ["CodebookQuantizedPackedTensor", "GroupwiseLutWeightConfig"]
+__all__ = [
+    "CodebookQuantizedPackedTensor",
+    "GroupwiseLutWeightConfig",
+    "QuantizedLutEmbedding",
+    "EmbeddingLutQuantizer",
+]
