891bd21efa6d4c37dff4e2aa7031e5dd16c762d5
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-08-26 09:22:54 -0700

    Update IntxUnpackedTensor to support dynamic activation (#2861)
    * Updates to unpacked tensor to support activation quant
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up

--- torchao/quantization/quantize_/workflows/__init__.py
+++ torchao/quantization/quantize_/workflows/__init__.py
@@ -11,8 +11,8 @@ from .int4.int4_preshuffled_tensor import (
 from .int4.int4_tensor import (
     Int4Tensor,
 )
-from .intx.intx_unpacked_tensor import (
-    IntxUnpackedTensor,
+from .intx.intx_unpacked_to_int8_tensor import (
+    IntxUnpackedToInt8Tensor,
 )
 
 __all__ = [
@@ -21,5 +21,5 @@ __all__ = [
     "Int4MarlinSparseTensor",
     "Float8Tensor",
     "QuantizeTensorToFloat8Kwargs",
-    "IntxUnpackedTensor",
+    "IntxUnpackedToInt8Tensor",
 ]
