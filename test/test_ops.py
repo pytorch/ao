2a12cf985c859a13e86efaaab645b73dd100af9d
wengshiy <shiyang.weng@intel.com>
2025-09-01 22:41:19 -0400

    add int8 embeddingbag

--- test/test_ops.py
+++ test/test_ops.py
@@ -768,6 +768,7 @@ EMBEDINGBAG_MULTIHOT_SIZES = [1, 2, 3, 10]
 EMBEDINGBAG_BAG_SIZES = [1, 2, 128, 1024]
 EMBEDINGBAG_VECTOR_SIZES = [1, 128, 512]
 EMBEDINGBAG_INDEX_DTYPES = [torch.int64, torch.int32]
+EMBEDINGBAG_QTYPE = [torch.float8_e4m3fn, torch.int8]
 
 EMBEDINGBAG_TEST_PARAMS = list(
     itertools.product(
@@ -775,6 +776,7 @@ EMBEDINGBAG_TEST_PARAMS = list(
         EMBEDINGBAG_BAG_SIZES,
         EMBEDINGBAG_VECTOR_SIZES,
         EMBEDINGBAG_INDEX_DTYPES,
+        EMBEDINGBAG_QTYPE,
     )
 )
 
@@ -784,14 +786,12 @@ EMBEDINGBAG_TEST_PARAMS = list(
     reason="cpp kernels not built",
 )
 @pytest.mark.parametrize(
-    "multi_hot, batch_size, vector_size, index_type",
+    "multi_hot, batch_size, vector_size, index_type, qtype",
     EMBEDINGBAG_TEST_PARAMS,
     ids=str,
 )
-def test_scaled_embedding_bag_cpu(multi_hot, batch_size, vector_size, index_type):
-    qtype = torch.float8_e4m3fn
+def test_scaled_embedding_bag_cpu(multi_hot, batch_size, vector_size, index_type, qtype):
     dtype = torch.float32
-    weight_scale = torch.tensor([2.0])
     include_last_offset = True
     mode = "sum"
 
@@ -811,13 +811,18 @@ def test_scaled_embedding_bag_cpu(multi_hot, batch_size, vector_size, index_type
         dtype=dtype,
         include_last_offset=include_last_offset,
     )
-    fp8_weight = m.weight.data.to(qtype)
-    m.weight.data = fp8_weight.to(m.weight.dtype)
+    if qtype == torch.int8:
+        weight_scale = 127.0 / m.weight.data.abs().max()
+        qweight = (m.weight.data*weight_scale).to(qtype)
+    else:
+        weight_scale = torch.tensor([2.0])
+        qweight = m.weight.data.to(qtype)
+    m.weight.data = qweight.to(m.weight.dtype)
 
     with torch.no_grad():
         refe_out = m.forward(indices, offsets) * weight_scale
         test_out = torch.ops.torchao._scaled_embedding_bag(
-            fp8_weight,
+            qweight,
             indices,
             offsets,
             weight_scale,
--- torchao/csrc/cpu/scaled_embedding_bag.cpp
+++ torchao/csrc/cpu/scaled_embedding_bag.cpp
@@ -102,6 +102,119 @@ inline void _scaled_embedding_bag_krnl(
   }
 }
 
+template <typename index_t>
+inline void _scaled_embedding_bag_krnl(
+    const int64_t bs_begin, const int64_t bs_end, const int64_t num_emb,
+    const int64_t emb_dim, const index_t last_offset, const index_t *indices,
+    const index_t *offsets, const int8_t *weight, const double scale,
+    float *result, const int64_t num_batch) {
+#if defined(CPU_CAPABILITY_AVX512)
+  if (emb_dim % 128 == 0) {
+    constexpr int64_t block_dim = 128;
+    const int64_t num_blocks = emb_dim / block_dim;
+    __m512 scale_v = _mm512_set1_ps(scale);
+    for (int64_t b = bs_begin; b < bs_end; ++b) {
+      __m512i x00, x64;
+      __m512i y0, y1, y2, y3, y4, y5, y6, y7;
+      __m512 f0, f1, f2, f3, f4, f5, f6, f7;
+      int64_t start_idx = offsets[b];
+      int64_t end_idx = ((b + 1) == num_batch && last_offset != -1)
+                            ? last_offset
+                            : offsets[b + 1];
+      for (int64_t block_id = 0; block_id < num_blocks; block_id++) {
+        // load first indices
+        int64_t idx = indices[start_idx] * emb_dim + block_dim * block_id;
+            float *block_result = result + block_dim * block_id;
+        x00 = _mm512_load_si512(&weight[idx]);
+        x64 = _mm512_load_si512(&weight[idx + 64]);
+        y0 = _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x00, 0));
+        y1 = _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x00, 1));
+        y2 = _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x00, 2));
+        y3 = _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x00, 3));
+        y4 = _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x64, 0));
+        y5 = _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x64, 1));
+        y6 = _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x64, 2));
+        y7 = _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x64, 3));
+        for (int64_t j = start_idx + 1; j < end_idx; ++j) {
+            // add following idx
+            idx = indices[j] * emb_dim + block_dim * block_id;
+            x00 = _mm512_load_si512(&weight[idx]);
+            x64 = _mm512_load_si512(&weight[idx + 64]);
+            y0 = _mm512_add_epi32(
+                y0, _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x00, 0)));
+            y1 = _mm512_add_epi32(
+                y1, _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x00, 1)));
+            y2 = _mm512_add_epi32(
+                y2, _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x00, 2)));
+            y3 = _mm512_add_epi32(
+                y3, _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x00, 3)));
+            y4 = _mm512_add_epi32(
+                y4, _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x64, 0)));
+            y5 = _mm512_add_epi32(
+                y5, _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x64, 1)));
+            y6 = _mm512_add_epi32(
+                y6, _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x64, 2)));
+            y7 = _mm512_add_epi32(
+                y7, _mm512_cvtepi8_epi32(_mm512_extracti32x4_epi32(x64, 3)));
+        }
+        f0 = _mm512_cvt_roundepi32_ps(
+            y0, (_MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC));
+        f1 = _mm512_cvt_roundepi32_ps(
+            y1, (_MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC));
+        f2 = _mm512_cvt_roundepi32_ps(
+            y2, (_MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC));
+        f3 = _mm512_cvt_roundepi32_ps(
+            y3, (_MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC));
+        f4 = _mm512_cvt_roundepi32_ps(
+            y4, (_MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC));
+        f5 = _mm512_cvt_roundepi32_ps(
+            y5, (_MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC));
+        f6 = _mm512_cvt_roundepi32_ps(
+            y6, (_MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC));
+        f7 = _mm512_cvt_roundepi32_ps(
+            y7, (_MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC));
+        f0 = _mm512_mul_ps(f0, scale_v);
+        f1 = _mm512_mul_ps(f1, scale_v);
+        f2 = _mm512_mul_ps(f2, scale_v);
+        f3 = _mm512_mul_ps(f3, scale_v);
+        f4 = _mm512_mul_ps(f4, scale_v);
+        f5 = _mm512_mul_ps(f5, scale_v);
+        f6 = _mm512_mul_ps(f6, scale_v);
+        f7 = _mm512_mul_ps(f7, scale_v);
+        // store
+        _mm512_store_ps(block_result, f0);
+        _mm512_store_ps(block_result + 16, f1);
+        _mm512_store_ps(block_result + 32, f2);
+        _mm512_store_ps(block_result + 48, f3);
+        _mm512_store_ps(block_result + 64, f4);
+        _mm512_store_ps(block_result + 80, f5);
+        _mm512_store_ps(block_result + 96, f6);
+        _mm512_store_ps(block_result + 112, f7);
+      }
+      result += num_emb * emb_dim;
+    }
+    return;
+  }
+#endif
+  for (int64_t b = bs_begin; b < bs_end; ++b) {
+    int64_t start_idx = offsets[b];
+    int64_t end_idx = ((b + 1) == num_batch && last_offset != -1)
+                          ? last_offset
+                          : offsets[b + 1];
+    for (int64_t d = 0; d < emb_dim; d++) {
+      int64_t idx = indices[start_idx] * emb_dim;
+      float value = float(weight[idx + d]);
+      for (int64_t j = start_idx + 1; j < end_idx; ++j) {
+        idx = indices[j] * emb_dim;
+        value += float(weight[idx + d]);
+      }
+      value = value * scale;
+      result[d] = value;
+    }
+    result += num_emb * emb_dim;
+  }
+}
+
 template <typename index_t, typename data_t>
 void _scaled_embedding_bag(float *o_ptr, data_t *w_ptr, index_t *indices_ptr,
                            index_t *offsets_ptr, int64_t num_batch,
@@ -143,6 +256,7 @@ at::Tensor _scaled_embedding_bag_impl(const at::Tensor &qweight,
   int64_t emb_dim = qweight.size(1);
 
   auto index_type = indices.scalar_type();
+  auto qtype = qweight.scalar_type();
   float w_scale = w_scales.data_ptr<float>()[0];
 
   TORCH_CHECK(indices.is_contiguous() && offsets.is_contiguous(),
@@ -154,22 +268,35 @@ at::Tensor _scaled_embedding_bag_impl(const at::Tensor &qweight,
               "_scaled_embedding_bag: only accept contiguous weight");
   TORCH_CHECK(qweight.dim() == 2,
               "_scaled_embedding_bag: only accept weight with dim == 2");
-  TORCH_CHECK(qweight.scalar_type() == c10::ScalarType::Float8_e4m3fn,
-              "_scaled_embedding_bag: only support e4m3fn weight")
+  TORCH_CHECK(qweight.scalar_type() == c10::ScalarType::Float8_e4m3fn || qweight.scalar_type() == c10::ScalarType::Char,
+              "_scaled_embedding_bag: only support e4m3fn and int8 weight")
   // handle last offsets
   int64_t last_offset = indices.numel();
 
   at::Tensor output =
       at::empty({batch_size, emb_dim}, qweight.options().dtype(at::kFloat));
-  AT_DISPATCH_INDEX_TYPES(indices.scalar_type(), "embeddingbag_cat", [&] {
-    at::Float8_e4m3fn *qweight_ptr = qweight.data_ptr<at::Float8_e4m3fn>();
-    index_t *indices_ptr = indices.data_ptr<index_t>();
-    index_t *offsets_ptr = offsets.data_ptr<index_t>();
-    float *output_ptr = output.data_ptr<float>();
-    _scaled_embedding_bag<index_t, at::Float8_e4m3fn>(
-        output_ptr, qweight_ptr, indices_ptr, offsets_ptr, batch_size, emb_dim,
-        last_offset, w_scale, o_scale);
-  });
+  if (qweight.scalar_type() == c10::ScalarType::Float8_e4m3fn) {
+    AT_DISPATCH_INDEX_TYPES(indices.scalar_type(), "_scaled_embedding_bag", [&] {
+      at::Float8_e4m3fn *qweight_ptr = qweight.data_ptr<at::Float8_e4m3fn>();
+      index_t *indices_ptr = indices.data_ptr<index_t>();
+      index_t *offsets_ptr = offsets.data_ptr<index_t>();
+      float *output_ptr = output.data_ptr<float>();
+      _scaled_embedding_bag<index_t, at::Float8_e4m3fn>(
+          output_ptr, qweight_ptr, indices_ptr, offsets_ptr, batch_size, emb_dim,
+          last_offset, w_scale, o_scale);
+    });
+  } else {
+    AT_DISPATCH_INDEX_TYPES(indices.scalar_type(), "_scaled_embedding_bag", [&] {
+      int8_t *qweight_ptr = qweight.data_ptr<int8_t>();
+      index_t *indices_ptr = indices.data_ptr<index_t>();
+      index_t *offsets_ptr = offsets.data_ptr<index_t>();
+      float *output_ptr = output.data_ptr<float>();
+      _scaled_embedding_bag<index_t, int8_t>(
+          output_ptr, qweight_ptr, indices_ptr, offsets_ptr, batch_size, emb_dim,
+          last_offset, w_scale, o_scale);
+    });
+  }
+
   return output;
 }
 
