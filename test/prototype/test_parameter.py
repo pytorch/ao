d2ebe99325a156f2858b26d272214a399a3cf0c2
Jesse Cai <jcjessecai@gmail.com>
2025-09-23 11:32:20 -0700

    update

--- test/prototype/test_parameter.py
+++ test/prototype/test_parameter.py
@@ -187,24 +187,13 @@ class TestQuantizeParameterMoE(common_utils.TestCase):
 class TestTorchAOCheckpoint(TestCase):
 
     def test_comprehensive_checkpoint_loading(self):
-        from transformers import AutoTokenizer, Llama4ForConditionalGeneration
-        import torch
+        from transformers import AutoConfig, AutoModel
+        from transformers.models.llama4.modeling_llama4 import Llama4TextMoe
 
-        model_id = "RedHatAI/Llama-4-Scout-17B-16E-Instruct"
-
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
-
-        messages = [
-            {"role": "user", "content": "Who are you?"},
-        ]
-        inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt", return_dict=True)
-
-        model = Llama4ForConditionalGeneration.from_pretrained(
-            model_id,
-            device_map="auto",
-            dtype=torch.bfloat16
-        )
-
-        outputs = model.generate(**inputs.to(model.device), max_new_tokens=100)
-        outputs = tokenizer.batch_decode(outputs[:, inputs["input_ids"].shape[-1]:])
-        print(outputs[0])
+        config = AutoConfig.from_pretrained("unsloth/Llama-4-Scout-17B-16E-Instruct")
+        model = Llama4TextMoe(config.text_config).to(torch.bfloat16).cuda()
+        quantize_(
+            model,
+            Float8DynamicActivationFloat8WeightConfig(granularity=PerRow()))
+        print(model)
+        print("DONE")
