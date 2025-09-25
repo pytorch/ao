858ea7b39b2bbb7648c07fd2e0d573dd832ea15b
Angel Li <liangel@meta.com>
2025-09-25 06:55:53 -0700

    add int4preshuffledtensor to safetensors
    ghstack-source-id: c7f9d15b04592b053350e7452bd80954ed1237af
    Pull Request resolved: https://github.com/pytorch-labs/ao/pull/3063

--- test/prototype/safetensors/test_safetensors_support.py
+++ test/prototype/safetensors/test_safetensors_support.py
@@ -19,6 +19,7 @@ from torchao.prototype.safetensors.safetensors_support import (
 from torchao.quantization.granularity import PerRow
 from torchao.quantization.quant_api import (
     Float8DynamicActivationFloat8WeightConfig,
+    Float8DynamicActivationInt4WeightConfig,
     Int4WeightOnlyConfig,
 )
 from torchao.utils import is_sm_at_least_89
@@ -45,6 +46,7 @@ class TestSafeTensors(TestCase):
             (Float8DynamicActivationFloat8WeightConfig(granularity=PerRow()), False),
             (Int4WeightOnlyConfig(), False),
             (Int4WeightOnlyConfig(), True),
+            (Float8DynamicActivationInt4WeightConfig(), False),
         ],
     )
     def test_safetensors(self, config, act_pre_scale=False):
