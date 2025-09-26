b46ca6394a3a2e48072642664a8bd2082794bfee
DiweiSun <105627594+DiweiSun@users.noreply.github.com>
2025-09-26 09:28:51 +0800

    refine ci test script

--- .github/scripts/ci_test_xpu.sh
+++ .github/scripts/ci_test_xpu.sh
@@ -8,8 +8,10 @@ export CXX=/usr/bin/g++
 export SCCACHE_DISABLE=1
 
 python3 -m pip install torch torchvision torchaudio pytorch-triton-xpu --index-url https://download.pytorch.org/whl/nightly/xpu --force-reinstall --no-cache-dir 
-cd torchao && python3 setup.py install
+cd torchao && python3 setup.py install && cd ..
+
+python3 -c "import torch; import torchao; print(f'Torch version: {torch.__version__}')"
 
 pip install pytest expecttest parameterized accelerate hf_transfer 'modelscope!=1.15.0'
 
-pytest -v -s test/quantization/quantize_/workflows/int4/test_int4_plain_int32_tensor.py 
+pytest -v -s torchao/test/quantization/quantize_/workflows/int4/test_int4_plain_int32_tensor.py 
