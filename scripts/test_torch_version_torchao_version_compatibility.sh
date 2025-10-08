e5d724480b6802fe9aea8a4f6500d29e67081e56
jainapurva <apurvajain.kota@gmail.com>
2025-10-08 11:57:29 -0700

    more updates

--- scripts/test_torch_version_torchao_version_compatibility.sh
+++ scripts/test_torch_version_torchao_version_compatibility.sh
@@ -1,10 +1,16 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
+
 # Testing compatibility
 # We know that torchao .so files built using PyTorch 2.8.0 are not ABI compatible with PyTorch 2.9+. (see #2919)
 # If the version of torch is not compatible with the version of torchao,
 # we expect to skip loading the .so files and a warning should be logged but no error
 
 PREV_TORCH_VERSION = 2.8.0
-PREV_TORCHAO_VERSION = 0.13.0
+PREV_TORCHAO_VERSION = 0.14.0
 
 # Function to check torchao import with configurable expectations
 check_torchao_import() {
