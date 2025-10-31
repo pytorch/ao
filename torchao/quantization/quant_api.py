b49178c617e8c8a60e7580f58a564273d5d1be4f
Vasiliy Kuznetsov <vkuzo@users.noreply.github.com>
2025-10-31 15:43:11 -0400

    add a_1_128_w_128_128 (DeepSeek) float8 scaling for inference (#3257)
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]

--- torchao/quantization/quant_api.py
+++ torchao/quantization/quant_api.py
@@ -62,6 +62,7 @@ from torchao.float8.inference import (
     Float8MMConfig,
     FP8Granularity,
     _check_hardware_support,
+    _granularity_is_a_1_128_w_128_128,
     _normalize_granularity,
 )
 from torchao.quantization.linear_activation_weight_observed_tensor import (
@@ -1770,12 +1771,25 @@ class Float8DynamicActivationFloat8WeightConfig(AOBaseConfig):
         torch._C._log_api_usage_once(
             "torchao.quantization.Float8DynamicActivationFloat8WeightConfig"
         )
-        if self.mm_config is None:
-            self.mm_config = Float8MMConfig(use_fast_accum=True)
         activation_granularity, weight_granularity = _normalize_granularity(
             self.granularity
         )
         self.granularity = [activation_granularity, weight_granularity]
+
+        default_use_fast_accum = True
+        if _granularity_is_a_1_128_w_128_128(self.granularity):
+            assert self.activation_value_lb is None, "unimplemented"
+            assert self.activation_value_ub is None, "unimplemented"
+            assert self.kernel_preference in (
+                KernelPreference.AUTO,
+                KernelPreference.TORCH,
+            ), "unimplemented"
+            assert self.mm_config is None, "unimplemented"
+            assert self.version >= 2, "unimplemented"
+            default_use_fast_accum = False
+
+        if self.mm_config is None:
+            self.mm_config = Float8MMConfig(use_fast_accum=default_use_fast_accum)
 
 
 # for bc
