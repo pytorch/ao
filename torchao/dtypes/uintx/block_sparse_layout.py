d23530543adbc1147abd529b5051a7da49f8c64d
Apurva Jain <apurvajain.kota@gmail.com>
2025-11-05 11:19:39 -0800

    Update deprecation warning message for BlockSparseLayout

--- torchao/dtypes/uintx/block_sparse_layout.py
+++ torchao/dtypes/uintx/block_sparse_layout.py
@@ -10,7 +10,7 @@ import warnings
 warnings.warn(
     "Importing BlockSparseLayout from torchao.dtypes is deprecated. "
     "Please use 'from torchao.prototype.dtypes import BlockSparseLayout' instead. "
-    "This import path will be removed in torchao v0.16.0. "
+    "This import path will be removed in future torchao releases. "
     "Please check issue: https://github.com/pytorch/ao/issues/2752 for more details. ",
     DeprecationWarning,
     stacklevel=2,
