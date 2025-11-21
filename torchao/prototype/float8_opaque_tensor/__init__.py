20c657924d77747e19c912ec31990f19804e7d8d
Xia, Weiwen <weiwen.xia@intel.com>
2025-11-21 13:33:40 +0000

    Move float8_opaque_tensor to prototype

---
+++ torchao/prototype/float8_opaque_tensor/__init__.py
@@ -0,0 +1,7 @@
+from .float8_opaque_tensor import Float8OpaqueTensor
+from .inference_workflow import Float8DynamicActivationFloat8WeightOpaqueTensorConfig
+
+__all__ = [
+    "Float8OpaqueTensor",
+    "Float8DynamicActivationFloat8WeightOpaqueTensorConfig",
+]
