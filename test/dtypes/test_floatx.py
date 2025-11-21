a55d5f4b37d24764867abb056e7c9cce67e792c6
Zeng, Xiangdong <xiangdong.zeng@intel.com>
2025-11-21 14:49:25 +0800

    fix format issue

--- test/dtypes/test_floatx.py
+++ test/dtypes/test_floatx.py
@@ -35,7 +35,11 @@ from torchao.quantization import (
 from torchao.testing.utils import skip_if_rocm
 from torchao.utils import is_fbcode, get_current_accelerator_device
 
-_DEVICES = ["cpu"] + (["cuda"] if torch.cuda.is_available() else []) + (["xpu"] if torch.xpu.is_available() else [])
+_DEVICES = (
+    ["cpu"]
+    + (["cuda"] if torch.cuda.is_available() else [])
+    + (["xpu"] if torch.xpu.is_available() else [])
+)
 _Floatx_DTYPES = [(3, 2), (2, 2)]
 _DEVICE = get_current_accelerator_device()
 
