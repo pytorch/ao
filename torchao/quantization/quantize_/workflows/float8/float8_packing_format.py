16ff85de73d93613200f0bb390f0c3255b657362
Jesse Cai <jcjessecai@gmail.com>
2025-11-24 08:35:48 -0800

    update docstring

--- torchao/quantization/quantize_/workflows/float8/float8_packing_format.py
+++ torchao/quantization/quantize_/workflows/float8/float8_packing_format.py
@@ -25,6 +25,12 @@ class Float8PackingFormat(str, Enum):
 
     """
     Sparse packing formats for 2:4 sparsity + FP8 quantization
+
+    SPARSE_CUTLASS will pack the quantized_data into two tensors, sparse_qdata and sparse_metadata, for the specified values and metadata respectively. 
+    This packing format will dispatch to `rowwise_scaled_linear_sparse_cutlass_f8f8`, which will fuse the per-row scaling into the sparse matmul. 
+
+    SPARSE_CUSPARSELT will pack the quantized_data into a single tensor, sparse_qdata, which contains both the specified values and appends the metadata. 
+    This packing format will dispatch to `_cslt_sparse_mm`, which does not fuse per-row scaling into the matmul.
     """
     SPARSE_CUTLASS = "sparse_cutlass"
     SPARSE_CUSPARSELT = "sparse_cusparselt"
