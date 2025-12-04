4741eb7c6cbe2b052e1be59c7a2beebf13588e73
Zeng, Xiangdong <xiangdong.zeng@intel.com>
2025-12-04 10:02:46 +0800

    fix macos-14

--- test/prototype/test_parq.py
+++ test/prototype/test_parq.py
@@ -51,11 +51,12 @@ from torchao.utils import (
     torch_version_at_least,
 )
 
-_DEVICE = torch.device(
-    torch.accelerator.current_accelerator().type
-    if torch.accelerator.is_available()
-    else "cpu"
-)
+if torch.cuda.is_available():
+    _DEVICE = "cuda"
+elif torch.xpu.is_available():
+    _DEVICE = "xpu"
+else:
+    _DEVICE = "cpu"
 
 
 class M(nn.Module):
