3955b6ccbd8c084c8c543a466d2b9a136a0c3531
Vasiliy Kuznetsov <vkuzo@users.noreply.github.com>
2026-01-06 08:34:31 -0500

    add performance and accuracy eval of flux-1.schnell (#3502)
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]

--- benchmarks/quantization/create_quantized_model.py
+++ benchmarks/quantization/create_quantized_model.py
@@ -9,50 +9,7 @@ import subprocess
 
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, TorchAoConfig
-
-from torchao.prototype.mx_formats.inference_workflow import (
-    MXDynamicActivationMXWeightConfig,
-    NVFP4DynamicActivationNVFP4WeightConfig,
-)
-from torchao.quantization import (
-    Float8DynamicActivationFloat8WeightConfig,
-    Float8DynamicActivationInt4WeightConfig,
-    Int4WeightOnlyConfig,
-    Int8DynamicActivationInt8WeightConfig,
-    Int8WeightOnlyConfig,
-    PerRow,
-)
-
-
-def string_to_config(s):
-    if s == "None":
-        return None
-    elif s == "float8_rowwise":
-        return Float8DynamicActivationFloat8WeightConfig(granularity=PerRow())
-    elif s == "int4_groupwise_weight_float8_rowwise_activation":
-        return Float8DynamicActivationInt4WeightConfig()
-    elif s == "int4_groupwise_hqq_weight_only":
-        return Int4WeightOnlyConfig(
-            group_size=32,
-            int4_packing_format="tile_packed_to_4d",
-            int4_choose_qparams_algorithm="hqq",
-        )
-    elif s == "int8_rowwise_weight_only":
-        return Int8WeightOnlyConfig()
-    elif s == "int8_rowwise":
-        return Int8DynamicActivationInt8WeightConfig()
-    elif s == "mxfp8":
-        return MXDynamicActivationMXWeightConfig(
-            activation_dtype=torch.float8_e4m3fn,
-            weight_dtype=torch.float8_e4m3fn,
-        )
-    elif s == "nvfp4":
-        return NVFP4DynamicActivationNVFP4WeightConfig(
-            use_dynamic_per_tensor_scale=True,
-            use_triton_kernel=True,
-        )
-    else:
-        raise AssertionError(f"unsupported {s}")
+from utils import string_to_config
 
 
 def quantize_model_and_save(model_id, quant_config, output_dir):
