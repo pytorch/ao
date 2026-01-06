917246172c79034ccf0a1c9f7ddaa7da6b6d6854
Daniel Vega-Myhre <danvm@meta.com>
2026-01-06 17:48:54 +0000

    [mxfp8 moe training] mxfp8 a2a_dispatch autograd function
    stack-info: PR: https://github.com/pytorch/ao/pull/3579, branch: danielvegamyhre/stack/103

---
+++ torchao/prototype/moe_training/ep/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+Expert Parallelism (EP) autograd functions for MoE training with MXFP8.
+
+This module contains custom autograd functions that enable efficient expert parallelism
+with selective MXFP8 quantization during forward and backward passes.
+
+The functions are designed to work together in the following pipeline:
+
+Forward: bf16 -> a2a_dispatch (quantize) -> permute -> mxfp8 grouped GEMM -> unpermute -> a2a_combine
+Backward: bf16 <- a2a_dispatch <- permute <- mxfp8 grouped GEMMs <- unpermute <- a2a_combine (quantize)
+"""
+
+from .a2a_dispatch import a2a_dispatch_mxfp8_fwd_hp_bwd
+
+__all__ = [
+    "a2a_dispatch_mxfp8_fwd_hp_bwd",
+]
