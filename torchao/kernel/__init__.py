f8728e3ee96e691d55b66024ad2ff4beb461b7eb
namgyu-youn <namgyu.dev@gmail.com>
2026-01-06 22:26:48 +0900

    fix import

--- torchao/kernel/__init__.py
+++ torchao/kernel/__init__.py
@@ -1,4 +1,4 @@
-from torchao.kernel import int8_scaled_mm_triton
+from torchao.kernel import int8_mm
 from torchao.kernel.bsr_triton_ops import bsr_dense_addmm
 from torchao.kernel.intmm import int_scaled_matmul, safe_int_mm
 
@@ -6,5 +6,5 @@ __all__ = [
     "bsr_dense_addmm",
     "safe_int_mm",
     "int_scaled_matmul",
-    "int8_scaled_mm_triton",
+    "int8_mm",
 ]
