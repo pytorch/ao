0dd73d22cc5ab7a550c1c30b0ddbd7e9b9fbc39f
xiaobochen-amd <xiaobo.chen@amd.com>
2026-01-22 11:47:08 +0000

    fix format error

--- test/prototype/moe_training/test_scaled_grouped_mm.py
+++ test/prototype/moe_training/test_scaled_grouped_mm.py
@@ -4,8 +4,8 @@
 # This source code is licensed under the BSD 3-Clause license found in the
 # LICENSE file in the root directory of this source tree.
 
+
 import pytest
-import sys
 import torch
 from torch.nn import functional as F
 
