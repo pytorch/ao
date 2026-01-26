pytorch/ao

commit 9939f699f85785162ada5b69bcf8d3cda3659b82

Enable TorchAO V2 for sparsity (#3711)
Summary:

As titled ~

Reviewed By: jcaip

Differential Revision: D91339968

torchao/quantization/quantize_/workflows/float8/sparse_2x4_cutlass_float8_tensor.py
@@ -193,11 +193,14 @@ implements_torch_function = Sparse2x4CUTLASSFloat8Tensor.implements_torch_functi
 @implements(aten.linear.default)
 @implements_torch_function(torch.nn.functional.linear)
 def _(func, types, args, kwargs):
-    input_tensor, weight_tensor, bias = (
-        args[0],
-        args[1],
-        args[2] if len(args) > 2 else None,
-    )
+
+    input_tensor = kwargs.get("input", args[0] if len(args) > 0 else None)
+    weight_tensor = kwargs.get("weight", args[1] if len(args) > 1 else None)
+    bias = kwargs.get("bias", args[2] if len(args) > 2 else None)
+
+    assert input_tensor is not None, "input tensor must not be None"
+    assert weight_tensor is not None, "weight tensor must not be None"
+
     act_quant_kwargs = weight_tensor.act_quant_kwargs
     # quantize activation, if `act_quant_kwargs` is specified
     if act_quant_kwargs is not None:
@@ -229,6 +232,22 @@ def _(func, types, args, kwargs):
             *args[1:],
             dtype=kwargs.get("dtype", args[0].dtype),
             device=kwargs.get("device", args[0].device),
+        )
+    )
+
+
+# implement to.dtype for cases where dtype specified in args[1]
+@implements(aten.to.dtype)
+def _(func, types, args, kwargs):
+
+    dtype = kwargs.get("dtype", args[1] if len(args) > 1 else None)
+    assert dtype is not None, "dtype must not be None"
+
+    return (
+        args[0]
+        .dequantize()
+        .to(
+            dtype = dtype,
         )
     )
 

test/quantization/quantize_/workflows/float8/test_sparse_2x4_cutlass_float8_tensor.py
@@ -112,12 +112,18 @@ class TestSparse2x4Float8Tensor(common_utils.TestCase):
                 ),
             )
 
-            original = torch.ops.aten.to.dtype_layout(
+            original_by_to_dtype_layout = torch.ops.aten.to.dtype_layout(
                 model.weight,
                 dtype=torch.float,
                 layout=torch.strided,
             )
-            torch.testing.assert_close(expected, original, atol=1e-1, rtol=1e-1)
+            torch.testing.assert_close(expected, original_by_to_dtype_layout, atol=1e-1, rtol=1e-1)
+
+            original_by_to_dtype = torch.ops.aten.to.dtype(
+                model.weight,
+                torch.float,
+            )
+            torch.testing.assert_close(expected, original_by_to_dtype, atol=1e-1, rtol=1e-1)
 
 
 common_utils.instantiate_parametrized_tests(TestSparse2x4Float8Tensor)

commit 7d39b166d4a7d1765df23479c2b9d6c1bbcaf400

fix ruff after #3711
Summary:

Broken in https://github.com/pytorch/ao/pull/3711, fixing

Test Plan:

Reviewers:

Subscribers:

Tasks:

Tags:
ghstack-source-id: dc1cd99cfd498ead6ff8fc483c12c50fe0c5e6fa
ghstack-comment-id: 3799157069
Pull-Request: https://github.com/pytorch/ao/pull/3719

torchao/quantization/quantize_/workflows/float8/sparse_2x4_cutlass_float8_tensor.py
@@ -193,7 +193,6 @@ implements_torch_function = Sparse2x4CUTLASSFloat8Tensor.implements_torch_functi
 @implements(aten.linear.default)
 @implements_torch_function(torch.nn.functional.linear)
 def _(func, types, args, kwargs):
-
     input_tensor = kwargs.get("input", args[0] if len(args) > 0 else None)
     weight_tensor = kwargs.get("weight", args[1] if len(args) > 1 else None)
     bias = kwargs.get("bias", args[2] if len(args) > 2 else None)
@@ -239,7 +238,6 @@ def _(func, types, args, kwargs):
 # implement to.dtype for cases where dtype specified in args[1]
 @implements(aten.to.dtype)
 def _(func, types, args, kwargs):
-
     dtype = kwargs.get("dtype", args[1] if len(args) > 1 else None)
     assert dtype is not None, "dtype must not be None"
 
@@ -247,7 +245,7 @@ def _(func, types, args, kwargs):
         args[0]
         .dequantize()
         .to(
-            dtype = dtype,
+            dtype=dtype,
         )
     )
 

test/quantization/quantize_/workflows/float8/test_sparse_2x4_cutlass_float8_tensor.py
@@ -117,13 +117,17 @@ class TestSparse2x4Float8Tensor(common_utils.TestCase):
                 dtype=torch.float,
                 layout=torch.strided,
             )
-            torch.testing.assert_close(expected, original_by_to_dtype_layout, atol=1e-1, rtol=1e-1)
+            torch.testing.assert_close(
+                expected, original_by_to_dtype_layout, atol=1e-1, rtol=1e-1
+            )
 
             original_by_to_dtype = torch.ops.aten.to.dtype(
                 model.weight,
                 torch.float,
             )
-            torch.testing.assert_close(expected, original_by_to_dtype, atol=1e-1, rtol=1e-1)
+            torch.testing.assert_close(
+                expected, original_by_to_dtype, atol=1e-1, rtol=1e-1
+            )
 
 
 common_utils.instantiate_parametrized_tests(TestSparse2x4Float8Tensor)