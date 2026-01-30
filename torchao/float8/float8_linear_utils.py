b795037725b0fba94ff1123d79b355e7373eeee7
Vasiliy Kuznetsov <vkuzo@users.noreply.github.com>
2026-01-30 13:55:53 -0500

    [15/x] docs update: move float8 training README.md to docs (#3791)
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]

--- torchao/float8/float8_linear_utils.py
+++ torchao/float8/float8_linear_utils.py
@@ -102,47 +102,10 @@ def convert_to_float8_training(
     Returns:
      nn.Module: The modified module with swapped linear layers.
 
-    Example::
+    Example:
 
-        import torch
-        import torch.nn as nn
-        from torchao.float8 import convert_to_float8_training, Float8LinearConfig
-
-        # create model and sample input
-        m = nn.Sequential(
-            nn.Linear(8192, 4096, bias=False),
-            nn.Linear(4096, 128, bias=False),
-        ).bfloat16().cuda()
-        optimizer = torch.optim.SGD(m.parameters(), lr=0.1)
-
-        # optional: filter modules from being eligible for float8 conversion
-        def module_filter_fn(mod: torch.nn.Module, fqn: str):
-            # don't convert the last module
-            if fqn == "1":
-                return False
-            # don't convert linear modules with weight dimensions not divisible by 16
-            if isinstance(mod, torch.nn.Linear):
-                if mod.in_features % 16 != 0 or mod.out_features % 16 != 0:
-                    return False
-            return True
-
-        # configure float8 recipe
-        # valid recipe names: "tensorwise", "rowwise", "rowwise_with_gw_hp"
-        config = Float8LinearConfig.from_recipe_name("tensorwise")
-
-        # convert specified `torch.nn.Linear` modules to `Float8Linear`
-        convert_to_float8_training(m, config=config, module_filter_fn=module_filter_fn)
-
-        # enable torch.compile for competitive performance
-        m = torch.compile(m)
-
-        # training loop
-        x = torch.randn(4096, 8192, device="cuda", dtype=torch.bfloat16)
-        for _ in range(10):
-            optimizer.zero_grad()
-            y = m(x)
-            y.sum().backward()
-            optimizer.step()
+    .. literalinclude:: ../../examples/float8_training_example.py
+       :language: python
     """
     torch._C._log_api_usage_once("torchao.float8.convert_to_float8_training")
     if config is None:
