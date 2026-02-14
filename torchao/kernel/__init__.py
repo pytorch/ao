61de87ab6d3186b8312c69039e923fa23c6c837d
namgyu-youn <namgyu.dev@gmail.com>
2026-02-14 18:02:13 +0900

    fix triton import

--- torchao/kernel/__init__.py
+++ torchao/kernel/__init__.py
@@ -1,4 +1,5 @@
-from torchao.kernel import int8_mm
+from torch.utils._triton import has_triton
+
 from torchao.kernel.bsr_triton_ops import bsr_dense_addmm
 from torchao.kernel.intmm import int_scaled_matmul, safe_int_mm
 
@@ -6,5 +7,9 @@ __all__ = [
     "bsr_dense_addmm",
     "safe_int_mm",
     "int_scaled_matmul",
-    "int8_mm",
 ]
+
+if has_triton():
+    from torchao.kernel import int8_mm as int8_mm
+
+    __all__.append("int8_mm")
