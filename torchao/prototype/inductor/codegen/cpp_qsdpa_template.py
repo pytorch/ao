c893917e2ad03d128697238223eb9d90fd727c69
yuchengliu1 <yucheng.liu@intel.com>
2026-03-04 15:18:49 +0800

    update

--- torchao/prototype/inductor/codegen/cpp_qsdpa_template.py
+++ torchao/prototype/inductor/codegen/cpp_qsdpa_template.py
@@ -1012,9 +1012,7 @@ extern "C"
 {%- endif %}
 
   // Data ptrs
-  const scalar_t* q_data = query;
-  const scalar_t* k_data = key;
-  const scalar_t* v_data = value;
+  {{template.codegen_qkv_ptr(kernel)}}
   scalar_t* out_data = output;
 
   bool headSize_mul64 = headSize % 64 == 0;
@@ -1437,9 +1435,7 @@ extern "C"
 {%- endif %}
 
   // Data ptrs
-  const scalar_t* q_data = query;
-  const scalar_t* k_data = key;
-  const scalar_t* v_data = value;
+  {{template.codegen_qkv_ptr(kernel)}}
   scalar_t* out_data = output;
 
   int64_t qk_reduce_strideL = qSplitSize * rndkvSplitSize;
@@ -2296,3 +2292,18 @@ class CppQsdpaTemplate(CppFlexAttentionTemplate):
                 buffer_size=buffer_size,
             )
         )
+
+    def codegen_qkv_ptr(self, kernel):
+        if len(kernel.args.input_buffers)==1:
+            buf_name = next(iter(kernel.args.input_buffers.values()))
+            return f"""
+  const scalar_t* q_data = {buf_name} + {self.input_nodes[0].layout.offset};
+  const scalar_t* k_data = {buf_name} + {self.input_nodes[1].layout.offset};
+  const scalar_t* v_data = {buf_name} + {self.input_nodes[2].layout.offset};
+"""
+        else:
+            return f"""
+  const scalar_t* q_data = query;
+  const scalar_t* k_data = key;
+  const scalar_t* v_data = value;
+"""
