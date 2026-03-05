a2d18008973a7b15f8b8658f7252ca435befcf75
Howard Zhang <hwd15508@meta.com>
2026-03-05 12:30:25 -0800

    Update (base update)
    [ghstack-poisoned]

---
+++ torchao/prototype/attention/quantization/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
+
+from torchao.prototype.attention.quantization.triton_qkv_quantization import (
+    triton_fp8_sdpa_quantize as _fp8_sdpa_quantize,
+)
+from torchao.prototype.attention.quantization.triton_rope_qkv_quantization import (
+    triton_fp8_rope_sdpa_quantize as _fp8_rope_sdpa_quantize,
+)
+
+__all__ = [
+    "_fp8_sdpa_quantize",
+    "_fp8_rope_sdpa_quantize",
+]
