1d291170fa986ab9a8c4ff0b6e13c1d567a052ca
Howard Zhang <hwd15508@meta.com>
2026-03-05 12:30:17 -0800

    Update
    [ghstack-poisoned]

---
+++ torchao/prototype/attention/fp8_fa3/attention.py
@@ -0,0 +1,22 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""FP8 SDPA using FA3 backend.
+
+Thin wrapper around ``shared_utils/attention.py``. When using directly,
+activate the FA3 flash attention implementation before calling.
+"""
+
+from functools import partial
+
+from torchao.prototype.attention.shared_utils.attention import (
+    _fp8_sdpa,
+)
+
+fp8_fa3_sdpa = partial(_fp8_sdpa, backend_name="FA3")
+fp8_fa3_sdpa.__doc__ = _fp8_sdpa.__doc__
+fp8_fa3_sdpa.__name__ = "fp8_fa3_sdpa"
+fp8_fa3_sdpa.__qualname__ = "fp8_fa3_sdpa"
