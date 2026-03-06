2e12a3825a5e275b75ba4a3911faf0910bff5f62
Howard Zhang <hwd15508@meta.com>
2026-03-06 10:32:50 -0800

    Update (base update)
    [ghstack-poisoned]

--- torchao/prototype/attention/fp8_fa3/fusion_pass.py
+++ torchao/prototype/attention/fp8_fa3/fusion_pass.py
@@ -4,17 +4,12 @@
 # This source code is licensed under the BSD 3-Clause license found in the
 # LICENSE file in the root directory of this source tree.
 
-"""
-FA3-specific FX graph fusion pass and compile helper.
-"""
-
 from torchao.prototype.attention.fp8_fa3.attention import (
     fp8_fa3_rope_sdpa,
     fp8_fa3_sdpa,
 )
 from torchao.prototype.attention.shared_utils.custom_ops import (
-    make_compile_fn,
-    make_fusion_pass,
+    make_backend_fn,
     register_fp8_attention_ops,
 )
 
@@ -24,5 +19,4 @@ _ops = register_fp8_attention_ops(
     sdpa_fn=fp8_fa3_sdpa,
 )
 
-rope_sdpa_fusion_pass = make_fusion_pass(_ops, backend_name="FA3", max_head_dim=256)
-compile_with_fp8_fusion = make_compile_fn(rope_sdpa_fusion_pass, flash_impl_name="FA3")
+make_fp8_backend = make_backend_fn(_ops, backend_name="FA3", flash_impl_name="FA3")
