c4e769bad3728367d1917b903d5ca5079acf3d4a
Xia, Weiwen <weiwen.xia@intel.com>
2026-03-18 07:30:10 +0000

    Fix circular import

--- torchao/prototype/quantization/__init__.py
+++ torchao/prototype/quantization/__init__.py
@@ -1,13 +1,5 @@
 from .gguf import GGUFWeightOnlyConfig
-from .int4 import (
-    Int4OpaqueTensor,
-    Int4WeightOnlyOpaqueTensorConfig,
-    Int8DynamicActInt4WeightConfig,
-)
 
 __all__ = [
     "GGUFWeightOnlyConfig",
-    "Int4OpaqueTensor",
-    "Int4WeightOnlyOpaqueTensorConfig",
-    "Int8DynamicActInt4WeightConfig",
 ]
