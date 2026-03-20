b9adc04e46f5573fbe71476c7f8319c4f560d4ce
Jerry Zhang <jerryzh168@gmail.com>
2026-03-19 19:55:32 -0700

    Delete deprecated autoquant v1 and all references
    Summary:
    Autoquant was deprecated in #3739 with a FutureWarning. This removes:
    - torchao/quantization/autoquant.py (1338 lines)
    - All autoquant imports/references in torchao/__init__.py, quantization/__init__.py, quant_api.py
    - autoquant code paths in eval_combo.py, server.py, mp_quant_eval.py
    - autoquant benchmark step in dashboard_perf_test.yml
    - TestAutoQuant class and all test_aq_* methods from test_integration.py
    
    Test Plan:
    CI
    
    [ghstack-poisoned]

--- torchao/_models/sam/eval_combo.py
+++ torchao/_models/sam/eval_combo.py
@@ -12,7 +12,6 @@ import torch
 import tqdm
 from metrics import calculate_miou, create_result_entry
 
-import torchao
 from data import build_data, setup_coco_img_ids
 from torchao._models.utils import (
     get_arch_name,
@@ -22,7 +21,6 @@ from torchao._models.utils import (
 from torchao.dtypes import SemiSparseLayout
 from torchao.quantization import (
     Int8DynamicActivationInt8WeightConfig,
-    autoquant,
     quantize_,
 )
 from torchao.sparsity import apply_fake_sparsity, semi_sparse_weight, sparsify_
@@ -391,51 +389,6 @@ def run(
         )
         sparsify_(predictor.model.image_encoder, semi_sparse_weight(), mlp_lin2_only)
 
-    elif compress is not None and "autoquant" in compress:
-        example_input = torch.randn(
-            1, 3, 1024, 1024, dtype=torch.bfloat16, device=device
-        )
-        if "autoquant-int4" == compress:
-            autoquant(
-                predictor.model.image_encoder,
-                example_input=example_input,
-                manual=True,
-                qtensor_class_list=torchao.quantization.DEFAULT_INT4_AUTOQUANT_CLASS_LIST,
-                min_sqnr=min_sqnr,
-            )
-        elif "autoquant-float8" == compress:
-            autoquant(
-                predictor.model.image_encoder,
-                example_input=example_input,
-                manual=True,
-                qtensor_class_list=torchao.quantization.OTHER_AUTOQUANT_CLASS_LIST,
-                min_sqnr=min_sqnr,
-            )
-        elif "autoquant-sparse" == compress:
-            autoquant(
-                predictor.model.image_encoder,
-                example_input=example_input,
-                manual=True,
-                qtensor_class_list=torchao.quantization.DEFAULT_SPARSE_AUTOQUANT_CLASS_LIST,
-                min_sqnr=min_sqnr,
-            )
-        elif "autoquant-all" == compress:
-            autoquant(
-                predictor.model.image_encoder,
-                example_input=example_input,
-                manual=True,
-                qtensor_class_list=torchao.quantization.ALL_AUTOQUANT_CLASS_LIST,
-                min_sqnr=min_sqnr,
-            )
-        else:
-            autoquant(
-                predictor.model.image_encoder,
-                example_input=example_input,
-                manual=True,
-                min_sqnr=min_sqnr,
-            )
-        predictor.model.image_encoder(example_input)
-        predictor.model.image_encoder.finalize_autoquant()
     else:
         assert compress is None, f"Unsupported compress mode {compress}"
 
