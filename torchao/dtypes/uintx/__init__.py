b1c8255c62da93bddb9590ea13565cc4bf2cf74d
Jerry Zhang <jerryzh168@gmail.com>
2026-03-23 14:14:19 -0700

    Update base for Update on "Delete deprecated Int4CPULayout and related code"
    [ghstack-poisoned]

--- torchao/dtypes/uintx/__init__.py
+++ torchao/dtypes/uintx/__init__.py
@@ -7,6 +7,7 @@ from .packed_linear_int8_dynamic_activation_intx_weight_layout import (
 from .tensor_core_tiled_layout import (
     TensorCoreTiledLayout,
 )
+
 __all__ = [
     "TensorCoreTiledLayout",
     "Int4CPULayout",
