916a63527086f7e7091dfde3ae24a8973ba19d3c
Jerry Zhang <jerryzh168@gmail.com>
2026-03-23 13:08:51 -0700

    Delete deprecated Int4XPULayout and related code
    [ghstack-poisoned]

--- torchao/dtypes/uintx/__init__.py
+++ torchao/dtypes/uintx/__init__.py
@@ -1,9 +1,6 @@
 from .int4_cpu_layout import (
     Int4CPULayout,
 )
-from .int4_xpu_layout import (
-    Int4XPULayout,
-)
 from .packed_linear_int8_dynamic_activation_intx_weight_layout import (
     PackedLinearInt8DynamicActivationIntxWeightLayout,
 )
@@ -18,5 +15,4 @@ __all__ = [
     "TensorCoreTiledLayout",
     "Int4CPULayout",
     "PackedLinearInt8DynamicActivationIntxWeightLayout",
-    "Int4XPULayout",
 ]
