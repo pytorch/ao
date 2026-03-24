1f90b4dad479fe1f4adc28d43217d9a6c6d1aeea
Jerry Zhang <jerryzh168@gmail.com>
2026-03-24 14:29:26 -0700

    Delete deprecated PackedLinearInt8DynamicActivationIntxWeightLayout and related code (#4150)
    * Delete deprecated PackedLinearInt8DynamicActivationIntxWeightLayout and related code

--- torchao/dtypes/uintx/__init__.py
+++ torchao/dtypes/uintx/__init__.py
@@ -1,11 +1,7 @@
-from .packed_linear_int8_dynamic_activation_intx_weight_layout import (
-    PackedLinearInt8DynamicActivationIntxWeightLayout,
-)
 from .tensor_core_tiled_layout import (
     TensorCoreTiledLayout,
 )
 
 __all__ = [
     "TensorCoreTiledLayout",
-    "PackedLinearInt8DynamicActivationIntxWeightLayout",
 ]
