22cc01ba3da13e251f00a2e55fece5992a75ddd5
Vasiliy Kuznetsov <vkuzo@users.noreply.github.com>
2026-04-02 20:29:37 -0400

    unbreak gptq int4 test and add to CI (#4223)
    Update
    
    [ghstack-poisoned]

--- test/prototype/gptq/test_gptqv2.py
+++ test/prototype/gptq/test_gptqv2.py
@@ -429,7 +429,7 @@ class TestGPTQFlow:
                 id="int4",
                 marks=pytest.mark.skipif(
                     not _is_mslk_available(),
-                    reason="fbgemm_gpu not available",
+                    reason="mslk not available",
                 ),
             ),
             pytest.param(
