7e85d61f17fc3bd965b1f4e832bbc96e683ae786
vaibhavhariram <vaibhuhariram@gmail.com>
2026-04-12 22:42:01 -0700

    [optim] Add GrokAdamW optimizer with low-bit quantization support

--- torchao/optim/__init__.py
+++ torchao/optim/__init__.py
@@ -1,5 +1,6 @@
 from .adam import Adam4bit, Adam8bit, AdamFp8, AdamW4bit, AdamW8bit, AdamWFp8, _AdamW
 from .cpu_offload import CPUOffloadOptimizer
+from .grokadamw import GrokAdamW4bit, GrokAdamW8bit, GrokAdamWFp8
 
 __all__ = [
     "Adam4bit",
@@ -8,6 +9,9 @@ __all__ = [
     "AdamW4bit",
     "AdamW8bit",
     "AdamWFp8",
+    "GrokAdamW4bit",
+    "GrokAdamW8bit",
+    "GrokAdamWFp8",
     "_AdamW",
     "CPUOffloadOptimizer",
 ]
