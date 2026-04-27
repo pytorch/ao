e0c04f6303413faa84bb0008b2c08417085810b2
Vasiliy Kuznetsov <vasiliy@meta.com>
2026-04-27 12:16:28 +0000

    make gptq convert work for moe
    Summary:
    
    Makes gptq + moe + nvfp4 work e2e, results as expected on tiny model +
    tiny dataset
    
    Test Plan:
    
    ```
    > TRITON_ALLOW_NON_CONSTEXPR_GLOBALS=1 torchao/prototype/gptq/gptq_nvfp4_olmoe_1b_7b_nonsequential_wikitext.sh
    
    bf16
    | Tasks  |Version|Filter|n-shot|    Metric     |   |Value |   |Stderr|
    |--------|------:|------|-----:|---------------|---|-----:|---|------|
    |wikitext|      2|none  |     0|bits_per_byte  |↓  |0.5895|±  |   N/A|
    |        |       |none  |     0|byte_perplexity|↓  |1.5047|±  |   N/A|
    |        |       |none  |     0|word_perplexity|↓  |8.8910|±  |   N/A|
    real    0m59.219s
    user    0m42.554s
    sys     0m20.534s
    
    nvfp4-rtn
    
    | Tasks  |Version|Filter|n-shot|    Metric     |   |Value |   |Stderr|
    |--------|------:|------|-----:|---------------|---|-----:|---|------|
    |wikitext|      2|none  |     0|bits_per_byte  |↓  |0.6024|±  |   N/A|
    |        |       |none  |     0|byte_perplexity|↓  |1.5183|±  |   N/A|
    |        |       |none  |     0|word_perplexity|↓  |9.3277|±  |   N/A|
    real    0m42.528s
    user    0m41.217s
    sys     0m12.817s
    
    nvfp4-nonsequential with 4096 calibration samples on c4
    | Tasks  |Version|Filter|n-shot|    Metric     |   |Value |   |Stderr|
    |--------|------:|------|-----:|---------------|---|-----:|---|------|
    |wikitext|      2|none  |     0|bits_per_byte  |↓  |0.6019|±  |   N/A|
    |        |       |none  |     0|byte_perplexity|↓  |1.5177|±  |   N/A|
    |        |       |none  |     0|word_perplexity|↓  |9.3087|±  |   N/A|
    real    22m28.505s
    user    22m36.008s
    sys     0m13.872s
    
    ```
    ghstack-source-id: 94d4d29237751ccd517bb8d9c97ce9c111e59fb2
    ghstack-comment-id: 4315147581
    Pull-Request: https://github.com/pytorch/ao/pull/4330

--- torchao/prototype/gptq/gptq_nvfp4_olmoe_1b_7b_nonsequential_wikitext.sh
+++ torchao/prototype/gptq/gptq_nvfp4_olmoe_1b_7b_nonsequential_wikitext.sh
@@ -8,18 +8,16 @@ COMMON_ARGS="--output-dir-prefix /home/dev/tmp/20260421 --model-id allenai/OLMoE
 
 # baseline (bf16)
 echo -e "\n\nbaseline (bf16)\n\n"
-# python -u torchao/prototype/gptq/gptq_example.py $COMMON_ARGS --quantization none 
+time python -u torchao/prototype/gptq/gptq_example.py $COMMON_ARGS --quantization none 
 echo -e "done"
 
 # nvfp4-rtn
 echo -e "\n\nnvfp4-rtn\n\n"
-# python -u torchao/prototype/gptq/gptq_example.py $COMMON_ARGS --quantization nvfp4-rtn
+time python -u torchao/prototype/gptq/gptq_example.py $COMMON_ARGS --quantization nvfp4-rtn
 echo -e "done"
 
 # nvfp4-gptq-nonsequential
 echo -e "\n\nnvfp4-gptq-nonsequential\n\n"
-# TODO(future PR): fix https://gist.github.com/vkuzo/51b2bfcee77fc193253faf007d99d694
-# and enable this
-# python -u torchao/prototype/gptq/gptq_example.py $COMMON_ARGS --quantization nvfp4-gptq-nonsequential --dataset-id c4 --dataset-split train
+time python -u torchao/prototype/gptq/gptq_example.py $COMMON_ARGS --quantization nvfp4-gptq-nonsequential --dataset-id c4 --dataset-split train --num-calibration-samples 4096
 echo -e "done"
 
