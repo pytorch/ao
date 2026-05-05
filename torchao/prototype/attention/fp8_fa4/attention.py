de85ada69fbd4faf1ae72e6e2ef55027d88c22df
Howard Zhang <hwd15508@meta.com>
2026-05-05 14:54:16 -0700

    Add FA4 monkey-patch and rope fusion torch compile path for low-precision attention
    ghstack-source-id: 9f59447a677cebd5baf70eb4997dad263b7d88c9
    Pull-Request: https://github.com/pytorch/ao/pull/3960
    
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

---
+++ torchao/prototype/attention/fp8_fa4/attention.py
@@ -0,0 +1,44 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+FP8 SDPA using FA4 backend.
+
+When using these functions directly (not through apply_low_precision_attention),
+you must activate FA4 yourself::
+
+    activate_flash_attention_impl("FA4")
+    try:
+        out = fp8_fa4_sdpa(q, k, v, is_causal=True)
+    finally:
+        restore_flash_attention_impl()
+"""
+
+from functools import partial
+
+from torchao.prototype.attention.shared_utils.attention import (
+    _fp8_rope_sdpa,
+    _fp8_sdpa,
+)
+from torchao.prototype.attention.shared_utils.custom_ops import (
+    register_fp8_attention_ops,
+)
+
+fp8_fa4_sdpa = partial(_fp8_sdpa, backend_name="FA4")
+fp8_fa4_sdpa.__doc__ = _fp8_sdpa.__doc__
+fp8_fa4_sdpa.__name__ = "fp8_fa4_sdpa"
+fp8_fa4_sdpa.__qualname__ = "fp8_fa4_sdpa"
+
+fp8_fa4_rope_sdpa = partial(_fp8_rope_sdpa, backend_name="FA4")
+fp8_fa4_rope_sdpa.__doc__ = _fp8_rope_sdpa.__doc__
+fp8_fa4_rope_sdpa.__name__ = "fp8_fa4_rope_sdpa"
+fp8_fa4_rope_sdpa.__qualname__ = "fp8_fa4_rope_sdpa"
+
+_ops = register_fp8_attention_ops(
+    backend_name="fa4",
+    rope_sdpa_fn=fp8_fa4_rope_sdpa,
+    sdpa_fn=fp8_fa4_sdpa,
+)
