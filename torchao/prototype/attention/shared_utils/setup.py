2eb90433c2a8f1188232d52598d524468c82bd66
Howard Zhang <hwd15508@meta.com>
2026-05-05 13:52:28 -0700

    add inplace method for low precision attention
    ghstack-source-id: bef2855f13b17a75f50bfbbc0713cf9a284449aa
    Pull-Request: https://github.com/pytorch/ao/pull/4366

--- torchao/prototype/attention/shared_utils/setup.py
+++ torchao/prototype/attention/shared_utils/setup.py
@@ -8,6 +8,11 @@ from functools import partial
 
 import torch._inductor.config as inductor_config
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.attention import (
+    activate_flash_attention_impl,
+    restore_flash_attention_impl,
+)
 
 from torchao.prototype.attention.shared_utils.fusion_utils import (
     detect_causal_mask,
@@ -23,6 +28,7 @@ def setup_fp8_backend(
     model: nn.Module,
     flash_impl_name: str,
     hadamard: str = "NONE",
+    inplace: bool = False,
 ) -> nn.Module:
     if flash_impl_name == "FA3":
         from torchao.prototype.attention.fp8_fa3.attention import _ops
@@ -38,10 +44,31 @@ def setup_fp8_backend(
         backend_name=flash_impl_name,
     )
 
+    sdpa_patch_fn = _make_causal_aware_sdpa(
+        _ops.fp8_sdpa_op, strip_causal_mask, hadamard=hadamard
+    )
+
+    if inplace:
+        original_forward = model.forward
+
+        def _patched_forward(*args, **kwargs):
+            activate_flash_attention_impl(flash_impl_name)
+            try:
+                orig_sdpa = F.scaled_dot_product_attention
+                F.scaled_dot_product_attention = sdpa_patch_fn
+                try:
+                    return original_forward(*args, **kwargs)
+                finally:
+                    F.scaled_dot_product_attention = orig_sdpa
+            finally:
+                restore_flash_attention_impl()
+
+        model.forward = _patched_forward
+        model._low_precision_attention_applied = True
+        return model
+
     return _FP8FlashAttentionMonkeyPatchWrapper(
         model,
         flash_impl_name=flash_impl_name,
-        sdpa_patch_fn=_make_causal_aware_sdpa(
-            _ops.fp8_sdpa_op, strip_causal_mask, hadamard=hadamard
-        ),
+        sdpa_patch_fn=sdpa_patch_fn,
     )
