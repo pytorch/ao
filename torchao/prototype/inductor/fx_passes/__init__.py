abea9e0c41995cd4c120866579194c1084f8dc2f
yuchengliu1 <yucheng.liu@intel.com>
2026-06-09 13:22:10 +0800

    add quantized qkv-fusion pass for x86 (#4434)
    * add fp8 qkv fusion
    
    * merge int8 fp8 to one pass
    
    * update
    
    * update comment
    
    * update ut
    
    * fix for lint

--- torchao/prototype/inductor/fx_passes/__init__.py
+++ torchao/prototype/inductor/fx_passes/__init__.py
@@ -1,9 +1,11 @@
 from .da8w4_concat_linear_fusion_cpu import register_da8w4_concat_linear_cpu_pass
-from .int8_concat_linear_fusion_cpu import register_int8_concat_linear_cpu_pass
 from .qsdpa_fusion import _qsdpa_init
+from .quantized_concat_linear_fusion_cpu import (
+    register_quantized_concat_linear_cpu_pass,
+)
 
 __all__ = [
     "_qsdpa_init",
     "register_da8w4_concat_linear_cpu_pass",
-    "register_int8_concat_linear_cpu_pass",
+    "register_quantized_concat_linear_cpu_pass",
 ]
