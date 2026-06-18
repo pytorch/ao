ee796af87d46ce6b8a59217f2d6dea2f3a26c145
Artur Lesniak <artur.lesniak@intel.com>
2026-06-18 13:04:34 +0000

    [xpu][float8] Enabled test fsdp for Intel XPU

--- test/float8/test_fsdp.sh
+++ test/float8/test_fsdp.sh
@@ -13,14 +13,14 @@ launch() {
 
     # the NCCL_DEBUG setting is to avoid log spew
     # the CUDA_VISIBLE_DEVICES setting is for easy debugging
-    NCCL_DEBUG=WARN CUDA_VISIBLE_DEVICES=0,1 python test/float8/test_fsdp.py \
+    CCL_LOG_LEVEL=info NCCL_DEBUG=WARN CUDA_VISIBLE_DEVICES=0,1 python test/float8/test_fsdp.py \
         --compile_fsdp $COMPILE
 
     echo "✅ All Tests Passed ✅"
 }
 
-if python -c 'import torch;print(torch.cuda.is_available())' | grep -q "False"; then
-    echo "Skipping test_fsdp.sh because no CUDA devices are available."
+if python -c 'import torch;print(torch.accelerator.is_available())' | grep -q "False"; then
+    echo "Skipping test_fsdp.sh because no accelerator devices are available."
     exit
 fi
 
