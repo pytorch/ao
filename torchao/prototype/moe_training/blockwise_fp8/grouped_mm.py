9ed353bb75c2ce24c0ae009f44506fe02cc0a942
iamzainhuda <zainhuda1@gmail.com>
2026-07-06 21:01:23 +0000

    Expose FP8 blockwise grouped MM function

--- torchao/prototype/moe_training/blockwise_fp8/grouped_mm.py
+++ torchao/prototype/moe_training/blockwise_fp8/grouped_mm.py
@@ -29,7 +29,7 @@ from torchao.quantization.quantize_.common import KernelPreference
 
 
 @conditional_nostrict_trace
-def _to_fp8_blockwise_then_scaled_grouped_mm(
+def fp8_blockwise_grouped_mm(
     A: torch.Tensor,
     B_t: torch.Tensor,
     offs: Optional[torch.Tensor] = None,
@@ -60,6 +60,9 @@ def _to_fp8_blockwise_then_scaled_grouped_mm(
         pad_token_groups_for_grouped_mm,
         kernel_preference,
     )
+
+
+_to_fp8_blockwise_then_scaled_grouped_mm = fp8_blockwise_grouped_mm
 
 
 @conditional_nostrict_trace
