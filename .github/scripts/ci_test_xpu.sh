87e2d97378b91dd590c94d6e3ae18e02dd684af6
zengxian <xiangdong.zeng@intel.com>
2026-08-19 16:37:51 +0800

    add timeout for xpu ci

--- .github/scripts/ci_test_xpu.sh
+++ .github/scripts/ci_test_xpu.sh
@@ -6,6 +6,7 @@ source activate xpu_ao_ci
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 export SCCACHE_DISABLE=1
+export PYTHONFAULTHANDLER=1
 
 python -m pip install --upgrade pip setuptools wheel
 
@@ -14,9 +15,10 @@ cd torchao && python -m pip install . --no-build-isolation && cd ..
 
 python -c "import torch; import torchao; print(f'Torch version: {torch.__version__}')"
 
-python -m pip install pytest expecttest parameterized accelerate hf_transfer 'modelscope!=1.15.0' transformers tabulate fire
+python -m pip install pytest pytest-timeout expecttest parameterized accelerate hf_transfer 'modelscope!=1.15.0' transformers tabulate fire
 
-pytest -v -s --ignore=torchao/test/quantization/pt2e/test_x86inductor_fusion.py \
+pytest -v -s --timeout=600 --timeout-method=thread \
+        --ignore=torchao/test/quantization/pt2e/test_x86inductor_fusion.py \
         torchao/test/quantization/pt2e/ \
         torchao/test/quantization/*.py \
         torchao/test/dtypes/ \
