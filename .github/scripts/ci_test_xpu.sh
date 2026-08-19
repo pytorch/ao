9a6edec92947e39b3394ae68ef8738f0da5a48b3
zengxian <xiangdong.zeng@intel.com>
2026-08-19 16:37:51 +0800

    add timeout for xpu ci

--- .github/scripts/ci_test_xpu.sh
+++ .github/scripts/ci_test_xpu.sh
@@ -6,6 +6,7 @@ source activate xpu_ao_ci
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 export SCCACHE_DISABLE=1
+export PYTHONFAULTHANDLER=1
 
 python -m pip install --upgrade pip setuptools wheel
 
@@ -14,9 +15,9 @@ cd torchao && python -m pip install . --no-build-isolation && cd ..
 
 python -c "import torch; import torchao; print(f'Torch version: {torch.__version__}')"
 
-python -m pip install pytest expecttest parameterized accelerate hf_transfer 'modelscope!=1.15.0' transformers tabulate fire
+python -m pip install pytest pytest-timeout expecttest parameterized accelerate hf_transfer 'modelscope!=1.15.0' transformers tabulate fire
 
-pytest -v -s -k "not (bf16_stochastic_round_device_xpu)" \
+pytest -v -s --timeout=600 --timeout-method=thread -k "not (bf16_stochastic_round_device_xpu)" \
         --ignore=torchao/test/quantization/pt2e/test_x86inductor_fusion.py \
         --ignore=torchao/test/prototype/moe_training/nvfp4_training/test_group_weight_amax.py \
         torchao/test/quantization/pt2e/ \
