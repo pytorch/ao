74710359b8d961f87eb0b7135a5fb1fc95d50681
Aleksandar Samardžić <asamardzic@openteams.com>
2026-09-14 20:12:08 +0200

    Add CuTeDSL MXFP8 pad/unpad kernels
    ghstack-source-id: c0c7fda7097eaf7a6f489b3b5ea9dd5d2f755294
    ghstack-comment-id: 5195368742
    Pull-Request: https://github.com/pytorch/ao/pull/4703

--- torchao/prototype/moe_training/kernels/mxfp8/__init__.py
+++ torchao/prototype/moe_training/kernels/mxfp8/__init__.py
@@ -1,3 +1,7 @@
+from torchao.prototype.moe_training.kernels.mxfp8.cutedsl_pad_token_groups import (  # noqa: F401
+    _pad_token_groups_cutedsl,
+    _unpad_token_groups_cutedsl,
+)
 from torchao.prototype.moe_training.kernels.mxfp8.quant import (
     _mxfp8_cuda_kernels_available,  # noqa: F401
     _mxfp8_flydsl_kernels_available,  # noqa: F401
