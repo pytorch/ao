5c64c9071c511b1433ccdd6f555e4448cd5e9701
Insoo Chung <insoo@fb.com>
2026-09-19 07:22:27 -0700

    Update
    [ghstack-poisoned]

--- torchao/prototype/quantization/__init__.py
+++ torchao/prototype/quantization/__init__.py
@@ -1,8 +1,14 @@
 from .gguf import GGUFWeightOnlyConfig
+from .static_activation_qat import (
+    IntxStaticActQATConfig,
+    IntxStaticActQATLinear,
+)
 from .quant_api import Int8DynamicActivationUIntxWeightConfig, UIntxWeightOnlyConfig
 
 __all__ = [
     "GGUFWeightOnlyConfig",
     "Int8DynamicActivationUIntxWeightConfig",
+    "IntxStaticActQATConfig",
+    "IntxStaticActQATLinear",
     "UIntxWeightOnlyConfig",
 ]
