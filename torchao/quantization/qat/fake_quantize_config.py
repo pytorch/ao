2c150b35a15142d078910e10a817851430fad17c
insoochung <81752849+insoochung@users.noreply.github.com>
2026-09-22 08:25:00 -0700

    [a16-support] Add no-clipping symmetric mapping (#4920)
    * Update (base update)
    
    [ghstack-poisoned]
    
    * Update
    
    [ghstack-poisoned]
    
    ---------
    
    Co-authored-by: Insoo Chung <insoo@fb.com>
    Co-authored-by: andrewor14 <andrewor14@gmail.com>

--- torchao/quantization/qat/fake_quantize_config.py
+++ torchao/quantization/qat/fake_quantize_config.py
@@ -117,7 +117,10 @@ class IntxFakeQuantizeConfig(FakeQuantizeConfigBase):
                    with separate `group_size` kwarg, Alternatively, just set the
                    `group_size` kwarg and leave this field empty.
                4) 'per_tensor': equivalent to PerTensor()
-        mapping_type: whether to use symmetric (default) or asymmetric quantization
+        mapping_type: mapping from floating-point values to integers. Supported
+            values are `MappingType.SYMMETRIC`,
+            `MappingType.SYMMETRIC_NO_CLIPPING_ERR`, and
+            `MappingType.ASYMMETRIC`.
             Alternatively, set `is_symmetric` (bool) and leave this field empty.
         scale_precision: scale dtype (default torch.fp32)
         zero_point_precision: zero point dtype (default torch.int32)
@@ -129,8 +132,9 @@ class IntxFakeQuantizeConfig(FakeQuantizeConfigBase):
     Keyword args:
         group_size: size of each group in per group fake quantization,
             can be set instead of `granularity`
-        is_symmetric: whether to use symmetric or asymmetric quantization,
-            can be set instead of `mapping_type`
+        is_symmetric: whether to use symmetric or asymmetric quantization.
+            Setting this to True selects `MappingType.SYMMETRIC`. Use
+            `mapping_type` to select `MappingType.SYMMETRIC_NO_CLIPPING_ERR`.
 
     Example usage::
 
@@ -198,6 +202,14 @@ class IntxFakeQuantizeConfig(FakeQuantizeConfigBase):
         if dtype not in all_dtypes:
             raise ValueError(
                 "Unsupported dtype '%s', choose from %s" % (dtype, all_dtypes)
+            )
+        if (
+            isinstance(self.granularity, PerToken)
+            and self.mapping_type == MappingType.SYMMETRIC_NO_CLIPPING_ERR
+        ):
+            raise ValueError(
+                "MappingType.SYMMETRIC_NO_CLIPPING_ERR is not supported "
+                "for per-token quantization"
             )
 
         # Dynamic is not compatible with range learning
@@ -286,7 +298,7 @@ class IntxFakeQuantizeConfig(FakeQuantizeConfigBase):
         Parse the `MappingType` represented in the args.
 
         Mapping type can be specified in one of two ways:
-            1): `MappingType` object: one of SYMMETRIC or ASYMMETRIC
+            1): `MappingType` object: one of SYMMETRIC, SYMMETRIC_NO_CLIPPING_ERR, or ASYMMETRIC
             2): is_symmetric bool
         """
         if mapping_type is not None and is_symmetric is not None:
@@ -298,7 +310,11 @@ class IntxFakeQuantizeConfig(FakeQuantizeConfigBase):
 
         # Case 1: MappingType object
         if mapping_type is not None:
-            if mapping_type not in [MappingType.SYMMETRIC, MappingType.ASYMMETRIC]:
+            if mapping_type not in [
+                MappingType.SYMMETRIC,
+                MappingType.SYMMETRIC_NO_CLIPPING_ERR,
+                MappingType.ASYMMETRIC,
+            ]:
                 raise ValueError("MappingType '%s' is not supported" % mapping_type)
             return mapping_type
 
@@ -325,9 +341,16 @@ class IntxFakeQuantizeConfig(FakeQuantizeConfigBase):
     @property
     def is_symmetric(self) -> bool:
         """
-        Return True if mapping type is symmetric, else False (asymmetric).
+        Return True for either symmetric mapping type.
+
+        Setting this property to True selects `MappingType.SYMMETRIC`. Setting
+        it to False selects `MappingType.ASYMMETRIC`. Set `mapping_type`
+        directly to choose `MappingType.SYMMETRIC_NO_CLIPPING_ERR`.
         """
-        return self.mapping_type == MappingType.SYMMETRIC
+        return self.mapping_type in [
+            MappingType.SYMMETRIC,
+            MappingType.SYMMETRIC_NO_CLIPPING_ERR,
+        ]
 
     def __setattr__(self, name: str, value: Any):
         """
